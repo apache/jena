92736d373e5904810eaf63cd5b09c09b5580e9f8
Chris Dolan <chrisdolan@apache.org>
2003-01-17 12:13:10 +0000

    *** empty log message ***
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jena/Import/Jena-CVS/jena2/trunk@1109089 13f79535-47bb-0310-9956-ffa450edef68

--- src/com/hp/hpl/jena/graph/Node_ANY.java
+++ src/com/hp/hpl/jena/graph/Node_ANY.java
@@ -2,12 +2,16 @@ package com.hp.hpl.jena.graph;
 
 /**
 	@author kers
+<br>
+    A Node_ANY (there should be only one) is a meta-node that is used to stand
+    for any other node in a query.
 */
 
 public class Node_ANY extends Node
     {
     public Node_ANY() { super( "" ); }
     
+    /** Node_ANY's are only equal to other Node_ANY's */
     public boolean equals( Object other )
         { return other instanceof Node_ANY; }
     }
