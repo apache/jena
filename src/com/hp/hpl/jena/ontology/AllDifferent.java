f677816d0f4d28631972ddd9e777a3b6a8d1755a
Ian Dickinsone <ian_dickinson@apache.org>
2003-02-13 22:48:46 +0000

    Added stub ontology modelling classes
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jena/Import/Jena-CVS/jena2/trunk@1109198 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/com/hp/hpl/jena/ontology/AllDifferent.java
@@ -0,0 +1,109 @@
+/*****************************************************************************
+ * Source code information
+ * -----------------------
+ * Original author    Ian Dickinson, HP Labs Bristol
+ * Author email       Ian.Dickinson@hp.com
+ * Package            Jena 2
+ * Web                http://sourceforge.net/projects/jena/
+ * Created            10 Feb 2003
+ * Filename           $RCSfile: AllDifferent.java,v $
+ * Revision           $Revision: 1.1 $
+ * Release status     $State: Exp $
+ *
+ * Last modified on   $Date: 2003-02-13 22:48:46 $
+ *               by   $Author: ian_dickinson $
+ *
+ * (c) Copyright 2002-2003, Hewlett-Packard Company, all rights reserved. 
+ * (see footer for full conditions)
+ * ****************************************************************************/
+
+// Package
+///////////////
+package com.hp.hpl.jena.ontology;
+
+
+// Imports
+///////////////
+
+
+/**
+ * <p>
+ * Interface defining the axiom form in which all members of a collection are
+ * declared pair-wise disjoint.  This allows ontologies that wish to support the
+ * unique names assumption to add this condition in languages (like OWL) that
+ * do not make the same assumption, with a minimum number of statements.
+ * Instances of the all different axiom are expected to have a property
+ * (e.g. <code>owl:distinctMembers</code> defining the list of distinct
+ * individuals in the ontology.  For a given vocabulary, this will be defined by
+ * the {@link OntVocabulary#distinctMembers distinctMembers} entry.
+ * </p>
+ *
+ * @author Ian Dickinson, HP Labs
+ *         (<a  href="mailto:Ian.Dickinson@hp.com" >email</a>)
+ * @version CVS $Id: AllDifferent.java,v 1.1 2003-02-13 22:48:46 ian_dickinson Exp $
+ */
+public interface AllDifferent
+    extends OntResource
+{
+    // Constants
+    //////////////////////////////////
+
+
+    // Static variables
+    //////////////////////////////////
+
+
+    // Instance variables
+    //////////////////////////////////
+
+
+    // Constructors
+    //////////////////////////////////
+
+
+    // External signature methods
+    //////////////////////////////////
+
+
+    // Internal implementation methods
+    //////////////////////////////////
+
+
+    //==============================================================================
+    // Inner class definitions
+    //==============================================================================
+
+
+}
+
+
+/*
+    (c) Copyright Hewlett-Packard Company 2002-2003
+    All rights reserved.
+
+    Redistribution and use in source and binary forms, with or without
+    modification, are permitted provided that the following conditions
+    are met:
+
+    1. Redistributions of source code must retain the above copyright
+       notice, this list of conditions and the following disclaimer.
+
+    2. Redistributions in binary form must reproduce the above copyright
+       notice, this list of conditions and the following disclaimer in the
+       documentation and/or other materials provided with the distribution.
+
+    3. The name of the author may not be used to endorse or promote products
+       derived from this software without specific prior written permission.
+
+    THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+    IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+    OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+    IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+    INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+    NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+    DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+    THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+    (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+    THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
---
+++ src/com/hp/hpl/jena/ontology/ObjectProperty.java
@@ -0,0 +1,84 @@
+/*****************************************************************************
+ * Source code information
+ * -----------------------
+ * Original author    Ian Dickinson, HP Labs Bristol
+ * Author email       Ian.Dickinson@hp.com
+ * Package            Jena 2
+ * Web                http://sourceforge.net/projects/jena/
+ * Created            10 Feb 2003
+ * Filename           $RCSfile: ObjectProperty.java,v $
+ * Revision           $Revision: 1.1 $
+ * Release status     $State: Exp $
+ *
+ * Last modified on   $Date: 2003-02-13 22:48:46 $
+ *               by   $Author: ian_dickinson $
+ *
+ * (c) Copyright 2002-2003, Hewlett-Packard Company, all rights reserved. 
+ * (see footer for full conditions)
+ * ****************************************************************************/
+
+// Package
+///////////////
+package com.hp.hpl.jena.ontology;
+
+
+// Imports
+///////////////
+
+
+/**
+ * <p>
+ * Interface encapsulating properties whose range values are restricted to
+ * individuals (as distinct from datatype valued {@link DatatypeProperty
+ * properties}). Some ontology languages, such as OWL Full, do not distinguish
+ * between object and datatype properties.
+ * </p>
+ *
+ * @author Ian Dickinson, HP Labs
+ *         (<a  href="mailto:Ian.Dickinson@hp.com" >email</a>)
+ * @version CVS $Id: ObjectProperty.java,v 1.1 2003-02-13 22:48:46 ian_dickinson Exp $
+ */
+public interface ObjectProperty
+    extends OntProperty
+{
+    // Constants
+    //////////////////////////////////
+
+
+    // External signature methods
+    //////////////////////////////////
+
+
+}
+
+
+/*
+    (c) Copyright Hewlett-Packard Company 2002-2003
+    All rights reserved.
+
+    Redistribution and use in source and binary forms, with or without
+    modification, are permitted provided that the following conditions
+    are met:
+
+    1. Redistributions of source code must retain the above copyright
+       notice, this list of conditions and the following disclaimer.
+
+    2. Redistributions in binary form must reproduce the above copyright
+       notice, this list of conditions and the following disclaimer in the
+       documentation and/or other materials provided with the distribution.
+
+    3. The name of the author may not be used to endorse or promote products
+       derived from this software without specific prior written permission.
+
+    THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+    IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+    OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+    IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+    INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+    NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+    DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+    THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+    (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+    THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
