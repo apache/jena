8718bed3e0de9ea4d5c73b4653d27b7319ee2fcd
Chris Dolan <chrisdolan@apache.org>
2003-02-20 10:58:01 +0000

    New test cases for Model polymorphism.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jena/Import/Jena-CVS/jena2/trunk@1109202 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/com/hp/hpl/jena/rdf/model/test/TestModelPolymorphism.java
@@ -0,0 +1,27 @@
+package com.hp.hpl.jena.rdf.model.test;
+
+import com.hp.hpl.jena.graph.GraphTestBase;
+import com.hp.hpl.jena.graph.*;
+import com.hp.hpl.jena.rdf.model.*;
+import junit.framework.*;
+
+public class TestModelPolymorphism extends GraphTestBase
+    {
+    public static TestSuite suite()
+        { return new TestSuite( TestModelPolymorphism.class ); }   
+        
+    public TestModelPolymorphism(String name)
+        {
+        super(name);
+        }
+
+    public void testPoly()
+        {
+        Model m = ModelFactory.createDefaultModel();
+        Resource r = m.createResource( "http://www.electric-hedgehog.net/a-o-s.html" );
+        assertFalse( "the Resouce should not be null", r == null );
+        Property p = (Property) r.as( Property.class );
+        assertFalse( "the Property should not be null", p == null );
+        assertFalse( "the Resource and Property should not be identical", r == p );
+        }
+    }
