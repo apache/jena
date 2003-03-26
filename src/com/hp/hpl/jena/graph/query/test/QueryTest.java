fc9624e655afb86cea7ae4aeb772b2289f6a82ad
Chris Dolan <chrisdolan@apache.org>
2003-03-26 12:18:10 +0000

    extracted assertEquals into GraphTestBase
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jena/Import/Jena-CVS/jena2/trunk@1109286 13f79535-47bb-0310-9956-ffa450edef68

--- src/com/hp/hpl/jena/graph/query/test/QueryTest.java
+++ src/com/hp/hpl/jena/graph/query/test/QueryTest.java
@@ -1,7 +1,7 @@
 /*
   (c) Copyright 2003, Hewlett-Packard Company, all rights reserved.
   [See end of file]
-  $Id: QueryTest.java,v 1.2 2003-03-14 13:38:25 chris-dollin Exp $
+  $Id: QueryTest.java,v 1.3 2003-03-26 12:18:10 chris-dollin Exp $
 */
 
 package com.hp.hpl.jena.graph.query.test;
@@ -42,12 +42,6 @@ public class QueryTest extends GraphTestBase
         single = graphWith( "spindizzies drive cities" );
         }
         
-	public void assertEquals( String name, Graph wanted, Graph obtained )
-		{
-		Model mWanted = new ModelMem( wanted ), mObtained = new ModelMem( obtained );
-		assertTrue( name, mWanted.isIsomorphicWith( mObtained ) );
-		}
-		
     private void checkQueryVariables()
         {
         assertEquals( X, Query.X );
