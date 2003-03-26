a9b86f230eaa8463068d86ff293f1dd0bd81bb98
Chris Dolan <chrisdolan@apache.org>
2003-03-26 11:50:59 +0000

    DoesNotReifyException - exception raised when trying to get a Statement from a Resource that doesn't have the reification quads.
    ReifiedStatement - the type of reifiedStatement resources, from which
    you can extract Statement's.
    
    RSIterator - an iterator that delivers ReifiedStatement objects.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jena/Import/Jena-CVS/jena2/trunk@1109280 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/com/hp/hpl/jena/rdf/model/DoesNotReifyException.java
@@ -0,0 +1,53 @@
+/*
+	(c) Copyright 2003, Hewlett-Packard Company, all rights reserved.
+	[see end of file]
+	$Id: DoesNotReifyException.java,v 1.1 2003-03-26 11:50:59 chris-dollin Exp $
+*/
+
+package com.hp.hpl.jena.rdf.model;
+
+import com.hp.hpl.jena.graph.*;
+
+/**
+    This exception may be raised when an attempt to convert an RDFNode
+    to a ReifiedStatement fails because the RDFNode does not correspond
+    to a reification.
+
+    @author chris.dollin@hp.com
+*/
+
+public class DoesNotReifyException extends RDFException
+    {
+    public DoesNotReifyException( Node n ) { super( n.toString() ); }
+    }
+
+/*
+    (c) Copyright Hewlett-Packard Company 2003
+    All rights reserved.
+
+    Redistribution and use in source and binary forms, with or without
+    modification, are permitted provided that the following conditions
+    are met:
+
+    1. Redistributions of source code must retain the above copyright
+       notice, this list of conditions and the following disclaimer.
+
+    2. Redistributions in binary form must reproduce the above copyright
+       notice, this list of conditions and the following disclaimer in the
+       documentation and/or other materials provided with the distribution.
+
+    3. The name of the author may not be used to endorse or promote products
+       derived from this software without specific prior written permission.
+
+    THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+    IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+    OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+    IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+    INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+    NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+    DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+    THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+    (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+    THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
