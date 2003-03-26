04c75547e3dc3f8991197038d7b17a0f987a0a03
Chris Dolan <chrisdolan@apache.org>
2003-03-26 11:40:26 +0000

    GetTriple  expresses the idea of a type from which triples associated with nodes can be extracted.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jena/Import/Jena-CVS/jena2/trunk@1109277 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/com/hp/hpl/jena/graph/GetTriple.java
@@ -0,0 +1,52 @@
+/*
+	(c) Copyright 2003, Hewlett-Packard Company, all rights reserved.
+	[see end of file]
+	$Id: GetTriple.java,v 1.1 2003-03-26 11:40:26 chris-dollin Exp $
+*/
+package com.hp.hpl.jena.graph;
+
+/**
+    this interface describes types that can have a triple extracted using
+    a _getTriple_ method. It was constructed so that Node's can have
+    possibly embedded triples but defer to a GetTriple object if they have
+    no triple of their own; the particular GetTriple used initially is in Reifier,
+    but that seemed excessively special.
+*/
+
+public interface GetTriple
+    {
+    /**
+        return the triple associated with the node _n_.
+    */
+    public Triple getTriple( Node n );
+    }
+
+/*
+    (c) Copyright Hewlett-Packard Company 2003
+    All rights reserved.
+
+    Redistribution and use in source and binary forms, with or without
+    modification, are permitted provided that the following conditions
+    are met:
+
+    1. Redistributions of source code must retain the above copyright
+       notice, this list of conditions and the following disclaimer.
+
+    2. Redistributions in binary form must reproduce the above copyright
+       notice, this list of conditions and the following disclaimer in the
+       documentation and/or other materials provided with the distribution.
+
+    3. The name of the author may not be used to endorse or promote products
+       derived from this software without specific prior written permission.
+
+    THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+    IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+    OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+    IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+    INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+    NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+    DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+    THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+    (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+    THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
