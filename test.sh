e33269b2c0e3df133559247b698292b7a530cda1
Chris Dolan <chrisdolan@apache.org>
2003-04-09 08:21:00 +0000

    Updated release notes for J2P2, ditto readme's and migration doc. Attempt to make test.sh run on Cygwin.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jena/Import/Jena-CVS/jena2/trunk@1109395 13f79535-47bb-0310-9956-ffa450edef68

--- test.sh
+++ test.sh
@@ -1,1 +1,4 @@
-java -classpath "lib/antlr.debug.jar:lib/concurrent-1.3.0.jar:lib/db3-3-11.jar:lib/icu4j.jar:lib/jakarta-oro-2.0.5.jar:lib/jena.jar:lib/junit.jar:lib/log4j-1.2.7.jar:lib/rdf-api-2001-01-19.jar:lib/sesame-client.jar:lib/xercesImpl.jar:lib/xmlParserAPIs.jar" junit.textui.TestRunner com.hp.hpl.jena.test.TestPackage
+#!/bin/bash
+export S=":"
+if [ "$OSTYPE" == "cygwin" ]; then export S=";"; fi
+java -classpath "lib/antlr.debug.jar${S}lib/concurrent-1.3.0.jar${S}lib/db3-3-11.jar${S}lib/icu4j.jar${S}lib/jakarta-oro-2.0.5.jar${S}lib/jena.jar${S}lib/junit.jar${S}lib/log4j-1.2.7.jar${S}lib/rdf-api-2001-01-19.jar${S}lib/sesame-client.jar${S}lib/xercesImpl.jar${S}lib/xmlParserAPIs.jar" junit.textui.TestRunner com.hp.hpl.jena.test.TestPackage
