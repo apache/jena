ace26e81902f44025616964b2532dbb153789d0e
Jeremy Carroll <jeremy_carroll@apache.org>
2003-04-11 01:51:04 +0000

    Deleted unused imports.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jena/Import/Jena-CVS/jena2/trunk@1109424 13f79535-47bb-0310-9956-ffa450edef68

--- src/com/hp/hpl/jena/rdf/model/test/TestStatements.java
+++ src/com/hp/hpl/jena/rdf/model/test/TestStatements.java
@@ -1,13 +1,12 @@
 /*
 	(c) Copyright 2003, Hewlett-Packard Company, all rights reserved.
 	[see end of file]
-	$Id: TestStatements.java,v 1.3 2003-04-10 13:52:48 chris-dollin Exp $
+	$Id: TestStatements.java,v 1.4 2003-04-11 01:51:04 jeremy_carroll Exp $
 */
 
 package com.hp.hpl.jena.rdf.model.test;
 
 import com.hp.hpl.jena.rdf.model.*;
-import com.hp.hpl.jena.rdf.model.impl.*;
 import com.hp.hpl.jena.graph.*;
 import junit.framework.*;
 import com.hp.hpl.jena.vocabulary.RDF;
