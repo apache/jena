18662229c282178c917d0fc5458fe2c5bb0e79ef
Jeremy Carroll <jeremy_carroll@apache.org>
2003-04-15 21:08:37 +0000

    fixing -ko to -kkv
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jena/Import/Jena-CVS/jena2/trunk@1109514 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/com/hp/hpl/jena/ontology/tidy/CBuiltin.java
@@ -0,0 +1,23 @@
+package com.hp.hpl.jena.ontology.tidy;
+import com.hp.hpl.jena.graph.*;
+import com.hp.hpl.jena.enhanced.*;
+
+/**
+ * @author jjc
+ *
+ */
+class CBuiltin extends CNode {
+    final int category;
+    CBuiltin(Node n, EnhGraph eg, int i) {
+        super(n, eg);
+        category = i;
+    }
+    public int getCategories() {
+        return category;
+    }
+    public void setCategories(int c) {
+        if (c!=category)
+          throw new SyntaxException("Internal error in syntax checker.");
+    }
+    
+}
