5cf16249fa1c9a53bd960dbdde87168188f75b2b
Jeremy Carroll <jeremy_carroll@apache.org>
2003-04-15 21:17:28 +0000

    fixing -ko to -kkv
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jena/Import/Jena-CVS/jena2/trunk@1109537 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/com/hp/hpl/jena/reasoner/Finder.java
@@ -0,0 +1,75 @@
+/******************************************************************
+ * File:        Finder.java
+ * Created by:  Dave Reynolds
+ * Created on:  18-Jan-03
+ * 
+ * (c) Copyright 2003, Hewlett-Packard Company, all rights reserved.
+ * [See end of file]
+ * $Id: Finder.java,v 1.3 2003-04-15 21:17:18 jeremy_carroll Exp $
+ *****************************************************************/
+package com.hp.hpl.jena.reasoner;
+
+import com.hp.hpl.jena.util.iterator.ExtendedIterator;
+
+/**
+ * Minimal interface for preforming simple pattern find operations.
+ * Should be implemented by reasoners, caches and related datastructures.
+ * 
+ * @author <a href="mailto:der@hplb.hpl.hp.com">Dave Reynolds</a>
+ * @version $Revision: 1.3 $ on $Date: 2003-04-15 21:17:18 $
+ */
+public interface Finder {
+
+    /**
+     * Basic pattern lookup interface.
+     * @param pattern a TriplePattern to be matched against the data
+     * @return a ClosableIterator over all Triples in the data set
+     *  that match the pattern
+     */
+    public ExtendedIterator find(TriplePattern pattern);
+    
+    /**
+     * Extended find interface used in situations where the implementator
+     * may or may not be able to answer the complete query. It will
+     * attempt to answer the pattern but if its answers are not known
+     * to be complete then it will also pass the request on to the nested
+     * Finder to append more results.
+     * @param pattern a TriplePattern to be matched against the data
+     * @param continuation either a Finder or a normal Graph which
+     * will be asked for additional match results if the implementor
+     * may not have completely satisfied the query.
+     */
+    public ExtendedIterator findWithContinuation(TriplePattern pattern, Finder continuation);
+
+}
+
+/*
+    (c) Copyright Hewlett-Packard Company 2003
+    All rights reserved.
+
+    Redistribution and use in source and binary forms, with or without
+    modification, are permitted provided that the following conditions
+    are met:
+
+    1. Redistributions of source code must retain the above copyright
+       notice, this list of conditions and the following disclaimer.
+
+    2. Redistributions in binary form must reproduce the above copyright
+       notice, this list of conditions and the following disclaimer in the
+       documentation and/or other materials provided with the distribution.
+
+    3. The name of the author may not be used to endorse or promote products
+       derived from this software without specific prior written permission.
+
+    THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+    IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+    OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+    IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+    INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+    NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+    DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+    THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+    (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+    THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
