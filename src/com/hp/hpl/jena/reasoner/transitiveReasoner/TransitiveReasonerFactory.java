7899a9adb35e056772e396d6d2da09cab0443b18
Jeremy Carroll <jeremy_carroll@apache.org>
2003-04-15 21:29:10 +0000

    fixing -ko to -kkv
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jena/Import/Jena-CVS/jena2/trunk@1109568 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/com/hp/hpl/jena/reasoner/transitiveReasoner/TransitiveReasonerFactory.java
@@ -0,0 +1,103 @@
+/******************************************************************
+ * File:        TransitiveReasonerFactory.java
+ * Created by:  Dave Reynolds
+ * Created on:  19-Jan-03
+ * 
+ * (c) Copyright 2003, Hewlett-Packard Company, all rights reserved.
+ * [See end of file]
+ * $Id: TransitiveReasonerFactory.java,v 1.4 2003-04-15 21:29:00 jeremy_carroll Exp $
+ *****************************************************************/
+package com.hp.hpl.jena.reasoner.transitiveReasoner;
+
+import com.hp.hpl.jena.mem.ModelMem;
+import com.hp.hpl.jena.rdf.model.*;
+import com.hp.hpl.jena.vocabulary.*;
+import com.hp.hpl.jena.reasoner.*;
+
+/**
+ * Factory class for creating blank instances of the transitive reasoner.
+ * 
+ * @author <a href="mailto:der@hplb.hpl.hp.com">Dave Reynolds</a>
+ * @version $Revision: 1.4 $ on $Date: 2003-04-15 21:29:00 $
+ */
+public class TransitiveReasonerFactory implements ReasonerFactory {
+    
+    /** Single global instance of this factory */
+    private static ReasonerFactory theInstance = new TransitiveReasonerFactory();
+    
+    /** Static URI for this reasoner type */
+    public static final String URI = "http://www.hpl.hp.com/semweb/2003/TransitiveReasoner";
+    
+    /**
+     * Return the single global instance of this factory
+     */
+    public static ReasonerFactory theInstance() {
+        return theInstance;
+    }
+    
+    /**
+     * Constructor method that builds an instance of the associated Reasoner
+     * @param configuration a set of arbitrary configuration information to be 
+     * passed the reasoner encoded within an RDF graph.
+     */
+    public Reasoner create(Model configuration) {
+        return new TransitiveReasoner();
+    }
+   
+    /**
+     * Return a description of the capabilities of this reasoner encoded in
+     * RDF. This method is normally called by the ReasonerRegistry which caches
+     * the resulting information so dynamically creating here is not really an overhead.
+     */
+    public Model getCapabilities() {
+        Model capabilities = new ModelMem();
+        Resource base = capabilities.createResource(getURI());
+        base.addProperty(ReasonerRegistry.nameP, "Transitive Reasoner")
+            .addProperty(ReasonerRegistry.descriptionP, "Provides reflexive-transitive closure of subClassOf and subPropertyOf")
+            .addProperty(ReasonerRegistry.supportsP, RDFS.subClassOf)
+            .addProperty(ReasonerRegistry.supportsP, RDFS.subPropertyOf)
+            .addProperty(ReasonerRegistry.supportsP, TransitiveReasoner.directSubClassOf)
+            .addProperty(ReasonerRegistry.supportsP, TransitiveReasoner.directSubPropertyOf)
+            .addProperty(ReasonerRegistry.versionP, "0.1");
+        return capabilities;
+    }
+    
+    /**
+     * Return the URI labelling this type of reasoner
+     */
+    public String getURI() {
+        return URI;
+    }
+    
+}
+
+/*
+    (c) Copyright Hewlett-Packard Company 2003
+    All rights reserved.
+
+    Redistribution and use in source and binary forms, with or without
+    modification, are permitted provided that the following conditions
+    are met:
+
+    1. Redistributions of source code must retain the above copyright
+       notice, this list of conditions and the following disclaimer.
+
+    2. Redistributions in binary form must reproduce the above copyright
+       notice, this list of conditions and the following disclaimer in the
+       documentation and/or other materials provided with the distribution.
+
+    3. The name of the author may not be used to endorse or promote products
+       derived from this software without specific prior written permission.
+
+    THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+    IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+    OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+    IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+    INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+    NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+    DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+    THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+    (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+    THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
