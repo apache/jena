b9599ef69b73560e2bf47c3c96b33428ec60ee58
Jeremy Carroll <jeremy_carroll@apache.org>
2003-04-15 21:10:23 +0000

    fixing -ko to -kkv
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jena/Import/Jena-CVS/jena2/trunk@1109519 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/com/hp/hpl/jena/ontology/tidy/CURIref.java
@@ -0,0 +1,21 @@
+package com.hp.hpl.jena.ontology.tidy;
+
+import com.hp.hpl.jena.enhanced.EnhGraph;
+import com.hp.hpl.jena.graph.Node;
+
+/**
+ * @author jjc
+ *
+ */
+class CURIref extends CGeneral {
+
+	/**
+	 * Constructor for CURIref.
+	 * @param n
+	 * @param eg
+	 */
+	public CURIref(Node n, EnhGraph eg) {
+		super(n, eg);
+	}
+
+}
