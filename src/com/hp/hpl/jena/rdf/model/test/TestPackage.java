b015fde564af265c70bf7a8ebb9cc76704667e46
Jeremy Carroll <jeremy_carroll@apache.org>
2003-04-15 21:15:14 +0000

    fixing -ko to -kkv
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jena/Import/Jena-CVS/jena2/trunk@1109531 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/com/hp/hpl/jena/rdf/model/test/TestPackage.java
@@ -0,0 +1,71 @@
+/*
+  (c) Copyright 2002, Hewlett-Packard Company, all rights reserved.
+  [See end of file]
+  $Id: TestPackage.java,v 1.10 2003-04-15 21:14:58 jeremy_carroll Exp $
+*/
+
+package com.hp.hpl.jena.rdf.model.test;
+
+import junit.framework.*;
+
+/**
+    Collected test suite for the .graph package.
+    @author  jjc + kers
+*/
+
+public class TestPackage extends TestSuite {
+
+    static public TestSuite suite() {
+        return new TestPackage();
+    }
+    
+    /** Creates new TestPackage */
+    private TestPackage() {
+        super("Model");
+        addTest( "TestModel", TestModelFactory.suite() );
+        addTest( "TestModelFactory", TestModelFactory.suite() );
+        addTest( "TestSimpleListStatements", TestSimpleListStatements.suite() );
+        addTest( "TestModelPolymorphism", TestModelPolymorphism.suite() );
+        addTest( "TestSimpleSelector", TestSimpleSelector.suite() );
+        addTest( "TestStatements", TestReifiedStatements.suite() );
+        addTest( "TestReifiedStatements", TestReifiedStatements.suite() );
+        addTest( "TestIterators", TestIterators.suite() );
+        addTest( "TestContains", TestContains.suite() );
+        }
+
+    private void addTest(String name, TestSuite tc) {
+        tc.setName(name);
+        addTest(tc);
+    }
+
+}
+
+/*
+    (c) Copyright Hewlett-Packard Company 2002
+    All rights reserved.
+
+    Redistribution and use in source and binary forms, with or without
+    modification, are permitted provided that the following conditions
+    are met:
+
+    1. Redistributions of source code must retain the above copyright
+       notice, this list of conditions and the following disclaimer.
+
+    2. Redistributions in binary form must reproduce the above copyright
+       notice, this list of conditions and the following disclaimer in the
+       documentation and/or other materials provided with the distribution.
+
+    3. The name of the author may not be used to endorse or promote products
+       derived from this software without specific prior written permission.
+
+    THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+    IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+    OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+    IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+    INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+    NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+    DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+    THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+    (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+    THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
