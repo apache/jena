8336b37884ae6cc1d6f127d5a9ea70e851c5a90a
Jeremy Carroll <jeremy_carroll@apache.org>
2003-04-15 21:11:47 +0000

    fixing -ko to -kkv
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jena/Import/Jena-CVS/jena2/trunk@1109522 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/com/hp/hpl/jena/ontology/tidy/SyntaxException.java
@@ -0,0 +1,43 @@
+package com.hp.hpl.jena.ontology.tidy;
+
+import com.hp.hpl.jena.util.JenaException;
+
+/**
+ * @author jjc
+ *
+ */
+public class SyntaxException extends JenaException {
+
+    /**
+     * Constructor for SyntaxException.
+     */
+    public SyntaxException() {
+        super();
+    }
+
+    /**
+     * Constructor for SyntaxException.
+     * @param message
+     */
+    public SyntaxException(String message) {
+        super(message);
+    }
+
+    /**
+     * Constructor for SyntaxException.
+     * @param message
+     * @param cause
+     */
+    public SyntaxException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    /**
+     * Constructor for SyntaxException.
+     * @param cause
+     */
+    public SyntaxException(Throwable cause) {
+        super(cause);
+    }
+
+}
