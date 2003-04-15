f72ea762dfe9b1433d29e667aea11656027291c4
Jeremy Carroll <jeremy_carroll@apache.org>
2003-04-15 21:06:07 +0000

    fixing -ko to -kkv
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jena/Import/Jena-CVS/jena2/trunk@1109507 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/com/hp/hpl/jena/datatypes/xsd/impl/XSDDurationType.java
@@ -0,0 +1,81 @@
+/******************************************************************
+ * File:        XSDDurationType.java
+ * Created by:  Dave Reynolds
+ * Created on:  16-Dec-02
+ * 
+ * (c) Copyright 2002, Hewlett-Packard Company, all rights reserved.
+ * [See end of file]
+ * $Id: XSDDurationType.java,v 1.3 2003-04-15 21:05:56 jeremy_carroll Exp $
+ *****************************************************************/
+package com.hp.hpl.jena.datatypes.xsd.impl;
+
+import com.hp.hpl.jena.datatypes.*;
+import com.hp.hpl.jena.datatypes.xsd.*;
+import com.hp.hpl.jena.graph.LiteralLabel;
+
+/**
+ * The XSD duration type, the only job of this extra layer is to
+ * wrap the return value in a more convenient accessor type. We could
+ * avoid this proliferation of trivial types by use of reflection but
+ * since that causes allergic reactions in some we use brute force.
+ * 
+ * @author <a href="mailto:der@hplb.hpl.hp.com">Dave Reynolds</a>
+ * @version $Revision: 1.3 $ on $Date: 2003-04-15 21:05:56 $
+ */
+public class XSDDurationType extends XSDDatatype {
+    
+    /**
+     * Constructor
+     */
+    public XSDDurationType() {
+        super("duration");
+    }
+        
+    /**
+     * Parse a lexical form of this datatype to a value
+     * @return a Duration value
+     * @throws DatatypeFormatException if the lexical form is not legal
+     */
+    public Object parse(String lexicalForm) throws DatatypeFormatException {
+        return new XSDDuration(super.parse(lexicalForm), typeDeclaration);
+    }
+    
+    /**
+     * Compares two instances of values of the given datatype.
+     * This ignores lang tags and just uses the java.lang.Number 
+     * equality.
+     */
+    public boolean isEqual(LiteralLabel value1, LiteralLabel value2) {
+       return value1.getValue().equals(value2.getValue());
+    }
+}
+
+/*
+    (c) Copyright Hewlett-Packard Company 2002
+    All rights reserved.
+
+    Redistribution and use in source and binary forms, with or without
+    modification, are permitted provided that the following conditions
+    are met:
+
+    1. Redistributions of source code must retain the above copyright
+       notice, this list of conditions and the following disclaimer.
+
+    2. Redistributions in binary form must reproduce the above copyright
+       notice, this list of conditions and the following disclaimer in the
+       documentation and/or other materials provided with the distribution.
+
+    3. The name of the author may not be used to endorse or promote products
+       derived from this software without specific prior written permission.
+
+    THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+    IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+    OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+    IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+    INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+    NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+    DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+    THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+    (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+    THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
