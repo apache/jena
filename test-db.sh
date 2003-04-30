5667eb272ac4b97385c04de4c956021fe78107d5
Chris Dolan <chrisdolan@apache.org>
2003-04-30 08:11:44 +0000

    build.xml: added db sources to the compile and javadoc lists. test-db.sh: a file to run the current DB tests.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jena/Import/Jena-CVS/jena2/trunk@1109702 13f79535-47bb-0310-9956-ffa450edef68

---
+++ test-db.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#
+# run the DB tests. This is a HACK to get started. The test for "a server is available"
+# is too weak and in any case we want a version that will work for any supported
+# server. Still, have to begin somehow ...
+#
+
+if mysql --user=test < /dev/null ; then
+    if [ "$OSTYPE" == "cygwin" ]; then export S=";"; else export S=":"; fi
+
+    export CP=""
+    for jar in lib/*jar; do export CP=$CP$S$jar; done
+
+    java -classpath $CP junit.textui.TestRunner com.hp.hpl.jena.db.test.TestPackage
+
+else
+    echo mysql does not seem to be installed.
+fi
