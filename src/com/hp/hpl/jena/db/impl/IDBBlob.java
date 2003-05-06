fdb32271443ca4fd31060d180f209c26349d673b
H Kuno <hkuno@apache.org>
2003-05-06 09:08:51 +0000

    Generic Blob interface (e.g., to wrap Oracle Blob type)
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jena/Import/Jena-CVS/jena2/trunk@1109791 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/com/hp/hpl/jena/db/impl/IDBBlob.java
@@ -0,0 +1,67 @@
+/*
+ *  (c) Copyright Hewlett-Packard Company 2003
+ *  All rights reserved.
+ *
+ *
+ */
+
+
+//=======================================================================
+// Package
+package com.hp.hpl.jena.db.impl;
+
+import java.sql.Blob;
+import java.sql.SQLException;
+
+//=======================================================================
+// Imports
+
+
+//=======================================================================
+/**
+* Interface for database blob objects.
+* 
+* @author <a href="mailto:harumi.kuno@hp.com">Harumi Kuno</a>
+* @version $Revision: 1.1 $ on $Date: 2003-05-06 09:08:51 $
+*/
+
+public interface IDBBlob {
+	
+	/**
+	 * Put data to the BLOB at the requested position. 
+	 * @return int the number of bytes actually written
+	 */
+	public int putBytes(long pos, byte bytes[]) throws SQLException;
+								 
+    /** Return as blob  */
+    public Blob getBlob();
+}
+
+
+/*
+ *  (c) Copyright Hewlett-Packard Company 2000, 2001
+ *  All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. The name of the author may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
