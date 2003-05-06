1e15518709dd4446423b4d966a8bae3d62c8f3be
H Kuno <hkuno@apache.org>
2003-05-06 09:40:03 +0000

    Initial revision.  Not complete.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jena/Import/Jena-CVS/jena2/trunk@1109792 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/com/hp/hpl/jena/db/impl/DBBlob.java
@@ -0,0 +1,65 @@
+/*
+ * Created on May 5, 2003
+ *
+ * To change the template for this generated file go to
+ * Window>Preferences>Java>Code Generation>Code and Comments
+ */
+package com.hp.hpl.jena.db.impl;
+
+import java.sql.Blob;
+
+import com.hp.hpl.jena.db.RDFRDBException;
+
+/**
+ * @author hkuno
+ *
+ * To change the template for this generated type comment go to
+ * Window>Preferences>Java>Code Generation>Code and Comments
+ */
+public abstract class DBBlob implements IDBBlob {
+
+	/** the real blob */
+	protected Object m_blob;
+	
+	
+	/** the target database type */
+	protected String m_dbtype;
+
+	/** constructor */
+	public DBBlob(Object ablob, String dbType) {
+			m_dbtype = dbType;
+			m_blob = ablob;
+			
+	}
+	
+	public  DBBlob() {
+	}
+	
+	/**
+	 * Creates and returns instance of appropriate subclass of DBBlob.
+	 * @param ablob
+	 * @param dbType
+	 * @return
+	 */
+	public static IDBBlob getDBBlob(Object ablob, String dbType) {
+		IDBBlob result = null;
+		if (dbType.equalsIgnoreCase("oracle")) {
+			// result = new DBBlob_Oracle(ablob, dbType);
+		} else {
+			throw new RDFRDBException("No appropriate blob type found for " + dbType);
+		}
+		return (result);
+		
+	
+	}
+	
+
+	/* (non-Javadoc)
+	 * @see com.hp.hpl.jena.db.impl.IDBBlob#asOracleBlob()
+	 */
+	public Blob getBlob() {
+		// TODO Auto-generated method stub
+		return ((java.sql.Blob) m_blob);
+	}
+
+}
