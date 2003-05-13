2ebd81fbf741fd74c3a239d6e74e7aa3855a3926
H Kuno <hkuno@apache.org>
2003-05-13 16:44:30 +0000

    Added exception that says that Oracle is not yet supported for Jena.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jena/Import/Jena-CVS/jena2/trunk@1109867 13f79535-47bb-0310-9956-ffa450edef68

--- src/com/hp/hpl/jena/db/impl/Driver_Oracle.java
+++ src/com/hp/hpl/jena/db/impl/Driver_Oracle.java
@@ -1,7 +1,7 @@
 /*
   (c) Copyright 2003, Hewlett-Packard Company, all rights reserved.
   [See end of file]
-  $Id: Driver_Oracle.java,v 1.3 2003-05-11 00:36:52 hkuno Exp $
+  $Id: Driver_Oracle.java,v 1.4 2003-05-13 16:44:30 hkuno Exp $
 */
 
 package com.hp.hpl.jena.db.impl;
@@ -28,8 +28,15 @@ public class Driver_Oracle extends DriverRDB {
 	/** 
 	 * Constructor
 	 */
-	public Driver_Oracle(){
+	public Driver_Oracle() {
+		
 		super();
+		
+		//	Oracle not supported at this time
+	    if (true) {
+	    	throw(new RDFRDBException("Oracle is not yet supported for Jena."));
+	    }
+		
 
 		String myPackageName = this.getClass().getPackage().getName();
 		
