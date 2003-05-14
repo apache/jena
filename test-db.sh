3552b53e8bd134051fdf5d83e592d19663dec62b
Chris Dolan <chrisdolan@apache.org>
2003-05-14 13:42:47 +0000

    Changed mode to binary.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jena/Import/Jena-CVS/jena2/trunk@1109874 13f79535-47bb-0310-9956-ffa450edef68

--- test-db.sh
+++ test-db.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#
+
 # run the DB tests. This is a HACK to get started. The test for "a server is available"
 # is too weak and in any case we want a version that will work for any supported
 # server. Still, have to begin somehow ...
-#
+
 
 if mysql --user=test < /dev/null ; then
     if [ "$OSTYPE" == "cygwin" ]; then export S=";"; else export S=":"; fi
--- test.sh
+++ test.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# run Jena tests; try and guess path separator.
 export S=":"
 if [ "$OSTYPE" == "cygwin" ]; then export S=";"; fi
 java -classpath "lib/antlr.debug.jar${S}lib/concurrent.jar${S}lib/db3-3-11.jar${S}lib/icu4j.jar${S}lib/jakarta-oro-2.0.5.jar${S}lib/jena.jar${S}lib/junit.jar${S}lib/log4j-1.2.7.jar${S}lib/rdf-api-2001-01-19.jar${S}lib/sesame-client.jar${S}lib/xercesImpl.jar${S}lib/xmlParserAPIs.jar" junit.textui.TestRunner com.hp.hpl.jena.test.TestPackage
