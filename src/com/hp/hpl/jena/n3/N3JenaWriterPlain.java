b0563e8acaad217b0e4c8f2c2f36bdb8ef74bd6a
Andy Seaborne <andy@apache.org>
2003-06-10 10:15:49 +0000

    no message
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jena/Import/Jena-CVS/jena2/trunk@1109996 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/com/hp/hpl/jena/n3/N3JenaWriterPlain.java
@@ -0,0 +1,46 @@
+/*
+ * (c) Copyright 2001-2003, Hewlett-Packard Company, all rights reserved.
+ * [See end of file]
+ */
+
+package com.hp.hpl.jena.n3;
+
+/** An simple N3 writer - writes N3 out as records, one per subject in the graph.
+ *
+ * @author		Andy Seaborne
+ * @version 	$Id: N3JenaWriterPlain.java,v 1.1 2003-06-10 10:15:49 andy_seaborne Exp $
+ */
+
+
+
+public class N3JenaWriterPlain extends N3JenaWriterCommon
+{
+ 
+}
+
+/*
+ *  (c) Copyright Hewlett-Packard Company 2001-2003
+ *  All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. The name of the author may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
