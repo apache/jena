ee10b89a4d5402a14d257b1c59d36522d7075430
Ian Dickinsone <ian_dickinson@apache.org>
2003-06-13 19:32:01 +0000

    Fixed bug in hasValueRestriction: the value can be a literal as well as an individual
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jena/Import/Jena-CVS/jena2/trunk@1110047 13f79535-47bb-0310-9956-ffa450edef68

--- src/com/hp/hpl/jena/ontology/HasValueRestriction.java
+++ src/com/hp/hpl/jena/ontology/HasValueRestriction.java
@@ -7,10 +7,10 @@
  * Web                http://sourceforge.net/projects/jena/
  * Created            07-May-2003
  * Filename           $RCSfile: HasValueRestriction.java,v $
- * Revision           $Revision: 1.4 $
+ * Revision           $Revision: 1.5 $
  * Release status     $State: Exp $
  *
- * Last modified on   $Date: 2003-06-10 22:58:18 $
+ * Last modified on   $Date: 2003-06-13 19:32:01 $
  *               by   $Author: ian_dickinson $
  *
  * (c) Copyright 2002-2003, Hewlett-Packard Company, all rights reserved.
@@ -35,7 +35,7 @@ import com.hp.hpl.jena.rdf.model.*;
  *
  * @author Ian Dickinson, HP Labs
  *         (<a  href="mailto:Ian.Dickinson@hp.com" >email</a>)
- * @version CVS $Id: HasValueRestriction.java,v 1.4 2003-06-10 22:58:18 ian_dickinson Exp $
+ * @version CVS $Id: HasValueRestriction.java,v 1.5 2003-06-13 19:32:01 ian_dickinson Exp $
  */
 public interface HasValueRestriction
     extends Restriction 
@@ -52,35 +52,36 @@ public interface HasValueRestriction
      * <p>Assert that this restriction restricts the property to have the given
      * value. Any existing statements for <code>hasValue</code>
      * will be removed.</p>
-     * @param individual The individual that is the value that the restricted property must have to be a member of the
+     * @param value The RDF value (an individual or a literal) 
+     * that is the value that the restricted property must have to be a member of the
      * class defined by this restriction.
      * @exception OntProfileException If the {@link Profile#HAS_VALUE()} property is not supported in the current language profile.   
      */ 
-    public void setHasValue( Resource individual );
+    public void setHasValue( RDFNode value );
 
     /**
-     * <p>Answer the individual that all values of the restricted property must be equal to.</p>
-     * @return An individual that is the value of the restricted property
+     * <p>Answer the RDF value that all values of the restricted property must be equal to.</p>
+     * @return An RDFNode that is the value of the restricted property
      * @exception OntProfileException If the {@link Profile#HAS_VALUE()} property is not supported in the current language profile.   
      */ 
-    public Individual getHasValue();
+    public RDFNode getHasValue();
 
     /**
-     * <p>Answer true if this property restriction has the given individual as the value which all 
+     * <p>Answer true if this property restriction has the given RDF value as the value which all 
      * values of the restricted property must equal.</p>
-     * @param individual An individual to test 
-     * @return True if the given individual is the value of the restricted property in this restriction
+     * @param value An RDF value to test 
+     * @return True if the given value is the value of the restricted property in this restriction
      * @exception OntProfileException If the {@link Profile#HAS_VALUE()} property is not supported in the current language profile.   
      */
-    public boolean hasValue( Resource individual );
+    public boolean hasValue( RDFNode value );
     
     /**
      * <p>Remove the statement that this restriction requires the restricted property to have
      * the given value.  If this statement
      * is not true of the current model, nothing happens.</p>
-     * @param individual A resource that is to be removed as the required value for the restricted property
+     * @param value An RDF value that is to be removed as the required value for the restricted property
      */
-    public void removeHasValue( Resource individual );
+    public void removeHasValue( RDFNode value );
     
 
 }
--- src/com/hp/hpl/jena/ontology/impl/HasValueRestrictionImpl.java
+++ src/com/hp/hpl/jena/ontology/impl/HasValueRestrictionImpl.java
@@ -7,10 +7,10 @@
  * Web                http://sourceforge.net/projects/jena/
  * Created            07-May-2003
  * Filename           $RCSfile: HasValueRestrictionImpl.java,v $
- * Revision           $Revision: 1.5 $
+ * Revision           $Revision: 1.6 $
  * Release status     $State: Exp $
  *
- * Last modified on   $Date: 2003-06-10 22:58:58 $
+ * Last modified on   $Date: 2003-06-13 19:32:01 $
  *               by   $Author: ian_dickinson $
  *
  * (c) Copyright 2002-2003, Hewlett-Packard Company, all rights reserved.
@@ -28,7 +28,7 @@ package com.hp.hpl.jena.ontology.impl;
 import com.hp.hpl.jena.enhanced.*;
 import com.hp.hpl.jena.graph.*;
 import com.hp.hpl.jena.ontology.*;
-import com.hp.hpl.jena.rdf.model.Resource;
+import com.hp.hpl.jena.rdf.model.*;
 
 
 /**
@@ -38,7 +38,7 @@ import com.hp.hpl.jena.rdf.model.Resource;
  *
  * @author Ian Dickinson, HP Labs
  *         (<a  href="mailto:Ian.Dickinson@hp.com" >email</a>)
- * @version CVS $Id: HasValueRestrictionImpl.java,v 1.5 2003-06-10 22:58:58 ian_dickinson Exp $
+ * @version CVS $Id: HasValueRestrictionImpl.java,v 1.6 2003-06-13 19:32:01 ian_dickinson Exp $
  */
 public class HasValueRestrictionImpl
     extends RestrictionImpl
@@ -102,42 +102,44 @@ public class HasValueRestrictionImpl
      * <p>Assert that this restriction restricts the property to have the given
      * value. Any existing statements for <code>hasValue</code>
      * will be removed.</p>
-     * @param individual The individual that is the value that the restricted property must have to be a member of the
+     * @param value The RDF value (an individual or a literal) 
+     * that is the value that the restricted property must have to be a member of the
      * class defined by this restriction.
      * @exception OntProfileException If the {@link Profile#HAS_VALUE()} property is not supported in the current language profile.   
      */ 
-    public void setHasValue( Resource individual ) {
-        setPropertyValue( getProfile().HAS_VALUE(), "HAS_VALUE", individual );
+    public void setHasValue( RDFNode value ) {
+        setPropertyValue( getProfile().HAS_VALUE(), "HAS_VALUE", value );
     }
 
     /**
-     * <p>Answer the individual that all values of the restricted property must be equal to.</p>
-     * @return An individual that is the value of the restricted property
+     * <p>Answer the RDF value that all values of the restricted property must be equal to.</p>
+     * @return An RDFNode that is the value of the restricted property
      * @exception OntProfileException If the {@link Profile#HAS_VALUE()} property is not supported in the current language profile.   
      */ 
-    public Individual getHasValue() {
-        return (Individual) objectAs( getProfile().HAS_VALUE(), "HAS_VALUE", Individual.class );
+    public RDFNode getHasValue() {
+        checkProfile( getProfile().HAS_VALUE(), "HAS_VALUE" );
+        return getPropertyValue( getProfile().HAS_VALUE() );
     }
 
     /**
-     * <p>Answer true if this property restriction has the given individual as the value which all 
+     * <p>Answer true if this property restriction has the given RDF value as the value which all 
      * values of the restricted property must equal.</p>
-     * @param individual An individual to test 
-     * @return True if the given individual is the value of the restricted property in this restriction
+     * @param value An RDF value to test 
+     * @return True if the given value is the value of the restricted property in this restriction
      * @exception OntProfileException If the {@link Profile#HAS_VALUE()} property is not supported in the current language profile.   
      */
-    public boolean hasValue( Resource individual ) {
-        return hasPropertyValue( getProfile().HAS_VALUE(), "HAS_VALUE", individual );
+    public boolean hasValue( RDFNode value ) {
+        return hasPropertyValue( getProfile().HAS_VALUE(), "HAS_VALUE", value );
     }
     
     /**
      * <p>Remove the statement that this restriction requires the restricted property to have
      * the given value.  If this statement
      * is not true of the current model, nothing happens.</p>
-     * @param individual A resource that is to be removed as the required value for the restricted property
+     * @param value An RDF value that is to be removed as the required value for the restricted property
      */
-    public void removeHasValue( Resource individual ) {
-        removePropertyValue( getProfile().HAS_VALUE(), "HAS_VALUE", individual );
+    public void removeHasValue( RDFNode value ) {
+        removePropertyValue( getProfile().HAS_VALUE(), "HAS_VALUE", value );
     }
     
 
