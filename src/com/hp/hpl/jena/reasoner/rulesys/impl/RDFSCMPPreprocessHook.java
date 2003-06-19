ca46b62974837c9983a66c11b9ecceee6d5ef801
Dave Reynolds <der@apache.org>
2003-06-19 12:56:19 +0000

    Support for scanning for container membership properies
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jena/Import/Jena-CVS/jena2/trunk@1110107 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/com/hp/hpl/jena/reasoner/rulesys/impl/RDFSCMPPreprocessHook.java
@@ -0,0 +1,92 @@
+/******************************************************************
+ * File:        RDFSCMPPreprocessHook.java
+ * Created by:  Dave Reynolds
+ * Created on:  19-Jun-2003
+ * 
+ * (c) Copyright 2003, Hewlett-Packard Company, all rights reserved.
+ * [See end of file]
+ * $Id: RDFSCMPPreprocessHook.java,v 1.1 2003-06-19 12:56:19 der Exp $
+ *****************************************************************/
+package com.hp.hpl.jena.reasoner.rulesys.impl;
+
+import com.hp.hpl.jena.reasoner.rulesys.*;
+import com.hp.hpl.jena.reasoner.*;
+import com.hp.hpl.jena.graph.*;
+import com.hp.hpl.jena.vocabulary.RDF;
+import com.hp.hpl.jena.vocabulary.RDFS;
+import com.hp.hpl.jena.util.iterator.ExtendedIterator;
+
+import java.util.*;
+
+/**
+ * A rule preprocessor that scans all supplied data looking for instances
+ * of container membership properties and adds those to the deductions set.  
+ * 
+ * @author <a href="mailto:der@hplb.hpl.hp.com">Dave Reynolds</a>
+ * @version $Revision: 1.1 $ on $Date: 2003-06-19 12:56:19 $
+ */
+public class RDFSCMPPreprocessHook implements RulePreprocessHook {
+
+    /**
+     * Invoke the preprocessing hook. This will be called during the
+     * preparation time of the hybrid reasoner.
+     * @param infGraph the inference graph which is being prepared,
+     * the hook code can use this to addDeductions or add additional
+     * rules (using addRuleDuringPrepare).
+     * @param dataFind the finder which packages up the raw data (both
+     * schema and data bind) and any cached transitive closures.
+     * @param inserts a temporary graph into which the hook should insert
+     * all new deductions that should be seen by the rules.
+     */
+    public void run(FBRuleInfGraph infGraph, Finder dataFind, Graph inserts) {
+        ExtendedIterator it = dataFind.findWithContinuation(new TriplePattern(null, null, null), null);
+        HashSet properties = new HashSet();
+        String memberPrefix = RDF.getURI() + "_";
+        while (it.hasNext()) {
+            Triple triple = (Triple)it.next();
+            Node prop = triple.getPredicate();
+            if (prop.equals(RDF.type.getNode()) && prop.equals(RDF.Property.getNode()) ) {
+                prop = triple.getSubject();
+            }
+            if (properties.add(prop)) {
+                if (prop.getURI().startsWith(memberPrefix)) {
+                    // A container property
+                    inserts.add(new Triple(prop, RDF.type.getNode(), RDFS.ContainerMembershipProperty.getNode()));
+                }
+            }
+        }
+    }
+
+}
+
+
+
+/*
+    (c) Copyright Hewlett-Packard Company 2003
+    All rights reserved.
+
+    Redistribution and use in source and binary forms, with or without
+    modification, are permitted provided that the following conditions
+    are met:
+
+    1. Redistributions of source code must retain the above copyright
+       notice, this list of conditions and the following disclaimer.
+
+    2. Redistributions in binary form must reproduce the above copyright
+       notice, this list of conditions and the following disclaimer in the
+       documentation and/or other materials provided with the distribution.
+
+    3. The name of the author may not be used to endorse or promote products
+       derived from this software without specific prior written permission.
+
+    THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+    IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+    OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+    IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+    INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+    NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+    DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+    THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+    (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+    THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
