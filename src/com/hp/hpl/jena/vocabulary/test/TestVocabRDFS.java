f9d0bd38a0530347c3ec692cf7254d7ffc6c6c6a
Chris Dolan <chrisdolan@apache.org>
2003-06-23 13:05:59 +0000

    test for RDFS vocab
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jena/Import/Jena-CVS/jena2/trunk@1110163 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/com/hp/hpl/jena/vocabulary/test/TestVocabRDFS.java
@@ -0,0 +1,77 @@
+/*
+  (c) Copyright 2003, Hewlett-Packard Company, all rights reserved.
+  [See end of file]
+  $Id: TestVocabRDFS.java,v 1.1 2003-06-23 13:05:59 chris-dollin Exp $
+*/
+
+package com.hp.hpl.jena.vocabulary.test;
+
+import com.hp.hpl.jena.rdf.model.test.ModelTestBase;
+import com.hp.hpl.jena.vocabulary.*;
+import junit.framework.*;
+
+/**
+ 	@author kers
+*/
+public class TestVocabRDFS extends ModelTestBase
+    {
+    public TestVocabRDFS(String name)
+        {  super(name); }
+
+    public static TestSuite suite()
+        { return new TestSuite( TestVocabRDFS.class ); }
+        
+    public void testVocabRDFS()
+        {
+        String ns = "http://www.w3.org/2000/01/rdf-schema#";
+        assertEquals( ns, RDFS.getURI() );
+        assertEquals( ns + "Class", RDFS.Class.getURI() );
+        assertEquals( ns + "Datatype", RDFS.Datatype.getURI() );
+        assertEquals( ns + "ConstraintProperty", RDFS.ConstraintProperty.getURI() );
+        assertEquals( ns + "Container", RDFS.Container.getURI() );
+        assertEquals( ns + "ContainerMembershipProperty", RDFS.ContainerMembershipProperty.getURI() );
+        assertEquals( ns + "ConstraintResource", RDFS.ConstraintResource.getURI() );
+        assertEquals( ns + "Literal", RDFS.Literal.getURI() );
+        assertEquals( ns + "Resource", RDFS.Resource.getURI() );
+        assertEquals( ns + "comment", RDFS.comment.getURI() );
+        assertEquals( ns + "domain", RDFS.domain.getURI() );
+        assertEquals( ns + "label", RDFS.label.getURI() );
+        assertEquals( ns + "isDefinedBy", RDFS.isDefinedBy.getURI() );
+        assertEquals( ns + "range", RDFS.range.getURI() );
+        assertEquals( ns + "seeAlso", RDFS.seeAlso.getURI() );
+        assertEquals( ns + "subClassOf", RDFS.subClassOf.getURI() );
+        assertEquals( ns + "subPropertyOf", RDFS.subPropertyOf.getURI() );
+        assertEquals( ns + "member", RDFS.member.getURI() );
+        }
+    }
+
+
+/*
+    (c) Copyright Hewlett-Packard Company 2003
+    All rights reserved.
+
+    Redistribution and use in source and binary forms, with or without
+    modification, are permitted provided that the following conditions
+    are met:
+
+    1. Redistributions of source code must retain the above copyright
+       notice, this list of conditions and the following disclaimer.
+
+    2. Redistributions in binary form must reproduce the above copyright
+       notice, this list of conditions and the following disclaimer in the
+       documentation and/or other materials provided with the distribution.
+
+    3. The name of the author may not be used to endorse or promote products
+       derived from this software without specific prior written permission.
+
+    THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+    IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+    OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+    IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+    INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+    NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+    DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+    THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+    (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+    THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
