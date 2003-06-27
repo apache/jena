a859505a60083f9c18378e2c01d527fd4e46061d
Wk W <wkw@apache.org>
2003-06-27 15:43:36 +0000

    model/graph names are now case-sensitive.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jena/Import/Jena-CVS/jena2/trunk@1110222 13f79535-47bb-0310-9956-ffa450edef68

--- src/com/hp/hpl/jena/db/DBConnection.java
+++ src/com/hp/hpl/jena/db/DBConnection.java
@@ -23,7 +23,7 @@ import com.hp.hpl.jena.util.iterator.ExtendedIterator;
 * This is mostly used to simplify the calling pattern for ModelRDB factory methods.
 *
 * @author csayers (based in part on the jena 1 implementation by der).
-* @version $Revision: 1.5 $
+* @version $Revision: 1.6 $
 */
 
 public class DBConnection implements IDBConnection { 
@@ -277,7 +277,7 @@ public class DBConnection implements IDBConnection {
 	 public boolean containsModel(String name) throws RDFRDBException {
 		if (m_driver == null)
 			m_driver = getDriver();
-		return (DBPropGraph.findPropGraphByName(m_driver.getSystemSpecializedGraph(), name.toUpperCase() ) != null );		
+		return (DBPropGraph.findPropGraphByName(m_driver.getSystemSpecializedGraph(), name ) != null );		
 	 }
 
 	/**
