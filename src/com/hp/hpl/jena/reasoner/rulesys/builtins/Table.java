966a7b6da1fc5fa534d9befa12a8a1e51c174563
Dave Reynolds <der@apache.org>
2003-08-08 16:11:43 +0000

    Removed exception so that the directive is safe to use in rule sets even when run under the old engine
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jena/Import/Jena-CVS/jena2/trunk@1110525 13f79535-47bb-0310-9956-ffa450edef68

--- src/com/hp/hpl/jena/reasoner/rulesys/builtins/Table.java
+++ src/com/hp/hpl/jena/reasoner/rulesys/builtins/Table.java
@@ -5,7 +5,7 @@
  * 
  * (c) Copyright 2003, Hewlett-Packard Company, all rights reserved.
  * [See end of file]
- * $Id: Table.java,v 1.2 2003-08-08 10:08:55 der Exp $
+ * $Id: Table.java,v 1.3 2003-08-08 16:11:43 der Exp $
  *****************************************************************/
 package com.hp.hpl.jena.reasoner.rulesys.builtins;
 
@@ -18,7 +18,7 @@ import com.hp.hpl.jena.graph.*;
  * Arrange that the given predicate is tabled by the backchaining engine.
  * 
  * @author <a href="mailto:der@hplb.hpl.hp.com">Dave Reynolds</a>
- * @version $Revision: 1.2 $ on $Date: 2003-08-08 10:08:55 $
+ * @version $Revision: 1.3 $ on $Date: 2003-08-08 16:11:43 $
  */
 public class Table extends BaseBuiltin {
 
@@ -47,7 +47,10 @@ public class Table extends BaseBuiltin {
                 ((FBLPRuleInfGraph)infgraph).setTabled(args[i]);
             }
         } else {
-            throw new BuiltinException(this, context, "builtin " + getName() + " only usable in LP hybrid rule sets");
+            // Quietly ignore as an irrelevant directive
+            // Could log or throw exception but currently I want to be able to use
+            // the same rule base from different contexts which do and do not need
+            // to know about this.
         }
     }
  
