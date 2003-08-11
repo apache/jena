7477aea3ce10be938928a4ca6a146755ca659af8
Wk W <wkw@apache.org>
2003-08-11 02:46:33 +0000

    add subsumes and drop dead code.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jena/Import/Jena-CVS/jena2/trunk@1110538 13f79535-47bb-0310-9956-ffa450edef68

--- src/com/hp/hpl/jena/db/impl/SpecializedGraph_TripleStore_RDB.java
+++ src/com/hp/hpl/jena/db/impl/SpecializedGraph_TripleStore_RDB.java
@@ -7,8 +7,9 @@
 
 package com.hp.hpl.jena.db.impl;
 
+import com.hp.hpl.jena.graph.Node;
 import com.hp.hpl.jena.graph.Triple;
-import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.shared.JenaException;
 
 /**
  *
@@ -17,44 +18,9 @@ import com.hp.hpl.jena.rdf.model.Model;
  *
  * TripleStoreGraph_RDB is a table-specific, database-independent 
  * implementation of TripleStoreGraph.
- *
- * The underlying database will be formatted with the following 
- * tables:
- *     RDF_Literals
- *     RDF_StmtAsserted
- *     RDF_Property
- *
- * structure as specified in the Jena2 Persistence Architecture document.
- *
- * TripleStoreGraph_RDB relies upon the underlying database-specific driver
- * (IPSet m_pset) to implement database-dependent code.
- * TripleStoreGraph_RDB interfaces with the driver by passing it operation
- * names for SQL operations (along with arguments).
- * Although the driver is layout-independent, it can use these operations
- * to extract com.hp.hpl.jena.graph.* structures (e.g., Node_Literal, 
- * Node_URI, Triple, etc.) because the operations return results in a known
- * format, regardless of the underlying table layouts.
- * (I.e., the database performs any required joins.)
  */
 
 public class SpecializedGraph_TripleStore_RDB extends SpecializedGraph_TripleStore {
-	
-	// cache property model
-	// hardcode it for now.  
-	private Model propertyModel;
-	
-	// cache maximum literal
-	// hardcode it to 1000 for now.  Fix when propertyModel works.
-	private int MAX_LITERAL = 1000;
-	
-	// cache SKIP_DUPLICATE_CHECK
-	// hardcode it to "false" for now.  Fix when propertyModel works.
-	private boolean SKIP_DUPLICATE_CHECK = false;
-	
-	// cache SQLCache instance
-	private SQLCache sCache;
-	
-	   
 	
 	/**
 	 * Constructor.
@@ -66,29 +32,19 @@ public class SpecializedGraph_TripleStore_RDB extends SpecializedGraph_TripleSto
 	public SpecializedGraph_TripleStore_RDB(IPSet pSet, Integer dbGraphId) {
 		super(pSet, dbGraphId);
 	}
-
-    /** 
-     * Compute the number of unique triples added to the Specialized Graph.
-     * 
-     * @return int count.
-     */
-    public int tripleCount() {
-    	return(m_pset.tripleCount());
-    }
-    
-    /**
-     * Tests if a triple is contained in the specialized graph.
-     * @param t is the triple to be tested
-     * @param complete is true if the graph can guarantee that 
-     *  no other specialized graph 
-     * could hold any matching triples.
-     * @return boolean result to indicte if the tripple was contained
-     */
-	public boolean contains(Triple t, CompletionFlag complete) {
-		complete.setDone(); 
-		return (m_pset.statementTableContains(my_GID, t));
-	}
-       
+	
+	public char subsumes ( Triple pattern ) {
+			Node pred = pattern.getPredicate();
+			char res = noTriplesForPattern;
+			if ( pred.isConcrete() ) {
+				// assumes that other sg's have been called first
+				res = allTriplesForPattern;
+			} else if ( (pred.isVariable()) || pred.equals(Node.ANY) ) {
+				res = someTriplesForPattern;
+			} else
+				throw new JenaException("Unexpected predicate: " + pred.toString());
+			return res;
+		}
 }
 
 /*
