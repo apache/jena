f409ad67c6342c9ecc77ac30fdf2fbe08234e4cb
Dave Reynolds <der@apache.org>
2003-09-25 15:18:12 +0000

    Added LP profiling calls
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jena/Import/Jena-CVS/jena2/trunk@1110992 13f79535-47bb-0310-9956-ffa450edef68

--- src/com/hp/hpl/jena/reasoner/rulesys/test/WebOntTestHarness.java
+++ src/com/hp/hpl/jena/reasoner/rulesys/test/WebOntTestHarness.java
@@ -5,7 +5,7 @@
  * 
  * (c) Copyright 2003, Hewlett-Packard Company, all rights reserved.
  * [See end of file]
- * $Id: WebOntTestHarness.java,v 1.18 2003-09-24 21:11:28 der Exp $
+ * $Id: WebOntTestHarness.java,v 1.19 2003-09-25 15:18:12 der Exp $
  *****************************************************************/
 package com.hp.hpl.jena.reasoner.rulesys.test;
 
@@ -14,6 +14,7 @@ import com.hp.hpl.jena.graph.*;
 import com.hp.hpl.jena.ontology.OntModelSpec;
 import com.hp.hpl.jena.rdf.model.*;
 import com.hp.hpl.jena.reasoner.*;
+import com.hp.hpl.jena.reasoner.rulesys.FBRuleInfGraph;
 import com.hp.hpl.jena.reasoner.test.WGReasonerTester;
 import com.hp.hpl.jena.vocabulary.*;
 
@@ -26,7 +27,7 @@ import java.util.*;
  * core WG tests as part of the routine unit tests.
  * 
  * @author <a href="mailto:der@hplb.hpl.hp.com">Dave Reynolds</a>
- * @version $Revision: 1.18 $ on $Date: 2003-09-24 21:11:28 $
+ * @version $Revision: 1.19 $ on $Date: 2003-09-25 15:18:12 $
  */
 public class WebOntTestHarness {
 
@@ -38,6 +39,9 @@ public class WebOntTestHarness {
     
     /** Set to true to use approved tests only */
     public static boolean approvedOnly = true;
+    
+    /** Set to true to print LP engine profile information */
+    public static boolean printProfile = false;
     
 //  =======================================================================
 //  Internal state
@@ -235,8 +239,8 @@ public class WebOntTestHarness {
             resultFile = args[0];
         }
         WebOntTestHarness harness = new WebOntTestHarness();
-        harness.runTests();
-//        harness.runTest("http://www.w3.org/2002/03owlt/description-logic/Manifest664#test");
+//        harness.runTests();
+        harness.runTest("http://www.w3.org/2002/03owlt/description-logic/Manifest663#test");
         RDFWriter writer = harness.testResults.getWriter("RDF/XML-ABBREV");
         OutputStream stream = new FileOutputStream(resultFile);
         writer.setProperty("showXmlDeclaration", "true");
@@ -339,10 +343,16 @@ public class WebOntTestHarness {
             comprehensionAxioms(premises, conclusions);
             long t1 = System.currentTimeMillis();
             InfGraph graph = reasoner.bind(premises.getGraph());
+            if (printProfile) {
+                ((FBRuleInfGraph)graph).resetLPProfile(true);
+            }
             Model result = ModelFactory.createModelForGraph(graph);
             boolean correct = testEntailment(conclusions.getGraph(), result.getGraph());
             long t2 = System.currentTimeMillis();
             lastTestDuration = t2 - t1; 
+            if (printProfile) {
+                ((FBRuleInfGraph)graph).printLPProfile();
+            }
             if (test.hasProperty(RDF.type, OWLTest.NegativeEntailmentTest)) {
                 correct = !correct;
             }
