f5d7cf5100e19355d9a2acb5fabcd6e7c78ae4b2
Ian Dickinsone <ian_dickinson@apache.org>
2003-12-02 10:04:58 +0000

    Updated copyright notices
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jena/Import/Jena-CVS/jena2/trunk@1111190 13f79535-47bb-0310-9956-ffa450edef68

--- src/com/hp/hpl/jena/reasoner/dig/DIGIdentifier.java
+++ src/com/hp/hpl/jena/reasoner/dig/DIGIdentifier.java
@@ -3,17 +3,18 @@
  * -----------------------
  * Original author    Ian Dickinson, HP Labs Bristol
  * Author email       ian.dickinson@hp.com
- * Package            @package@
- * Web site           @website@
- * Created            17-Nov-2003
+ * Package            Jena 2
+ * Web                http://sourceforge.net/projects/jena/
+ * Created            11-Sep-2003
  * Filename           $RCSfile: DIGIdentifier.java,v $
- * Revision           $Revision: 1.1 $
+ * Revision           $Revision: 1.2 $
  * Release status     $State: Exp $
  *
- * Last modified on   $Date: 2003-12-01 22:40:07 $
+ * Last modified on   $Date: 2003-12-02 10:02:56 $
  *               by   $Author: ian_dickinson $
  *
- * @copyright@
+ * (c) Copyright 2002-2003, Hewlett-Packard Company, all rights reserved.
+ * (see footer for full conditions)
  *****************************************************************************/
 
 // Package
@@ -33,7 +34,7 @@ import java.util.Iterator;
  * </p>
  *
  * @author Ian Dickinson, HP Labs (<a href="mailto:Ian.Dickinson@hp.com">email</a>)
- * @version Release @release@ ($Id: DIGIdentifier.java,v 1.1 2003-12-01 22:40:07 ian_dickinson Exp $)
+ * @version Release @release@ ($Id: DIGIdentifier.java,v 1.2 2003-12-02 10:02:56 ian_dickinson Exp $)
  */
 public interface DIGIdentifier 
 {
@@ -85,5 +86,31 @@ public interface DIGIdentifier
 
 
 /*
-@footer@
+    (c) Copyright Hewlett-Packard Company 2002-2003
+    All rights reserved.
+
+    Redistribution and use in source and binary forms, with or without
+    modification, are permitted provided that the following conditions
+    are met:
+
+    1. Redistributions of source code must retain the above copyright
+       notice, this list of conditions and the following disclaimer.
+
+    2. Redistributions in binary form must reproduce the above copyright
+       notice, this list of conditions and the following disclaimer in the
+       documentation and/or other materials provided with the distribution.
+
+    3. The name of the author may not be used to endorse or promote products
+       derived from this software without specific prior written permission.
+
+    THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+    IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+    OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+    IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+    INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+    NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+    DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+    THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+    (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+    THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
