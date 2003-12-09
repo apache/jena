2a70531b73afc5c4420b1f977864d6bdf854ffe1
Ian Dickinsone <ian_dickinson@apache.org>
2003-12-09 13:01:08 +0000

    Updated NullIterator to be an extended iterator, so can use with map, compose, etc
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jena/Import/Jena-CVS/jena2/trunk@1111259 13f79535-47bb-0310-9956-ffa450edef68

--- src/com/hp/hpl/jena/util/iterator/NullIterator.java
+++ src/com/hp/hpl/jena/util/iterator/NullIterator.java
@@ -1,5 +1,27 @@
+/* (c) Copyright 2001, 2002, 2003, Hewlett-Packard Development Company, LP
+ * [See end of file]
+ *
+ * $Id: NullIterator.java,v 1.5 2003-12-09 13:01:08 ian_dickinson Exp $
+ */
+
+package com.hp.hpl.jena.util.iterator;
+
+/**
+ * An Iteration of nothing.
+ * 
+ * @author jjc
+ * @author ijd: updated to be an ExtendedIterator
+ * @version  Release='$Name: not supported by cvs2svn $' Revision='$Revision: 1.5 $' Date='$Date: 2003-12-09 13:01:08 $'
+ */
+public class NullIterator
+    extends NiceIterator 
+{
+    public static NullIterator instance = new NullIterator();
+}
+
+
 /*
- *  (c) Copyright 2000-2002  Hewlett-Packard Development Company, LP
+ *  (c) Copyright 2001, 2002, 2003 Hewlett-Packard Development Company, LP
  *  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -12,7 +34,7 @@
  *    documentation and/or other materials provided with the distribution.
  * 3. The name of the author may not be used to endorse or promote products
  *    derived from this software without specific prior written permission.
-
+ *
  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
  * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
@@ -23,44 +45,4 @@
  * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
- * $Id: NullIterator.java,v 1.4 2003-11-17 07:24:58 chris-dollin Exp $
- *
  */
-
-package com.hp.hpl.jena.util.iterator;
-
-import java.util.Iterator ;
-import java.util.NoSuchElementException ;
-
-/**
- * A useful class: an Iteration of nothing.
- * @author jjc
- * @version  Release='$Name: not supported by cvs2svn $' Revision='$Revision: 1.4 $' Date='$Date: 2003-11-17 07:24:58 $'
- */
-public class NullIterator implements Iterator
-{
-    public static NullIterator instance = new NullIterator();
-    /** No.
-     *
-     */
-	public boolean hasNext()
-	{
-		return false ;
-	}
-
-        /** NoSuchElementException.
-         *@exception java.lang.NoSuchElementException Always.
-         */
-    public Object next()
-	{
-		throw new NoSuchElementException("NullEnumerator") ;
-		//return null ;
-	}
-        /** IllegalStateException.
-         *@exception java.lang.IllegalStateException Always.
-         */
-        public void remove() {
-          throw new IllegalStateException();
-        }
-}
