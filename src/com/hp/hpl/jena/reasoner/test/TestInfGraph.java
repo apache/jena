178e691f91c8608a2236c327b5a0fd81f4e1f495
Chris Dolan <chrisdolan@apache.org>
2004-01-29 15:40:07 +0000

    new test for InfGraph base
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jena/Import/Jena-CVS/jena2/trunk@1111354 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/com/hp/hpl/jena/reasoner/test/TestInfGraph.java
@@ -0,0 +1,80 @@
+/*
+  (c) Copyright 2004, Hewlett-Packard Development Company, LP, all rights reserved.
+  [See end of file]
+  $Id: TestInfGraph.java,v 1.1 2004-01-29 15:40:07 chris-dollin Exp $
+*/
+
+package com.hp.hpl.jena.reasoner.test;
+
+import junit.framework.*;
+
+import com.hp.hpl.jena.graph.Graph;
+import com.hp.hpl.jena.graph.test.AbstractTestGraph;
+import com.hp.hpl.jena.rdf.model.ModelFactory;
+import com.hp.hpl.jena.reasoner.InfGraph;
+
+/**
+     Needs extending; relys on knowing that the only InfGraph currently used is
+     the Jena-provided base. Needs to be made into an abstract test and
+     parametrised with the InfGraph being tested (hence getInfGraph).
+    @author hedgehog
+*/
+
+public class TestInfGraph extends AbstractTestGraph
+    {
+    public TestInfGraph( String name )
+        { super( name ); }
+    
+    public static TestSuite suite()
+        { return new TestSuite( TestInfGraph.class ); }
+    
+    private InfGraph getInfGraph()
+        {
+        return (InfGraph) ModelFactory.createOntologyModel().getGraph();
+        }
+    
+    public Graph getGraph()
+        { return getInfGraph(); }
+    
+    public void testInfGraph()
+        {
+        InfGraph ig = getInfGraph();
+        assertSame( ig.getPrefixMapping(), ig.getRawGraph().getPrefixMapping() );
+        }
+    
+    public void testInfReification()
+        {
+        InfGraph ig = getInfGraph();
+        assertSame( ig.getReifier(), ig.getRawGraph().getReifier() );
+        }
+    }
+
+/*
+    (c) Copyright 2004, Hewlett-Packard Development Company, LP
+    All rights reserved.
+
+    Redistribution and use in source and binary forms, with or without
+    modification, are permitted provided that the following conditions
+    are met:
+
+    1. Redistributions of source code must retain the above copyright
+       notice, this list of conditions and the following disclaimer.
+
+    2. Redistributions in binary form must reproduce the above copyright
+       notice, this list of conditions and the following disclaimer in the
+       documentation and/or other materials provided with the distribution.
+
+    3. The name of the author may not be used to endorse or promote products
+       derived from this software without specific prior written permission.
+
+    THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+    IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+    OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+    IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+    INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+    NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+    DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+    THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+    (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+    THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
