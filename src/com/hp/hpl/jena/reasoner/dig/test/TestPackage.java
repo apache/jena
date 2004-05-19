fc549817a081f0ef918e64ed3bdf7aae0de2665a
Ian Dickinsone <ian_dickinson@apache.org>
2004-05-19 09:40:17 +0000

    Added collective test suite for DIG reasoner
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jena/Import/Jena-CVS/jena2/trunk@1111549 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/com/hp/hpl/jena/reasoner/dig/test/TestPackage.java
@@ -0,0 +1,92 @@
+/*****************************************************************************
+ * Source code information
+ * -----------------------
+ * Original author    Ian Dickinson, HP Labs Bristol
+ * Author email       Ian.Dickinson@hp.com
+ * Package            Jena 2
+ * Web                http://sourceforge.net/projects/jena/
+ * Created            17-Jun-2003
+ * Filename           $RCSfile: TestPackage.java,v $
+ * Revision           $Revision: 1.1 $
+ * Release status     $State: Exp $
+ *
+ * Last modified on   $Date: 2004-05-19 09:40:17 $
+ *               by   $Author: ian_dickinson $
+ *
+ * (c) Copyright 2002, 2003, Hewlett-Packard Development Company, LP
+ * (see footer for full conditions)
+ *****************************************************************************/
+
+// Package
+///////////////
+package com.hp.hpl.jena.reasoner.dig.test;
+
+
+// Imports
+///////////////
+import junit.framework.*;
+
+
+/**
+ * <p>
+ * Collected unit tests for DIG adapter
+ * </p>
+ *
+ * @author Ian Dickinson, HP Labs
+ *         (<a  href="mailto:Ian.Dickinson@hp.com" >email</a>)
+ * @version CVS $Id: TestPackage.java,v 1.1 2004-05-19 09:40:17 ian_dickinson Exp $
+ */
+public class TestPackage 
+    extends TestSuite 
+{
+    static public TestSuite suite() {
+        return new TestPackage();
+    }
+    
+    /** Creates new TestPackage */
+    private TestPackage() {
+        super("reasoner.dig");
+        addTestSuite( TestConsistency.class );
+        addTestSuite( TestRacer.class );
+        addTestSuite( TestDigReasoner.class );
+    }
+
+    private void addTest(String name, TestSuite tc) {
+        tc.setName(name);
+        addTest(tc);
+    }
+
+
+}
+
+
+/*
+    (c) Copyright 2002, 2003 Hewlett-Packard Development Company, LP
+    All rights reserved.
+
+    Redistribution and use in source and binary forms, with or without
+    modification, are permitted provided that the following conditions
+    are met:
+
+    1. Redistributions of source code must retain the above copyright
+       notice, this list of conditions and the following disclaimer.
+
+    2. Redistributions in binary form must reproduce the above copyright
+       notice, this list of conditions and the following disclaimer in the
+       documentation and/or other materials provided with the distribution.
+
+    3. The name of the author may not be used to endorse or promote products
+       derived from this software without specific prior written permission.
+
+    THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+    IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+    OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+    IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+    INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+    NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+    DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+    THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+    (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+    THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
