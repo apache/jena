ee2da58b1a87230122d378116b17f894cc045c86
Jeremy Carroll <jeremy_carroll@apache.org>
2004-07-05 13:59:54 +0000

    Added test for http://sourceforge.net/tracker/index.php?func=detail&aid=983999&group_id=40417&atid=430288
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jena/Import/Jena-CVS/jena2/trunk@1111609 13f79535-47bb-0310-9956-ffa450edef68

--- src/com/hp/hpl/jena/xmloutput/test/testWriterAndReader.java
+++ src/com/hp/hpl/jena/xmloutput/test/testWriterAndReader.java
@@ -2,7 +2,7 @@
     (c) Copyright 2001, 2002, 2003 Hewlett-Packard Development Company, LP
     All rights reserved.
     [See end of file]
-    $Id: testWriterAndReader.java,v 1.28 2004-03-05 15:52:36 chris-dollin Exp $
+    $Id: testWriterAndReader.java,v 1.29 2004-07-05 13:59:54 jeremy_carroll Exp $
 */
 
 package com.hp.hpl.jena.xmloutput.test;
@@ -27,13 +27,13 @@ import org.apache.commons.logging.LogFactory;
  * Quite what 'the same' means is debatable.
  * @author  jjc
  
- * @version  Release='$Name: not supported by cvs2svn $' Revision='$Revision: 1.28 $' Date='$Date: 2004-03-05 15:52:36 $'
+ * @version  Release='$Name: not supported by cvs2svn $' Revision='$Revision: 1.29 $' Date='$Date: 2004-07-05 13:59:54 $'
  */
 public class testWriterAndReader 
     extends ModelTestBase implements RDFErrorHandler {
 	static private boolean showProgress = false;
 	static private boolean errorDetail = false;
-	static private int firstTest = 5;
+	static private int firstTest = 4;
 	static private int lastTest = 9;
 	static private int repetitionsJ = 6;
     
@@ -537,5 +537,5 @@ public class testWriterAndReader
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *
- * $Id: testWriterAndReader.java,v 1.28 2004-03-05 15:52:36 chris-dollin Exp $
+ * $Id: testWriterAndReader.java,v 1.29 2004-07-05 13:59:54 jeremy_carroll Exp $
  */
