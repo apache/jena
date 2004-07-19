b4a3341edc0f48920d4d57bbc442287285f0b1f3
Chris Dolan <chrisdolan@apache.org>
2004-07-19 08:31:58 +0000

    mixed indexing for graphs
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jena/Import/Jena-CVS/jena2/trunk@1111642 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/com/hp/hpl/jena/mem/test/TestMixedGraphMem.java
@@ -0,0 +1,54 @@
+/*
+  (c) (c) Copyright 2004 Hewlett-Packard Development Company, LP
+  [See end of file]
+  $Id: TestMixedGraphMem.java,v 1.1 2004-07-19 08:31:58 chris-dollin Exp $
+*/
+
+package com.hp.hpl.jena.mem.test;
+
+import junit.framework.TestSuite;
+
+import com.hp.hpl.jena.graph.Graph;
+
+
+/**
+ @author hedgehog
+ */
+public class TestMixedGraphMem extends TestGraphMem
+    {
+    public TestMixedGraphMem( String name ) 
+        { super( name );}
+    
+    public static TestSuite suite()
+        { return new TestSuite( TestMixedGraphMem.class ); }
+        
+    public Graph getGraph()
+        { return new MixedGraphMem(); }
+    }
+
+/*
+	 *  (c) Copyright 2004 Hewlett-Packard Development Company, LP
+	 *  All rights reserved.
+	 *
+	 * Redistribution and use in source and binary forms, with or without
+	 * modification, are permitted provided that the following conditions
+	 * are met:
+	 * 1. Redistributions of source code must retain the above copyright
+	 *    notice, this list of conditions and the following disclaimer.
+	 * 2. Redistributions in binary form must reproduce the above copyright
+	 *    notice, this list of conditions and the following disclaimer in the
+	 *    documentation and/or other materials provided with the distribution.
+	 * 3. The name of the author may not be used to endorse or promote products
+	 *    derived from this software without specific prior written permission.
+	
+	 * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+	 * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+	 * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+	 * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+	 * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+	 * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+	 * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+	 * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+	 * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+	 * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
