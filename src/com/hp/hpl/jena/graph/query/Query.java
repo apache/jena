51c02dd39e70ec4ca08d6de913eff07cd3f7e464
Chris Dolan <chrisdolan@apache.org>
2004-07-21 14:15:15 +0000

    Added test for exactly 2 args for Q_StringMatch.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jena/Import/Jena-CVS/jena2/trunk@1111655 13f79535-47bb-0310-9956-ffa450edef68

--- src/com/hp/hpl/jena/graph/query/Query.java
+++ src/com/hp/hpl/jena/graph/query/Query.java
@@ -1,7 +1,7 @@
 /*
   (c) Copyright 2002, 2003, 2004 Hewlett-Packard Development Company, LP
   [See end of file]
-  $Id: Query.java,v 1.33 2004-07-21 13:12:06 chris-dollin Exp $
+  $Id: Query.java,v 1.34 2004-07-21 14:15:15 chris-dollin Exp $
 */
 
 package com.hp.hpl.jena.graph.query;
@@ -113,7 +113,7 @@ public class Query
         { 
         if (e.isApply() && e.getFun().equals( ExpressionFunctionURIs.AND ))
            for (int i = 0; i < e.argCount(); i += 1) addConstraint( e.getArg( i ) ); 
-        else if (e.isApply() && e.getFun().equals( ExpressionFunctionURIs.prefix + "Q_StringMatch"))
+        else if (e.isApply() && e.argCount() == 2 && e.getFun().equals( ExpressionFunctionURIs.prefix + "Q_StringMatch"))
             constraint.add( Rewrite.rewriteStringMatch( e ) );
         else
             constraint.add( e );
