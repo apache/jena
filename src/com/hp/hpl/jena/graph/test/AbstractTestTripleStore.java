6e4ea272a8cbbda44c4531adde9714f9c12246a7
Chris Dolan <chrisdolan@apache.org>
2004-09-10 12:32:19 +0000

    Added some actual tests.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jena/Import/Jena-CVS/jena2/trunk@1111789 13f79535-47bb-0310-9956-ffa450edef68

--- src/com/hp/hpl/jena/graph/test/AbstractTestTripleStore.java
+++ src/com/hp/hpl/jena/graph/test/AbstractTestTripleStore.java
@@ -1,7 +1,7 @@
 /*
   (c) Copyright 2004, Hewlett-Packard Development Company, LP, all rights reserved.
   [See end of file]
-  $Id: AbstractTestTripleStore.java,v 1.1 2004-09-10 11:09:41 chris-dollin Exp $
+  $Id: AbstractTestTripleStore.java,v 1.2 2004-09-10 12:32:19 chris-dollin Exp $
 */
 package com.hp.hpl.jena.graph.test;
 
@@ -10,7 +10,7 @@ import com.hp.hpl.jena.graph.impl.TripleStore;
 import junit.framework.TestSuite;
 
 /**
-     AbstractTestTripleStore
+     AbstractTestTripleStore - post-hoc tests for TripleStores.
      @author kers
 */
 public abstract class AbstractTestTripleStore extends GraphTestBase
@@ -34,8 +34,78 @@ public abstract class AbstractTestTripleStore extends GraphTestBase
         }
     
     public void testEmpty()
-        { 
-        assertEquals( true, store.isEmpty() );
+        { testEmpty( store ); }
+    
+    public void testAddOne()
+        {
+        store.add( triple( "x P y" ) );
+        assertEquals( false, store.isEmpty() );
+        assertEquals( 1, store.size() );
+        assertEquals( true, store.contains( triple( "x P y" ) ) );
+        assertEquals( nodeSet( "x" ), iteratorToSet( store.listSubjects() ) );
+        assertEquals( nodeSet( "y" ), iteratorToSet( store.listObjects() ) );
+        assertEquals( tripleSet( "x P y" ), iteratorToSet( store.find( triple( "?? ?? ??" ) ) ) );
+        }
+
+    public void testListSubjects()
+        {
+        someStatements( store );
+        assertEquals( nodeSet( "a x _z r q" ), iteratorToSet( store.listSubjects() ) );
+        }
+    
+    public void testListObjects()
+        {
+        someStatements( store );
+        assertEquals( nodeSet( "b y i _j _t 17" ), iteratorToSet( store.listObjects() ) );
+        }
+    
+    public void testContains()
+        {
+        someStatements( store );
+        assertEquals( true, store.contains( triple( "a P b" ) ) );
+        assertEquals( true, store.contains( triple( "x P y" ) ) );
+        assertEquals( true, store.contains( triple( "a P i" ) ) );
+        assertEquals( true, store.contains( triple( "_z Q _j" ) ) );
+        assertEquals( true, store.contains( triple( "x R y" ) ) );
+        assertEquals( true, store.contains( triple( "r S _t" ) ) );
+        assertEquals( true, store.contains( triple( "q R 17" ) ) );
+    /* */
+        assertEquals( false, store.contains( triple( "a P x" ) ) );
+        assertEquals( false, store.contains( triple( "a P _j" ) ) );
+        assertEquals( false, store.contains( triple( "b Z r" ) ) );
+        assertEquals( false, store.contains( triple( "_a P x" ) ) );
+        }
+    
+    public void testFind()
+        {
+        someStatements( store );
+        assertEquals( tripleSet( "" ), iteratorToSet( store.find( triple( "no such thing" ) ) ) );
+        assertEquals( tripleSet( "a P b; a P i" ), iteratorToSet( store.find( triple( "a P ??" ) ) ) );
+        assertEquals( tripleSet( "a P b; x P y; a P i" ), iteratorToSet( store.find( triple( "?? P ??" ) ) ) );
+        assertEquals( tripleSet( "x P y; x R y" ), iteratorToSet( store.find( triple( "x ?? y" ) ) ) );
+        assertEquals( tripleSet( "_z Q _j" ), iteratorToSet( store.find( triple( "?? ?? _j" ) ) ) );
+        assertEquals( tripleSet( "q R 17" ), iteratorToSet( store.find( triple( "?? ?? 17" ) ) ) );
+        }
+    
+    public void someStatements( TripleStore ts )
+        {
+        ts.add( triple( "a P b" ) );
+        ts.add( triple( "x P y" ) );
+        ts.add( triple( "a P i" ) );
+        ts.add( triple( "_z Q _j" ) );
+        ts.add( triple( "x R y" ) );
+        ts.add( triple( "r S _t" ) );
+        ts.add( triple( "q R 17" ) );
+        }
+    
+    public void testEmpty( TripleStore ts )
+        {
+        assertEquals( true, ts.isEmpty() );
+        assertEquals( 0, ts.size() );
+        assertEquals( false, ts.find( triple( "?? ?? ??" ) ).hasNext() );
+        assertEquals( false, ts.listObjects().hasNext() );
+        assertEquals( false, ts.listSubjects().hasNext() );
+        assertFalse( ts.contains( triple( "x P y" ) ) );
         }
     }
 
