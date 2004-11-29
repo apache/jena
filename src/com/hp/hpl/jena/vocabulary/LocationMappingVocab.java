b009f5fe284462e4f9f49609b9ef77c96a4d3e48
Andy Seaborne <andy@apache.org>
2004-11-29 18:44:47 +0000

    no message
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jena/Import/Jena-CVS/jena2/trunk@1111924 13f79535-47bb-0310-9956-ffa450edef68

--- src/com/hp/hpl/jena/vocabulary/LocationMappingVocab.java
+++ src/com/hp/hpl/jena/vocabulary/LocationMappingVocab.java
@@ -1,4 +1,9 @@
-/* CVS $Id: LocationMappingVocab.java,v 1.1 2004-08-31 08:24:40 andy_seaborne Exp $ */
+/*
+ * (c) Copyright 2004 Hewlett-Packard Development Company, LP
+ * [See end of file]
+ */
+
+/* CVS $Id: LocationMappingVocab.java,v 1.2 2004-11-29 18:44:47 andy_seaborne Exp $ */
 
 package com.hp.hpl.jena.vocabulary;
 import com.hp.hpl.jena.rdf.model.*;
@@ -33,3 +38,31 @@ public class LocationMappingVocab {
     public static final Resource LocationMapping = ResourceFactory.createResource( "http://jena.hpl.hp.com/2004/08/location-mapping#LocationMapping" );
     
 }
+
+/*
+ *  (c) Copyright 2004 Hewlett-Packard Development Company, LP
+ *  All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. The name of the author may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
