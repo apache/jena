c4b6487fa21188feae52aa45de72802a28dec5b2
Wk W <wkw@apache.org>
2005-04-18 22:33:26 +0000

    adjusted comment-styles.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jena/Import/Jena-CVS/jena2/trunk@1112256 13f79535-47bb-0310-9956-ffa450edef68

--- src/com/hp/hpl/jena/db/test/TestPackage.java
+++ src/com/hp/hpl/jena/db/test/TestPackage.java
@@ -15,7 +15,7 @@ import com.hp.hpl.jena.rdf.model.*;
  * Based on earlier Jena tests by members of the Jena team.
  * 
  * @author		csayers
- * @version 	$Revision: 1.30 $
+ * @version 	$Revision: 1.31 $
  */
 public class TestPackage extends TestSuite
 {
@@ -42,12 +42,12 @@ public class TestPackage extends TestSuite
 	static String M_DBDRIVER_CLASS = "oracle.jdbc.OracleDriver";
 	// */
 	
-	// */ mysql settings
-//	static String M_DB_URL = "jdbc:mysql://localhost/test";
-//	static String M_DB_USER = "test";
-//	static String M_DB_PASSWD = "";
-//	static String M_DB = "MySQL";
-//	static String M_DBDRIVER_CLASS = "com.mysql.jdbc.Driver";
+	/*/ mysql settings
+	static String M_DB_URL = "jdbc:mysql://localhost/test";
+	static String M_DB_USER = "test";
+	static String M_DB_PASSWD = "";
+	static String M_DB = "MySQL";
+	static String M_DBDRIVER_CLASS = "com.mysql.jdbc.Driver";
 	// */
 		
 	/*/ postgresql settings
@@ -58,13 +58,13 @@ public class TestPackage extends TestSuite
 	static String M_DBDRIVER_CLASS = "org.postgresql.Driver";
 	// */
         
-	
+	// */
 	static String M_DB_URL = ModelFactoryBase.guessDBURL();
     static String M_DB_USER = ModelFactoryBase.guessDBUser();
     static String M_DB_PASSWD = ModelFactoryBase.guessDBPassword();
     static String M_DB = ModelFactoryBase.guessDBType();
     static String M_DBDRIVER_CLASS = ModelFactoryBase.guessDBDriver();  
-          
+    // */      
 
     static public TestSuite suite() {
         return new TestPackage();
