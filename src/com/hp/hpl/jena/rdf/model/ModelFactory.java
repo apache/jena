c0af9b4263a3da82c1af187cbbc750dca73ef9ea
Ian Dickinsone <ian_dickinson@apache.org>
2005-06-22 09:42:49 +0000

    Updated Javadoc for creating the defaul t ontology model
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jena/Import/Jena-CVS/jena2/trunk@1112316 13f79535-47bb-0310-9956-ffa450edef68

--- src/com/hp/hpl/jena/rdf/model/ModelFactory.java
+++ src/com/hp/hpl/jena/rdf/model/ModelFactory.java
@@ -1,7 +1,7 @@
 /*
   (c) Copyright 2002, 2003, 2004, 2005 Hewlett-Packard Development Company, LP
   [See end of file]
-  $Id: ModelFactory.java,v 1.42 2005-04-08 10:02:25 chris-dollin Exp $
+  $Id: ModelFactory.java,v 1.43 2005-06-22 09:42:49 ian_dickinson Exp $
 */
 
 package com.hp.hpl.jena.rdf.model;
@@ -21,7 +21,7 @@ import com.hp.hpl.jena.ontology.daml.impl.DAMLModelImpl;
 import com.hp.hpl.jena.ontology.impl.OntModelImpl;
 
 /**
-    ModelFactory provides methods for creating standard kinds of Model. 
+    ModelFactory provides methods for creating standard kinds of Model.
     (ModelFactoryBase is helper functions for it).
 */
 
@@ -32,25 +32,25 @@ public class ModelFactory extends ModelFactoryBase
     */
     private ModelFactory()
         {}
-        
+
     /**
         The standard reification style; quadlets contribute to reified statements,
         and are visible to listStatements().
     */
     public static final ReificationStyle Standard = ReificationStyle.Standard;
-    
+
     /**
         The convenient reification style; quadlets contribute to reified statements,
         but are invisible to listStatements().
     */
     public static final ReificationStyle Convenient = ReificationStyle.Convenient;
-    
+
     /**
         The minimal reification style; quadlets do not contribute to reified statements,
-        and are visible to listStatements(). 
+        and are visible to listStatements().
     */
     public static final ReificationStyle Minimal = ReificationStyle.Minimal;
-    
+
     /**
         Each Model created by ModelFactory has a default set of prefix mappings.
         These mappings are copied from a (static) default PrefixMapping which is
@@ -59,65 +59,65 @@ public class ModelFactory extends ModelFactoryBase
         and continue to modify it; the modifications will appear in the next model to
         be created.
     <p>
-        When a Model is created from an existing Graph, the prefixes of that Graph 
+        When a Model is created from an existing Graph, the prefixes of that Graph
         are not disturbed; only ones not present in the Graph are added.
-        
+
      	@param pm the default prefixes to use
      	@return the previous default prefix mapping
     */
     public static PrefixMapping setDefaultModelPrefixes( PrefixMapping pm )
         { return ModelCom.setDefaultModelPrefixes( pm ); }
-    
+
     /**
         Answer the current default model prefixes PrefixMapping object.
     */
     public static PrefixMapping getDefaultModelPrefixes()
         { return ModelCom.getDefaultModelPrefixes(); }
-    
+
     /**
         Answer a ModelSpec which can create models to the specifications in the RDF
         description. The root of the description is the unique resource of type ModelSpec.
     */
     public static ModelSpec createSpec( Model desc )
         { return ModelSpecFactory.createSpec( desc ); }
-        
+
     /**
         Answer a ModelSpec which can create models to the specifcations in the RDF
         description rooted at the given root.
     */
     public static ModelSpec createSpec( Resource root, Model desc )
         { return ModelSpecFactory.createSpec( ModelSpecFactory.withSchema( desc ), root ); }
-        
+
     /**
         Answer a fresh Model created according to the ModelSpec argument.
     */
     public static Model createModel( ModelSpec desc )
         { return desc.createModel(); }
-        
+
     /**
         Answer a fresh Model created according to the given specification and based on
         any underlying model with the given name.
-        
+
      	@param desc the ModelSpec which describes the kind of model to create
      	@param name the name of the base model in the underlying ModelMaker
      	@return a fresh model based over the named model
      */
     public static Model createModelOver( ModelSpec desc, String name )
         { return desc.createModelOver( name ); }
-    
-    /** 
+
+    /**
         Answer a fresh Model with the default specification and Standard reification style
         [reification triples contribute to ReifiedStatements, and are visible to listStatements,
-        etc]. 
+        etc].
     */
     public static Model createDefaultModel()
         { return createDefaultModel( Standard ); }
-               
-    /** 
+
+    /**
         Answer a new memory-based model with the given reification style
     */
     public static Model createDefaultModel( ReificationStyle style )
-        { return new ModelCom( new GraphMem( style ) ); }   
+        { return new ModelCom( new GraphMem( style ) ); }
 
     /**
         Answer a read-only Model with all the statements of this Model and any
@@ -126,105 +126,105 @@ public class ModelFactory extends ModelFactoryBase
     */
     public static Model withHiddenStatements( Model m )
         { return ModelReifier.withHiddenStatements( m ); }
-        
+
     /**
         construct a new memory-based model that does not capture reification triples
         (but still handles reifyAs() and .as(ReifiedStatement).
     */
     public static Model createNonreifyingModel()
         { return createDefaultModel( Minimal ); }
-        
-    /** 
+
+    /**
         Answer a model that encapsulates the given graph. Existing prefixes are
         undisturbed.
         @param g A graph structure
         @return A model presenting an API view of graph g
     */
     public static Model createModelForGraph( Graph g ) {
-        return new ModelCom( g ); 
+        return new ModelCom( g );
     }
-    
+
     /**
         Answer a ModelMaker that constructs memory-based Models that
         are backed by files in the root directory. The Model is loaded from the
         file when it is opened, and when the Model is closed it is written back.
         The model is given the Standard reification style.
-        
+
         @param root the name of the directory in which the backing files are held
         @return a ModelMaker linked to the files in the root
     */
     public static ModelMaker createFileModelMaker( String root )
         { return createFileModelMaker( root, Standard ); }
-    
+
     /**
         Answer a ModelMaker that constructs memory-based Models that
         are backed by files in the root directory. The Model is loaded from the
         file when it is opened, and when the Model is closed it is written back.
-        
+
         @param root the name of the directory in which the backing files are held
         @param style the desired reification style
         @return a ModelMaker linked to the files in the root
     */
     public static ModelMaker createFileModelMaker( String root, ReificationStyle style )
         { return new ModelMakerImpl( new FileGraphMaker( root, style ) ); }
-        
+
     /**
         Answer a ModelMaker that constructs memory-based Models that do
         not persist past JVM termination. The model has the Standard reification
         style.
-        
+
         @return a ModelMaker that constructs memory-based models
     */
     public static ModelMaker createMemModelMaker()
         { return createMemModelMaker( Standard ); }
-        
+
     /**
         Answer a ModelMaker that constructs memory-based Models that do
         not persist past JVM termination, with the given reification style.
-        
+
         @param style the reification style for the model
         @return a ModelMaker that constructs memory-based models
     */
       public static ModelMaker createMemModelMaker( ReificationStyle style )
         { return new ModelMakerImpl( new SimpleGraphMaker( style ) ); }
-        
+
     /**
         Answer a ModelMaker that accesses database-backed Models on
         the database at the other end of the connection c with the usual
         Standard reification style.
-        
+
         @param c a connection to the database holding the models
         @return a ModelMaker whose Models are held in the database at c
     */
     public static ModelMaker createModelRDBMaker( IDBConnection c )
         { return createModelRDBMaker( c, Standard ); }
-        
+
     /**
         Answer a ModelMaker that accesses database-backed Models on
         the database at the other end of the connection c with the given
         reification style.
-        
+
         @param c a connection to the database holding the models
         @param style the desired reification style
         @return a ModelMaker whose Models are held in the database at c
-    */        
+    */
     public static ModelMaker createModelRDBMaker
         ( IDBConnection c, ReificationStyle style )
         { return new ModelRDBMaker( new GraphRDBMaker( c, style ) ); }
-        
+
     static class ModelRDBMaker extends ModelMakerImpl implements ModelMaker
         {
         public ModelRDBMaker( GraphRDBMaker gm ) { super( gm ); }
-        
+
         public Model makeModel( Graph graphRDB )
             { return new ModelRDB( (GraphRDB) graphRDB ); }
         }
-    
+
     /**
         Answer a plain IDBConnection to a database with the given URL, with
         the given user having the given password. For more complex ways of
         forming a connection, see the DBConnection documentation.
-        
+
         @param url the URL of the database
         @param user the user name to use to access the database
         @param password the password to use. WARNING: open text.
@@ -235,28 +235,28 @@ public class ModelFactory extends ModelFactoryBase
     public static IDBConnection createSimpleRDBConnection
         ( String url, String user, String password, String dbType )
         { return new DBConnection( url, user, password, dbType ); }
-        
+
     /**
         Answer a plain IDBConnection to a database, with the arguments implicitly
         supplied by system properties:
-    <p>    
+    <p>
         The database URL - jena.db.url
         <br>The user - jena.db.user, or fails back to "test"
         <br>The password - jena.db.password, or fails back to ""
         <br>The db type - jena.db.type, or guessed from the URL
     */
     public static IDBConnection createSimpleRDBConnection()
-        { 
+        {
         return createSimpleRDBConnection
             ( guessDBURL(), guessDBUser(), guessDBPassword(), guessDBType() );
         }
-               
+
     /**
-     * Return a Model through which all the RDFS entailments 
+     * Return a Model through which all the RDFS entailments
      * derivable from the given model are accessible. Some work is done
      * when the inferenced model is created but each query will also trigger some
      * additional inference work.
-     * 
+     *
      * @param model the Model containing both instance data and schema assertions to be inferenced over
      */
     public static InfModel createRDFSModel(Model model) {
@@ -264,14 +264,14 @@ public class ModelFactory extends ModelFactoryBase
          InfGraph graph = reasoner.bind( model.getGraph() );
          return new InfModelImpl( graph );
     }
-               
+
     /**
-     * Return a Model through which all the RDFS entailments 
-     * derivable from the given data and schema models are accessible. 
+     * Return a Model through which all the RDFS entailments
+     * derivable from the given data and schema models are accessible.
      * There is no strict requirement to separate schema and instance data between the two
      * arguments.
-     * 
-     * @param model a Model containing instance data assertions 
+     *
+     * @param model a Model containing instance data assertions
      * @param schema a Model containing RDFS schema data
      */
     public static InfModel createRDFSModel( Model schema, Model model ) {
@@ -279,10 +279,10 @@ public class ModelFactory extends ModelFactoryBase
          InfGraph graph  = reasoner.bindSchema(schema.getGraph()).bind(model.getGraph());
          return new InfModelImpl( graph );
     }
-    
+
     /**
      * Build an inferred model by attaching the given RDF model to the given reasoner.
-     * 
+     *
      * @param reasoner the reasoner to use to process the data
      * @param model the Model containing both instance data and schema assertions to be inferenced over
      */
@@ -290,22 +290,22 @@ public class ModelFactory extends ModelFactoryBase
          InfGraph graph = reasoner.bind(model.getGraph());
          return new InfModelImpl(graph);
     }
-               
+
     /**
      * Build an inferred model by attaching the given RDF model to the given reasoner.
      * This form of the call allows two data sets to be merged and reasoned over -
      * conventionally one contains schema data and one instance data but this is not
      * a formal requirement.
-     * 
+     *
      * @param reasoner the reasoner to use to process the data
      * @param schema a Model containing RDFS schema data
-     * @param model a Model containing instance data assertions 
+     * @param model a Model containing instance data assertions
      */
     public static InfModel createInfModel(Reasoner reasoner, Model schema, Model model) {
          InfGraph graph = reasoner.bindSchema(schema.getGraph()).bind(model.getGraph());
          return new InfModelImpl( graph );
     }
-   
+
     /**
      * <p>
      * Answer a new ontology model which will process in-memory models of
@@ -313,15 +313,22 @@ public class ModelFactory extends ModelFactoryBase
      * The default document manager
      * will be used to load the ontology's included documents.
      * </p>
-     * 
+     * <p><strong>Note:</strong>The default model chosen for OWL, RDFS and DAML+OIL
+     * includes a weak reasoner that includes some entailments (such as
+     * transitive closure on the sub-class and sub-property hierarchies). Users
+     * who want either no inference at all, or alternatively
+     * more complete reasoning, should use
+     * one of the other <code>createOntologyModel</code> methods that allow the
+     * preferred OntModel specification to be stated.</p>
      * @return A new ontology model
      * @see OntModelSpec#getDefaultSpec
+     * @see #createOntologyModel(OntModelSpec, Model)
      */
     public static OntModel createOntologyModel() {
         return createOntologyModel( ProfileRegistry.OWL_LANG );
     }
-    
-    
+
+
     /**
      * <p>
      * Answer a new ontology model which will process in-memory models of
@@ -329,7 +336,7 @@ public class ModelFactory extends ModelFactoryBase
      * The default document manager
      * will be used to load the ontology's included documents.
      * </p>
-     * 
+     *
      * @param languageURI The URI specifying the ontology language we want to process
      * @return A new ontology model
      * @see OntModelSpec#getDefaultSpec
@@ -337,8 +344,8 @@ public class ModelFactory extends ModelFactoryBase
     public static OntModel createOntologyModel( String languageURI ) {
         return createOntologyModel( OntModelSpec.getDefaultSpec( languageURI ), null );
     }
-    
-    
+
+
     /**
      * <p>
      * Answer a new ontology model which will process in-memory models of
@@ -346,7 +353,7 @@ public class ModelFactory extends ModelFactoryBase
      * The default document manager
      * will be used to load the ontology's included documents.
      * </p>
-     * 
+     *
      * @param spec An ontology model specification that defines the language and reasoner to use
      * @param maker A model maker that is used to get the initial store for the ontology (unless
      * the base model is given),
@@ -358,17 +365,17 @@ public class ModelFactory extends ModelFactoryBase
     public static OntModel createOntologyModel( OntModelSpec spec, ModelMaker maker, Model base ) {
         OntModelSpec _spec = new OntModelSpec( spec );
         _spec.setImportModelMaker( maker );
-        
+
         return createOntologyModel( _spec, base );
     }
-    
-    
+
+
     /**
      * <p>
-     * Answer a new ontology model, constructed according to the given ontology model specification, 
+     * Answer a new ontology model, constructed according to the given ontology model specification,
      * and starting with the ontology data in the given model.
      * </p>
-     * 
+     *
      * @param spec An ontology model specification object, that will be used to construct the ontology
      * model with different options of ontology language, reasoner, document manager and storage model
      * @param base An existing model to treat as an ontology model, or null.
@@ -385,14 +392,14 @@ public class ModelFactory extends ModelFactoryBase
     */
     public static OntModel createOntologyModel( OntModelSpec spec )
         { return new OntModelImpl( spec ); }
-        
-    
+
+
     /**
      * <p>Answer a model for processing DAML+OIL, using the legacy Jena1 DAML API.  Users are encouraged
-     * to switch from the DAML-specific API to the new generic ontology API 
+     * to switch from the DAML-specific API to the new generic ontology API
      * (see {@link #createOntologyModel(OntModelSpec, Model)}).  The continuation of the DAML-specific
      * API in Jena is not assured beyond Jena version 2.</p>
-     * 
+     *
      * @return A model for in-memory processing of DAML objects.
      */
     public static DAMLModel createDAMLModel() {
@@ -406,7 +413,7 @@ public class ModelFactory extends ModelFactoryBase
         { return createModelForGraph( new Union( m1.getGraph(), m2.getGraph() ) );   }
 
     }
-    
+
 
 /*
     (c) Copyright 2002, 2003, 2004, 2005 Hewlett-Packard Development Company, LP
