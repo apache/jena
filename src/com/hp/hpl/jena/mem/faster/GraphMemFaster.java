cb33fd78ba432c2eeb0d36d9293a4bda747f66ce
Chris Dolan <chrisdolan@apache.org>
2005-07-04 13:53:56 +0000

    *** empty log message ***
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jena/Import/Jena-CVS/jena2/trunk@1112358 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/com/hp/hpl/jena/mem/faster/GraphMemFaster.java
@@ -0,0 +1,103 @@
+/*
+ 	(c) Copyright 2005 Hewlett-Packard Development Company, LP
+ 	All rights reserved - see end of file.
+ 	$Id: GraphMemFaster.java,v 1.1 2005-07-04 13:51:45 chris-dollin Exp $
+*/
+
+package com.hp.hpl.jena.mem.faster;
+
+import com.hp.hpl.jena.graph.*;
+import com.hp.hpl.jena.graph.TripleMatch;
+import com.hp.hpl.jena.graph.query.QueryHandler;
+import com.hp.hpl.jena.mem.*;
+import com.hp.hpl.jena.mem.GraphMemBase;
+import com.hp.hpl.jena.shared.ReificationStyle;
+import com.hp.hpl.jena.util.iterator.ExtendedIterator;
+
+public class GraphMemFaster extends GraphMemBase
+    {
+    public GraphMemFaster()
+        { this( ReificationStyle.Minimal ); }
+    
+    public GraphMemFaster( ReificationStyle style )
+        { super( style ); }
+
+    protected GraphTripleStore store = new GraphTripleStore( this );
+    
+    public GraphTripleStore forTestingOnly_getStore() 
+        { return (GraphTripleStore) store; }
+
+    protected void destroy()
+        { store.close(); }
+
+    public void performAdd( Triple t )
+        { if (!getReifier().handledAdd( t )) store.add( t ); }
+
+    public void performDelete( Triple t )
+        { if (!getReifier().handledRemove( t )) store.delete( t ); }
+
+    public int graphBaseSize()  
+        { return store.size(); }
+    
+    public QueryHandler queryHandler()
+        {
+        if (queryHandler == null) queryHandler = new GraphMemFasterQueryHandler( this );
+        return queryHandler;
+        }
+        
+    public BulkUpdateHandler getBulkUpdateHandler()
+        {
+        if (bulkHandler == null) bulkHandler = new GraphMemFasterBulkUpdateHandler( this );
+        return bulkHandler;
+        }
+
+    /**
+         Answer an ExtendedIterator over all the triples in this graph that match the
+         triple-pattern <code>m</code>. Delegated to the store.
+     */
+    public ExtendedIterator graphBaseFind( TripleMatch m ) 
+        { return store.find( m.asTriple() ); }
+    
+    /**
+         Answer true iff this graph contains <code>t</code>. If <code>t</code>
+         happens to be concrete, then we hand responsibility over to the store.
+         Otherwise we use the default implementation.
+    */
+    public boolean graphBaseContains( Triple t )
+        { return t.isConcrete() ? store.contains( t ) : super.graphBaseContains( t ); }
+    
+    /**
+        Clear this GraphMem, ie remove all its triples (delegated to the store).
+    */
+    public void clear()
+        { store.clear(); }
+    
+    }
+
+
+/*
+ * (c) Copyright 2005 Hewlett-Packard Development Company, LP
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. The name of the author may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
---
+++ src/com/hp/hpl/jena/mem/faster/GraphMemFasterQueryHandler.java
@@ -0,0 +1,75 @@
+/*
+ 	(c) Copyright 2005 Hewlett-Packard Development Company, LP
+ 	All rights reserved - see end of file.
+ 	$Id: GraphMemFasterQueryHandler.java,v 1.1 2005-07-04 13:51:45 chris-dollin Exp $
+*/
+
+package com.hp.hpl.jena.mem.faster;
+
+import com.hp.hpl.jena.graph.Node;
+import com.hp.hpl.jena.graph.query.*;
+import com.hp.hpl.jena.graph.query.QueryHandler;
+import com.hp.hpl.jena.util.iterator.ExtendedIterator;
+
+public class GraphMemFasterQueryHandler extends SimpleQueryHandler implements QueryHandler
+    {
+    protected GraphMemFaster graphMem;
+    
+    GraphMemFasterQueryHandler( GraphMemFaster graph ) 
+        { super( graph ); 
+        this.graphMem = graph; }
+
+    public ExtendedIterator objectsFor( Node p, Node o )
+        { return bothANY( p, o ) ? findObjects() : super.objectsFor( p, o ); }
+
+    public ExtendedIterator predicatesFor( Node s, Node o )
+        { return bothANY( s, o ) ? findPredicates() : super.predicatesFor( s, o ); }
+    
+    public ExtendedIterator subjectsFor( Node p, Node o )
+        { return bothANY( p, o ) ? findSubjects() : super.subjectsFor( p, o ); }   
+
+    /**
+         Answer true iff both <code>a</code> and <code>b</code> are ANY wildcards
+         or are null (legacy). 
+    */
+    private boolean bothANY( Node a, Node b )
+        { return (a == null || a.equals( Node.ANY )) && (b == null || b.equals( Node.ANY )); }
+
+    public ExtendedIterator findPredicates()
+        { return graphMem.store.listPredicates(); }
+
+    public ExtendedIterator findObjects()
+        { return graphMem.store.listObjects(); }
+    
+    public ExtendedIterator findSubjects()
+        { return graphMem.store.listSubjects(); }
+
+    }
+
+
+/*
+ * (c) Copyright 2005 Hewlett-Packard Development Company, LP
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. The name of the author may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
