c71e0bb05959750e15aab0e57e003b341d4cec0c
Chris Dolan <chrisdolan@apache.org>
2005-07-24 19:12:57 +0000

    Added header & footer comments to QueryTriple, oops.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jena/Import/Jena-CVS/jena2/trunk@1112410 13f79535-47bb-0310-9956-ffa450edef68

--- src/com/hp/hpl/jena/graph/query/QueryTriple.java
+++ src/com/hp/hpl/jena/graph/query/QueryTriple.java
@@ -1,6 +1,8 @@
-/**
- * 
- */
+/*
+    (c) Copyright 2005 Hewlett-Packard Development Company, LP
+    All rights reserved - see end of file.
+    $Id: QueryTriple.java,v 1.2 2005-07-24 19:12:57 chris-dollin Exp $
+*/
 package com.hp.hpl.jena.graph.query;
 
 import java.util.HashSet;
@@ -8,30 +10,56 @@ import java.util.HashSet;
 import com.hp.hpl.jena.graph.Triple;
 
 public class QueryTriple
-{
-public final QueryNode S;
-public final QueryNode P;
-public final QueryNode O;
-
-public QueryTriple( QueryNode S, QueryNode P, QueryNode O )
-    { this.S = S; this.P = P; this.O = O; }   
-
-public String toString()
-    { return "<qt " + S.toString() + " " + P.toString() + " " + O.toString() + ">"; }
-
-public static QueryTriple [] classify( Mapping m, Triple [] t )
     {
-    QueryTriple [] result = new QueryTriple [t.length];
-    for (int i = 0; i < t.length; i += 1) result[i] = classify( m, t[i] );
-    return result;
+    public final QueryNode S;
+    public final QueryNode P;
+    public final QueryNode O;
+    
+    public QueryTriple( QueryNode S, QueryNode P, QueryNode O )
+        { this.S = S; this.P = P; this.O = O; }   
+    
+    public String toString()
+        { return "<qt " + S.toString() + " " + P.toString() + " " + O.toString() + ">"; }
+    
+    public static QueryTriple [] classify( Mapping m, Triple [] t )
+        {
+        QueryTriple [] result = new QueryTriple [t.length];
+        for (int i = 0; i < t.length; i += 1) result[i] = classify( m, t[i] );
+        return result;
+        }
+    
+    public static QueryTriple classify( Mapping m, Triple t )
+        { 
+        HashSet fresh = new HashSet();
+        return new QueryTriple
+            ( QueryNode.classify( m, fresh, t.getSubject() ), 
+            QueryNode.classify( m, fresh, t.getPredicate() ),
+            QueryNode.classify( m, fresh, t.getObject() ) );
+        }
     }
-
-public static QueryTriple classify( Mapping m, Triple t )
-    { 
-    HashSet fresh = new HashSet();
-    return new QueryTriple
-        ( QueryNode.classify( m, fresh, t.getSubject() ), 
-        QueryNode.classify( m, fresh, t.getPredicate() ),
-        QueryNode.classify( m, fresh, t.getObject() ) );
-    }
-}
+/*
+ * (c) Copyright 2005 Hewlett-Packard Development Company, LP
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. The name of the author may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
