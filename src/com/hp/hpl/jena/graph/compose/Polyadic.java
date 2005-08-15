b112c81221e7898861d9787a4f0fe6efe227ea47
Ian Dickinsone <ian_dickinson@apache.org>
2005-08-15 15:36:07 +0000

    Added method to get the required base graph (i.e. to throw an execption if no base graph is defined)
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jena/Import/Jena-CVS/jena2/trunk@1112474 13f79535-47bb-0310-9956-ffa450edef68

--- src/com/hp/hpl/jena/graph/compose/Polyadic.java
+++ src/com/hp/hpl/jena/graph/compose/Polyadic.java
@@ -7,11 +7,11 @@
  * Web                http://sourceforge.net/projects/jena/
  * Created            4 Mar 2003
  * Filename           $RCSfile: Polyadic.java,v $
- * Revision           $Revision: 1.14 $
+ * Revision           $Revision: 1.15 $
  * Release status     $State: Exp $
  *
- * Last modified on   $Date: 2005-02-21 11:52:04 $
- *               by   $Author: andy_seaborne $
+ * Last modified on   $Date: 2005-08-15 15:36:07 $
+ *               by   $Author: ian_dickinson $
  *
  * (c) Copyright 2002, 2003, 2004, 2005 Hewlett-Packard Development Company, LP
  * (see footer for full conditions)
@@ -44,7 +44,7 @@ import java.util.*;
  *
  * @author Ian Dickinson, HP Labs
  *         (<a  href="mailto:Ian.Dickinson@hp.com" >email</a>)
- * @version CVS $Id: Polyadic.java,v 1.14 2005-02-21 11:52:04 andy_seaborne Exp $
+ * @version CVS $Id: Polyadic.java,v 1.15 2005-08-15 15:36:07 ian_dickinson Exp $
  */
 public abstract class Polyadic
     extends CompositionBase
@@ -62,11 +62,11 @@ public abstract class Polyadic
 
     /** A list of the sub-graphs that this composition contains */
     protected List m_subGraphs = new ArrayList();
-    
+
     /** The distinguished graph for adding to. If null, use the 0'th graph in the list. */
     protected Graph m_baseGraph = null;
-    
-    
+
+
     // Constructors
     //////////////////////////////////
 
@@ -78,12 +78,12 @@ public abstract class Polyadic
     public Polyadic() {
     }
 
-    
+
     /**
      * <p>
      * Construct a composition of all of the given graphs
      * </p>
-     * 
+     *
      * @param graphs An array of the sub-graphs of this composition
      */
     public Polyadic( Graph[] graphs) {
@@ -91,20 +91,20 @@ public abstract class Polyadic
             m_subGraphs.add( graphs[i] );
         }
     }
-    
+
     private PrefixMapping pm;
-    
+
     public PrefixMapping getPrefixMapping()
-        { 
+        {
         if (pm == null) pm = new PolyadicPrefixMappingImpl( this );
         return pm;
         }
-    
+
     /**
      * <p>
      * Construct a composition of all of the given graphs.
      * </p>
-     * 
+     *
      * @param graphs An iterator of the sub-graphs of this composition. If graphs is
      *               a closable iterator, it will be automatically closed.
      */
@@ -112,13 +112,13 @@ public abstract class Polyadic
         while (graphs.hasNext()) {
             m_subGraphs.add( graphs.next() );
         }
-        
+
         if (graphs instanceof ClosableIterator) {
             ((ClosableIterator) graphs).close();
         }
     }
-    
-    
+
+
     // External signature methods
     //////////////////////////////////
 
@@ -126,7 +126,7 @@ public abstract class Polyadic
      * <p>
      * Close the graph by closing all of the sub-graphs.
      * </p>
-     * 
+     *
      * @see com.hp.hpl.jena.graph.Graph#close()
      */
     public void close() {
@@ -134,13 +134,13 @@ public abstract class Polyadic
             ((Graph) i.next()).close();
         }
     }
- 
- 
+
+
     /**
      * <p>
      * Answer true if this graph contains the given graph as a sub-component.
      * </p>
-     * 
+     *
      * @param graph A graph to test
      * @return True if the graph is this graph, or is a sub-graph of this one.
      * @see com.hp.hpl.jena.graph.Graph#dependsOn(Graph)
@@ -148,45 +148,46 @@ public abstract class Polyadic
     public boolean dependsOn( Graph graph ) {
         return (graph == this) || m_subGraphs.contains( graph );
     }
-    
-    
+
+
     /**
      * <p>
      * Add the given graph to this composition.
      * </p>
-     * 
+     *
      * @param graph A sub-graph to add to this composition
-     */        
-    public void addGraph( Graph graph ) { 
+     */
+    public void addGraph( Graph graph ) {
         m_subGraphs.add( graph );
     }
-    
+
 
     /**
      * <p>
-     * Remove the given graph from this composition.  If the removed graph is the 
+     * Remove the given graph from this composition.  If the removed graph is the
      * designated updateable graph, the updatable graph goes back to the default
      * for this composition.
      * </p>
-     * 
+     *
      * @param graph A sub-graph to remove from this composition
-     */        
-    public void removeGraph( Graph graph ) { 
+     */
+    public void removeGraph( Graph graph ) {
         m_subGraphs.remove( graph );
-        
+
         if (m_baseGraph == graph) {
             m_baseGraph = null;
         }
     }
-    
+
 
     /**
      * <p>
-     * Answer the distinguished graph for the composition, which will be the graph 
-     * that receives triple adds and deletes.
+     * Answer the distinguished graph for the composition, which will be the graph
+     * that receives triple adds and deletes. If no base graph is defined,
+     * return null.
      * </p>
-     * 
-     * @return The distinguished updateable graph, or null if there are no graphs 
+     *
+     * @return The distinguished updateable graph, or null if there are no graphs
      *         in this composition
      */
     public Graph getBaseGraph() {
@@ -202,9 +203,30 @@ public abstract class Polyadic
 
     /**
      * <p>
+     * Answer the distinguished graph for the composition, which will be the graph
+     * that receives triple adds and deletes. If no base graph is defined, throw
+     * a {@link JenaException}.
+     * </p>
+     *
+     * @return The distinguished updateable graph, or null if there are no graphs
+     *         in this composition
+     */
+    public Graph getRequiredBaseGraph() {
+        Graph base = getBaseGraph();
+        if (base == null) {
+            throw new JenaException( "This polyadic graph should have a base graph, but none is defined" );
+        }
+        else {
+            return base;
+        }
+    }
+
+
+    /**
+     * <p>
      * Set the designated updateable graph for this composition.
      * </p>
-     * 
+     *
      * @param graph One of the graphs currently in this composition to be the
      *              designated graph to receive udpates
      * @exception IllegalArgumentException if graph is not one of the members of
@@ -219,42 +241,40 @@ public abstract class Polyadic
             throw new IllegalArgumentException( "The updateable graph must be one of the graphs from the composition" );
         }
     }
-    
-    
+
+
     /**
      * <p>
      * Answer a list of the graphs other than the updateable (base) graph
      * </p>
-     * 
+     *
      * @return A list of all of the sub-graphs, excluding the base graph.
      */
     public List getSubGraphs() {
         List sg = new ArrayList( m_subGraphs );
-        
+
         if (getBaseGraph() != null) {
             sg.remove( getBaseGraph() );
         }
-       
+
         return sg;
     }
 
     public BulkUpdateHandler getBulkUpdateHandler() {
-        if (getBaseGraph() == null)
-            throw new RuntimeException(); // return super.getBulkUpdateHandler();
-        if (bulkHandler == null)  
-            bulkHandler = new WrappedBulkUpdateHandler( this, getBaseGraph().getBulkUpdateHandler() );
+        if (bulkHandler == null)
+            bulkHandler = new WrappedBulkUpdateHandler( this, getRequiredBaseGraph().getBulkUpdateHandler() );
         return bulkHandler;
     }
 
     // the following methods all delegate handling capabilities to the base graph
     // TODO: this needs to be integrated with WrappedGraph, but we don't have time to do so before Jena 2.0 release
-    
-    public TransactionHandler getTransactionHandler() { 
-        return (getBaseGraph() == null) ? super.getTransactionHandler() : getBaseGraph().getTransactionHandler(); 
+
+    public TransactionHandler getTransactionHandler() {
+        return (getBaseGraph() == null) ? super.getTransactionHandler() : getBaseGraph().getTransactionHandler();
         }
 
-    public Capabilities getCapabilities() { 
-        return (getBaseGraph() == null) ? super.getCapabilities() : getBaseGraph().getCapabilities(); 
+    public Capabilities getCapabilities() {
+        return (getBaseGraph() == null) ? super.getCapabilities() : getBaseGraph().getCapabilities();
     }
 
 }
