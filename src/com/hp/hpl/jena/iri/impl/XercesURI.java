064b2074287e90ccd295d3e5d82cffca331ea79e
Jeremy Carroll <jeremy_carroll@apache.org>
2005-09-15 14:31:19 +0000

    Improved copyright
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jena/Import/Jena-CVS/jena2/trunk@1112552 13f79535-47bb-0310-9956-ffa450edef68

--- src/com/hp/hpl/jena/iri/impl/XercesURI.java
+++ src/com/hp/hpl/jena/iri/impl/XercesURI.java
@@ -58,6 +58,33 @@
  *
  * Was originally org/apache/xerces/utils/XercesURI.java in Xerces 1.4.4
  *
+ *
+ * Modifications are:
+ * (c) Copyright 2001-2005 Hewlett-Packard Development Company, LP
+ *  All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. The name of the author may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
  */
 
 package com.hp.hpl.jena.iri.impl;
@@ -100,7 +127,7 @@ import com.hp.hpl.jena.rdf.arp.impl.CharacterModel;
 * default port for a specific scheme). Rather, it only knows the
 * grammar and basic set of operations that can be applied to a XercesURI.
 *
-* @version  $Id: XercesURI.java,v 1.1 2005-08-09 03:30:19 jeremy_carroll Exp $
+* @version  $Id: XercesURI.java,v 1.2 2005-09-15 14:31:19 jeremy_carroll Exp $
 *
 **********************************************************************/
 public class XercesURI implements Serializable {
