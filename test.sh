c03b3352846fa322a12e860977f3a823200f7e62
Andy Seaborne <andy@apache.org>
2005-09-15 08:36:21 +0000

    test scripts updated for new jars
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jena/Import/Jena-CVS/jena2/trunk@1112546 13f79535-47bb-0310-9956-ffa450edef68

--- test.sh
+++ test.sh
@@ -5,15 +5,15 @@ S=":"
 if [ "$OSTYPE" == "cygwin" ]; then S=";"; fi
 
 LIBS="$(cat<<EOF
-antlr.jar
+antlr-2.7.5.jar
 commons-logging.jar
 concurrent.jar
 icu4j.jar
-jakarta-oro-2.0.5.jar
+jakarta-oro-2.0.8.jar
 jena.jar
 jenatest.jar
 junit.jar
-log4j-1.2.7.jar
+log4j-1.2.12.jar
 xercesImpl.jar
 xml-apis.jar
 EOF
