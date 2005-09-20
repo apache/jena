4bca7ff497560b4dd3d6dcbe7323a874802c14db
Jeremy Carroll <jeremy_carroll@apache.org>
2005-09-20 15:41:25 +0000

    Deleted seriously old scripts
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jena/Import/Jena-CVS/jena2/trunk@1112574 13f79535-47bb-0310-9956-ffa450edef68

--- modules/rdf/regression.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-cd ..\..
-cmd /K java -cp .;lib\sax2.jar;lib\xerces.jar;lib\rdffilter.jar;lib\jena.jar com.hp.hpl.mesa.rdf.jena.mem.MemRegression
--- modules/rdf/test.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-cmd /K java -cp ..\..\lib\sax2.jar;..\..\lib\xerces.jar;..\..\lib\rdffilter.jar;..\..\lib\jena.jar com.hp.hpl.mesa.rdf.jena.sample.Sample5
