425d989182a19ea6f7776c9f41072cf1ef9cda5b
Andy Seaborne <andy@apache.org>
2005-09-29 11:13:27 +0000

    Added ARQ tests
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jena/Import/Jena-CVS/jena2/trunk@1112611 13f79535-47bb-0310-9956-ffa450edef68

--- src/com/hp/hpl/jena/test/TestPackage.java
+++ src/com/hp/hpl/jena/test/TestPackage.java
@@ -1,7 +1,7 @@
 /*
   (c) Copyright 2003, 2004, 2005 Hewlett-Packard Development Company, LP
   [See end of file]
-  $Id: TestPackage.java,v 1.33 2005-09-19 10:36:19 jeremy_carroll Exp $
+  $Id: TestPackage.java,v 1.34 2005-09-29 11:13:27 andy_seaborne Exp $
 */
 
 package com.hp.hpl.jena.test;
@@ -31,6 +31,7 @@ public class TestPackage extends TestSuite {
         addTest("Model", com.hp.hpl.jena.rdf.model.test.TestPackage.suite());
         addTest("N3", com.hp.hpl.jena.n3.test.N3TestSuite.suite());
         addTest("RDQL", com.hp.hpl.jena.rdql.test.RDQLTestSuite.suite());
+        addTest("ARQ", com.hp.hpl.jena.query.test.ARQTestSuite.suite()) ;
         addTest("XML Output", com.hp.hpl.jena.xmloutput.test.TestPackage.suite());
         addTest("Util", com.hp.hpl.jena.util.test.TestPackage.suite());
         addTest( com.hp.hpl.jena.util.iterator.test.TestPackage.suite() );
