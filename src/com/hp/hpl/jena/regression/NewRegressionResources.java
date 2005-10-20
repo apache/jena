1778d582e480e5bebc4aa115cc35b678603233fe
Chris Dolan <chrisdolan@apache.org>
2005-10-20 09:55:03 +0000

    Extracted a bunch of resource/container testing into NewRegressionResources.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jena/Import/Jena-CVS/jena2/trunk@1112721 13f79535-47bb-0310-9956-ffa450edef68

--- src/com/hp/hpl/jena/regression/NewRegressionResources.java
+++ src/com/hp/hpl/jena/regression/NewRegressionResources.java
@@ -1,20 +1,24 @@
 /*
  	(c) Copyright 2005 Hewlett-Packard Development Company, LP
  	All rights reserved - see end of file.
- 	$Id: NewRegressionResources.java,v 1.1 2005-08-17 09:48:55 chris-dollin Exp $
+ 	$Id: NewRegressionResources.java,v 1.2 2005-10-20 09:55:03 chris-dollin Exp $
 */
 
 package com.hp.hpl.jena.regression;
 
+import java.util.*;
+
 import junit.framework.*;
 
 import com.hp.hpl.jena.rdf.model.*;
-import com.hp.hpl.jena.rdf.model.test.ModelTestBase;
+import com.hp.hpl.jena.rdf.model.impl.*;
 import com.hp.hpl.jena.regression.Regression.ResTestObjF;
-import com.hp.hpl.jena.shared.InvalidPropertyURIException;
+import com.hp.hpl.jena.shared.*;
+import com.hp.hpl.jena.util.iterator.Map1;
 import com.hp.hpl.jena.vocabulary.RDF;
+import com.hp.hpl.jena.regression.Regression.*;
 
-public class NewRegressionResources extends ModelTestBase
+public class NewRegressionResources extends NewRegressionBase
     {
     public NewRegressionResources( String name )
         { super( name ); }
@@ -126,32 +130,178 @@ public class NewRegressionResources extends ModelTestBase
         assertEquals( local, p.getLocalName() );
         assertEquals( RDF.getURI() + local, p.getURI() );
         }
+    
+    public void testEnhancedResources()
+        {
+        Model m = getModel();
+        Resource r = new ResourceImpl( (ModelCom) m );
+        testResource( m, r, 0 );
+    
+    
+        testResource( m, m.createBag(), 1 );
+        testContainer( m, m.createBag(), m.createBag());
+        // testBag( m, m.createBag(), m.createBag(), m.createBag() );
+    
+        testResource( m, m.createAlt(), 1);
+        testContainer( m, m.createAlt(), m.createAlt() );
+        // testAlt( m, m.createAlt(), m.createAlt(), m.createAlt(), m.createAlt() );
+    
+    
+        testResource( m, m.createSeq(), 1);
+        testContainer( m, m.createSeq(), m.createSeq() );
+        // testSeq( m, m.createSeq(), m.createSeq(), m.createSeq(),
+         //                     m.createSeq(), m.createSeq(), m.createSeq(),
+       //                       m.createSeq() );
+        }
+    
+    protected static final Map1 getSubject = new Map1() 
+        {
+        public Object map1( Object o ) { return ((Statement) o).getSubject(); }
+        };
+    
+    protected Set setOf( Object x )
+        {
+        Set result = new HashSet();
+        result.add( x );
+        return result;
+        }
+    public void testResource( Model m, Resource r, int numProps ) 
+        {
+        Literal tvLiteral = m.createLiteral( "test 12 string 2" );
+        Resource tvResource = m.createResource();
+        String lang = "fr";
+    //
+        assertTrue( r.addProperty( RDF.value, tvByte ).hasProperty( RDF.value, tvByte ) );
+        assertTrue( r.addProperty( RDF.value, tvShort ).hasProperty( RDF.value, tvShort ) );
+        assertTrue( r.addProperty( RDF.value, tvInt ).hasProperty( RDF.value, tvInt ) );
+        assertTrue( r.addProperty( RDF.value, tvLong ).hasProperty( RDF.value, tvLong ) );
+        assertTrue( r.addProperty( RDF.value, tvChar ).hasProperty( RDF.value, tvChar ) );
+        assertTrue( r.addProperty( RDF.value, tvFloat ).hasProperty( RDF.value, tvFloat ) );
+        assertTrue( r.addProperty( RDF.value, tvDouble ).hasProperty( RDF.value, tvDouble ) );
+        assertTrue( r.addProperty( RDF.value, tvString ).hasProperty( RDF.value, tvString ) );
+        assertTrue( r.addProperty( RDF.value, tvString, lang ).hasProperty( RDF.value, tvString, lang ) );
+        assertTrue( r.addProperty( RDF.value, tvObject ).hasProperty( RDF.value, tvObject ) );
+        assertTrue( r.addProperty( RDF.value, tvLiteral ).hasProperty( RDF.value, tvLiteral ) );
+        assertTrue( r.addProperty( RDF.value, tvResource ).hasProperty( RDF.value, tvResource ) );
+        assertTrue( r.getRequiredProperty( RDF.value ).getSubject().equals( r ) );
+    //
+        Property p = m.createProperty( "foo/", "bar" );
+        try {r.getRequiredProperty( p ); fail( "should detect missing property" ); }
+        catch (PropertyNotFoundException e) { pass(); }
+    //
+        assertEquals( 12, iteratorToSet( r.listProperties( RDF.value ) ).size() );
+        assertEquals( setOf( r ), iteratorToSet( r.listProperties( RDF.value ).mapWith( getSubject ) ) );
+    //
+        assertEquals( 0, iteratorToSet( r.listProperties( p ) ).size() );
+        assertEquals( new HashSet(), iteratorToSet( r.listProperties( p ).mapWith( getSubject ) ) );
+    //
+        assertEquals( 12 + numProps, iteratorToSet( r.listProperties() ).size() );
+        assertEquals( setOf( r ), iteratorToSet( r.listProperties().mapWith( getSubject ) ) );
+    //
+        r.removeProperties();
+        assertEquals( 0, m.query( new SimpleSelector( r, null, (RDFNode) null ) ).size() );
+        }
+    
+  public void testContainer( Model m, Container cont1, Container cont2 ) 
+        {
+        NodeIterator nIter;
+        StmtIterator sIter;
+
+        Literal tvLiteral = m.createLiteral( "test 12 string 2" );
+        Resource tvResObj = m.createResource( new ResTestObjF() );
+        Object tvLitObj = new LitTestObj( 1234 );
+        Bag tvBag = m.createBag();
+        Alt tvAlt = m.createAlt();
+        Seq tvSeq = m.createSeq();
+        String lang = "en";
+        Statement stmt;
+    //    
+        assertEquals( 0, cont1.size() );
+        assertEquals( 0, cont2.size() );
+    //
+        assertTrue( cont1.add( tvBoolean ).contains( tvBoolean ) );
+        assertTrue( cont1.add( tvByte ).contains( tvByte ) );
+        assertTrue( cont1.add( tvShort ).contains( tvShort ) );
+        assertTrue( cont1.add( tvInt ).contains( tvInt ) );
+        assertTrue( cont1.add( tvLong ).contains( tvLong ) );
+        assertTrue( cont1.add( tvFloat ).contains( tvFloat ) );
+        assertTrue( cont1.add( tvDouble ).contains( tvDouble ) );
+        assertTrue( cont1.add( tvChar ).contains( tvChar ) );
+        assertTrue( cont1.add( tvString ).contains( tvString ) );
+        assertFalse( cont1.contains( tvString, lang ) );
+        assertTrue( cont1.add( tvString, lang ).contains( tvString, lang ) );
+        assertTrue( cont1.add( tvLiteral ).contains( tvLiteral ) );
+        assertTrue( cont1.add( tvResObj ).contains( tvResObj ) );
+        assertTrue( cont1.add( tvLitObj ).contains( tvLitObj ) );
+        assertEquals( 13, cont1.size() );
+    //
+        int num = 10;
+        for (int i = 0; i < num; i += 1) cont2.add( i );
+        assertEquals( num, cont2.size() );
+        checkNumericContent( cont2, num );
+    //    
+        boolean[] found = new boolean[num];
+        boolean[] retain = { true, true, true, false, false, false, false, false, true, true };
+        retainOnlySpecified( cont2, num, retain );
+        seeWhatsThere( cont2, found );
+        for (int i = 0; i < num; i += 1)
+            assertEquals( i + "th element of array", retain[i], found[i] );
+        }
+
+    protected void seeWhatsThere( Container cont2, boolean[] found )
+        {
+        NodeIterator nit = cont2.iterator();
+        while (nit.hasNext())
+                {
+                int v = ((Literal) nit.nextNode()).getInt();
+                assertFalse( found[v] );
+                found[v] = true;
+                }
+        }
+
+    protected void retainOnlySpecified( Container cont2, int num, boolean[] retain )
+        {
+        NodeIterator nit = cont2.iterator();
+        for (int i = 0; i < num; i++)
+            {
+            nit.nextNode();
+            if (retain[i] == false) nit.remove();
+            }
+        assertFalse( nit.hasNext() );
+        }
+
+    protected void checkNumericContent( Container cont2, int num )
+        {
+        NodeIterator nit = cont2.iterator();
+        for (int i = 0; i < num; i += 1)
+            assertEquals( i, ((Literal) nit.nextNode()).getInt() );
+        assertFalse( nit.hasNext() );
+        }
     }
 
 
 /*
- * (c) Copyright 2005 Hewlett-Packard Development Company, LP
- * All rights reserved.
- *
+ * (c) Copyright 2005 Hewlett-Packard Development Company, LP All rights
+ * reserved.
+ * 
  * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- * 3. The name of the author may not be used to endorse or promote products
- *    derived from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
- * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
- * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
- * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
- * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
- * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
- * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
- * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
- * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*/
+ * modification, are permitted provided that the following conditions are met:
+ * 1. Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer. 2. Redistributions in
+ * binary form must reproduce the above copyright notice, this list of
+ * conditions and the following disclaimer in the documentation and/or other
+ * materials provided with the distribution. 3. The name of the author may not
+ * be used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
+ * EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
