af2ff7524e924e81444a0caeeaad0923304bb876
Chris Dolan <chrisdolan@apache.org>
2006-01-12 16:31:29 +0000

    Added BadObjectException to report that the object of some statement is "bad", typically a literal when a resource is required.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jena/Import/Jena-CVS/jena2/trunk@1112822 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/com/hp/hpl/jena/assembler/BadObjectException.java
@@ -0,0 +1,60 @@
+/*
+ 	(c) Copyright 2006 Hewlett-Packard Development Company, LP
+ 	All rights reserved - see end of file.
+ 	$Id: BadObjectException.java,v 1.1 2006-01-12 16:31:29 chris-dollin Exp $
+*/
+
+package com.hp.hpl.jena.assembler;
+
+import com.hp.hpl.jena.assembler.exceptions.AssemblerException;
+import com.hp.hpl.jena.rdf.model.*;
+
+public class BadObjectException extends AssemblerException
+    {
+    protected final RDFNode object;
+    
+    public BadObjectException( Statement s )
+        { 
+        super( s.getSubject(), makeMessage( s ) ); 
+        this.object = s.getObject();
+        }
+
+    private static String makeMessage( Statement s )
+        { 
+        return 
+            nice( s.getObject() ) 
+            + " is unsuitable as the object of a " + nice( s.getPredicate() ) 
+            + " statement";
+        }
+
+    public RDFNode getObject()
+        { return object; }
+    }
+
+
+/*
+ * (c) Copyright 2006 Hewlett-Packard Development Company, LP
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. The name of the author may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
