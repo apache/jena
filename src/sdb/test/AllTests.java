554243be7db945678790fe3aaaba90a9b4911048
Andy Seaborne <andy@apache.org>
2006-10-11 10:18:08 +0000

    git-svn-id: https://svn.apache.org/repos/asf/incubator/jena/Import/Jena-SVN/SDB/trunk@1115574 13f79535-47bb-0310-9956-ffa450edef68

--- src/sdb/test/AllTests.java
+++ src/sdb/test/AllTests.java
@@ -11,7 +11,7 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite ;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({TestStringBasic.class,TestI18N.class,})
+@Suite.SuiteClasses({TestStringBasic.class,TestI18N.class})
 
 public class AllTests
 {
