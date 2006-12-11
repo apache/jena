da5dafc4a664ac69ef6fc11e23ba6b81be8e5df4
Andy Seaborne <andy@apache.org>
2006-12-11 10:07:10 +0000

    *** empty log message ***
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jena/Import/Jena-CVS/jena2/trunk@1113132 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/jena/turtle.java
@@ -0,0 +1,75 @@
+/*
+ * (c) Copyright 2006 Hewlett-Packard Development Company, LP
+ * All rights reserved.
+ * [See end of file]
+ */
+
+package jena;
+
+import java.io.Reader;
+
+import com.hp.hpl.jena.n3.turtle.TripleDump;
+import com.hp.hpl.jena.n3.turtle.TurtleParseException;
+import com.hp.hpl.jena.n3.turtle.parser.ParseException;
+import com.hp.hpl.jena.n3.turtle.parser.TokenMgrError;
+import com.hp.hpl.jena.n3.turtle.parser.TurtleParser;
+import com.hp.hpl.jena.shared.JenaException;
+import com.hp.hpl.jena.util.FileUtils;
+
+public class turtle
+{
+    /** Run the Turtle parser in debugging mode */  
+    public static void main(String[] args)
+    {
+        String filename = args[0] ;
+        Reader reader = FileUtils.readerFromURL(filename) ;
+
+        try {
+            TurtleParser parser = new TurtleParser(reader) ;
+            parser.setTripleHandler(new TripleDump()) ;
+            parser.setBaseURI(filename) ;
+            parser.parse() ;
+        }
+        catch (ParseException ex)
+        { throw new TurtleParseException(ex.getMessage()) ; }
+        catch (TokenMgrError tErr)
+        { throw new TurtleParseException(tErr.getMessage()) ; }
+
+        catch (JenaException ex)  { throw new TurtleParseException(ex.getMessage(), ex) ; }
+        catch (Error err)
+        {
+            throw new TurtleParseException(err.getMessage() , err) ;
+        }
+        catch (Throwable th)
+        {
+            throw new TurtleParseException(th.getMessage(), th) ;
+        }
+    }
+}
+
+/*
+ * (c) Copyright 2006 Hewlett-Packard Development Company, LP
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. The name of the author may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
