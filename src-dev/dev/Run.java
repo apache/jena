652b4ba59065b01363b6a48280bb1a9ae742ae74
Andy Seaborne <andy@apache.org>
2007-02-12 16:02:08 +0000

    git-svn-id: https://svn.apache.org/repos/asf/incubator/jena/Import/Jena-SVN/ARQ-dev/trunk@1115881 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src-dev/dev/Run.java
@@ -0,0 +1,219 @@
+/*
+ * (c) Copyright 2007 Hewlett-Packard Development Company, LP
+ * All rights reserved.
+ * [See end of file]
+ */
+
+package dev;
+
+import arq.qparse;
+import arq.sparql;
+import arq.cmd.QueryCmdUtils;
+import arq.cmd.ResultsFormat;
+
+import com.hp.hpl.jena.query.*;
+import com.hp.hpl.jena.query.algebra.op.Op;
+import com.hp.hpl.jena.query.algebra.op.OpJoin;
+import com.hp.hpl.jena.query.algebra.op.OpLeftJoin;
+import com.hp.hpl.jena.query.core.DataSourceImpl;
+import com.hp.hpl.jena.query.engine.QueryIterator;
+import com.hp.hpl.jena.query.engine.main.JoinClassifier;
+import com.hp.hpl.jena.query.engine.main.LeftJoinClassifier;
+import com.hp.hpl.jena.query.engine.main.QueryEngineX;
+import com.hp.hpl.jena.query.engine.ref.QueryEngineRef;
+import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.util.FileManager;
+
+
+public class Run
+{
+    public static void main(String[] argv)
+    {
+        //classifyJ() ;
+        //classifyLJ() ;
+        //execQuery("D.ttl", "Q.rq") ;
+        parseQuery() ;
+        query() ;
+    }
+        
+    private static void classifyJ()
+    {
+        classifyJ("{?s :p :o . { ?s :p :o FILTER(true) } }", true) ;
+        classifyJ("{?s :p :o . { ?s :p :o FILTER(?s) } }", true) ;
+        classifyJ("{?s :p :o . { ?s :p ?o FILTER(?o) } }", true) ;
+        classifyJ("{?s :p :o . { ?s :p :o FILTER(?o) } }", true) ;
+        classifyJ("{?s :p :o . { ?x :p :o FILTER(?s) } }", false) ;
+
+        classifyJ("{ { ?s :p :o FILTER(true) } ?s :p :o }", true) ;
+        classifyJ("{ { ?s :p :o FILTER(?s) }   ?s :p :o }", true) ;
+        classifyJ("{ { ?s :p ?o FILTER(?o) }   ?s :p :o }", true) ;
+        classifyJ("{ { ?s :p :o FILTER(?o) }   ?s :p :o }", true) ;
+        classifyJ("{ { ?x :p :o FILTER(?s) }   ?s :p :o }", false) ;
+
+        classifyJ("{?s :p :o . { OPTIONAL { ?s :p :o FILTER(true) } } }", true) ;
+        classifyJ("{?s :p :o . { OPTIONAL { ?s :p :o FILTER(?s) } } }", true) ;
+        classifyJ("{?s :p :o . { ?x :p :o OPTIONAL { ?s :p :o FILTER(?x) } } }", true) ;
+        classifyJ("{?s :p :o . { OPTIONAL { ?s :p ?o FILTER(?o) } } }", true) ;
+        classifyJ("{?s :p :o . { OPTIONAL { ?s :p :o FILTER(?o) } } }", true) ;
+        classifyJ("{?s :p :o . { OPTIONAL { ?x :p :o FILTER(?s) } } }", false) ;
+
+        classifyJ("{?s :p :o . { OPTIONAL { ?s :p :o } } }", true) ;
+        
+        System.exit(0) ;
+    }
+    
+        
+    private static void classifyJ(String pattern, boolean expected)
+    {
+        System.out.println("--------------------------------") ;
+        System.out.println(pattern) ;
+        String qs1 = "PREFIX : <http://example/>\n" ;
+        String qs = qs1+"SELECT * "+pattern;
+        Query query = QueryFactory.create(qs) ;
+        QueryEngineX qe = new QueryEngineX(query) ;
+        Op op = ((QueryEngineX)qe).getPatternOp() ;
+        
+        if ( op instanceof OpJoin )
+        {
+            boolean nonLinear = JoinClassifier.isLinear((OpJoin)op) ;
+            System.out.println("Linear: "+nonLinear) ;
+            if ( nonLinear != expected )
+            {
+                System.out.print(op) ;
+                System.out.println("**** Mismatch with expectation") ;
+            }
+        }
+        else
+            System.out.println("Not a join") ;
+
+    }
+
+    private static void classifyLJ()
+    {
+        String pattern = "" ;
+        classifyLJ("{ ?s ?p ?o OPTIONAL { ?s1 ?p2 ?x} }", true)  ;
+        classifyLJ("{ ?s ?p ?o OPTIONAL { ?s1 ?p2 ?o3 OPTIONAL { ?s1 ?p2 ?x} } }", true)  ;
+        classifyLJ("{ ?s ?p ?x OPTIONAL { ?s1 ?p2 ?o3 OPTIONAL { ?s1 :p ?o3} } }", true)  ;
+        classifyLJ("{ ?s ?p ?x OPTIONAL { ?s1 ?p2 ?o3 OPTIONAL { ?s1 :p ?x} } }", false)  ;
+        System.exit(0) ;
+    }
+        
+    
+    private static void classifyLJ(String pattern, boolean expected)
+    {
+        System.out.println() ;
+        System.out.println(pattern) ;
+        String qs1 = "PREFIX : <http://example/>\n" ;
+        String qs = qs1+"SELECT * "+pattern;
+        Query query = QueryFactory.create(qs) ;
+        QueryEngineX qe = new QueryEngineX(query) ;
+        Op op = ((QueryEngineX)qe).getPatternOp() ;
+        
+        if ( op instanceof OpLeftJoin )
+        {
+            boolean nonLinear = LeftJoinClassifier.isLinear((OpLeftJoin)op) ;
+            System.out.println("Linear: "+nonLinear) ;
+            if ( nonLinear != expected )
+                System.out.println("**** Mismatch with expectation") ;
+        }
+        else
+            System.out.println("Not a left join") ;
+        
+    }
+
+    public static void parseQuery()
+    {
+        QueryEngineX.register() ;
+        String a[] = new String[]{
+            //"-v",
+            //"--engine=ref",
+            "--print=query",
+            "--print=op",
+            "--print=plan",
+            "--query=Q.rq" , 
+        } ;
+        qparse.main(a) ;
+    }
+
+    public static void query()
+    {
+//        String qs1 = "PREFIX : <http://example/>\n" ;
+//        String qs = qs1+"SELECT * { }" ;
+//        Query query = QueryFactory.create(qs) ;
+        
+        Query query = QueryFactory.read("Q.rq") ;
+        Model data = FileManager.get().loadModel("D.ttl") ;
+        DataSource ds = new DataSourceImpl() ;
+        ds.setDefaultModel(data) ;
+        //ds.addNamedModel("http://example/g", data) ;
+        
+        if ( false )
+        {
+            QueryExecution qExec = QueryExecutionFactory.create(query, ds) ;
+            System.out.println("==== Engine1") ;
+            QueryCmdUtils.executeQuery(query, qExec, ResultsFormat.FMT_RS_TEXT) ;
+        }
+
+        if ( false )
+        {
+            QueryEngineRef.register() ;
+            QueryExecution qExec = QueryExecutionFactory.create(query, ds) ;
+            System.out.println("==== EngineRef") ;
+            //System.out.print(((QueryEngineBase)qExec).getPlan()) ;
+            QueryCmdUtils.executeQuery(query, qExec, ResultsFormat.FMT_RS_TEXT) ;
+            QueryEngineRef.unregister() ;
+        }
+
+        QueryEngineX.register() ;
+        System.out.println("==== EngineX") ;
+        QueryEngineX qe = new QueryEngineX(query) ;
+        qe.setDataset(ds) ;
+        QueryIterator qIter = qe.getPlan().iterator() ; 
+        System.out.println(qIter) ;
+        System.out.print(qe.getPlan()) ;
+        QueryCmdUtils.executeQuery(query, qe, ResultsFormat.FMT_RS_TEXT) ;
+        System.exit(0) ;
+    }
+    
+    private static void execQuery(String datafile, String queryfile)
+    {
+        QueryEngineX.register() ;
+        String a[] = new String[]{
+            //"-v",
+            //"--engine=ref",
+            "--data="+datafile,
+            "-query="+queryfile , 
+        } ;
+        
+        sparql.main(a) ;
+        System.exit(0) ;
+        
+    }
+}
+
+/*
+ * (c) Copyright 2007 Hewlett-Packard Development Company, LP
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. The name of the author may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
