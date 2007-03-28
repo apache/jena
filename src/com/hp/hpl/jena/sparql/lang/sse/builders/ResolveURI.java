6c637159fe95d30da2d6530b4639917917714598
Andy Seaborne <andy@apache.org>
2007-03-28 15:58:45 +0000

    git-svn-id: https://svn.apache.org/repos/asf/incubator/jena/Import/Jena-SVN/ARQ/trunk@1116324 13f79535-47bb-0310-9956-ffa450edef68

--- src/com/hp/hpl/jena/sparql/lang/sse/builders/ResolveURI.java
+++ src/com/hp/hpl/jena/sparql/lang/sse/builders/ResolveURI.java
@@ -5,46 +5,107 @@
  */
 
 package com.hp.hpl.jena.sparql.lang.sse.builders;
-
+import java.util.Iterator;
 
 import com.hp.hpl.jena.graph.Node;
 import com.hp.hpl.jena.n3.RelURI;
 import com.hp.hpl.jena.shared.PrefixMapping;
 import com.hp.hpl.jena.sparql.lang.sse.Item;
+import com.hp.hpl.jena.sparql.lang.sse.ItemList;
 import com.hp.hpl.jena.sparql.lang.sse.ItemTransformBase;
 import com.hp.hpl.jena.sparql.lang.sse.ItemTransformer;
+import com.hp.hpl.jena.sparql.util.PrefixMapping2;
 
 /** Resolve URIs, expand any prefixed names */
 
 public class ResolveURI
 {
-    // TODO Syntax for prefixes: c.f. Lisp "let"
     /*
      * (prefix LIST_PAIRS BODY ...)
      * 
-     * (prefix  ((x: <http://example)
-     *           (y: <http://example))
+     * (prefix  ((x: <http://example>)
+     *           (y: <http://example>))
      *      body
      *  ) => body with expansions
      */
-    
-    
     
     public static Item resolve(Item item, PrefixMapping pmap)
     {
         return ItemTransformer.transform(new Resolver(pmap), item) ;
     }
     
+    public static Item resolve(Item item)
+    {
+        return ItemTransformer.transform(new Resolver(), item) ;
+    }
+
     private static class Resolver extends ItemTransformBase
     {
         private PrefixMapping pmap ;
-        Resolver(PrefixMapping pmap)
-        { this.pmap = pmap ; }
+        Resolver(PrefixMapping pmap)    { this.pmap = pmap ; }
+        Resolver()                      { this.pmap = null ; }
         
-//        @Override
-//        public Item transform(Item item, ItemList list)
-//        { }
+        //@Override
+        public Item transform(Item item, ItemList list)
+        {
+            if ( list.isEmpty() )
+                return super.transform(item, list) ;
+                
+            Item head = list.get(0) ;
+            if ( ! head.isWord() || ! head.getWord().equalsIgnoreCase("prefix") ) 
+                return super.transform(item, list) ;
+            
+            // It's (prefix ...)
+            BuilderUtils.checkLength(3, list, "List is "+list.size()+"not 3 :: (prefix ...)") ;
+            
+            if ( ! list.get(1).isList() )
+                BuilderUtils.broken(list, "(prefix ...) is not list of prefix/IRI pairs") ;
+            
+            ItemList prefixes = list.get(1).getList() ;
+            Item body = list.get(2) ;
+
+            PrefixMapping2 ext = new PrefixMapping2(pmap) ;
+            PrefixMapping newMappings = ext.getLocalPrefixMapping() ;
+
+            parsePrefixes(newMappings, prefixes) ;
+            
+            // Push (on the current execution frame) the existing prefix map. 
+            PrefixMapping pmapPush = pmap ;
+            pmap = ext ;
+            Item result = body.transform(this) ;
+            pmap = pmapPush ;
+            return result ;
+        }
         
+        static private void parsePrefixes(PrefixMapping newMappings, ItemList prefixes)
+        {
+            for ( Iterator iter = prefixes.iterator() ; iter.hasNext() ; )
+            {
+                Item pair = (Item)iter.next() ;
+                if ( !pair.isList() || pair.getList().size() != 2 )
+                    BuilderUtils.broken(pair, "Not a prefix/IRI pair") ;
+                Item prefixItem = pair.getList().get(0) ;
+                Item iriItem = pair.getList().get(1) ;
+
+                Node n = prefixItem.getNode() ;
+                if ( ! n.isURI() )
+                    BuilderUtils.broken(pair, "Prefix part is not a prefixed name: "+pair) ;
+                
+                String prefix = n.getURI();
+                Node iriNode = iriItem.getNode() ;
+                
+                if ( iriNode == null || ! iriNode.isURI() )
+                    BuilderUtils.broken(pair, "Not an IRI: "+iriItem) ;
+                
+                String iri = iriNode.getURI();
+                // It will look like :x:
+                prefix = prefix.substring(1) ;
+                prefix = prefix.substring(0, prefix.length()-1) ;
+                
+                newMappings.setNsPrefix(prefix, iri) ;
+            }
+        }
+
 //        @Override
 //        public Item transform(Item item, String word)
 //        { }
@@ -58,9 +119,10 @@ public class ResolveURI
                 if ( uri.startsWith(":") )
                 {
                     String qname = uri.substring(1) ;
-                    uri = pmap.expandPrefix(qname) ;
-                    if ( uri == null )
-                        throw new RuntimeException("Can't resolve "+qname) ;
+                    if ( pmap != null )
+                        uri = pmap.expandPrefix(qname) ;
+                    if ( uri == null || uri.equals(qname) )
+                        BuilderUtils.broken(item, "Can't resolve "+qname) ;
                     return Item.createNode(Node.createURI(uri)) ;
                 }
                 else
@@ -72,8 +134,8 @@ public class ResolveURI
             return super.transform(item, node) ;
         }
     }
-    
 }
+
 
 /*
  * (c) Copyright 2006, 2007 Hewlett-Packard Development Company, LP
