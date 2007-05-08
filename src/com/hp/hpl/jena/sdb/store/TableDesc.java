6824aaf9977e813349ff14a50a846de738c29658
Andy Seaborne <andy@apache.org>
2007-05-08 16:57:37 +0000

    git-svn-id: https://svn.apache.org/repos/asf/incubator/jena/Import/Jena-SVN/SDB/trunk@1116568 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/com/hp/hpl/jena/sdb/store/TableDesc.java
@@ -0,0 +1,80 @@
+/*
+ * (c) Copyright 2006, 2007 Hewlett-Packard Development Company, LP
+ * All rights reserved.
+ * [See end of file]
+ */
+
+package com.hp.hpl.jena.sdb.store;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public abstract class TableDesc
+{
+    private String tableName ;
+    private List<String> columnNames = new ArrayList<String>() ;
+    
+    protected TableDesc(String tableName, String keyColName, String... colNames)
+    { 
+        this.tableName = tableName ;
+        if ( colNames != null )
+            for ( int i = 0 ; i < colNames.length ; i++ )
+                columnNames.add(colNames[i]) ;
+    }
+
+    public String getTableName()
+    { return tableName ; }
+    
+    public boolean hasColumn(String colName)
+    { return columnNames.contains(colName) ; }
+   
+    public Iterator<String> colNames()
+    { return columnNames.iterator() ; } 
+}
+
+class TableDescNode2 extends TableDesc
+{
+    private static final String tableNodes          = "Nodes" ;
+    
+    private static final String colId               = "id" ;
+    private static final String colHash             = "hash" ;
+    private static final String colLex              = "lex" ;
+    private static final String colLang             = "lang" ;
+    private static final String colDatatype         = "datatype" ;
+    private static final String colType             = "type" ;
+    
+    protected TableDescNode2()
+    {
+        super(tableNodes,
+              colId, colHash, colLex, colLang, colDatatype, colType) ;
+    }
+    
+}
+
+/*
+ * (c) Copyright 2006, 2007 Hewlett-Packard Development Company, LP
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. The name of the author may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
