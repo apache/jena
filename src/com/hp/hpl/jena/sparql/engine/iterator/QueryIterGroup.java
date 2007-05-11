ec59e447ef3ced23a17c03ad5b87e514e71320ec
Andy Seaborne <andy@apache.org>
2007-05-11 09:48:18 +0000

    git-svn-id: https://svn.apache.org/repos/asf/incubator/jena/Import/Jena-SVN/ARQ/trunk@1116593 13f79535-47bb-0310-9956-ffa450edef68

--- src/com/hp/hpl/jena/sparql/engine/iterator/QueryIterGroup.java
+++ src/com/hp/hpl/jena/sparql/engine/iterator/QueryIterGroup.java
@@ -18,11 +18,56 @@ public class QueryIterGroup extends QueryIter
 {
     // Base implementation - single group over the whole result set.
     // Maybe this should not be a QueryIter at all.  Just the grouping 
+    
+    private QueryIterator qIter ;
+    private boolean hasYieledGroup = false ;
+    
+    public QueryIterGroup(QueryIterator qIter, ExecutionContext execCxt)
+    {
+        super(execCxt) ;
+        this.qIter = qIter ;
+    }
+
+    public boolean hasNextGroup()
+    { return ! hasYieledGroup ; }
+
+    public QueryIterator nextGroup()
+    {
+        if ( ! hasNextGroup() )
+            throw new NoSuchElementException("QueryIterGroup.nextGroup") ;
+        hasYieledGroup = true ;
+        return qIter ;
+    }
+    
+    protected void closeIterator()
+    {
+        if ( qIter != null )
+            qIter.close() ;
+        qIter = null ;
+    }
+
+    protected boolean hasNextBinding()
+    {
+        return qIter.hasNext() ;
+    }
+
+    protected Binding moveToNextBinding()
+    {
+        return qIter.nextBinding() ;
+    }
+}
+
+
+
+class QueryIterGroup2 extends QueryIter
+{
+    // Base implementation - single group over the whole result set.
+    // Maybe this should not be a QueryIter at all.  Just the grouping 
     private List groups ;
     private Iterator groupIter ;
     private QueryIterator current ;
     
-    public QueryIterGroup(List iterators, ExecutionContext execCxt)
+    public QueryIterGroup2(List iterators, ExecutionContext execCxt)
     {
         super(execCxt) ;
         this.groups = iterators ;
@@ -67,6 +112,34 @@ public class QueryIterGroup extends QueryIter
         return current.nextBinding() ;
     }
 }
+
+
+/*
+ * (c) Copyright 2007 Hewlett-Packard Development Company, LP
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. The name of the author may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 
 
 /*
