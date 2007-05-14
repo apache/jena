2269e92c3401d655888704426aa0c624ee8f1b3a
Andy Seaborne <andy@apache.org>
2007-05-14 19:59:45 +0000

    git-svn-id: https://svn.apache.org/repos/asf/incubator/jena/Import/Jena-SVN/ARQ/trunk@1116602 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/com/hp/hpl/jena/sparql/engine/aggregate/AggregatorBase.java
@@ -0,0 +1,75 @@
+/*
+ * (c) Copyright 2007 Hewlett-Packard Development Company, LP
+ * All rights reserved.
+ * [See end of file]
+ */
+
+package com.hp.hpl.jena.sparql.engine.aggregate;
+
+import java.util.Map;
+
+import com.hp.hpl.jena.graph.Node;
+import com.hp.hpl.jena.sparql.ARQInternalErrorException;
+import com.hp.hpl.jena.sparql.core.Var;
+import com.hp.hpl.jena.sparql.engine.binding.Binding;
+
+public abstract class AggregatorBase implements Aggregator
+{
+    private Map buckets ;       // Key => accumulator
+    private Var var ;
+
+    public AggregatorBase(Var var)
+    { this.var = var ; }
+    
+    final
+    public void accumulate(Binding key, Binding binding)
+    {
+        Accumulator acc = (Accumulator)buckets.get(key) ;
+        if ( acc == null )
+        {
+            acc = createAccumulator() ;
+            buckets.put(key, acc) ;
+        }
+        acc.accumulate(binding) ;
+    }
+
+    protected abstract Accumulator createAccumulator() ;
+
+    public Node getValue(Binding key)
+    {
+        Accumulator acc = (Accumulator)buckets.get(key) ;
+        if ( acc == null )
+            throw new ARQInternalErrorException("Null for accumuator") ;
+        return acc.getValue().asNode() ;
+    }
+
+    public Var getVariable()
+    { return var ; }
+}
+
+/*
+ * (c) Copyright 2007 Hewlett-Packard Development Company, LP
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. The name of the author may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
