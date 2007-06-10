d8a4080cfcc190ed263b25300e1c0ea5a93a17ed
Andy Seaborne <andy@apache.org>
2007-06-10 13:51:13 +0000

    [N3, Turtle] Refacored IRI resolver tests to call the IRIresolver warpper class.  Split RelURI specific test out.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jena/Import/Jena-CVS/jena2/trunk@1113308 13f79535-47bb-0310-9956-ffa450edef68

--- src/com/hp/hpl/jena/n3/test/N3TestSuite.java
+++ src/com/hp/hpl/jena/n3/test/N3TestSuite.java
@@ -9,7 +9,7 @@ import junit.framework.* ;
 
 /**
  * @author		Andy Seaborne
- * @version 	$Id: N3TestSuite.java,v 1.11 2007-03-21 16:09:30 andy_seaborne Exp $
+ * @version 	$Id: N3TestSuite.java,v 1.12 2007-06-10 13:51:13 andy_seaborne Exp $
  */
 public class N3TestSuite extends TestSuite
 {
@@ -23,6 +23,7 @@ public class N3TestSuite extends TestSuite
 	{
 		super("N3") ;
         addTest(TestRelURI.suite()) ;
+        addTest(TestResolver.suite()) ;
 		addTest(new N3InternalTests()) ;
 		addTest(new N3ExternalTests()) ;
 		addTest(new N3JenaReaderTests()) ;
