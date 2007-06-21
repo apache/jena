cf9909511831a1818c0b42bab5c7290b3102f7d2
Andy Seaborne <andy@apache.org>
2007-06-21 11:30:11 +0000

    git-svn-id: https://svn.apache.org/repos/asf/incubator/jena/Import/Jena-SVN/SDB/trunk@1116838 13f79535-47bb-0310-9956-ffa450edef68

--- src/com/hp/hpl/jena/sdb/test/SDBQueryTestSuite.java
+++ src/com/hp/hpl/jena/sdb/test/SDBQueryTestSuite.java
@@ -6,9 +6,7 @@
 
 package com.hp.hpl.jena.sdb.test;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import static com.hp.hpl.jena.sdb.test.SDBTest.manifest;
 import junit.framework.TestSuite;
 import org.junit.runner.RunWith;
 import org.junit.runners.AllTests;
@@ -17,37 +15,17 @@ import com.hp.hpl.jena.query.ARQ;
 import com.hp.hpl.jena.sdb.SDB;
 import com.hp.hpl.jena.sdb.junit.QueryTestSDBFactory;
 import com.hp.hpl.jena.sdb.store.Store;
-import com.hp.hpl.jena.sdb.store.StoreFactory;
 import com.hp.hpl.jena.sdb.util.Pair;
-import com.hp.hpl.jena.sdb.util.StoreUtils;
-
-import static com.hp.hpl.jena.sdb.util.Alg.append ;
-import static com.hp.hpl.jena.sdb.test.SDBTest.manifest;
-import static com.hp.hpl.jena.sdb.test.SDBTest.storeDescBase;
 
 @RunWith(AllTests.class)
 public class SDBQueryTestSuite extends TestSuite
 {
-    // 1 - call "QueryTestSuite"
-    // 2 - read an RDF file.
-    
     //  static suite() becomes in JUnit 4:... 
     //  @RunWith(Suite.class) and SuiteClasses(TestClass1.class, ...)
     
     // @RunWith(Parameterized.class) and parameters are sdb files or Stores
     // But does not allow for programmatic construction of a test suite.
 
-    static boolean includeHash      = false ;
-    static boolean includeIndex     = true ;
-    
-    static boolean formatStores     = false ;
-    
-    static boolean includeDerby     = false ;
-    static boolean includePGSQL     = false ;
-    static boolean includeHSQL      = false ;
-    static boolean includeSQLServer = true ;
-    static boolean includeMySQL     = false ;
-    
     // Old style (JUnit3) but it allows programmatic
     // construction of the test suite hierarchy from a script.
     
@@ -56,7 +34,12 @@ public class SDBQueryTestSuite extends TestSuite
     private SDBQueryTestSuite()
     {
         super("SDB") ;
-        for ( Pair<Store, String> p : append(stores1(), stores2()) )
+        
+        if ( true )
+            // PostgreSQL gets upset with comments in comments??
+            ARQ.getContext().setFalse(SDB.annotateGeneratedSQL) ;
+        
+        for ( Pair<Store, String> p : StoreList.stores() )
         {
             TestSuite ts2 = makeSuite(p.getLeft(), p.getRight()) ;
             //ts2.setName(ts2.getName()+" - "+p.getRight()) ;
@@ -71,68 +54,6 @@ public class SDBQueryTestSuite extends TestSuite
         return ts ;
     }
 
-    private static List<Pair<Store, String>> stores1()
-    {
-        return null ;
-    }
-    
-    
-    private static List<Pair<Store, String>> stores2() 
-    {
-        // Move this to an "sdb manifest" file.
-        // A list of configs/names
-        
-        //  [ :assembler <file:sdb.ttl> ; :rdfs:label "foobar" ] ;
-        
-        
-        List<Pair<Store, String>> stores = new ArrayList<Pair<Store, String>>() ;
-
-        if ( true )
-            // PostgreSQL gets upset with comments in comments.
-            ARQ.getContext().setFalse(SDB.annotateGeneratedSQL) ;
-        
-        if ( includeDerby )
-        {
-            if ( includeHash )  worker(stores, "Derby/Hash",  storeDescBase+"derby-hash.ttl") ;
-            if ( includeIndex ) worker(stores, "Derby/Index", storeDescBase+"derby-index.ttl") ;
-        }
-        
-        if ( includeMySQL )
-        {
-            if ( includeHash )  worker(stores, "MySQL/Hash",  storeDescBase+"mysql-hash.ttl") ;
-            if ( includeIndex ) worker(stores, "MySQL/Index", storeDescBase+"mysql-index.ttl") ;
-        }
-
-        if ( includePGSQL )
-        {
-            if ( includeHash )  worker(stores, "PGSQL/Hash",  storeDescBase+"pgsql-hash.ttl") ;
-            if ( includeIndex ) worker(stores, "PGSQL/Index", storeDescBase+"pgsql-index.ttl") ;
-        }
-
-        if ( includeSQLServer )
-        {
-            if ( includeHash )  worker(stores, "MS-SQL-e/Hash",  storeDescBase+"ms-sql-e-hash.ttl") ;
-            if ( includeIndex ) worker(stores, "MS-SQL-e/Index", storeDescBase+"ms-sql-e-index.ttl") ;
-        }
-        
-        if ( includeHSQL )
-        {
-            if ( includeHash )  worker(stores, "HSQLDB/Hash",  storeDescBase+"hsqldb-hash.ttl") ;
-            if ( includeIndex ) worker(stores, "HSQLDB/Index", storeDescBase+"hsqldb-index.ttl") ;
-        }
-        
-        return stores ;
-    }
-
-    private static void worker(List<Pair<Store, String>> data, String label, String storeDescFile)
-    {
-        Store store = StoreFactory.create(storeDescFile) ;
-        if ( formatStores || StoreUtils.isHSQL(store) )
-            // HSQL (in memory) needs formatting always.
-            store.getTableFormatter().create() ;
-        Pair<Store, String> e = new Pair<Store, String>(store, label) ;
-        data.add(e) ;
-    }
 }
 
 /*
