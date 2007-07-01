edb9f08252148973e45a160f6508fd4c270d281e
Andy Seaborne <andy@apache.org>
2007-07-01 16:53:13 +0000

    git-svn-id: https://svn.apache.org/repos/asf/incubator/jena/Import/Jena-SVN/SDB/trunk@1116912 13f79535-47bb-0310-9956-ffa450edef68

--- README.txt
+++ README.txt
@@ -1,6 +1,6 @@
 SDB - SPARQL Database 
+=====================
 
 A database subsystem for Jena, optimized for SPARQL.
 
-Documentation: http://jena.hpl.hp.com/wiki/SDB/
-
+Home page:		http://jena.sf.net/SDB
--- src/com/hp/hpl/jena/sdb/SDBFactory.java
+++ src/com/hp/hpl/jena/sdb/SDBFactory.java
@@ -12,6 +12,7 @@ import com.hp.hpl.jena.graph.Graph;
 import com.hp.hpl.jena.graph.Node;
 import com.hp.hpl.jena.rdf.model.Model;
 import com.hp.hpl.jena.rdf.model.ModelFactory;
+import com.hp.hpl.jena.rdf.model.Resource;
 
 import com.hp.hpl.jena.query.Dataset;
 
@@ -35,31 +36,80 @@ import com.hp.hpl.jena.sdb.store.StoreFactory;
 public class SDBFactory
 {
     // ---- Connections
-    
+    /**
+     * Create a connection to a database.
+     * @param jdbcURL
+     * @param user
+     * @param password
+     * @return SDBConnection
+     */
     public static SDBConnection createConnection(String jdbcURL, String user, String password)
     { return SDBConnectionFactory.create(jdbcURL, user, password); }
     
+    /**
+     * Create a connection to a database from connection description. 
+     * @param desc              SDB connection description
+     * @return SDBConnection
+     */
     public static SDBConnection createConnection(SDBConnectionDesc desc)
     { return SDBConnectionFactory.create(desc) ; }
 
+    /**
+     * Create a connection to a database from connection description in a file.
+     * @param configFile        Filename to be read and parsed (Jena assembler)
+     * @return SDBConnection
+     */
     public static SDBConnection createConnection(String configFile)
     { return SDBConnectionFactory.create(configFile) ; }
 
+    /**
+     * Create JDBC connection associated with the SDB connection description.
+     * @param desc              SDB connection description
+     * @return Connection
+     */
     public static Connection createSqlConnection(SDBConnectionDesc desc)
     { return SDBConnectionFactory.createSqlConnection(desc) ; }
 
+    /**
+     * Create JDBC connection associated with the SDB connection description in a file.
+     * @param configFile        Filename
+     * @return Connection
+     */
     public static Connection createSqlConnection(String configFile)
     { return SDBConnectionFactory.createJDBC(configFile) ; }
     
+    /**
+     * Connect to a store, based on store and connection descriptions in a file. 
+     * @param configFile        Filename for assembler for Store and SDBConenction
+     * @return Store
+     */
     public static Store connectStore(String configFile) 
     { return StoreFactory.create(configFile) ; }
     
+    /**
+     * Connect to a store, based on a store description. 
+     * @param desc              Store description object
+     * @return Store
+     */
     public static Store connectStore(StoreDesc desc) 
     { return StoreFactory.create(desc) ; }
 
+    
+    /**
+     * Connect to a store, based on an existing SDB connection and a store description.
+     * @param sdbConnection     SDBConnection object 
+     * @param desc              Store description object
+     * @return Store
+     */
     public static Store connectStore(SDBConnection sdbConnection, StoreDesc desc) 
     { return StoreFactory.create(sdbConnection, desc) ; }
     
+    /**
+     * Connect to a store, based on an existing JDBC connection and a store description. 
+     * @param sqlConnection     JDBC connection
+     * @param desc              Store description object
+     * @return  Store
+     */
     public static Store connectStore(Connection sqlConnection, StoreDesc desc) 
     {
         SDBConnection sdb = SDBConnectionFactory.create(sqlConnection) ;
@@ -67,68 +117,194 @@ public class SDBFactory
     }
 
     // ---- Dataset
-    
+
+    /**
+     * Connect to the RDF dataset in a store.
+     * @param store
+     * @return Dataset
+     */
     public static Dataset connectDataset(Store store)
     { return DatasetStore.create(store) ; }
 
+    /**
+     * Connect to the RDF dataset in a store, using existing SDBConnection and a store description.
+     * @param sqlConnection     JDBC connection
+     * @param desc              Store description object
+     * @return Dataset
+     */
     public static Dataset connectDataset(SDBConnection sdbConnection, StoreDesc desc)
     { return DatasetStore.create(connectStore(sdbConnection, desc)) ; }
     
+    /**
+     * Connect to the RDF dataset in a store, based on a Store assembler file.
+     * @param configFile
+     * @return Dataset
+     */
     public static Dataset connectDataset(String configFile)
     { return DatasetStore.create(connectStore(configFile)) ; }
     
     // ---- Graph
     
+    /**
+     * Connect to the default graph in a store, based on a Store assembler file.
+     * @param configFile
+     * @return Graph
+     */
     public static Graph connectDefaultGraph(String configFile)
     { return connectDefaultGraph(StoreFactory.create(configFile)) ; }
 
+    /**
+     * Connect to the default graph in a store, based on a Store description.
+     * @param desc              Store description object    
+     * @return Graph
+     */
     public static Graph connectDefaultGraph(StoreDesc desc)
     { return connectDefaultGraph(StoreFactory.create(desc)) ; }
 
+    /**
+     * Connect to the default graph in a store, based on a Store description.
+     * @param configFile
+     * @return Graph
+     */
     public static Graph connectDefaultGraph(Store store)
     { return new GraphSDB(store) ; }
 
-    
+    /**
+     * Connect to a named graph in a store, based on a store description file. 
+     * @param configFile
+     * @param iri
+     * @return Graph
+     */
     public static Graph connectNamedGraph(String configFile, String iri)
     { return connectNamedGraph(StoreFactory.create(configFile), iri) ; }
 
+    /**
+     * Connect to a named graph in a store, based on a store description. 
+     * @param desc
+     * @param iri
+     * @return Graph
+     */
     public static Graph connectNamedGraph(StoreDesc desc, String iri)
     { return connectNamedGraph(StoreFactory.create(desc), iri) ; }
 
+    /**
+     * Connect to a named graph in a store.
+     * @param store
+     * @param iri
+     * @return Graph
+     */
     public static Graph connectNamedGraph(Store store, String iri)
     { return new GraphSDB(store, iri) ; }
 
+    /**
+     * Connect to a named graph in a store, based on a store description file. 
+     * @param configFile
+     * @param node
+     * @return
+     */
+    public static Graph connectNamedGraph(String configFile, Node node)
+    { return connectNamedGraph(StoreFactory.create(configFile), node) ; }
+    
+    /**
+     * Connect to a named graph in a store, based on a store description. 
+     * @param desc
+     * @param node
+     * @return
+     */
+    public static Graph connectNamedGraph(StoreDesc desc, Node node)
+    { return connectNamedGraph(StoreFactory.create(desc), node) ; }
+    
+    /**
+     * Connect to a named graph in a store.
+     * @param store
+     * @param node
+     * @return
+     */
+    public static Graph connectNamedGraph(Store store, Node node)
+    { return new GraphSDB(store, node) ; }
+    
+    
     // ---- Model
-
+    
+    /**
+     * Connect to the default model in a store, using the store description in a file.
+     * @param configFile        Filename
+     * @return Model
+     */
     public static Model connectDefaultModel(String configFile)
     { return connectDefaultModel(StoreFactory.create(configFile)) ; }
 
+    /**
+     * Connect to the default model in a store
+     * @param desc
+     * @return Model
+     */
     public static Model connectDefaultModel(StoreDesc desc)
     { return connectDefaultModel(StoreFactory.create(desc)) ; }
 
+    /**
+     * Connect to the default model in a store
+     * @param store
+     * @return Model
+     */
     public static Model connectDefaultModel(Store store)
     { return createModelSDB(store) ; }
 
-    
+    /**
+     * Connect to the named model in a store
+     * @param desc
+     * @param iri
+     * @return Model
+     */
     public static Model connectNamedModel(StoreDesc desc, String iri)
     { return connectNamedModel(StoreFactory.create(desc), iri) ; }
 
+    /**
+     * Connect to the named model in a store
+     * @param store
+     * @param iri
+     * @return Model
+     */
     public static Model connectNamedModel(Store store, String iri)
     { return createModelSDB(store, iri) ; }
 
+    /**
+     * Connect to the named model in a store
+     * @param configFile
+     * @param iri
+     * @return Model
+     */
     public static Model connectNamedModel(String configFile, String iri)
     { return connectNamedModel(StoreFactory.create(configFile), iri) ; }
 
-    public static Model connectNamedModel(StoreDesc desc, Node gn)
-    { return connectNamedModel(StoreFactory.create(desc), gn) ; }
+    /**
+     * Connect to the named model in a store
+     * @param desc
+     * @param resource
+     * @return Model
+     */
+    public static Model connectNamedModel(StoreDesc desc, Resource resource)
+    { return connectNamedModel(StoreFactory.create(desc), resource) ; }
 
-    public static Model connectNamedModel(Store store, Node gn)
-    { return createModelSDB(store, gn) ; }
+    /**
+     * Connect to the named model in a store
+     * @param store
+     * @param resource
+     * @return Model
+     */
+    public static Model connectNamedModel(Store store, Resource resource)
+    { return createModelSDB(store, resource) ; }
 
-    public static Model connectNamedModel(String configFile, Node gn)
-    { return connectNamedModel(StoreFactory.create(configFile), gn) ; }
+    /**
+     * Connect to the named model in a store
+     * @param configFile
+     * @param resource
+     * @return Model
+     */
+    public static Model connectNamedModel(String configFile, Resource resource)
+    { return connectNamedModel(StoreFactory.create(configFile), resource) ; }
 
-    // ----
+    // ---- Workers
     
     private static Model createModelSDB(Store store)
     { return ModelFactory.createModelForGraph(new GraphSDB(store)) ; }
@@ -136,8 +312,8 @@ public class SDBFactory
     private static Model createModelSDB(Store store, String iri)
     { return ModelFactory.createModelForGraph(new GraphSDB(store, iri)) ; }
 
-    private static Model createModelSDB(Store store, Node gn)
-    { return ModelFactory.createModelForGraph(new GraphSDB(store, gn)) ; }
+    private static Model createModelSDB(Store store, Resource resource)
+    { return ModelFactory.createModelForGraph(new GraphSDB(store, resource.asNode())) ; }
 }
 
 /*
