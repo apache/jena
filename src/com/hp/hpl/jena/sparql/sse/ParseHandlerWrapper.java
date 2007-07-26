54198d0674e54e9bd3ece91ac77638dc2cac9902
Andy Seaborne <andy@apache.org>
2007-07-26 09:27:42 +0000

    git-svn-id: https://svn.apache.org/repos/asf/incubator/jena/Import/Jena-SVN/ARQ/trunk@1117082 13f79535-47bb-0310-9956-ffa450edef68

--- src/com/hp/hpl/jena/sparql/sse/ParseHandlerWrapper.java
+++ src/com/hp/hpl/jena/sparql/sse/ParseHandlerWrapper.java
@@ -6,7 +6,7 @@
 
 package com.hp.hpl.jena.sparql.sse;
 
-/** Splitter for parser handlers.
+/** Warpper parser handler with pass-through for all operations.
  *  Calls both, first one first.
  * @author Andy Seaborne
  * @version $Id$
