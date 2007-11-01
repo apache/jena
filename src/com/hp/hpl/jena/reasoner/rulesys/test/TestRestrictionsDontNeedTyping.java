7bb5c221da8ccdd00833ca4f9f343ffd7fa612d0
Chris Dolan <chrisdolan@apache.org>
2007-11-01 12:14:15 +0000

    Tests now cover full/mini/micro rules, and allValuesFrom as well as someValuesFrom.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jena/Import/Jena-CVS/jena2/trunk@1113392 13f79535-47bb-0310-9956-ffa450edef68

--- src/com/hp/hpl/jena/reasoner/rulesys/test/TestRestrictionsDontNeedTyping.java
+++ src/com/hp/hpl/jena/reasoner/rulesys/test/TestRestrictionsDontNeedTyping.java
@@ -1,7 +1,7 @@
 /*
  	(c) Copyright 2007 Hewlett-Packard Development Company, LP
  	All rights reserved.
- 	$Id: TestRestrictionsDontNeedTyping.java,v 1.1 2007-10-31 16:05:15 chris-dollin Exp $
+ 	$Id: TestRestrictionsDontNeedTyping.java,v 1.2 2007-11-01 12:14:15 chris-dollin Exp $
 */
 
 package com.hp.hpl.jena.reasoner.rulesys.test;
@@ -24,10 +24,33 @@ public class TestRestrictionsDontNeedTyping extends ModelTestBase
     public TestRestrictionsDontNeedTyping( String name )
         { super( name ); }
 
-    public void testSomeValuesFrom()
+    public void testAllValuesFromFullRules()
+        { testAllValuesFrom( OntModelSpec.OWL_MEM_RULE_INF ); }
+
+    public void testAllValuesFromMiniRules()
+        { testAllValuesFrom( OntModelSpec.OWL_MEM_MINI_RULE_INF ); }
+
+    public void testAllValuesFromMicroRules()
         {
-        testSomeValuesFrom( OntModelSpec.OWL_MEM_RULE_INF );
+        /* micro doesn't support this anyway */
+        // testAllValuesFrom( OntModelSpec.OWL_MEM_MICRO_RULE_INF ); 
         }
+
+    private void testAllValuesFrom( OntModelSpec owlSpec )
+        {
+        Model m = model( "V owl:equivalentClass _R; _R owl:onProperty P; _R owl:allValuesFrom T; X rdf:type V; X P t" );
+        OntModel ont = ModelFactory.createOntologyModel( owlSpec, m );
+        assertTrue( ont.contains( resource( "t" ), RDF.type, resource( "T" ) ) );
+        }
+    
+    public void testSomeValuesFromMiniRules()
+        { testSomeValuesFrom( OntModelSpec.OWL_MEM_MINI_RULE_INF ); }
+
+    public void testSomeValuesFromMicroRules()
+        { testSomeValuesFrom( OntModelSpec.OWL_MEM_MICRO_RULE_INF ); }
+
+    public void testSomeValuesFromFullRules()
+        { testSomeValuesFrom( OntModelSpec.OWL_MEM_RULE_INF ); }
 
     private void testSomeValuesFrom( OntModelSpec owlSpec )
         {
