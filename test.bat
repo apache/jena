c8350ab26fa4cb644e7ccb314c62fcbebba72864
Andy Seaborne <andy@apache.org>
2008-01-04 09:28:38 +0000

    *** empty log message ***
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jena/Import/Jena-CVS/jena2/trunk@1113450 13f79535-47bb-0310-9956-ffa450edef68

--- test.bat
+++ test.bat
@@ -1,6 +1,6 @@
 @echo off
 
-set CP=lib/antlr-2.7.5.jar;lib/arq.jar;lib/arq-extra.jar;lib/concurrent.jar;lib/iri.jar;lib/icu4j_3_4.jar;lib/jena.jar;lib/jenatest.jar;lib/json.jar;lib/junit.jar;lib/log4j-1.2.12.jar;lib/lucene-core-2.0.0.jar;lib/commons-logging-1.1.1.jar;lib/xercesImpl.jar;lib/xml-apis.jar;lib/wstx-asl-3.0.0.jar;lib/stax-api-1.0.jar
+set CP=lib/antlr-2.7.5.jar;lib/arq.jar;lib/arq-extra.jar;lib/concurrent.jar;lib/iri.jar;lib/icu4j_3_4.jar;lib/jena.jar;lib/jenatest.jar;lib/json.jar;lib/junit.jar;lib/log4j-1.2.12.jar;lib/lucene-core-2.2.0.jar;lib/commons-logging-1.1.1.jar;lib/xercesImpl.jar;lib/xml-apis.jar;lib/wstx-asl-3.0.0.jar;lib/stax-api-1.0.jar
 
 java -version
 
