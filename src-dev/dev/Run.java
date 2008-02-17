b8fc71985c7a660cc7845360d651b76b0761daf3
Andy Seaborne <andy@apache.org>
2008-02-17 17:52:00 +0000

    git-svn-id: https://svn.apache.org/repos/asf/incubator/jena/Import/Jena-SVN/ARQ/trunk@1118415 13f79535-47bb-0310-9956-ffa450edef68

--- src-dev/dev/Run.java
+++ src-dev/dev/Run.java
@@ -32,6 +32,8 @@ public class Run
 {
     public static void main(String[] argv) throws Exception
     {
+        remote() ; System.exit(0) ;
+        
         runUpdate() ;
         arq.qexpr.main(new String[]{"64*1000*4e9/(3600*24*365*1000)"} );
         System.exit(0) ;
@@ -49,6 +51,120 @@ public class Run
         System.exit(0) ;
     }
     
+    static void remote()
+    {
+
+        String url = "http://dbpedia.openlinksw.com:8890/sparql";
+        String defaultgraph = "http://dbpedia.org";
+        String shortQuery = "SELECT ?predicate ?object " +
+        "WHERE { " +
+        "<http://dbpedia.org/resource/Philolaus> ?predicate ?object. "+
+        "FILTER( "+
+        "(!isLiteral(?object)) "+
+        "&&( !regex(str(?predicate), 'http://dbpedia.org/property/relatedInstance') ) "+
+        "&&( !regex(str(?predicate), 'http://dbpedia.org/property/wikiPageUsesTemplate') ) "+
+        "&&( !regex(str(?predicate), 'http://www.w3.org/2002/07/owl#sameAs') ) "+
+        "&&( !regex(str(?predicate), 'http://xmlns.com/foaf/0.1/') ) "+
+        "&&( !regex(str(?predicate), 'http://www.w3.org/2004/02/skos/core') ) "+
+        "&&( !regex(str(?object), 'http://xmlns.com/foaf/0.1/') ) "+
+        "&&( !regex(str(?object), 'http://upload.wikimedia.org/wikipedia') ) "+
+        "&&( !regex(str(?object), 'http://www4.wiwiss.fu-berlin.de/flickrwrappr') ) "+
+        "&&( !regex(str(?object), 'http://dbpedia.org/resource/Template') ) "+
+        "&&( !regex(str(?object), 'http://upload.wikimedia.org/wikipedia/commons') ) "+
+        "&&( !regex(str(?object), 'http://www.w3.org/2006/03/wn/wn20/instances/synset') ) "+
+        "&&( !regex(str(?object), 'http://dbpedia.org/resource/Category:') ) "+
+        "&&( !regex(str(?object), 'http://www.w3.org/2004/02/skos/core') ) "+
+        "&&( !regex(str(?object), 'http://www.geonames.org') )).} ";
+
+        String longQuery="SELECT ?predicate ?object " +
+        "WHERE { " +
+        "<http://dbpedia.org/resource/Philolaus> ?predicate ?object. "+
+        "FILTER( "+
+        "(!isLiteral(?object)) "+
+        "&&( !regex(str(?predicate), 'http://dbpedia.org/property/relatedInstance') ) "+
+        "&&( !regex(str(?predicate), 'http://dbpedia.org/property/wikiPageUsesTemplate') ) "+
+        "&&( !regex(str(?predicate), 'http://www.w3.org/2002/07/owl#sameAs') ) "+
+        "&&( !regex(str(?predicate), 'http://xmlns.com/foaf/0.1/') ) "+
+        "&&( !regex(str(?predicate), 'http://www.w3.org/2004/02/skos/core') ) "+
+        "&&( !regex(str(?object), 'http://xmlns.com/foaf/0.1/') ) "+
+        "&&( !regex(str(?object), 'http://upload.wikimedia.org/wikipedia') ) "+
+        "&&( !regex(str(?object), 'http://www4.wiwiss.fu-berlin.de/flickrwrappr') ) "+
+        "&&( !regex(str(?object), 'http://dbpedia.org/resource/Template') ) "+
+        "&&( !regex(str(?object), 'http://upload.wikimedia.org/wikipedia/commons') ) "+
+        "&&( !regex(str(?object), 'http://www.w3.org/2006/03/wn/wn20/instances/synset') ) "+
+        "&&( !regex(str(?object), 'http://www.w3.org/2006/03/wn/wn20/instances/synset') ) "+
+        "&&( !regex(str(?object), 'http://www.w3.org/2006/03/wn/wn20/instances/synset') ) "+
+        "&&( !regex(str(?object), 'http://www.w3.org/2006/03/wn/wn20/instances/synset') ) "+
+        "&&( !regex(str(?object), 'http://www.w3.org/2006/03/wn/wn20/instances/synset') ) "+
+        "&&( !regex(str(?object), 'http://www.w3.org/2006/03/wn/wn20/instances/synset') ) "+
+        "&&( !regex(str(?object), 'http://dbpedia.org/resource/Category:') ) "+
+        "&&( !regex(str(?object), 'http://www.w3.org/2004/02/skos/core') ) "+
+        "&&( !regex(str(?object), 'http://www.geonames.org') )).} ";
+
+
+        ResultSet rs;
+        String xml;
+        QueryEngineHTTP queryExecution;
+        queryExecution = new QueryEngineHTTP(url, shortQuery);
+        queryExecution.addDefaultGraph(defaultgraph);
+        rs = queryExecution.execSelect();
+        xml = ResultSetFormatter.asXMLString(rs);
+        System.out.println("Short Query ResultSet length: "+xml.length());
+
+        try{
+            queryExecution=new QueryEngineHTTP(url,longQuery);
+            queryExecution.addDefaultGraph(defaultgraph);
+            rs = queryExecution.execSelect();
+            xml = ResultSetFormatter.asXMLString(rs);
+            System.out.println("Long Query ResultSet length: "+xml.length()+"\n");
+            System.out.println("Long query XML: "+xml);
+        }catch (Exception e) {e.printStackTrace();}
+
+        String queryWithIncreasingLength="";
+        for (int i = 0; i < 30; i++) {
+            queryWithIncreasingLength = makeQueryString ( i);
+            queryExecution=new QueryEngineHTTP(url,queryWithIncreasingLength);
+            queryExecution.addDefaultGraph(defaultgraph);
+            rs = queryExecution.execSelect();
+            System.out.println("Query with length: "+queryWithIncreasingLength.length()
+                               +" produces ResultSet length: "+ResultSetFormatter.toList(rs).size());
+        }
+    }
+
+
+    public static String makeQueryString ( int charsToAdd){
+        String add="";
+
+        for (int i = 0; i <charsToAdd; i++) {
+            add+="0123456789"+"0123456789";
+        }
+        // NOTE THE LAST LINE
+        String query = "SELECT ?predicate ?object " +
+        "WHERE { " +
+        "<http://dbpedia.org/resource/Philolaus> ?predicate ?object. "+
+        "FILTER( "+
+        "(!isLiteral(?object)) "+
+        "&&( !regex(str(?predicate), 'http://dbpedia.org/property/relatedInstance') ) "+
+        "&&( !regex(str(?predicate), 'http://dbpedia.org/property/wikiPageUsesTemplate') ) "+
+        "&&( !regex(str(?predicate), 'http://www.w3.org/2002/07/owl#sameAs') ) "+
+        "&&( !regex(str(?predicate), 'http://xmlns.com/foaf/0.1/') ) "+
+        "&&( !regex(str(?predicate), 'http://www.w3.org/2004/02/skos/core') ) "+
+        "&&( !regex(str(?object), 'http://xmlns.com/foaf/0.1/') ) "+
+        "&&( !regex(str(?object), 'http://upload.wikimedia.org/wikipedia') ) "+
+        "&&( !regex(str(?object), 'http://www4.wiwiss.fu-berlin.de/flickrwrappr') ) "+
+        "&&( !regex(str(?object), 'http://dbpedia.org/resource/Template') ) "+
+        "&&( !regex(str(?object), 'http://upload.wikimedia.org/wikipedia/commons') ) "+
+        "&&( !regex(str(?object), 'http://www.w3.org/2006/03/wn/wn20/instances/synset') ) "+
+        "&&( !regex(str(?object), 'http://dbpedia.org/resource/Category:') ) "+
+        "&&( !regex(str(?object), 'http://www.w3.org/2004/02/skos/core') ) "+
+        "&&( !regex(str(?object), 'http://www.geonames.org"+add+"') )).} ";
+
+
+        return query;
+    }
+
+
+
     static void Service() throws Exception
     {
         
