76cbd3cc3e5d7acf4ca58e3a9f0f7418c367cb8d
Andy Seaborne <andy@apache.org>
2008-05-07 20:27:24 +0000

    git-svn-id: https://svn.apache.org/repos/asf/incubator/jena/Import/Jena-SVN/SDB/trunk@1118939 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src-dev/dev/UpdateProcVisitorSDB.java
@@ -0,0 +1,98 @@
+/*
+ * (c) Copyright 2008 Hewlett-Packard Development Company, LP
+ * All rights reserved.
+ * [See end of file]
+ */
+
+package dev;
+
+import com.hp.hpl.jena.sparql.engine.binding.Binding;
+import com.hp.hpl.jena.sparql.modify.UpdateProcessorMain;
+import com.hp.hpl.jena.sparql.modify.UpdateProcessorVisitor;
+import com.hp.hpl.jena.sparql.modify.UpdateVisitor;
+import com.hp.hpl.jena.sparql.modify.op.*;
+
+import com.hp.hpl.jena.update.GraphStore;
+import com.hp.hpl.jena.update.UpdateRequest;
+
+import com.hp.hpl.jena.sdb.SDBException;
+
+public class UpdateProcVisitorSDB extends UpdateProcessorVisitor
+{
+
+    UpdateProcVisitorSDB(GraphStore graphStore, UpdateRequest request, Binding inputBinding)
+    {
+        super(graphStore, request, inputBinding) ;
+    }
+
+    @Override
+    public void visit(UpdateModify modify)
+    {}
+
+    @Override
+    public void visit(UpdateDelete delete)
+    {}
+
+    @Override
+    public void visit(UpdateInsert insert)
+    {}
+
+//    @Override
+//    public void visit(UpdateInsertData arg)
+//    {}
+//
+//    @Override
+//    public void visit(UpdateDeleteData arg)
+//    {}
+
+    @Override
+    public void visit(UpdateClear clear)
+    {}
+
+//    @Override
+//    public void visit(UpdateLoad load)
+//    {}
+
+    @Override
+    public void visit(UpdateDrop drop)
+    { 
+        // Delete triples.
+    }
+   
+
+    @Override
+    public void visit(UpdateCreate create)
+    { /* No op in SDB (until a graph management module is written) */ }
+
+    @Override
+    public void visit(UpdateExt arg)
+    { throw new SDBException("UpdateExt - not supported") ; }
+
+}
+
+/*
+ * (c) Copyright 2008 Hewlett-Packard Development Company, LP
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. The name of the author may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
