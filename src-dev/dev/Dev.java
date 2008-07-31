f1852f2a990b50884630acb318b2b51a94b75b43
Andy Seaborne <andy@apache.org>
2008-07-31 17:15:51 +0000

    git-svn-id: https://svn.apache.org/repos/asf/incubator/jena/Import/Jena-SVN/TDB/trunk@1119631 13f79535-47bb-0310-9956-ffa450edef68

--- src-dev/dev/Dev.java
+++ src-dev/dev/Dev.java
@@ -8,34 +8,38 @@ package dev;
 
 public class Dev
 {
+    // B+Tree rewriter
+    
     // New cache API alloc/return/invalidate (shrink/grow?)
     //   Stats.
-    // Check caches.
-    //   More aggressively cache B+Tree indexes
-    
-    // tdbloader: 
-    //   close indexes not in use in a given phase
-    //   Especially efficient iterator() for B+Trees (not mmap).
-
-    // Node table caching?
-    // Use of java properties for key values.
-
-    // CI:     CC dashboard assumes the use of ant default target
-
-    // Huge store : 96bits hash ids?
-    // Cluster store: machine/type/value 8/8/64 => 80 bits 
-    // IndexBuilder to migrate to be policy for data files as well.
-    
-    // removeAll implementation: depends on iterator.remove
-    // but can do faster as a specific operation.
     
     // Documentation on the wiki
     //   Assembler
     //   TDBFactory
     //   Commands
     
-    // TDBFactory ==> "create" ==> connect(... , boolean canCeate) ;
- 
+    // tdbloader: 
+    //   close indexes not in use in a given phase
+    //   More aggressively cache B+Tree indexes, less on leaves (enough for 32 bit?)
+    //   Especially efficient iterator() for B+Trees (not mmap).
+
+    // Node table caching?
+    
+    // Use of java properties for key values.
+
+    // FileFactory2
+    //   IndexBuilder to migrate to be policy for data files as well.
+    //   Combine with FileFactory, BlockMgrFactory
+    
+    // Reopenable BlockMgrs (and the object file?) 
+    //   Memory versions are a "file" (fit into framework for testing).
+    //     Maybe not worth the effort to rework but needs to be reopenable.
+    //   Root is an NIO FileChannel
+    
+    // removeAll implementation: depends on iterator.remove
+    // but can do faster as a specific operation.
+    
+    // TDBFactory ==> "create" ==> connect(... , boolean canCreate) ;
     // Location-keyed cache of TDB graphs 
     
     // BulkLoader
@@ -47,10 +51,8 @@ public class Dev
     //   bound variable tracking
     //   LARQ++
     
-    // Plan for a mega-hash id version (96 bits, hash based)
-    //    Parameter of hash size.
     // Version of NodeTable that does Logical => Physical id translation
-    //    And a PageMgr wrapper for same.
+    //    And a PageMgr wrapper for same?
 
     // Inlines => Inline56, Inline64
 
