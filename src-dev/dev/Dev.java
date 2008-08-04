d561939473818b2704a2bcb5d821a1a5932d77e3
Andy Seaborne <andy@apache.org>
2008-08-04 12:25:34 +0000

    git-svn-id: https://svn.apache.org/repos/asf/incubator/jena/Import/Jena-SVN/TDB/trunk@1119653 13f79535-47bb-0310-9956-ffa450edef68

--- src-dev/dev/Dev.java
+++ src-dev/dev/Dev.java
@@ -9,11 +9,8 @@ package dev;
 public class Dev
 {
     // Build
-    // Jar - to lib dir (or build/tdb.jar) then as artifact?
     // Put in organisation name for publishing.  com.hp.. vs com/hp/ 
     //   [module]/maven-metadata.xml ??
-    // Make pom an artifact => publish it.
-    // Make the file layout correct for Maven (see that email message)
     
     // B+Tree rewriter
     
