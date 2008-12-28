0349c1f216b20bb0e81337fd7a2ac94a8eddcfed
Andy Seaborne <andy@apache.org>
2008-12-28 23:44:08 +0000

    git-svn-id: https://svn.apache.org/repos/asf/incubator/jena/Import/Jena-SVN/ARQ/trunk@1120568 13f79535-47bb-0310-9956-ffa450edef68

--- src-test/com/hp/hpl/jena/sparql/suites/TestPath.java
+++ src-test/com/hp/hpl/jena/sparql/suites/TestPath.java
@@ -1,5 +1,5 @@
 /*
- * (c) Copyright 2008 Hewlett-Packard Development Company, LP
+ * (c) Copyright 2008, 2009 Hewlett-Packard Development Company, LP
  * All rights reserved.
  * [See end of file]
  */
@@ -194,7 +194,7 @@ public class TestPath extends TestCase
 }
 
 /*
- * (c) Copyright 2008 Hewlett-Packard Development Company, LP
+ * (c) Copyright 2008, 2009 Hewlett-Packard Development Company, LP
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
--- src-test/com/hp/hpl/jena/sparql/suites/TestSSE_Basic.java
+++ src-test/com/hp/hpl/jena/sparql/suites/TestSSE_Basic.java
@@ -1,5 +1,5 @@
 /*
- * (c) Copyright 2007, 2008 Hewlett-Packard Development Company, LP
+ * (c) Copyright 2007, 2008, 2009 Hewlett-Packard Development Company, LP
  * All rights reserved.
  * [See end of file]
  */
@@ -375,7 +375,7 @@ public class TestSSE_Basic extends TestCase
 }
 
 /*
- * (c) Copyright 2007, 2008 Hewlett-Packard Development Company, LP
+ * (c) Copyright 2007, 2008, 2009 Hewlett-Packard Development Company, LP
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
