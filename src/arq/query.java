3a4a7ec1bb5134391dae3e5298126fd22aaa2c2d
Andy Seaborne <andy@apache.org>
2008-12-28 23:22:18 +0000

    git-svn-id: https://svn.apache.org/repos/asf/incubator/jena/Import/Jena-SVN/ARQ/trunk@1120560 13f79535-47bb-0310-9956-ffa450edef68

--- src/arq/query.java
+++ src/arq/query.java
@@ -1,5 +1,5 @@
 /*
- * (c) Copyright 2006, 2007, 2008 Hewlett-Packard Development Company, LP
+ * (c) Copyright 2006, 2007, 2008, 2009 Hewlett-Packard Development Company, LP
  * All rights reserved.
  * [See end of file]
  */
@@ -138,7 +138,7 @@ public class query extends CmdARQ
 }
 
 /*
- * (c) Copyright 2006, 2007, 2008 Hewlett-Packard Development Company, LP
+ * (c) Copyright 2006, 2007, 2008, 2009 Hewlett-Packard Development Company, LP
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
