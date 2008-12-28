6b9bdb20272d4413ef3de7a9ea79dfbdb7a2f18c
Andy Seaborne <andy@apache.org>
2008-12-28 23:23:59 +0000

    git-svn-id: https://svn.apache.org/repos/asf/incubator/jena/Import/Jena-SVN/ARQ/trunk@1120561 13f79535-47bb-0310-9956-ffa450edef68

--- src/arq/cmdline/ModAssembler.java
+++ src/arq/cmdline/ModAssembler.java
@@ -1,5 +1,5 @@
 /*
- * (c) Copyright 2006, 2007, 2008 Hewlett-Packard Development Company, LP
+ * (c) Copyright 2006, 2007, 2008, 2009 Hewlett-Packard Development Company, LP
  * All rights reserved.
  * [See end of file]
  */
@@ -68,7 +68,7 @@ public class ModAssembler extends ModBase
 }
 
 /*
- * (c) Copyright 2006, 2007, 2008 Hewlett-Packard Development Company, LP
+ * (c) Copyright 2006, 2007, 2008, 2009 Hewlett-Packard Development Company, LP
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
--- src/arq/cmdline/ModQueryIn.java
+++ src/arq/cmdline/ModQueryIn.java
@@ -1,5 +1,5 @@
 /*
- * (c) Copyright 2006, 2007, 2008 Hewlett-Packard Development Company, LP
+ * (c) Copyright 2006, 2007, 2008, 2009 Hewlett-Packard Development Company, LP
  * All rights reserved.
  * [See end of file]
  */
@@ -170,7 +170,7 @@ public class ModQueryIn implements ArgModuleGeneral
 }
 
 /*
- * (c) Copyright 2006, 2007, 2008 Hewlett-Packard Development Company, LP
+ * (c) Copyright 2006, 2007, 2008, 2009 Hewlett-Packard Development Company, LP
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
