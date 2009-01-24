0ab1eb89ec332d733c6d378eae0f8edb63431329
Andy Seaborne <andy@apache.org>
2009-01-24 20:39:24 +0000

    git-svn-id: https://svn.apache.org/repos/asf/incubator/jena/Import/Jena-SVN/TDB/trunk@1120741 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/com/hp/hpl/jena/tdb/base/reader/RollForwardErrorHandler.java
@@ -0,0 +1,72 @@
+/*
+ * (c) Copyright 2008, 2009 Hewlett-Packard Development Company, LP
+ * All rights reserved.
+ * [See end of file]
+ */
+
+package com.hp.hpl.jena.tdb.base.reader;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.hp.hpl.jena.rdf.model.RDFErrorHandler;
+import com.hp.hpl.jena.shared.JenaException;
+
+public class RollForwardErrorHandler implements RDFErrorHandler
+{
+    String label = null ;
+    private static Logger logger = LoggerFactory.getLogger("ErrorHandler") ;
+    private Logger log = logger ;
+    
+    public RollForwardErrorHandler() {}
+    public RollForwardErrorHandler(String label) { this.label = label ; }
+    public RollForwardErrorHandler( Logger log) { this.log = log ; }
+    
+    @Override
+    public void error(Exception e)
+    {
+        if ( label != null )
+            log.warn(label+" : "+e.getMessage()) ;
+        else
+            log.warn(e.getMessage()) ;
+    }
+
+    @Override
+    public void warning(Exception e)
+    { error(e) ; }
+
+    @Override
+    public void fatalError(Exception e)
+    {
+        throw e instanceof JenaException 
+            ? (JenaException) e : new JenaException( e );
+    }
+
+}
+
+/*
+ * (c) Copyright 2008, 2009 Hewlett-Packard Development Company, LP
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. The name of the author may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
