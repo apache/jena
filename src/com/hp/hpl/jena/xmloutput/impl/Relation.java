c6400d0c738efd2d787c678e074d45f98fcc66a2
Andy Seaborne <andy@apache.org>
2009-01-27 15:57:05 +0000

    *** empty log message ***
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jena/Import/Jena-CVS/jena2/trunk@1113671 13f79535-47bb-0310-9956-ffa450edef68

--- src/com/hp/hpl/jena/xmloutput/impl/Relation.java
+++ src/com/hp/hpl/jena/xmloutput/impl/Relation.java
@@ -1,32 +1,8 @@
 /*
- *  (c) Copyright 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009 Hewlett-Packard Development Company, LP
- *  All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- * 3. The name of the author may not be used to endorse or promote products
- *    derived from this software without specific prior written permission.
-
- * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
- * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
- * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
- * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
- * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
- * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
- * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
- * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
- * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
- * $Id: Relation.java,v 1.13 2009-01-27 15:32:56 andy_seaborne Exp $
- *
- */
+    (c) Copyright 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009 Hewlett-Packard Development Company, LP
+ [See end of file]
+  $Id: Relation.java,v 1.14 2009-01-27 15:57:05 andy_seaborne Exp $
+*/
 
 package com.hp.hpl.jena.xmloutput.impl;
 
@@ -35,6 +11,7 @@ import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.Set;
+import java.util.Map.Entry;
 
 import com.hp.hpl.jena.util.iterator.IteratorIterator;
 import com.hp.hpl.jena.util.iterator.Map1;
@@ -45,7 +22,7 @@ import com.hp.hpl.jena.util.iterator.Map1Iterator;
  *
  * Complete with transitive closure algorithm.
  * @author jjc
- * @version  Release='$Name: not supported by cvs2svn $' Revision='$Revision: 1.13 $' Date='$Date: 2009-01-27 15:32:56 $'
+ * @version  Release='$Name: not supported by cvs2svn $' Revision='$Revision: 1.14 $' Date='$Date: 2009-01-27 15:57:05 $'
  */
 class Relation<T> {
     final private Map<T, Set<T>> rows;
@@ -186,6 +163,17 @@ class Relation<T> {
         }
         return rslt;
     }
+
+    synchronized public Relation<T> copy() {
+        Relation<T> rslt = new Relation<T>();
+        Iterator<PairEntry<T, T>> it = iterator();
+        while ( it.hasNext() ) {
+            Map.Entry<T, T> e = it.next();
+            rslt.set(e.getKey(),e.getValue());
+        }
+        return rslt;
+    }
+    
     /**
      * The set of <code>b</code> such that <code>a</code> is related to <code>b</code>.
      *
@@ -200,41 +188,100 @@ class Relation<T> {
     public Set<T> backward(T b) {
         return cols.get(b);
     }
+ 
+    // ------------
+    private static <T> Map1<T, PairEntry<T, T>> inner(final T a)
+    {
+        return new Map1<T, PairEntry<T, T>>() {
+            public PairEntry<T, T> map1(T b)
+            {
+                return new PairEntry<T, T>(a, b) ;
+            }
+        } ;
+    }
+
+    private static <T> Iterator<PairEntry<T, T>> pairEntry(Map.Entry<T, Set<T>> pair)
+    {
+        final T a = pair.getKey() ;
+        Set<T> bs = pair.getValue() ;
+        return new Map1Iterator<T, PairEntry<T, T>>(inner(a), bs.iterator()) ;
+    }
+
     /**
      * An Iterator over the pairs of the Relation.
      * Each pair is returned as a java.util.Map.Entry.
      * The first element is accessed through <code>getKey()</code>,
      * the second through <code>getValue()</code>.
      *@see java.util.Map.Entry
-     */
+     */   
     public Iterator<PairEntry<T, T>> iterator()
     {
-        return new IteratorIterator<PairEntry<T, T>>(new Map1Iterator<Map.Entry<T, Set<T>>, Iterator<PairEntry<T, T>>>(new Map1<Map.Entry<T, Set<T>>, Iterator<PairEntry<T, T>>>() {
-            // Convert a Map.Entry into an iterator over Map.Entry
-            public Iterator<PairEntry<T, T>> map1(Map.Entry<T, Set<T>> pair)
-            {
-                final T a = pair.getKey() ;
-                Set<T> bs = pair.getValue() ;
-                return new Map1Iterator<T, PairEntry<T, T>>(
-                // Converts a b into a Map.Entry pair.
-                                                            new Map1<T, PairEntry<T, T>>() {
-                                                                public PairEntry<T, T> map1(T b)
-                                                                {
-                                                                    return new PairEntry<T, T>(a, b) ;
-                                                                }
-                                                            }, bs.iterator()) ;
-            }
-            //Map<T, Set<T>>
-        }, rows.entrySet().iterator())) ;
-    }
-    
-    synchronized public Relation<T> copy() {
-        Relation<T> rslt = new Relation<T>();
-        Iterator<PairEntry<T, T>> it = iterator();
-        while ( it.hasNext() ) {
-            Map.Entry<T, T> e = it.next();
-            rslt.set(e.getKey(),e.getValue());
-        }
-        return rslt;
+        // Map<T, Set<T>> => Map.Entry<T, Set<T>> via "rows.entrySet()"
+        // Map.Entry<T, Set<T>> => Iterator<PairEntry<T, T>>
+        // Iterator<PairEntry<T, T>> ==> Iterator<PairEntry<T, T>> via IteratorIterator
+        
+        // Convert  Map.Entry<T, Set<T>> to Iterator<PairEntry<T, T>>
+        // applied to an entrySet.
+        Map1Iterator<Map.Entry<T, Set<T>>,Iterator<PairEntry<T, T>>> iter1 =
+            new Map1Iterator<Map.Entry<T, Set<T>>,Iterator<PairEntry<T, T>>>(
+                new Map1<Map.Entry<T, Set<T>>,Iterator<PairEntry<T, T>>>(){
+                    public Iterator<PairEntry<T, T>> map1(Entry<T, Set<T>> entry)
+                    {
+                        return pairEntry(entry) ;
+                    }}
+                , rows.entrySet().iterator()) ;
+        // And now flatten it.
+        Iterator<PairEntry<T, T>> iter2 = new IteratorIterator<PairEntry<T, T>>(iter1) ; 
+        return iter2 ;
     }
 }
+
+// Old version - exactly as original except with the generics.
+// Hard(er) to read.
+//        return new IteratorIterator<PairEntry<T, T>>(new Map1Iterator<Map.Entry<T, Set<T>>, Iterator<PairEntry<T, T>>>(new Map1<Map.Entry<T, Set<T>>, Iterator<PairEntry<T, T>>>() {
+//            // Convert a Map.Entry into an iterator over Map.Entry
+//            public Iterator<PairEntry<T, T>> map1(Map.Entry<T, Set<T>> pair)
+//            {
+//                final T a = pair.getKey() ;
+//                Set<T> bs = pair.getValue() ;
+//                return new Map1Iterator<T, PairEntry<T, T>>(
+//                // Converts a b into a Map.Entry pair.
+//                                                            new Map1<T, PairEntry<T, T>>() {
+//                                                                public PairEntry<T, T> map1(T b)
+//                                                                {
+//                                                                    return new PairEntry<T, T>(a, b) ;
+//                                                                }
+//                                                            }, bs.iterator()) ;
+//            }
+//            //Map<T, Set<T>>
+//        }, rows.entrySet().iterator())) ;
+
+/*
+ *  (c) Copyright 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009 Hewlett-Packard Development Company, LP
+ *  All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. The name of the author may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * $Id: Relation.java,v 1.14 2009-01-27 15:57:05 andy_seaborne Exp $
+ *
+ */
