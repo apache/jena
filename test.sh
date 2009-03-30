c5586da38634cd0e794711eff0784369e8205106
Andy Seaborne <andy@apache.org>
2009-03-30 13:18:54 +0000

    git-svn-id: https://svn.apache.org/repos/asf/incubator/jena/Import/Jena-SVN/ARQ/trunk@1121227 13f79535-47bb-0310-9956-ffa450edef68

--- test.sh
+++ test.sh
@@ -4,22 +4,19 @@ S=":"
 if [ "$OSTYPE" == "cygwin" ]; then S=";"; fi
 
 LIBS="$(cat<<EOF
-antlr-2.7.5.jar
 arq.jar
 arq-extra.jar
 commons-logging-1.1.1.jar
-concurrent.jar
 icu4j_3_4.jar
 jena.jar
 json.jar
-junit.jar
+junit-4.5.jar
 iri.jar
 log4j-1.2.12.jar
 lucene-core-2.3.1.jar
 wstx-asl-3.0.0.jar
 stax-api-1.0.jar
 xercesImpl.jar
-xml-apis.jar
 )"
 
 CP=""
