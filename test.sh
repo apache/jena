f79501c257eca80b36e55baa3effcf9d30ca31d5
Andy Seaborne <andy@apache.org>
2009-07-08 16:23:09 +0000

    git-svn-id: https://svn.apache.org/repos/asf/incubator/jena/Import/Jena-SVN/ARQ/trunk@1121690 13f79535-47bb-0310-9956-ffa450edef68

--- test.sh
+++ test.sh
@@ -6,26 +6,22 @@ if [ "$OSTYPE" == "cygwin" ]; then S=";"; fi
 X=$(echo lib/arq-*-tests.jar)
 [ -e "$X" ] || { echo "No such tests jar" 1>&2 ; exit 1 ; }
 VER=$X
-VER=${VER#lib/jena-}
+VER=${VER#lib/arq-}
 VER=${VER%-tests.jar}
 echo "Test: ARQ version: $VER"
 
 CP=""
 for jar in lib/*.jar
 do
-  # File expansion.
-  for j in lib/$jar
-  do
-    if [ "$CP" == "" ]
-	then
-	CP="${j}"
-    else
-	CP="$CP${S}${j}"
-    fi
-  done
+  if [ "$CP" == "" ]
+      then
+      CP="${jar}"
+  else
+      CP="$CP${S}${jar}"
+  fi
 done
 
-echo $CP
+echo "Classpath:: $CP"
 ## java  -cp "$CP" junit.textui.TestRunner com.hp.hpl.jena.query.test.ARQTestSuite
 
 java -version
