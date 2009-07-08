16107188a529b80af45242a835f69b714c56aafa
Andy Seaborne <andy@apache.org>
2009-07-08 14:47:25 +0000

    git-svn-id: https://svn.apache.org/repos/asf/incubator/jena/Import/Jena-SVN/ARQ/trunk@1121686 13f79535-47bb-0310-9956-ffa450edef68

--- test.sh
+++ test.sh
@@ -3,24 +3,15 @@
 S=":"
 if [ "$OSTYPE" == "cygwin" ]; then S=";"; fi
 
-LIBS="$(cat<<EOF
-arq*.jar
-slf4j-api-1.5.6.jar
-slf4j-log4j12-1.5.6.jar
-icu4j_3_4.jar
-jena.jar
-json.jar
-junit-4.5.jar
-iri.jar
-log4j-1.2.12.jar
-lucene-core-2.3.1.jar
-wstx-asl-3.0.0.jar
-stax-api-1.0.jar
-xercesImpl.jar
-)"
+X=$(echo lib/arq-*-tests.jar)
+[ -e "$X" ] || { echo "No such tests jar" 1>&2 ; exit 1 ; }
+VER=$X
+VER=${VER#lib/jena-}
+VER=${VER%-tests.jar}
+echo "Test: ARQ version: $VER"
 
 CP=""
-for jar in $LIBS
+for jar in lib/*.jar
 do
   # File expansion.
   for j in lib/$jar
