361c764a213de9e1b496d90a90943c8c572e050e
Andy Seaborne <andy@apache.org>
2009-07-09 16:28:22 +0000

    git-svn-id: https://svn.apache.org/repos/asf/incubator/jena/Import/Jena-SVN/TDB/trunk@1121701 13f79535-47bb-0310-9956-ffa450edef68

--- src-lib/atlas/lib/Chars.java
+++ src-lib/atlas/lib/Chars.java
@@ -14,6 +14,7 @@ public class Chars
 {
     private Chars() {}
     
+    // So also Bytes.hexDigits to get bytes.
     final public static char[] hexDigits = {
         '0' , '1' , '2' , '3' , '4' , '5' , '6' , '7' , '8' ,
         '9' , 'A' , 'B' , 'C' , 'D' , 'E' , 'F' 
