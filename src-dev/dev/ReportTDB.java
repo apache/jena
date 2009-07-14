aaf54a5a1f6d3d66184d6d051c48be7328c1c4cf
Andy Seaborne <andy@apache.org>
2009-07-14 16:37:30 +0000

    git-svn-id: https://svn.apache.org/repos/asf/incubator/jena/Import/Jena-SVN/TDB/trunk@1121733 13f79535-47bb-0310-9956-ffa450edef68

--- src-dev/dev/ReportTDB.java
+++ src-dev/dev/ReportTDB.java
@@ -1,89 +1,23 @@
 package dev;
 
 
+import java.io.FileInputStream;
+import java.io.InputStream;
+
 import atlas.logging.Log;
 
-import com.hp.hpl.jena.query.*;
-import com.hp.hpl.jena.rdf.model.Model;
-import com.hp.hpl.jena.rdf.model.ModelFactory;
-import com.hp.hpl.jena.sparql.util.StringUtils;
+import com.hp.hpl.jena.riot.JenaReaderTurtle2;
 import com.hp.hpl.jena.tdb.TDB;
-import com.hp.hpl.jena.tdb.TDBFactory;
-import com.hp.hpl.jena.vocabulary.RDF;
-import com.hp.hpl.jena.vocabulary.RDFS;
 
 public class ReportTDB
 {
-    public static void main(String[] args){
-
+    public static void main(String[] args) throws Exception{
         Log.setLog4j() ;
-        String testNS = "http://test.com/test#";
-
-        Model domain = ModelFactory.createDefaultModel();
-        domain.read("file:tmp/domain.n3", "N3");
-
-        Model instance = ModelFactory.createDefaultModel();
-        instance.read("file:tmp/instance.n3", "N3");
-
-        DataSource src = DatasetFactory.create();
-        Model domain_instance = ModelFactory.createDefaultModel();
-        domain_instance.add(domain);
-        domain_instance.add(instance);
-
-        src.setDefaultModel(domain_instance);
-        executePathQuery(src);
-
-        System.out.println("==== START TDB ====") ;
-        
-        TDB.getContext().setTrue(TDB.symUnionDefaultGraph); // Has no effect on Path evaluation?
-        TDB.getContext().setTrue(TDB.symLogExec);
-        //Dataset ds = TDBFactory.createDataset("/Users/Gyaan/eclipse_workspace_etr/EtrConfiguration/UserDatasetTest/users/");
-        Dataset ds = TDBFactory.createDataset();
-        Model tdbDomain = ds.getNamedModel("urn:test:domain");
-        tdbDomain.add(domain);
-
-        Model tdbInstance = ds.getNamedModel("urn:test:instance");
-        tdbInstance.add(instance);
-
-        TDB.sync(ds);
-
-//        Query q = QueryFactory.create("SELECT * {?s ?p ?o}") ;
-//        ResultSetFormatter.out(QueryExecutionFactory.create(q, ds).execSelect()) ;
-        
-        
-        
-        //Dataset tdbSrc = TDBFactory.createDataset("/Users/Gyaan/eclipse_workspace_etr/EtrConfiguration/UserDatasetTest/users/");
-        executePathQuery(ds);
-    }
-
-    public static void executePathQuery(Dataset src){
-
-        String[] sparql = {
-            "PREFIX rdf: <"+RDF.getURI()+"> " ,
-            "PREFIX rdfs: <"+RDFS.getURI()+"> " ,
-            "SELECT ?typeUri ?type ",
-            "WHERE { " ,
-            " ?typeUri rdf:type/rdfs:subClassOf* ?type . ",
-            "} ",
-            //"ORDER BY ?typeUri ",
-            //"LIMIT 10"
-        };
-
-        try{
-            QueryExecution cqexec = null;
-            Query q = QueryFactory.create(StringUtils.join("\n",sparql), Syntax.syntaxARQ) ;
-//            Op op = Algebra.compile(q) ;
-//            op = Algebra.optimize(op) ;
-//            System.out.println(op) ;
-            
-            cqexec = QueryExecutionFactory.create( q,  src) ;
-            ResultSet results = cqexec.execSelect();
-            ResultSetFormatter.out(System.out, results);
-        }
-        catch(Exception e){
-            e.printStackTrace();
-        }
-
+        //TDB.init();
+        InputStream input = new FileInputStream("D.ttl") ;
+        JenaReaderTurtle2.parse(input) ;
+        System.out.println("END") ;
+        System.exit(0) ;
     }
 
 
