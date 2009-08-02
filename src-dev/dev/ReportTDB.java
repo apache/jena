0ecd8e2a21e69fca5703771cc2214b4d1b0e4fb8
Andy Seaborne <andy@apache.org>
2009-08-02 21:20:22 +0000

    git-svn-id: https://svn.apache.org/repos/asf/incubator/jena/Import/Jena-SVN/TDB/trunk@1121787 13f79535-47bb-0310-9956-ffa450edef68

--- src-dev/dev/ReportTDB.java
+++ src-dev/dev/ReportTDB.java
@@ -1,13 +1,11 @@
 package dev;
+import java.util.Iterator;
+
 import com.hp.hpl.jena.graph.Graph;
 import com.hp.hpl.jena.graph.Node;
-import com.hp.hpl.jena.query.Dataset;
-import com.hp.hpl.jena.query.Query;
-import com.hp.hpl.jena.query.QueryExecution;
-import com.hp.hpl.jena.query.QueryExecutionFactory;
-import com.hp.hpl.jena.query.QueryFactory;
-import com.hp.hpl.jena.query.ResultSetFormatter;
+import com.hp.hpl.jena.query.*;
 import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.rdf.model.ModelFactory;
 import com.hp.hpl.jena.sparql.sse.SSE;
 import com.hp.hpl.jena.tdb.TDB;
 import com.hp.hpl.jena.tdb.TDBFactory;
@@ -16,6 +14,8 @@ public class ReportTDB
 {
     public static void main(String[] args) throws Exception
     {
+        report() ; System.exit(0) ;
+        
         Dataset ds = TDBFactory.createDataset();
         Model m1 = ds.getNamedModel("http://example/g1")  ;
         
@@ -28,6 +28,42 @@ public class ReportTDB
         TDB.sync(ds) ;
         
         execContains(ds) ;
+    }
+ 
+    public static void report()
+    {
+        DataSource ds = DatasetFactory.create() ;
+        Model m = ModelFactory.createDefaultModel() ;
+        m.getGraph().add(SSE.parseTriple("(<x> <p> 1)")) ;
+        ds.addNamedModel("http://example/g1", m) ;
+
+        // Internal debugging code - show there is some data.
+//        System.out.println("Dataset ds") ;
+//        WriterSSE.out(IndentedWriter.stdout, ds.asDatasetGraph(), null) ;
+//        System.out.println("Model") ;
+//        WriterSSE.out(IndentedWriter.stdout, ds.getNamedModel("http://example/g1").getGraph(), null) ;
+
+        
+        Query query2 = QueryFactory.create("SELECT * FROM NAMED <http://example/g1> { ?s ?p ?o }"); 
+        System.out.println("FROM NAMED Datasource, union set true") ;
+        System.out.println();
+        Dataset ds2 = TDBFactory.createDataset(); 
+        for (Iterator<String> iterator = query2.getNamedGraphURIs().iterator(); iterator.hasNext();) { 
+            String name = iterator.next(); 
+            System.out.println("adding " + name); 
+            Model model = ds2.getNamedModel(name); 
+            System.out.println("isEmpty => "+model.isEmpty());
+            ds.getNamedModel(name).write(System.out, "TTL") ;
+            model.add(ds.getNamedModel(name).listStatements()); 
+            TDB.sync(ds2); 
+            System.out.println("isEmpty => "+model.isEmpty());
+            System.out.println();
+        } 
+        
+        QueryExecution qExec = QueryExecutionFactory.create(query2, ds2) ; 
+        qExec.getContext().set(TDB.symUnionDefaultGraph, true) ; 
+        ResultSetFormatter.out(qExec.execSelect()) ; 
+        qExec.close() ; 
     }
     
     public static void execContains(Dataset ds) throws Exception
