1317487f3aa014bf4dbcbdca2c471105446e508d
Andy Seaborne <andy@apache.org>
2009-08-05 15:26:37 +0000

    *** empty log message ***
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jena/Import/Jena-CVS/jena2/trunk@1113901 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/com/hp/hpl/jena/rdf/model/impl/RDFWriterFImpl.java
+++ src/main/java/com/hp/hpl/jena/rdf/model/impl/RDFWriterFImpl.java
@@ -24,7 +24,7 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *
- * $Id: RDFWriterFImpl.java,v 1.1 2009-06-29 08:55:32 castagna Exp $
+ * $Id: RDFWriterFImpl.java,v 1.2 2009-08-05 15:26:37 andy_seaborne Exp $
  */
 
 package com.hp.hpl.jena.rdf.model.impl;
@@ -41,7 +41,7 @@ import com.hp.hpl.jena.JenaRuntime ;
 /**
  *
  * @author  bwm
- * @version $Revision: 1.1 $ $Date: 2009-06-29 08:55:32 $
+ * @version $Revision: 1.2 $ $Date: 2009-08-05 15:26:37 $
  */
 public class RDFWriterFImpl extends Object implements RDFWriterF {
 
@@ -73,10 +73,14 @@ public class RDFWriterFImpl extends Object implements RDFWriterF {
             Jena.PATH + ".rdf.model.impl.NTripleWriter",
             Jena.PATH + ".n3.N3JenaWriter",
             Jena.PATH + ".n3.N3JenaWriterPP",
-            Jena.PATH + ".n3.N3JenaWriterPlain",
+//            Jena.PATH + ".n3.N3TurtleJenaWriter",   // Write Turtle to ensure safe round tripping.
+//            Jena.PATH + ".n3.N3TurtleJenaWriter",   // Ditto.
+
+            Jena.PATH + ".n3.N3JenaWriterPlain",      // Keep as N3 for now - a test fails.
             Jena.PATH + ".n3.N3JenaWriterTriples",
+            
             Jena.PATH + ".n3.N3JenaWriterTriples",  // Same writer, different writer name
-            Jena.PATH + ".n3.N3TurtleJenaWriter",     // Alternative names for Turtle
+            Jena.PATH + ".n3.N3TurtleJenaWriter",   // Alternative names for Turtle
             Jena.PATH + ".n3.N3TurtleJenaWriter",
             Jena.PATH + ".n3.N3TurtleJenaWriter",
              };
