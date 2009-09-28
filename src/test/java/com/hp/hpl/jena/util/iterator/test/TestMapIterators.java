2b39a1858da2140b4b3fb882438df58893c37098
Chris Dolan <chrisdolan@apache.org>
2009-09-28 11:17:34 +0000

    Adding tests for Map iterator
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jena/Import/Jena-CVS/jena2/trunk@1113923 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/test/java/com/hp/hpl/jena/util/iterator/test/TestMapIterators.java
@@ -0,0 +1,19 @@
+/*
+ 	(c) Copyright 2009 Hewlett-Packard Development Company, LP
+ 	All rights reserved.
+ 	$Id: TestMapIterators.java,v 1.1 2009-09-28 11:17:34 chris-dollin Exp $
+*/
+
+package com.hp.hpl.jena.util.iterator.test;
+
+import com.hp.hpl.jena.rdf.model.test.ModelTestBase;
+
+public class TestMapIterators extends ModelTestBase
+    {
+    public TestMapIterators( String name )
+        { super( name ); }
+
+    public void testMe()
+        { fail( "SPOO" ); }
+    }
+
