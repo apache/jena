d612cf8f5dc5647b8eb32248f809b60acccfe241
Chris Dolan <chrisdolan@apache.org>
2009-09-29 09:17:06 +0000

    Corrected comment.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jena/Import/Jena-CVS/jena2/trunk@1113928 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/com/hp/hpl/jena/rdf/model/ModelMaker.java
+++ src/main/java/com/hp/hpl/jena/rdf/model/ModelMaker.java
@@ -1,7 +1,7 @@
 /*
   (c) Copyright 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009 Hewlett-Packard Development Company, LP
   [See end of file]
-  $Id: ModelMaker.java,v 1.1 2009-06-29 08:55:38 castagna Exp $
+  $Id: ModelMaker.java,v 1.2 2009-09-29 09:17:06 chris-dollin Exp $
 */
 
 package com.hp.hpl.jena.rdf.model;
@@ -39,7 +39,8 @@ public interface ModelMaker extends ModelSource
     public Model createModel( String name, boolean strict );
     
     /**
-        Create a Model that does not already exist - equivalent to
+        Create a Model with the given name if no such model exists. Otherwise,
+        answer the existing model. Equivalent to 
         <br><code>createModel( name, false )</code>.
     */
     public Model createModel( String name );
