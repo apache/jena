7e408d853f8637dc11aa9c3b83cae855ec61ac4a
Andy Seaborne <andy@apache.org>
2009-10-14 12:47:45 +0000

    git-svn-id: https://svn.apache.org/repos/asf/incubator/jena/Import/Jena-SVN/ARQ/trunk@1122111 13f79535-47bb-0310-9956-ffa450edef68

--- src-dev/dev/TestExprLib.java
+++ src-dev/dev/TestExprLib.java
@@ -15,36 +15,43 @@ import org.junit.Test ;
 
 public class TestExprLib
 {
-    @Test public void safeEqualityNot_01()     { testSafeEquality("123", false) ;}
-    @Test public void safeEqualityNot_02()     { testSafeEquality("?x != <y>", false) ;}
-    @Test public void safeEqualityNot_03()     { testSafeEquality("<x> = <y>", false) ;}
+    @Test public void safeEqualityNot_01()      { testSafeEquality("123", false) ;}
+    @Test public void safeEqualityNot_02()      { testSafeEquality("?x != <y>", false) ;}
+    @Test public void safeEqualityNot_03()      { testSafeEquality("<x> = <y>", false) ;}
     
-    @Test public void safeSameTerm_01()     { testSafeEquality("sameTerm(?x, <x>)", true) ;}
-    @Test public void safeSameTerm_02()     { testSafeEquality("sameTerm(<x>, ?x)", true) ;}
+    @Test public void safeSameTerm_01()         { testSafeEquality("sameTerm(?x, <x>)", true) ;}
+    @Test public void safeSameTerm_02()         { testSafeEquality("sameTerm(<x>, ?x)", true) ;}
+    
+    @Test public void safeSameTerm_03()         { testSafeEquality("sameTerm(?x, 'xyz')", false, true, true) ;}
+    @Test public void safeSameTerm_04()         { testSafeEquality("sameTerm(?x, 'xyz')", true, false, false) ;}
 
-    
-    @Test public void safeEquality_01()     { testSafeEquality("?x = <x>", true) ;}
-    @Test public void safeEquality_02()     { testSafeEquality("<x> = ?x", true) ;}
-    
-    // 
-    @Test public void safeEquality_03()     { testSafeEquality("<x> = 'xyz'", true, true, true) ;}
-    @Test public void safeEquality_04()     { testSafeEquality("<x> = 'xyz'", false, false, true) ;}
+    @Test public void safeSameTerm_05()         { testSafeEquality("sameTerm(?x, 'xyz'^^xsd:string)", false, true, true) ;}
+    @Test public void safeSameTerm_06()         { testSafeEquality("sameTerm(?x, 'xyz'^^xsd:string)", true, false, false) ;}
 
-    @Test public void safeEquality_05()     { testSafeEquality("<x> = 123", false, false, true) ;}
-    @Test public void safeEquality_06()     { testSafeEquality("<x> = 123", false, true, false) ;}
+    @Test public void safeSameTerm_07()         { testSafeEquality("sameTerm(?x, 'xyz'@en)", true, true, true) ;}
+    @Test public void safeSameTerm_08()         { testSafeEquality("sameTerm(?x, 'xyz'@en)", true, false, false) ;}
 
-    
-    
-    //@Test public void safeEquality_03()     { testSafeEquality("", false) ;}
-//    @Test public void safeEquality_04()     { testSafeEquality("123", false) ;}
-//    @Test public void safeEquality_05()     { testSafeEquality("123", false) ;}
-//    @Test public void safeEquality_06()     { testSafeEquality("123", false) ;}
-//    @Test public void safeEquality_07()     { testSafeEquality("123", false) ;}
-//    @Test public void safeEquality_08()     { testSafeEquality("123", false) ;}
-//    @Test public void safeEquality_09()     { testSafeEquality("123", false) ;}
-//    @Test public void safeEquality_10()     { testSafeEquality("123", false) ;}
-//    @Test public void safeEquality_11()     { testSafeEquality("123", false) ;}
+    @Test public void safeSameTerm_10()         { testSafeEquality("sameTerm(?x, 123)", false, true, true) ;}
+    @Test public void safeSameTerm_11()         { testSafeEquality("sameTerm(?x, 123)", true, false, false) ;}
 
+    @Test public void safeSameTerm_12()         { testSafeEquality("sameTerm(?x, 'foo'^^<http://example>)", true) ;}
+    
+    @Test public void safeEquality_01()         { testSafeEquality("?x = <x>", true) ;}
+    @Test public void safeEquality_02()         { testSafeEquality("<x> = ?x", true) ;}
+    
+    @Test public void safeEquality_03()         { testSafeEquality("?x = 'xyz'", true, true, true) ;}
+    @Test public void safeEquality_04()         { testSafeEquality("?x = 'xyz'", false, false, true) ;}
+
+    @Test public void safeEquality_05()         { testSafeEquality("?x = 'xyz'^^xsd:string", true, true, true) ;}
+    @Test public void safeEquality_06()         { testSafeEquality("?x = 'xyz'^^xsd:string", false, false, true) ;}
+
+    @Test public void safeEquality_07()         { testSafeEquality("?x = 'xyz'@en", true, true, true) ;}
+    @Test public void safeEquality_08()         { testSafeEquality("?x = 'xyz'@en", true, false, false) ;}
+
+    @Test public void safeEquality_10()         { testSafeEquality("?x = 123", true, true, true) ;}
+    @Test public void safeEquality_11()         { testSafeEquality("?x = 123", false, true, false) ;}
+
+    @Test public void safeEquality_12()         { testSafeEquality("?x = 'foo'^^<http://example>", true) ;}
     
     private static void testSafeEquality(String string, boolean b)
     {
