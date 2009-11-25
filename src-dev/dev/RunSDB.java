d8e5ceb0859c70cef353fca69198531e99bfd6a7
Andy Seaborne <andy@apache.org>
2009-11-25 19:58:23 +0000

    git-svn-id: https://svn.apache.org/repos/asf/incubator/jena/Import/Jena-SVN/SDB/trunk@1122243 13f79535-47bb-0310-9956-ffa450edef68

--- src-dev/dev/RunSDB.java
+++ src-dev/dev/RunSDB.java
@@ -53,7 +53,7 @@ public class RunSDB
     public static void main(String ... argv) throws SQLException
     {
         //sdb.sdbconfig.main("--sdb=sdb.ttl", "--create") ;
-        sdb.sdbprint.main("--sdb=sdb.ttl", "select ?s ?p ?o  where {?s ?p ?o} limit 5") ;
+        sdb.sdbprint.main("--sdb=sdb.ttl", "select *  where { GRAPH<G> { ?s ?p ?o}} limit 5") ;
         System.exit(0) ;
         
         {
