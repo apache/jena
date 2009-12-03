ce217129ab7a911db150d87fc3c0bb4a935cff94
Andy Seaborne <andy@apache.org>
2009-12-03 21:43:47 +0000

    git-svn-id: https://svn.apache.org/repos/asf/incubator/jena/Import/Jena-SVN/ARQ/trunk@1122276 13f79535-47bb-0310-9956-ffa450edef68

--- src/arq/cmdline/ModResultsOut.java
+++ src/arq/cmdline/ModResultsOut.java
@@ -35,7 +35,7 @@ public class ModResultsOut implements ArgModuleGeneral
         cmdLine.getUsage().startCategory("Results") ;
         cmdLine.add(resultsFmtDecl,
                     "--results",
-                    "Results format (Result set: text, XML, JSON; Graph: RDF serialization)") ;  
+                    "Results format (Result set: text, XML, JSON, CSV, TSV; Graph: RDF serialization)") ;  
     }
 
     public void checkCommandLine(CmdArgModule cmdLine)
