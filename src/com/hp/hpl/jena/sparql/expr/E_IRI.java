a39bff1b02f84e3e34a785b94f14dcbddb7df6bf
Andy Seaborne <andy@apache.org>
2010-02-23 22:51:13 +0000

    git-svn-id: https://svn.apache.org/repos/asf/incubator/jena/Import/Jena-SVN/ARQ/trunk@1122455 13f79535-47bb-0310-9956-ffa450edef68

--- src/com/hp/hpl/jena/sparql/expr/E_IRI.java
+++ src/com/hp/hpl/jena/sparql/expr/E_IRI.java
@@ -27,7 +27,7 @@ public class E_IRI extends ExprFunction1
     @Override
     public NodeValue eval(NodeValue v)
     { 
-        if ( v.getNode().isURI() )
+        if ( v.asNode().isURI() )
             return v ;
         if ( v.isString() )
         {
