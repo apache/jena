cd7742bd61d8e8a77faed1471c46d099d75964a1
Andy Seaborne <andy@apache.org>
2010-04-22 11:58:49 +0000

    git-svn-id: https://svn.apache.org/repos/asf/incubator/jena/Import/Jena-SVN/ARQ/trunk@1122669 13f79535-47bb-0310-9956-ffa450edef68

--- src-test/com/hp/hpl/jena/sparql/TS_General.java
+++ src-test/com/hp/hpl/jena/sparql/TS_General.java
@@ -11,6 +11,7 @@ package com.hp.hpl.jena.sparql;
 import junit.framework.TestSuite ;
 
 import com.hp.hpl.jena.sparql.core.TestContext ;
+import com.hp.hpl.jena.sparql.core.TestDatasetDataSource ;
 import com.hp.hpl.jena.sparql.core.TestDatasetGraphMem ;
 import com.hp.hpl.jena.sparql.core.TestDatasetGraphMemTriplesQuads ;
 import com.hp.hpl.jena.sparql.core.TestEsc ;
@@ -43,6 +44,7 @@ public class TS_General extends TestSuite
         
         addTest(TestContext.suite()) ;
         
+        addTest(TestDatasetDataSource.suite()) ;
         addTest(TestDatasetGraphMem.suite()) ;
         addTest(TestDatasetGraphMemTriplesQuads.suite()) ;
     }
--- src-test/com/hp/hpl/jena/sparql/core/TestDataset.java
+++ src-test/com/hp/hpl/jena/sparql/core/TestDataset.java
@@ -6,20 +6,43 @@
 
 package com.hp.hpl.jena.sparql.core;
 
+import static junit.framework.Assert.assertEquals ;
+import static junit.framework.Assert.assertFalse ;
 import static junit.framework.Assert.assertNotNull ;
+import static junit.framework.Assert.assertNull ;
+import static junit.framework.Assert.assertTrue ;
+
+import java.util.List ;
+
 import org.junit.Test ;
 
+import com.hp.hpl.jena.query.DataSource ;
 import com.hp.hpl.jena.query.Dataset ;
+import com.hp.hpl.jena.rdf.model.Model ;
+import com.hp.hpl.jena.rdf.model.ModelFactory ;
+import com.hp.hpl.jena.rdf.model.Property ;
+import com.hp.hpl.jena.rdf.model.Resource ;
+import com.hp.hpl.jena.sparql.lib.iterator.Iter ;
 
 public abstract class TestDataset
 {
-    protected abstract Dataset create() ;
+    protected abstract DataSource create() ;
     
-    @Test public void dataset_01()
-    {
-        Dataset ds = create() ;
-        assertNotNull(ds.getDefaultModel()) ;
-        assertNotNull(ds.asDatasetGraph()) ;
+    static Model model1 = ModelFactory.createDefaultModel() ;
+    static Model model2 = ModelFactory.createDefaultModel() ;
+    
+    static Resource s1 = model1.createResource("s1") ;
+    static Resource s2 = model1.createResource("s2") ;
+
+    static Property p1 = model1.createProperty("p1") ;
+    static Property p2 = model1.createProperty("p2") ;
+    
+    static Resource o1 = model1.createResource("o1") ;
+    static Resource o2 = model1.createResource("o2") ;
+    
+    static {
+        model1.add(s1, p1, o1) ;
+        model2.add(s2, p2, o2) ;
     }
     
     /*
@@ -37,12 +60,64 @@ public abstract class TestDataset
     public void  replaceNamedModel(String uri, Model model) ;
      */
     
+    @Test public void dataset_01()
+    {
+        Dataset ds = create() ;
+        assertNotNull(ds.getDefaultModel()) ;
+        assertNotNull(ds.asDatasetGraph()) ;
+    }
+    
     @Test public void dataset_02()
     {
         Dataset ds = create() ;
-        //
+        ds.getDefaultModel().add(s1,p1,o1) ;
+        assertTrue(model1.isIsomorphicWith(ds.getDefaultModel())) ;
     }
-    
+
+    @Test public void datasource_01()
+    {
+        DataSource ds = create() ;
+        ds.setDefaultModel(model2) ;
+        assertTrue(model2.isIsomorphicWith(ds.getDefaultModel())) ;
+    }
+
+    @Test public void datasource_02()
+    {
+        String graphName = "http://example/" ;
+        DataSource ds = create() ;
+        ds.addNamedModel(graphName, model1) ;
+        assertTrue(ds.containsNamedModel(graphName)) ;
+        
+        List<String> x = Iter.toList(ds.listNames()) ;
+        assertEquals(1, x.size()) ;
+        assertEquals(graphName, x.get(0)) ;
+        
+        assertFalse(model1.isIsomorphicWith(ds.getDefaultModel())) ;
+        Model m = ds.getNamedModel(graphName) ;
+
+        assertNotNull(m) ;
+        assertTrue(model1.isIsomorphicWith(m)) ;
+        
+        ds.removeNamedModel(graphName) ;
+        Model m2 = ds.getNamedModel(graphName) ;
+        assertNull(m2) ;
+    }
+
+    @Test public void datasource_03()
+    {
+        String graphName = "http://example/" ;
+        DataSource ds = create() ;
+        ds.addNamedModel(graphName, model1) ;
+        ds.replaceNamedModel(graphName, model2) ;
+        assertTrue(ds.containsNamedModel(graphName)) ;
+        
+        List<String> x = Iter.toList(ds.listNames()) ;
+        assertEquals(1, x.size()) ;
+        assertEquals(graphName, x.get(0)) ;
+        
+        assertFalse(model1.isIsomorphicWith(ds.getNamedModel(graphName))) ;
+        assertTrue(model2.isIsomorphicWith(ds.getNamedModel(graphName))) ;
+    }
 }
 
 /*
