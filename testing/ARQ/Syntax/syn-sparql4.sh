dffd7964e3e8723506df04541d667de61b9ec859
Andy Seaborne <andy@apache.org>
2010-04-27 15:21:14 +0000

    git-svn-id: https://svn.apache.org/repos/asf/incubator/jena/Import/Jena-SVN/ARQ/trunk@1122677 13f79535-47bb-0310-9956-ffa450edef68

--- testing/ARQ/Syntax/syn-sparql4.sh
+++ testing/ARQ/Syntax/syn-sparql4.sh
@@ -30,6 +30,67 @@ SELECT (COUNT(*) AS ?count) {}
 EOF
 
 ## ---- Aggregates
+N=0
+
+N=$((N+1)) ; testGood $(fname "syntax-aggregate-" $N arq) <<EOF
+SELECT (COUNT(*) AS ?count) {}
+EOF
+
+N=$((N+1)) ; testGood $(fname "syntax-aggregate-" $N arq) <<EOF
+SELECT (COUNT(DISTINCT *) AS ?count) {}
+EOF
+
+N=$((N+1)) ; testGood $(fname "syntax-aggregate-" $N arq) <<EOF
+SELECT (COUNT(?x) AS ?count) {}
+EOF
+
+N=$((N+1)) ; testGood $(fname "syntax-aggregate-" $N arq) <<EOF
+SELECT (COUNT(DISTINCT ?x) AS ?count) {}
+EOF
+
+N=$((N+1)) ; testGood $(fname "syntax-aggregate-" $N arq) <<EOF
+SELECT (SUM(?x) AS ?y) {}
+EOF
+
+N=$((N+1)) ; testGood $(fname "syntax-aggregate-" $N arq) <<EOF
+SELECT (SUM(DISTINCT ?x) AS ?y) {}
+EOF
+
+N=$((N+1)) ; testGood $(fname "syntax-aggregate-" $N arq) <<EOF
+SELECT (MIN(?x) AS ?y) {}
+EOF
+
+N=$((N+1)) ; testGood $(fname "syntax-aggregate-" $N arq) <<EOF
+SELECT (MIN(DISTINCT ?x) AS ?y) {}
+EOF
+
+N=$((N+1)) ; testGood $(fname "syntax-aggregate-" $N arq) <<EOF
+SELECT (MAX(?x) AS ?y) {}
+EOF
+
+N=$((N+1)) ; testGood $(fname "syntax-aggregate-" $N arq) <<EOF
+SELECT (MAX(DISTINCT ?x) AS ?y) {}
+EOF
+
+N=$((N+1)) ; testGood $(fname "syntax-aggregate-" $N arq) <<EOF
+SELECT (AVG(?x) AS ?y) {}
+EOF
+
+N=$((N+1)) ; testGood $(fname "syntax-aggregate-" $N arq) <<EOF
+SELECT (AVG(DISTINCT ?x) AS ?y) {}
+EOF
+
+N=$((N+1)) ; testGood $(fname "syntax-aggregate-" $N arq) <<EOF
+SELECT (GROUP_CONCAT(?x) AS ?y) {}
+EOF
+
+N=$((N+1)) ; testGood $(fname "syntax-aggregate-" $N arq) <<EOF
+SELECT (GROUP_CONCAT(DISTINCT ?x) AS ?y) {}
+EOF
+
+N=$((N+1)) ; testGood $(fname "syntax-aggregate-" $N arq) <<EOF
+SELECT (GROUP_CONCAT(?x; SEPARATOR=';') AS ?y) {}
+EOF
 
 ## ---- Subquery
 
@@ -50,8 +111,12 @@ N=0
 
 N=$((N+1)) ; testBad $(fname "syn-bad-" $N) <<EOF
 SELECT (?x +?y) {}
-EOF---- 
+EOF
 
 N=$((N+1)) ; testBad $(fname "syn-bad-" $N) <<EOF
 SELECT COUNT(*) {}
+EOF
+
+N=$((N+1)) ; testBad $(fname "syn-bad-" $N) <<EOF
+SELECT (SUM(?x,?y) AS ?S) {}
 EOF
