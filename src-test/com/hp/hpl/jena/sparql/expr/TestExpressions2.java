7121ad12b35f1a3052d77c17235cfd795eaac931
Andy Seaborne <andy@apache.org>
2010-05-20 19:37:38 +0000

    git-svn-id: https://svn.apache.org/repos/asf/incubator/jena/Import/Jena-SVN/ARQ/trunk@1122764 13f79535-47bb-0310-9956-ffa450edef68

--- src-test/com/hp/hpl/jena/sparql/expr/TestExpressions2.java
+++ src-test/com/hp/hpl/jena/sparql/expr/TestExpressions2.java
@@ -10,6 +10,7 @@ import org.junit.Assert ;
 import org.junit.Test ;
 
 import com.hp.hpl.jena.sparql.expr.nodevalue.XSDFuncOp ;
+import com.hp.hpl.jena.sparql.function.FunctionEnvBase ;
 import com.hp.hpl.jena.sparql.util.ExprUtils ;
 
 /** Break expression testing suite into parts
@@ -24,7 +25,7 @@ public class TestExpressions2 extends Assert
     @Test public void gregorian_eq_02()         { eval("'1999'^^xsd:gYear != '1999'^^xsd:gYear", false) ; }
     
     @Test (expected=ExprEvalException.class)
-    public void gregorian_eq_03()               { eval("'1999'^^xsd:gYear = '1999Z'^^xsd:gYear", false) ; }
+    public void gregorian_eq_03()               { evalErr("'1999'^^xsd:gYear = '1999Z'^^xsd:gYear") ; }
 
     @Test  public void gregorian_eq_04()        { eval("'1999'^^xsd:gYear = '2001Z'^^xsd:gYear", false) ; }
     
@@ -41,21 +42,74 @@ public class TestExpressions2 extends Assert
     @Test public void gregorian_cmp_03()        { eval("'1999'^^xsd:gYear < '2000+01:00'^^xsd:gYear", true) ; }
 
     @Test (expected=ExprEvalException.class)
-    public void gregorian_cmp_04()              { eval("'1999'^^xsd:gYear < '1999+05:00'^^xsd:gYear", false) ; }
+    public void gregorian_cmp_04()              { evalErr("'1999'^^xsd:gYear < '1999+05:00'^^xsd:gYear") ; }
     
     public void gregorian_cast_01()             { eval("xsd:gYear('2010-03-22'^^xsd:date) = '2010'^^xsd:gYear", true ) ; }
 
     @Test (expected=ExprEvalException.class)
-    public void coalesce_01()                   { eval("COALESCE()", true) ; } 
+    public void coalesce_01()                   { evalErr("COALESCE()") ; } 
     @Test public void coalesce_02()             { eval("COALESCE(1) = 1", true) ; } 
     @Test public void coalesce_03()             { eval("COALESCE(?x,1) = 1", true) ; } 
     @Test public void coalesce_04()             { eval("COALESCE(9,1) = 9", true) ; } 
+    
+    // IF
+    @Test public void if_01()                   { eval("IF(1+2=3, 'yes', 'no') = 'yes'", true) ; }
+    @Test public void if_02()                   { eval("IF(1+2=4, 'yes', 'no') = 'no'", true) ; }
+    @Test public void if_03()                   { eval("IF(true, 'yes', 1/0) = 'yes'", true) ; }
+    @Test (expected=ExprEvalException.class)
+    public void if_04()                         { evalErr("IF(true, 1/0, 'no') = 'no'") ; }
+    
+    
+    // NOT IN, IN
+    @Test public void in_01()                   { eval("1 IN(1,2,3)", true) ; }
+    @Test public void in_02()                   { eval("1 IN(<x>,2,1)", true) ; }
+    @Test public void in_03()                   { eval("1 IN()", false) ; }
+    @Test public void in_04()                   { eval("1 IN(7)", false) ; }
+
+    @Test public void not_in_01()                   { eval("1 NOT IN(1,2,3)", false) ; }
+    @Test public void not_in_02()                   { eval("1 NOT IN(<x>,2,1)", false) ; }
+    @Test public void not_in_03()                   { eval("1 NOT IN()", true) ; }
+    @Test public void not_in_04()                   { eval("1 NOT IN(7)", true) ; }
+    
+    @Test public void term_constructor_iri_01()     { eval("IRI('http://example/') = <http://example/>", true) ; }
+    
+    @Test (expected=ExprEvalException.class)
+    public void term_constructor_iri_02()           { evalErr("IRI(123)") ; } 
+    @Test public void term_constructor_iri_03()     { eval("IRI(<http://example/>) = <http://example/>", true) ; }
+    @Test public void term_constructor_iri_04()     { eval("isIRI(IRI(BNODE()))", true) ; }            // SPARQL extension
+    @Test public void term_constructor_iri_05()     { eval("regex(str(IRI(BNODE())), '^_:' )", true) ; } // SPARQL extension
+
+    @Test  public void term_constructor_bnode_01()  { eval("isBlank(BNODE())", true) ; }
+    @Test public void term_constructor_bnode_02()   { eval("isBlank(BNODE('abc'))", true) ; }
+    @Test public void term_constructor_bnode_03()   { eval("isBlank(BNODE('abc'))", true) ; }
+    @Test public void term_constructor_bnode_04()   { eval("BNODE('abc') = BNODE('abc')", true) ; }
+    @Test public void term_constructor_bnode_05()   { eval("BNODE('abc') = BNODE('def')", false) ; }
+
+    @Test public void term_constructor_strdt_01()   { eval("STRDT('123',xsd:integer) = 123", true) ; }
+    @Test public void term_constructor_strdt_02()   { eval("STRDT('123',<http://example/DT>) = '123'^^<http://example/DT>", true) ; }
+    @Test (expected=ExprEvalException.class)
+    public void term_constructor_strdt_03()         { evalErr("STRDT('123','abc') = '123'") ; }
+    @Test (expected=ExprEvalException.class)
+    public void term_constructor_strdt_04()         { evalErr("STRDT('123'^^xsd:integer,<http://example/DT>) = '123'^^<http://example/DT>") ; }
+    
+    @Test public void term_constructor_strlang_01() { eval("STRLANG('abc', 'en') = 'abc'@en", true) ; }
+    @Test (expected=ExprEvalException.class)
+    public void term_constructor_strlang_02()       { evalErr("STRLANG(<http://example/>, 'en') = 'abc'@en") ; }
+
+    @Test (expected=ExprEvalException.class)
+    public void term_constructor_strlang_03()       { evalErr("STRLANG('abc'@en, 'en') = 'abc'@en") ; }
+    
+    private static void evalErr(String string)
+    { 
+        eval(string, true) ;
+        throw new RuntimeException() ; 
+    }
     
     // It's easier to write tests that simple are expected to return true/false 
     private static void eval(String string, boolean result)
     {
         Expr expr = ExprUtils.parse(string) ;
-        NodeValue nv = expr.eval(null, null) ;
+        NodeValue nv = expr.eval(null, FunctionEnvBase.createTest()) ;
         boolean b = XSDFuncOp.booleanEffectiveValue(nv) ;
         assertEquals(string, result, b) ;
     }
