57cfb1e917143d0ece90004ae198b1492dbb7ba1
Andy Seaborne <andy@apache.org>
2010-09-08 20:45:00 +0000

    git-svn-id: https://svn.apache.org/repos/asf/incubator/jena/Import/Jena-SVN/ARQ/trunk@1123120 13f79535-47bb-0310-9956-ffa450edef68

--- src/com/hp/hpl/jena/sparql/expr/E_Regex.java
+++ src/com/hp/hpl/jena/sparql/expr/E_Regex.java
@@ -120,18 +120,13 @@ public class E_Regex extends ExprFunction3
     }
 
     /** @return Returns the expr of the regex */
-    public final Expr getRegexExpr() { return expr2 ; }
+    public final Expr getRegexExpr() { return expr1 ; }
+
+    /** @return Returns the pattern. */
+    public final Expr getPattern()  { return expr2 ; }
 
     /** @return Returns the flags. */
     public final Expr getFlags() { return expr3 ; }
-
-    /**
-     * @return Returns the pattern.
-     */
-    public final Expr getPattern()
-    {
-        return expr1 ;
-    }
 
     @Override
     public Expr copy(Expr arg1, Expr arg2, Expr arg3)
