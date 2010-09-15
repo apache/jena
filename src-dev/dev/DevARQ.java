1c7b2f2207fed42578cf24e9db931a5641b8e24d
Andy Seaborne <andy@apache.org>
2010-09-15 20:36:45 +0000

    git-svn-id: https://svn.apache.org/repos/asf/incubator/jena/Import/Jena-SVN/ARQ/trunk@1123136 13f79535-47bb-0310-9956-ffa450edef68

--- src-dev/dev/DevARQ.java
+++ src-dev/dev/DevARQ.java
@@ -4,6 +4,7 @@ package dev ;
 public class DevARQ
 {
     // Result set comparison, not by graph isomorphism, and to include value based testing.
+    // ResultSetCompare
     
     // RIOT: cmd line : bad parse -> ???
     // ReportRemoteVirtuoso: StAX does not parse - times out too early? 
