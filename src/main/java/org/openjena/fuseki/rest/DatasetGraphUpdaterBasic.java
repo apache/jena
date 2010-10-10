345216daf5b16e1351db2180083773912b75f5da
Andy Seaborne <andy@apache.org>
2010-10-10 16:06:00 +0000

    git-svn-id: https://svn.apache.org/repos/asf/incubator/jena/Import/Jena-SVN/Fuseki/trunk@1123274 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/main/java/org/openjena/fuseki/rest/DatasetGraphUpdaterBasic.java
@@ -0,0 +1,134 @@
+/*
+ * (c) Copyright 2010 Talis Information Ltd.
+ * (c) Copyright 2010 Epimorphics Ltd.
+ * All rights reserved.
+ * [See end of file]
+ */
+
+package org.openjena.fuseki.rest;
+
+
+import com.hp.hpl.jena.graph.Graph ;
+import com.hp.hpl.jena.graph.Node ;
+import com.hp.hpl.jena.sparql.core.DatasetGraph ;
+
+/** 
+ * General implementation of operations for the SPARQL HTTP Update protocol 
+ */
+public class DatasetGraphUpdaterBasic implements DatasetGraphUpdater
+{
+    private DatasetGraph dataset ;
+    
+    public DatasetGraphUpdaterBasic(DatasetGraph dataset)
+    {
+        this.dataset = dataset ;
+    }
+    
+    @Override
+    public Graph httpGet()                    { return dataset.getDefaultGraph() ; }
+    
+    @Override
+    public Graph httpGet(Node graphName)      { return dataset.getGraph(graphName) ; }
+    
+    @Override
+    public void httpPut(Graph data) 
+    {  
+        putGraph(dataset.getDefaultGraph(), data) ;
+    }
+    
+    @Override
+    public void httpPut(Node graphName, Graph data)
+    {
+        Graph ng = dataset.getGraph(graphName) ;
+        if ( ng == null )
+            dataset.addGraph(graphName, ng) ;
+        else
+            putGraph(ng, data) ;
+    }
+
+    @Override
+    public void httpDelete()
+    {
+        clearGraph(dataset.getDefaultGraph()) ;
+    }
+    
+    @Override
+    public void httpDelete(Node graphName)
+    {
+        Graph ng = dataset.getGraph(graphName) ;
+        if ( ng == null )
+            return ;
+        dataset.removeGraph(graphName) ;
+        //clearGraph(ng) ;
+    }
+
+    @Override
+    public void httpPost(Graph data)
+    {
+        mergeGraph(dataset.getDefaultGraph(), data) ;
+    }
+    
+    @Override
+    public void httpPost(Node graphName, Graph data)
+    {
+        Graph ng = dataset.getGraph(graphName) ;
+        if ( ng == null )
+        {
+            dataset.addGraph(graphName, data) ;
+            return ;
+        }
+        mergeGraph(ng, data) ;
+    }
+
+    @Override
+    public void httpPatch(Graph data) {  httpPost(data) ; }
+    
+    @Override
+    public void httpPatch(Node graphName, Graph data) {  httpPost(graphName, data) ;}
+
+    private void putGraph(Graph destGraph, Graph data)
+    {
+        clearGraph(destGraph) ;
+        mergeGraph(destGraph, data) ; 
+    }
+
+    private void clearGraph(Graph graph)
+    {
+        if ( ! graph.isEmpty() )
+            graph.getBulkUpdateHandler().removeAll() ;
+    }
+
+    private void mergeGraph(Graph graph, Graph data)
+    {
+        graph.getBulkUpdateHandler().add(data) ;
+    }
+
+}
+
+/*
+ * (c) Copyright 2010 Talis Information Ltd.
+ * (c) Copyright 2010 Epimorphics Ltd.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. The name of the author may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
---
+++ src/main/java/org/openjena/fuseki/rest/UpdateRemote.java
@@ -0,0 +1,98 @@
+/*
+ * (c) Copyright 2010 Epimorphics Ltd.
+ * All rights reserved.
+ * [See end of file]
+ */
+
+package org.openjena.fuseki.rest;
+
+import java.io.ByteArrayOutputStream ;
+import java.io.IOException ;
+
+import org.apache.http.HttpResponse ;
+import org.apache.http.client.HttpClient ;
+import org.apache.http.client.methods.HttpPost ;
+import org.apache.http.entity.AbstractHttpEntity ;
+import org.apache.http.entity.ByteArrayEntity ;
+import org.apache.http.impl.client.DefaultHttpClient ;
+import org.apache.http.protocol.HTTP ;
+import org.eclipse.jetty.http.HttpStatus ;
+import org.openjena.atlas.io.IndentedWriter ;
+import org.openjena.riot.WebContent ;
+
+import com.hp.hpl.jena.sparql.modify.request.Target ;
+import com.hp.hpl.jena.sparql.modify.request.UpdateDrop ;
+import com.hp.hpl.jena.sparql.modify.request.UpdateWriter ;
+import com.hp.hpl.jena.update.Update ;
+import com.hp.hpl.jena.update.UpdateException ;
+import com.hp.hpl.jena.update.UpdateRequest ;
+
+public class UpdateRemote
+{
+    /** Reset the remote repository by execution a "DROP ALL" command on it */
+    public static void executeClear(String serviceURL)
+    {
+        Update clear = new UpdateDrop(Target.ALL) ;
+        UpdateRemote.execute(clear, serviceURL) ;
+    }
+    
+    public static void execute(Update request, String serviceURL)
+    {
+        execute(new UpdateRequest(request) , serviceURL) ;
+    }
+    
+    public static void execute(UpdateRequest request, String serviceURL)
+    {
+        HttpPost httpPost = new HttpPost(serviceURL) ;
+        ByteArrayOutputStream b_out = new ByteArrayOutputStream() ;
+        IndentedWriter out = new IndentedWriter(b_out) ; 
+        UpdateWriter.output(request, out) ;
+        out.flush() ;
+        byte[] bytes = b_out.toByteArray() ;
+        AbstractHttpEntity reqEntity = new ByteArrayEntity(bytes) ;
+        reqEntity.setContentType(WebContent.contentSPARQLUpdate) ;
+        reqEntity.setContentEncoding(HTTP.UTF_8) ;
+        httpPost.setEntity(reqEntity) ;
+        HttpClient httpclient = new DefaultHttpClient() ;
+
+        try
+        {
+            HttpResponse response = httpclient.execute(httpPost) ;
+            int responseCode = response.getStatusLine().getStatusCode() ;
+            String responseMessage = response.getStatusLine().getReasonPhrase() ;
+            if ( responseCode != HttpStatus.OK_200 )
+                throw new UpdateException(responseCode+" "+responseMessage) ;
+        } catch (IOException ex)
+        {
+            throw new UpdateException(ex) ;
+        }
+            
+    }
+}
+
+/*
+ * (c) Copyright 2010 Epimorphics Ltd.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. The name of the author may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
