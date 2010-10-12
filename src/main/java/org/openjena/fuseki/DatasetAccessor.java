e0cf95480227bf0967413fa90b9b70ab9a00fe84
Andy Seaborne <andy@apache.org>
2010-10-12 11:28:48 +0000

    git-svn-id: https://svn.apache.org/repos/asf/incubator/jena/Import/Jena-SVN/Fuseki/trunk@1123307 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/main/java/org/openjena/fuseki/DatasetAccessor.java
@@ -0,0 +1,69 @@
+/*
+ * (c) Copyright 2010 Epimorphics Ltd.
+ * All rights reserved.
+ * [See end of file]
+ */
+
+package org.openjena.fuseki;
+
+import com.hp.hpl.jena.rdf.model.Model ;
+
+// Accessor to a datset a a collection of graps.
+public interface DatasetAccessor
+{
+    /** Get the default model of a Dataset */
+    public Model getModel() ; 
+    /** Get a named model of a Dataset */
+    public Model getModel(String graphUri) ;
+
+//    /** Does the Dataset contain a default graph? */
+//    public boolean containsDefault() ;
+
+    /** Does the Dataset contain a named graph? */
+    public boolean containsModel(String graphURI) ;
+    
+    /** Put (replace) the default model of a Dataset */
+    public void putModel(Model data) ;
+    /** Put (create/replace) a named model of a Dataset */
+    public void putModel(String graphUri, Model data) ;
+
+    /** Delete (which means clear) the default model of a Dataset */
+    public void deleteDefault() ;
+    /** Delete a named model of a Dataset */
+    public void deleteModel(String graphUri) ;
+
+//    /** Clear the default graph, delete all the named models */
+//    public void reset() ;
+
+    /** Add statements to the default model of a Dataset */
+    public void add(Model data) ;
+    /** Add statements to a named model of a Dataset */
+    public void add(String graphUri, Model data) ;
+}
+
+/*
+ * (c) Copyright 2010 Epimorphics Ltd.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. The name of the author may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
