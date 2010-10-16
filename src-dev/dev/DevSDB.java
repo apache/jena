1878b80eda9294a9183eb2444f3cece2ea0406f4
Andy Seaborne <andy@apache.org>
2010-10-16 22:01:48 +0000

    git-svn-id: https://svn.apache.org/repos/asf/incubator/jena/Import/Jena-SVN/SDB/trunk@1123340 13f79535-47bb-0310-9956-ffa450edef68

--- src-dev/dev/DevSDB.java
+++ src-dev/dev/DevSDB.java
@@ -10,15 +10,15 @@ public class DevSDB
 {
     // Check named models for union graph work.
     
-    // Drop suport for SQL server 2000 - use nvarchar(max) not ntext  
-    
+    // Drop support for SQL server 2000
+    // - use nvarchar(max) not ntext
+    // - use TOP 
+
     // Scope and Join bug.
     // Need to clear constant calculation across left and right in a join.
     // Temporary fix applied (TransformSDB.transform(OpJoin) does not combine SQL)
     
     // ListSubjects etc - QueryHandlerSDB
-    
-    // Activate SDBRequest.LimitOffsetTranslation
     
 	// javadoc is javadoc all!
 
@@ -29,7 +29,6 @@ public class DevSDB
     //   Update in line with ARQ
     // Jars?
     
-    
 	// MySQL cursors: jdbc:mysql://127.0.0.1/rdswip?useCursorFetch=true&defaultFetchSize=8192&profileSQL=false&enableQueryTimeouts=false&netTimeoutForStreamingResults=0 
 
     // Slot compilation / index form. 
@@ -37,12 +36,4 @@ public class DevSDB
     // unit is compiled.  Currently, can end up with multiple lookups of the same thing (but they will be
     // cached in the DB but if not, the query is very expensive anyway and an extra lookup will not
     // add obseravble cost).
-    
-    // QueryEngineSDB.init ; enable some static optimizations.
-    
-    // Testing: SDBTestAll does not include the model tests yet because they are not linked to the store description files
-    // ParamAllStores for JUnit paramterized tests approach.
-    
-    // For DISTINCT do in a subquery on the node ids.
-    // Similarly, push in the PROJECT to the node ids phase. 
 }
