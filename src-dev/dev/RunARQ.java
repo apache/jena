9db21c43df63a49b49665cade07263c7de58397b
Andy Seaborne <andy@apache.org>
2010-11-24 22:23:17 +0000

    git-svn-id: https://svn.apache.org/repos/asf/incubator/jena/Import/Jena-SVN/ARQ/trunk@1123513 13f79535-47bb-0310-9956-ffa450edef68

--- src-dev/dev/RunARQ.java
+++ src-dev/dev/RunARQ.java
@@ -31,7 +31,6 @@ import com.hp.hpl.jena.query.QueryExecutionFactory ;
 import com.hp.hpl.jena.query.QueryFactory ;
 import com.hp.hpl.jena.query.QuerySolutionMap ;
 import com.hp.hpl.jena.query.ResultSetFormatter ;
-import com.hp.hpl.jena.query.Syntax ;
 import com.hp.hpl.jena.rdf.model.Model ;
 import com.hp.hpl.jena.sparql.algebra.Algebra ;
 import com.hp.hpl.jena.sparql.algebra.Op ;
@@ -81,14 +80,8 @@ public class RunARQ
     
     public static void main(String[] argv) throws Exception
     {
-        
-        String x = ""+1+2 ;
-        System.out.println(x) ;
-        
-        UpdateRequest req = 
-            UpdateFactory.read("testing/ARQ/Syntax/Syntax-SPARQL-Update/syntax-update-bad-09.ru", Syntax.syntaxSPARQL_11) ; 
-        System.out.println(req) ;
-        System.exit(0) ;
+
+        riotcmd.riot.main("D.nt") ; System.exit(0) ;
         
         arq.qparse.main("-query=Q.rq") ; System.exit(0) ;
         
