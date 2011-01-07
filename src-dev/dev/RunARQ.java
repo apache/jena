3865d2d7f3ad3a62aeb31f9532eac66446936908
Andy Seaborne <andy@apache.org>
2011-01-07 16:28:54 +0000

    git-svn-id: https://svn.apache.org/repos/asf/incubator/jena/Import/Jena-SVN/ARQ/trunk@1123675 13f79535-47bb-0310-9956-ffa450edef68

--- src-dev/dev/RunARQ.java
+++ src-dev/dev/RunARQ.java
@@ -11,7 +11,6 @@ package dev;
 import java.util.Iterator ;
 import java.util.concurrent.ArrayBlockingQueue ;
 import java.util.concurrent.BlockingQueue ;
-import java.util.concurrent.Callable ;
 import java.util.concurrent.ExecutorService ;
 import java.util.concurrent.Executors ;
 
