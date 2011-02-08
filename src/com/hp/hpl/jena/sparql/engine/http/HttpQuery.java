6f3a4325ee97bfdc6f0942e29e973e4bec400f29
Andy Seaborne <andy@apache.org>
2011-02-08 13:39:42 +0000

    git-svn-id: https://svn.apache.org/repos/asf/incubator/jena/Import/Jena-SVN/ARQ/trunk@1123770 13f79535-47bb-0310-9956-ffa450edef68

--- src/com/hp/hpl/jena/sparql/engine/http/HttpQuery.java
+++ src/com/hp/hpl/jena/sparql/engine/http/HttpQuery.java
@@ -5,7 +5,6 @@
 
 package com.hp.hpl.jena.sparql.engine.http;
 
-import java.io.BufferedInputStream ;
 import java.io.ByteArrayInputStream ;
 import java.io.IOException ;
 import java.io.InputStream ;
