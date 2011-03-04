c4a6b8aa4bbb85e32d03a1372b01c1fc043f24b0
Andy Seaborne <andy@apache.org>
2011-03-04 11:44:36 +0000

    git-svn-id: https://svn.apache.org/repos/asf/incubator/jena/Import/Jena-SVN/SDB/trunk@1123848 13f79535-47bb-0310-9956-ffa450edef68

--- src/com/hp/hpl/jena/sdb/compiler/TransformSliceProject.java
+++ src/com/hp/hpl/jena/sdb/compiler/TransformSliceProject.java
@@ -1,5 +1,5 @@
 /*
- * (c) Copyright 2009 Talis Information Ltd
+ * (c) Copyright 2009 Talis Systems Ltd.
  * All rights reserved.
  * [See end of file]
  */
@@ -32,7 +32,7 @@ public class TransformSliceProject extends TransformCopy
 }
 
 /*
- * (c) Copyright 2009 Talis Information Ltd
+ * (c) Copyright 2009 Talis Systems Ltd.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
