1e0dcd546a73b52e474bc7ebb74f897b80270ba9
Andy Seaborne <andy@apache.org>
2011-03-04 11:44:36 +0000

    git-svn-id: https://svn.apache.org/repos/asf/incubator/jena/Import/Jena-SVN/TDB/trunk@1123848 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/com/hp/hpl/jena/tdb/store/bulkloader/Destination.java
+++ src/main/java/com/hp/hpl/jena/tdb/store/bulkloader/Destination.java
@@ -1,5 +1,5 @@
 /*
- * (c) Copyright 2010 Talis Information Ltd
+ * (c) Copyright 2010 Talis Systems Ltd
  * All rights reserved.
  * [See end of file]
  */
@@ -16,7 +16,7 @@ public interface Destination<T> extends Sink<T>
 }
 
 /*
- * (c) Copyright 2010 Talis Information Ltd
+ * (c) Copyright 2010 Talis Systems Ltd
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
