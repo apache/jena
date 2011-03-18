e7c611a24fe40bc26a3783d07833525582c41016
Andy Seaborne <andy@apache.org>
2011-03-18 15:04:08 +0000

    git-svn-id: https://svn.apache.org/repos/asf/incubator/jena/Import/Jena-SVN/TDB/trunk@1123897 13f79535-47bb-0310-9956-ffa450edef68

--- src-dev/setup/DatasetBuilderStd.java
+++ src-dev/setup/DatasetBuilderStd.java
@@ -53,9 +53,11 @@ import com.hp.hpl.jena.tdb.sys.SystemTDB ;
 
 public class DatasetBuilderStd implements DatasetBuilder
 {
+    // TODO Testing (inc variations)
     // TODO Directory properties initial config.
     // TODO Separate out static for general creation purposes (a super factory)
     // TODO setting union graph?  Careful - updates!
+    // TODO Set direct/mapped modes 
     
     private static final Logger log = TDB.logInfo ;
     private static DatasetBuilderStd singleton ;
