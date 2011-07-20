9d5f6c7d6c90e6161342f6e270aad23d005757f6
Andy Seaborne <andy@apache.org>
2011-07-20 14:30:44 +0000

    Move tests for Reifier2 into ARQ
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jena/Experimental/TxTDB/trunk@1148789 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/java/com/hp/hpl/jena/tdb/graph/TS_Graph.java
+++ src/test/java/com/hp/hpl/jena/tdb/graph/TS_Graph.java
@@ -11,8 +11,7 @@ import org.junit.runners.Suite ;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses( {
-    TestReifier2.class
-    , TestPrefixMappingTDB.class
+    TestPrefixMappingTDB.class
     , TestBulkUpdateTDB.class
     , TestGraphsTDB.class
     , TestDatasetGraphTDB.class
