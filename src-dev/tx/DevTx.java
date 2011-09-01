0570fc70863af3578cd191b7b1d87f872a887021
Andy Seaborne <andy@apache.org>
2011-09-01 09:18:50 +0000

    git-svn-id: https://svn.apache.org/repos/asf/incubator/jena/Experimental/TxTDB/trunk@1163958 13f79535-47bb-0310-9956-ffa450edef68

--- src-dev/tx/DevTx.java
+++ src-dev/tx/DevTx.java
@@ -3,11 +3,7 @@ package tx;
 
 public class DevTx
 {
-    // ----
-    // Make sure that transactional object can't be used after abort/commit/close.
-    // 1 -- Root is not root! (overlapping reads?) [done]
-    // 2 -- BulkUpdateHandlerTDB.removeAll [failed to reproduce]
-    // Don't requite .close for write.
+    // Don't require .close for write.
     
     // --------
     // * Version per dataset
