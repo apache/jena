0b3d1a55d98ce4675a37f2c80141eae1c30dbb33
Paolo Castagna <castagna@apache.org>
2011-09-11 21:01:49 +0000

    JENA-111
    Uncommented (and fixed) valid test.
    
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jena/Jena2/ARQ/trunk@1169539 13f79535-47bb-0310-9956-ffa450edef68

--- src-test/com/hp/hpl/jena/sparql/algebra/optimize/TestOptimizer.java
+++ src-test/com/hp/hpl/jena/sparql/algebra/optimize/TestOptimizer.java
@@ -138,18 +138,6 @@ public class TestOptimizer extends BaseTest
         check(queryString, opExpectedString) ;
     }
 
-//    @Test public void slice_order_to_topn_01a()
-//    {
-//        assertTrue(ARQ.isTrueOrUndef(ARQ.optTopNSorting)) ;
-//        String queryString = "SELECT ?s { ?s ?p ?o } ORDER BY ?p ?o LIMIT 42"  ;  
-//        String opExpectedString = 
-//            "(slice _ 42\n" +
-//            "  (project (?s)\n" + 
-//            "    (top (42 ?p ?o)\n" + 
-//            "      (bgp (triple ?s ?p ?o)))))" ; 
-//        check(queryString, opExpectedString) ;
-//    }
-
     @Test public void slice_order_to_topn_02()
     {
         assertTrue(ARQ.isTrueOrUndef(ARQ.optTopNSorting)) ;
@@ -268,7 +256,18 @@ public class TestOptimizer extends BaseTest
             "      (bgp (triple ?s ?p ?o)))))" ; 
         check(queryString, opExpectedString) ;
     }
-    
+
+    @Test public void slice_order_to_topn_12()
+    {
+        assertTrue(ARQ.isTrueOrUndef(ARQ.optTopNSorting)) ;
+        String queryString = "SELECT ?s { ?s ?p ?o } ORDER BY ?p ?o LIMIT 42"  ;  
+        String opExpectedString = 
+            "(project (?s)\n" + 
+            "  (top (42 ?p ?o)\n" + 
+            "    (bgp (triple ?s ?p ?o))))" ; 
+        check(queryString, opExpectedString) ;
+    }
+
     @Test public void distinct_to_reduced_01()
     {
         assertTrue(ARQ.isTrueOrUndef(ARQ.optDistinctToReduced)) ;
