b18a43942baf1e2c011928a5a14d370e07e73001
Andy Seaborne <andy@apache.org>
2011-12-06 14:39:10 +0000

    Remove old, used stuff
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jena/Jena2/jena/trunk@1210945 13f79535-47bb-0310-9956-ffa450edef68

--- test.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-@REM Fix me.
-## set CP=lib/slf4j-api-1.5.6.jar;lib/slf4j-log4j12-1.5.6.jar;lib/icu4j_3_4.jar;lib/iri.jar;lib/jena.jar;lib/jenatest.jar;lib/json.jar;lib/junit-4.5.jar;lib/log4j-1.2.12.jar;lib/lucene-core-2.3.1.jar;lib/stax-api-1.0.jar;lib/wstx-asl-3.0.0.jar;lib/xercesImpl.jar
-#Java 6.
-set CP=lib/*
-
-java -classpath %CP% junit.textui.TestRunner com.hp.hpl.jena.test.TestPackage
