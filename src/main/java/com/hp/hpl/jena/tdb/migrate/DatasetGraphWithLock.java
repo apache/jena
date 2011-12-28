a9680603860e59b9b6fa2713a6220cccbe95916a
Andy Seaborne <andy@apache.org>
2011-12-28 22:10:26 +0000

    git-svn-id: https://svn.apache.org/repos/asf/incubator/jena/Jena2/TDB/trunk@1225358 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/main/java/com/hp/hpl/jena/tdb/migrate/DatasetGraphWithLock.java
@@ -0,0 +1,107 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.hp.hpl.jena.tdb.migrate;
+
+import com.hp.hpl.jena.query.ReadWrite ;
+import com.hp.hpl.jena.shared.JenaException ;
+import com.hp.hpl.jena.sparql.core.DatasetGraph ;
+
+/** A DatasetGraph that uses the dataset lock to give wek transactional behaviour.
+ *  Only supports multiple-reader OR single-writer, and no transction abort.
+ *  Transactions are not durable. 
+ */
+public class DatasetGraphWithLock extends DatasetGraphTrackActive 
+{
+    static class JenaLockException extends JenaException
+    {
+        public JenaLockException()                                  { super(); }
+        public JenaLockException(String message)                    { super(message); }
+        public JenaLockException(Throwable cause)                   { super(cause) ; }
+        public JenaLockException(String message, Throwable cause)   { super(message, cause) ; }
+    }
+    
+    private DatasetGraph dsg = null ;
+    private boolean locked ;
+    
+
+    public DatasetGraphWithLock(DatasetGraph dsg)
+    {
+        this.dsg = dsg ;
+        this.locked = false ;
+    }
+
+    @Override
+    protected DatasetGraph get()
+    {
+        return dsg ;
+    }
+
+    @Override
+    protected void checkActive()
+    {
+        if ( ! isInTransaction() )
+            throw new JenaLockException("Not in a locked region") ;
+    }
+
+    @Override
+    protected void checkNotActive()
+    {
+        if ( isInTransaction() )
+            throw new JenaLockException("Currently in a locked region") ;
+    }
+
+    @Override
+    protected void _begin(ReadWrite readWrite)
+    {
+        boolean b = ( readWrite == ReadWrite.READ ) ;
+        dsg.getLock().enterCriticalSection(b) ;
+        locked = true ;
+    }
+
+    @Override
+    protected void _commit()
+    {
+        locked = false ;
+        dsg.getLock().leaveCriticalSection() ;
+    }
+
+    @Override
+    protected void _abort()
+    {
+        locked = false ;
+        throw new JenaLockException("Can't abort a locked update") ;   
+        //dsg.getLock().leaveCriticalSection() ;
+    }
+
+    @Override
+    protected void _end()
+    {
+        if ( locked )
+            dsg.getLock().leaveCriticalSection() ;
+        locked = false ;
+    }
+
+    @Override
+    protected void _close()
+    {
+        if ( dsg != null )
+            dsg.close() ;
+        dsg = null ;
+    }
+}
