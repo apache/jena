a525e5e615dc01e62e03dadbd94a16963a706853
Andy Seaborne <andy@apache.org>
2012-03-23 14:05:51 +0000

    Tidy up UTF-8 encoding/decoding code.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jena/Jena2/ARQ/trunk@1304354 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/openjena/atlas/io/BlockUTF8.java
+++ src/main/java/org/openjena/atlas/io/BlockUTF8.java
@@ -22,7 +22,7 @@ import java.io.IOException ;
 import java.nio.ByteBuffer ;
 import java.nio.CharBuffer ;
 
-import org.openjena.atlas.AtlasException ;
+import org.openjena.atlas.lib.NotImplemented ;
 
 /**
  * Convert between bytes and chars, UTF-8 only.
@@ -52,270 +52,7 @@ public class BlockUTF8
     // and byte[] <-> char[]
 
     // It seems that chars -> bytes (on <100char strings) is faster with BlockUTF8
-    // but the conversion from bytes to string is faster with decoders (not by much though). 
-    
-    private static final Convert converter = new ConvertUTF8() ;
-    private static final Convert asciiConvert = new ConvertAscii() ;
-
-    /** Convert the bytes in the ByteBuffer to characters in the CharBuffer.
-     * The CharBuffer must be large enough. 
-     */
-    public static void toChars(ByteBuffer bb, CharBuffer cb)
-    {
-        //if ( bb.hasArray() && cb.hasArray() )
-            
-        
-        int len = bb.remaining() ;
-
-        for (int i = 0; i < len; )
-        {
-            //i += converter.convertBytesToChar(bb, cb) ;
-            i += _convertBytesToChar(bb, cb) ;
-        }
-    }
-    
-    /** Make a string from UTF-8 bytes in a ByteBuffer */ 
-    public static String toString(ByteBuffer bb)
-    {
-        // I think that the copy from some mutable collector to immutable string is inevitable in java.  
-        int len = bb.remaining() ;
-        CharBuffer cb = CharBuffer.allocate(len) ;
-        toChars(bb, cb) ;
-        return new String(cb.array(), 0, cb.position()) ;
-    }
-
-    /** Convert characters to UTF-8 bytes in the ByteBuffer.
-     * The ByteBuffer must be large enough. 
-     */
-    public static void fromChars(CharSequence cs, ByteBuffer bb)
-    {
-        int len = cs.length() ;
-        for (int i = 0; i < len; i++)
-        {
-            char c = cs.charAt(i) ;
-            //converter.convertCharToBytes(c, bb) ;
-            _convertCharToBytes(c, bb) ;
-        }
-    }
-    
-    /** Convert charcaters to UTF-8 bytes in the ByteBuffer.
-     * The ByteBuffer must be large enough. 
-     */
-    public static void fromChars(CharBuffer cb, ByteBuffer bb)
-    {
-        //if ( bb.hasArray() && cb.hasArray() )
-
-        // CharBuffers are CharSequences but charAt(i) adds a layer of work.
-        int len = cb.remaining() ;
-        for (int i = 0; i < len; i++)
-        {
-            char c = cb.get() ;
-            converter.convertCharToBytes(c, bb) ;
-        }
-    }
-
-    private interface Convert
-    {
-        /** Return number of bytes consumed */
-        int convertBytesToChar(ByteBuffer bb, CharBuffer cb) ;
-
-        /** Return number of bytes produced */
-        int convertCharToBytes(char c, ByteBuffer bb) ;
-    }
-
-    // ASCII
-    
-    private static final class ConvertAscii implements Convert
-    {
-        @Override
-        public int convertBytesToChar(ByteBuffer bb, CharBuffer cb)
-        {
-            byte b = bb.get() ;
-            // ASCII
-            char c = (char)b ;
-            cb.put(c) ;
-            return 1 ;
-        }
-
-        @Override
-        public int convertCharToBytes(char c, ByteBuffer bb)
-        {
-            byte b = (byte)(c | 0xFF) ;
-            bb.put(b) ;
-            return 1 ;
-        }
-    }
-    
-    private static final class ConvertUTF8 implements Convert
-    {
-        @Override
-        public final int convertBytesToChar(ByteBuffer bb, CharBuffer cb)
-        {
-            return _convertBytesToChar(bb, cb) ;
-        }
-        
-        @Override
-        public int convertCharToBytes(char ch, ByteBuffer bb)
-        {
-            return _convertCharToBytes(ch, bb) ;
-        }
-    }
-
-    static int _convertBytesToChar(ByteBuffer bb, CharBuffer cb)
-    {
-        int x = bb.get() ;
-        if ( x > 0 && x <= 127 )
-        {
-            cb.put((char)x) ;
-            return 1 ;
-        }
-        
-        if ( x == 0 )
-        {
-            // Pass through a null byte as the nul character (illegal Unicode).
-            cb.put((char)x) ;
-            return 1 ;
-        }
-            
-
-        // 10 => extension byte
-        // 110..... => 2 bytes
-        if ( (x & 0xE0) == 0xC0 )
-        {
-//          // Unwind.
-//          int ch = readMultiBytes(bb, x & 0x1F, 2) ;
-            int x2 = bb.get() ;
-            if ( (x2 & 0xC0) != 0x80 )
-                //throw new AtlasException("Illegal UTF-8 processing character "+count+": "+x2) ;
-                throw new AtlasException(String.format("Illegal UTF-8 processing character: 0x%04X",x2)) ;
-            // 6 bits of x2
-            int ch = ( (x&0x1F) << 6) | (x2 & 0x3F); 
-            cb.put((char)ch) ;
-            return 2 ;
-
-        }
-        //  1110.... => 3 bytes : 16 bits : not outside 16bit chars 
-        if ( (x & 0xF0) == 0xE0 ) 
-        {
-            int ch = readMultiBytes(bb, x & 0x0F, 3) ;
-            cb.put((char)ch) ;
-            return 3 ;
-        }
-
-        // Looking like 4 byte charcater.
-        // 11110zzz => 4 bytes.
-        if ( (x & 0xF8) == 0xF0 )
-        {
-            int ch = readMultiBytes(bb, x & 0x08, 4) ;
-            char chars[] = Character.toChars(ch) ;
-            cb.put(chars) ;
-            return 4 ;
-        }
-        else 
-        {
-            IO.exception(new IOException("Illegal UTF-8: "+x)) ;
-            return -1 ;
-        }
-
-//            // This test will go off.  We're processing a 4 byte sequence but Java only supports 16 bit chars. 
-//            if ( ch > Character.MAX_VALUE )
-//                throw new AtlasException("Out of range character (must use a surrogate pair)") ;
-//            if ( ! Character.isDefined(ch) ) throw new AtlasException(String.format("Undefined codepoint: 0x%04X", ch)) ;
-//            return ch ;
-    }
-
-    private static int readMultiBytes(ByteBuffer input, int start, int len) //throws IOException
-    {
-        int x = start ;
-        for ( int i = 0 ; i < len-1 ; i++ )
-        {
-            int x2 = input.get() ;
-            if ( x2 == -1 )
-                throw new AtlasException("Premature end to UTF-8 sequence at end of input") ;
-            
-            if ( (x2 & 0xC0) != 0x80 )
-                //throw new AtlasException("Illegal UTF-8 processing character "+count+": "+x2) ;
-                throw new AtlasException(String.format("Illegal UTF-8 processing character: 0x%04X",x2)) ;
-            // 6 bits of x2
-            x = (x << 6) | (x2 & 0x3F); 
-        }
-        return x ;
-    }
-
-    public static int _convertCharToBytes(char ch, ByteBuffer bb)
-    {
-        if ( ch != 0 && ch <= 127 )
-        {
-            // 7 bits
-            bb.put((byte)ch) ;
-            return 1 ;
-        }
-
-        if ( ch == 0 )
-        {
-            // Java.
-            bb.put((byte)0x00) ;
-            return 1 ;
-//            // Modified UTF-8.
-//            bb.put((byte)0xC0) ;
-//            bb.put((byte)0x80) ;
-//            return 2 ;
-        }
-
-        if ( ch <= 0x07FF )
-        {
-            // 11 bits : 110yyyyy 10xxxxxx
-            // int x1 = ( ((ch>>(11-5))&0x7) | 0xC0 ) ; outputBytes(out, x1, 2, ch) ; return ;
-            int x1 = ( ((ch>>(11-5))&0x01F ) | 0xC0 ) ; 
-            int x2 = ( (ch&0x3F)  | 0x80 ) ;
-            bb.put((byte)x1) ;
-            bb.put((byte)x2) ;
-            return 2 ;
-        }
-        if ( ch <= 0xFFFF )
-        {
-            // 16 bits : 1110aaaa  10bbbbbb  10cccccc
-            // int x1 = ( ((ch>>(16-4))&0x7) | 0xE0 ) ; outputBytes(out, x1, 3, ch) ; return ;
-            int x1 = ( ((ch>>(16-4))&0x0F) | 0xE0 ) ;
-            int x2 = ( ((ch>>6)&0x3F) | 0x80 ) ;
-            int x3 = ( (ch&0x3F) | 0x80 ) ;
-            bb.put((byte)x1) ;
-            bb.put((byte)x2) ;
-            bb.put((byte)x3) ;
-            return 3 ;
-        }
-
-        //            if ( Character.isDefined(ch) )
-        //                throw new AtlasException("not a character") ;
-
-        //if ( true ) throw new InternalErrorException("Valid code point for Java but not encodable") ;
-
-        // Not java, where chars are 16 bit.
-        if ( ch <= 0x1FFFFF )
-        {
-            // 21 bits : 11110xxx 10xxxxxx 10xxxxxx 10xxxxxx
-            int x1 = ( ((ch>>(21-3))&0x7) | 0xF0 ) ;
-            outputBytes(bb, x1, 4, ch) ;
-            return 4 ;
-        }
-        if ( ch <= 0x3FFFFFF )
-        {
-            // 26 bits : 111110xx 10xxxxxx 10xxxxxx 10xxxxxx 10xxxxxx
-            int x1 = ( ((ch>>(26-2))&0x3) | 0xF8 ) ;
-            outputBytes(bb, x1, 5, ch) ;
-            return 5 ;
-        }
-
-        if ( ch <= 0x7FFFFFFF )
-        {
-            // 32 bits : 1111110x 10xxxxxx 10xxxxxx 10xxxxxx 10xxxxxx 10xxxxxx
-            int x1 = ( ((ch>>(32-1))&0x1) | 0xFC ) ;
-            outputBytes(bb, x1, 6, ch) ;
-            return 6 ;
-        }
-
-        return -1 ;
-    }
+    // but the conversion from bytes to string is faster with Java decoders (not by much though). 
 
     /*
      * Bits 
@@ -327,6 +64,200 @@ public class BlockUTF8
      * 31   U+7FFFFFFF                        1111110x 10xxxxxx 10xxxxxx 10xxxxxx 10xxxxxx 10xxxxxx
      */
     
+    
+    /** Convert the bytes in the ByteBuffer to characters in the CharBuffer.
+     * The CharBuffer must be large enough. 
+     */
+    public static void toChars(ByteBuffer bb, CharBuffer cb)
+    {
+//        if ( bb.hasArray() && cb.hasArray() )
+//        {
+//            toCharsArray(bb.array(), cb.array()) ;
+//            return ;
+//        }
+        toCharsBuffer(bb, cb) ;
+    }
+
+    /** Convert characters to UTF-8 bytes in the ByteBuffer.
+     * The ByteBuffer must be large enough. 
+     */
+    public static void fromChars(CharBuffer cb, ByteBuffer bb)
+    {
+//        if ( bb.hasArray() && cb.hasArray() )
+//        {
+//            fromCharsArray(cb.array(), bb.array()) ;
+//            return ;
+//        }
+        fromCharsBuffer(cb, bb) ;
+    }
+
+    /** Make a string from UTF-8 bytes in a ByteBuffer */ 
+    public static String toString(ByteBuffer bb)
+    {
+        // I think that the copy from some mutable collector to immutable string is inevitable in java.  
+        int len = bb.remaining() ;
+        CharBuffer cb = CharBuffer.allocate(len) ;
+        toChars(bb, cb) ;
+        return new String(cb.array(), 0, cb.position()) ;
+    }
+
+    // Using buffer access.
+    private static void toCharsBuffer(ByteBuffer bb, CharBuffer cb)
+    {
+        int idx = bb.position();
+        int limit = bb.limit() ;
+
+        for ( ;  idx < limit ; )
+        {
+            int x = bb.get() ;
+            if ( x > 0 && x <= 127 )
+            {
+                cb.put((char)x) ;
+                idx += 1 ;
+            } else if ( x == 0 )
+            {
+                // Pass through a null byte as the null character (illegal Unicode, Java compatible).
+                cb.put((char)x) ;
+                idx += 1 ;
+            }
+            else if ( (x & 0xE0) == 0xC0 )
+            {
+                // 10 => extension byte
+                // 110..... => 2 bytes
+                // Unroll common path
+                //int ch = readMultiBytes(bb, x & 0x1F, 2) ;
+                int x2 = bb.get() ;
+                if ( (x2 & 0xC0) != 0x80 )
+                    exception("Illegal UTF-8 processing character: 0x%04X",x2) ;
+                // 6 bits of x2
+                int ch = ( (x&0x1F) << 6) | (x2 & 0x3F); 
+                cb.put((char)ch) ;
+                idx += 2  ;
+            } 
+            else if ( (x & 0xF0) == 0xE0 ) 
+            {
+                //  1110.... => 3 bytes : 16 bits : not outside 16bit chars 
+                int ch = readMultiBytes(bb, x & 0x0F, 3) ;
+                cb.put((char)ch) ;
+                idx += 3 ;
+            }
+            else if ( (x & 0xF8) == 0xF0 ) 
+            {
+                // Looking like 4 byte charcater.
+                // 11110zzz => 4 bytes.
+                int ch = readMultiBytes(bb, x & 0x08, 4) ;
+                char chars[] = Character.toChars(ch) ;
+                cb.put(chars) ;
+                idx += 4 ;
+            }
+            else 
+            {
+                exception("Illegal UTF-8: 0x%04X",x) ;
+                return ;
+            }
+        }
+    }
+
+    private static void toCharsArray(byte[] bytes, char[] chars)
+    {
+        throw new NotImplemented() ;
+    }
+    
+    private static void fromCharsBuffer(CharBuffer cb, ByteBuffer bb)
+    {
+        // CharBuffers are CharSequences but charAt(i) adds a layer of work.
+        //int bytesStart = bb.position() ;
+        int idx = cb.position() ;
+        int limit = cb.limit() ;
+        for ( ; idx < limit ; idx ++ )
+        {
+            char ch = cb.get() ;
+            if ( ch != 0 && ch <= 127 )
+            {
+                // 7 bits
+                bb.put((byte)ch) ;
+            } 
+            else if ( ch == 0 )
+            {
+                // Java.
+                bb.put((byte)0x00) ;
+                // Modified UTF-8.
+                //bb.put((byte)0xC0) ;
+                //bb.put((byte)0x80) ;
+            } 
+            else if ( ch <= 0x07FF )
+            {
+                // 11 bits : 110yyyyy 10xxxxxx
+                // int x1 = ( ((ch>>(11-5))&0x7) | 0xC0 ) ; outputBytes(out, x1, 2, ch) ; return ;
+                int x1 = ( ((ch>>(11-5))&0x01F ) | 0xC0 ) ; 
+                int x2 = ( (ch&0x3F)  | 0x80 ) ;
+                bb.put((byte)x1) ;
+                bb.put((byte)x2) ;
+            } 
+            else if ( ch <= 0xFFFF )
+            {
+                // 16 bits : 1110aaaa  10bbbbbb  10cccccc
+                // int x1 = ( ((ch>>(16-4))&0x7) | 0xE0 ) ; outputBytes(out, x1, 3, ch) ; return ;
+                int x1 = ( ((ch>>(16-4))&0x0F) | 0xE0 ) ;
+                int x2 = ( ((ch>>6)&0x3F) | 0x80 ) ;
+                int x3 = ( (ch&0x3F) | 0x80 ) ;
+                bb.put((byte)x1) ;
+                bb.put((byte)x2) ;
+                bb.put((byte)x3) ;
+            }
+            //            if ( Character.isDefined(ch) )
+            //                throw new AtlasException("not a character") ;
+            //if ( true ) throw new InternalErrorException("Valid code point for Java but not encodable") ;
+            // Not java, where chars are 16 bit.
+            else if ( ch <= 0x1FFFFF )
+            {
+                // 21 bits : 11110xxx 10xxxxxx 10xxxxxx 10xxxxxx
+                int x1 = ( ((ch>>(21-3))&0x7) | 0xF0 ) ;
+                outputBytes(bb, x1, 4, ch) ;
+            } 
+            else if ( ch <= 0x3FFFFFF )
+            {
+                // 26 bits : 111110xx 10xxxxxx 10xxxxxx 10xxxxxx 10xxxxxx
+                int x1 = ( ((ch>>(26-2))&0x3) | 0xF8 ) ;
+                outputBytes(bb, x1, 5, ch) ;
+            }
+            else if ( ch <= 0x7FFFFFFF )
+            {
+                // 32 bits : 1111110x 10xxxxxx 10xxxxxx 10xxxxxx 10xxxxxx 10xxxxxx
+                int x1 = ( ((ch>>(32-1))&0x1) | 0xFC ) ;
+                outputBytes(bb, x1, 6, ch) ;
+            }
+        }
+        //int bytesFinish = bb.position() ;
+    }
+
+    private static void fromCharsArray(char[] array, byte[] array2)
+    {
+        throw new NotImplemented() ;
+    }
+    
+    public static void fromChars(CharSequence cs, ByteBuffer bb)
+    {
+        fromChars(CharBuffer.wrap(cs), bb) ;
+    }
+
+    private static int readMultiBytes(ByteBuffer input, int start, int len)
+    {
+        // We have already read one byte.
+        if ( input.remaining() < (len-1) )
+            exception("Premature end to UTF-8 sequence at end of input") ;
+        int x = start ;
+        for ( int i = 0 ; i < len-1 ; i++ )
+        {
+            int x2 = input.get() ;
+            if ( (x2 & 0xC0) != 0x80 )
+                exception("Illegal UTF-8 processing character: 0x%04X",x2) ;
+            // 6 bits of x2
+            x = (x << 6) | (x2 & 0x3F); 
+        }
+        return x ;
+    }
+
     /** Put bytes to the output ByteBuffer for charcater ch.
      * The first byte is in x1 and already has the needed bits set. 
      */
@@ -343,5 +274,11 @@ public class BlockUTF8
             bb.put((byte)x) ;
         }
     }
-
+    
+    // Does not return 
+    private static void exception(String fmt, Object ...args)
+    {
+        String str = String.format(fmt,args) ;
+        IO.exception(new IOException(str)) ;
+    }
 }
