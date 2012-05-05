e8258ff7c026f17f53ee2b7fc8dc13266b9621bb
Andy Seaborne <andy@apache.org>
2012-05-05 20:38:18 +0000

    public static final
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jena/Jena2/Fuseki/trunk@1334492 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/jena/fuseki/DEF.java
+++ src/main/java/org/apache/jena/fuseki/DEF.java
@@ -24,25 +24,25 @@ import org.openjena.riot.WebContent ;
 
 public class DEF
 {
-    public static MediaType acceptRDFXML        = new MediaType(WebContent.contentTypeRDFXML) ;
-    public static MediaType acceptTurtle1       = new MediaType(WebContent.contentTypeTurtle) ;
-    public static MediaType acceptTurtle2       = new MediaType(WebContent.contentTypeTurtleAlt1) ;
-    public static MediaType acceptTurtle3       = new MediaType(WebContent.contentTypeTurtleAlt2) ;
-    public static MediaType acceptNTriples      = new MediaType(WebContent.contentTypeNTriples) ;
-    public static MediaType acceptNTriplesAlt   = new MediaType(WebContent.contentTypeNTriplesAlt) ;
-    public static MediaType acceptTriG          = new MediaType(WebContent.contentTypeTriG) ;
-    public static MediaType acceptTriGAlt       = new MediaType(WebContent.contentTypeTriGAlt) ;
-    public static MediaType acceptNQuads        = new MediaType(WebContent.contentTypeNQuads) ;
-    public static MediaType acceptNQuadsAlt     = new MediaType(WebContent.contentTypeNQuadsAlt) ;
-    public static MediaType charsetUTF8         = new MediaType(WebContent.charsetUTF8) ;
+    public static final MediaType acceptRDFXML        = new MediaType(WebContent.contentTypeRDFXML) ;
+    public static final MediaType acceptTurtle1       = new MediaType(WebContent.contentTypeTurtle) ;
+    public static final MediaType acceptTurtle2       = new MediaType(WebContent.contentTypeTurtleAlt1) ;
+    public static final MediaType acceptTurtle3       = new MediaType(WebContent.contentTypeTurtleAlt2) ;
+    public static final MediaType acceptNTriples      = new MediaType(WebContent.contentTypeNTriples) ;
+    public static final MediaType acceptNTriplesAlt   = new MediaType(WebContent.contentTypeNTriplesAlt) ;
+    public static final MediaType acceptTriG          = new MediaType(WebContent.contentTypeTriG) ;
+    public static final MediaType acceptTriGAlt       = new MediaType(WebContent.contentTypeTriGAlt) ;
+    public static final MediaType acceptNQuads        = new MediaType(WebContent.contentTypeNQuads) ;
+    public static final MediaType acceptNQuadsAlt     = new MediaType(WebContent.contentTypeNQuadsAlt) ;
+    public static final MediaType charsetUTF8         = new MediaType(WebContent.charsetUTF8) ;
     
-    public static MediaType acceptRSXML         = new MediaType(WebContent.contentTypeResultsXML) ;
+    public static final MediaType acceptRSXML         = new MediaType(WebContent.contentTypeResultsXML) ;
 
-    public static AcceptList rdfOffer           = new AcceptList(acceptRDFXML, acceptTurtle1, acceptTurtle2, acceptTurtle3, acceptNTriples, acceptNTriplesAlt) ;
-    public static AcceptList quadsOffer         = new AcceptList(acceptTriG, acceptTriGAlt, acceptNQuads, acceptNQuadsAlt) ;
-    public static AcceptList charsetOffer       = new AcceptList(charsetUTF8) ;
+    public static final AcceptList rdfOffer           = new AcceptList(acceptRDFXML, acceptTurtle1, acceptTurtle2, acceptTurtle3, acceptNTriples, acceptNTriplesAlt) ;
+    public static final AcceptList quadsOffer         = new AcceptList(acceptTriG, acceptTriGAlt, acceptNQuads, acceptNQuadsAlt) ;
+    public static final AcceptList charsetOffer       = new AcceptList(charsetUTF8) ;
     
-    public static AcceptList rsOffer            = new AcceptList(WebContent.contentTypeResultsXML, 
+    public static final AcceptList rsOffer            = new AcceptList(WebContent.contentTypeResultsXML, 
                                                                  WebContent.contentTypeResultsJSON,
                                                                  WebContent.contentTypeTextCSV,
                                                                  WebContent.contentTypeTextTSV,
