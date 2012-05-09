1a21857475ea636cf7371c100d1afa5cce8f9ee0
Andrew Franklin Seaborne <andy@apache.org>
2012-05-09 08:28:54 +0000

    Code re-organisation
    git-svn-id: https://svn.apache.org/repos/asf/jena/trunk@1335984 13f79535-47bb-0310-9956-ffa450edef68

---
+++ apache-jena/template.bat
@@ -0,0 +1,15 @@
+@echo off
+@rem Licensed under the terms of http://www.apache.org/licenses/LICENSE-2.0
+
+if NOT "%JENAROOT%" == "" goto :okRoot
+echo JENAROOT not set
+exit /B
+
+:okRoot
+
+set JVM_ARGS=-Xmx1024M
+set JENA_CP="%JENAROOT%\lib\*;"
+set LOGGING=-Dlog4j.configuration=file:%JENAROOT%/log4j.properties
+
+java %JVM_ARGS% %LOGGING% -cp %JENA_CP% JENA_CMD %*
+exit /B
