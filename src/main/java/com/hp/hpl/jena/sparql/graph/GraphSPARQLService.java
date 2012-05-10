7bd5e5afa9f3a22fe3011c1729fa317384aa1720
Andy Seaborne <andy@apache.org>
2012-05-10 18:34:40 +0000

    Bug fix : was failing to sync() complex arrangements of graphs.
    git-svn-id: https://svn.apache.org/repos/asf/jena/trunk/jena-arq@1336827 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/com/hp/hpl/jena/sparql/graph/GraphSPARQLService.java
+++ src/main/java/com/hp/hpl/jena/sparql/graph/GraphSPARQLService.java
@@ -32,7 +32,7 @@ import com.hp.hpl.jena.sparql.core.BasicPattern ;
 import com.hp.hpl.jena.sparql.core.Var ;
 import com.hp.hpl.jena.util.iterator.ExtendedIterator ;
 
-/** This class provides the Jena Graph interface to a remove SPARQL endpoint.
+/** This class provides the Jena Graph interface to a remote SPARQL endpoint.
  *  Efficiency not guaranteed. */
 
 public class GraphSPARQLService extends GraphBase implements Graph
