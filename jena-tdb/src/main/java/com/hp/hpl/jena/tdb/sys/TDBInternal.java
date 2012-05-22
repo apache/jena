5e003a61298daf202af417f9d6f84ed08f6bb144
Andy Seaborne <andy@apache.org>
2012-05-22 13:08:24 +0000

    Start function library to access TDB internals. Align quad filter example to new library.
    git-svn-id: https://svn.apache.org/repos/asf/jena/trunk@1341459 13f79535-47bb-0310-9956-ffa450edef68

---
+++ jena-tdb/src/main/java/com/hp/hpl/jena/tdb/sys/TDBInternal.java
@@ -0,0 +1,102 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.hp.hpl.jena.tdb.sys;
+
+import com.hp.hpl.jena.graph.Node ;
+import com.hp.hpl.jena.query.Dataset ;
+import com.hp.hpl.jena.sparql.core.DatasetGraph ;
+import com.hp.hpl.jena.tdb.nodetable.NodeTable ;
+import com.hp.hpl.jena.tdb.store.DatasetGraphTDB ;
+import com.hp.hpl.jena.tdb.store.NodeId ;
+import com.hp.hpl.jena.tdb.transaction.DatasetGraphTransaction ;
+
+/** A collection of helpers to abstract away from internal details of TDB. 
+ * Use with care.
+ */
+public class TDBInternal
+{
+    /** Return the NodeId for a node.
+     * Returns NodeId.NodeDoesNotExist when the node is not found. 
+     * Returns null when not a TDB-backed dataset.
+     */
+    public static NodeId getNodeId(Dataset ds, Node node)
+    {
+        return  getNodeId(ds.asDatasetGraph(), node) ;
+    }
+    
+    /** Return the NodeId for a node.
+     * Returns NodeId.NodeDoesNotExist when the node is not found. 
+     * Returns null when not a TDB-backed dataset.
+     */
+    public static NodeId getNodeId(DatasetGraph ds, Node node)
+    {
+        DatasetGraphTDB dsg = getDatasetGraphTDB(ds) ;
+        if ( dsg == null )
+            return null ;
+        NodeTable nodeTable = dsg.getQuadTable().getNodeTupleTable().getNodeTable() ;
+        NodeId nodeId = nodeTable.getNodeIdForNode(node) ;
+        return nodeId ;
+    }
+    
+    /** Return the node for a NodeId (if any).
+     *  Returns null if the NodeId does not exist in the dataset. 
+     */
+    public static Node getNode(Dataset ds, NodeId nodeId)
+    {
+        return getNode(ds.asDatasetGraph(), nodeId) ;
+    }
+    
+    /** Return the node for a NodeId (if any).
+     *  Returns null if the NodeId does not exist in the dataset. 
+     */
+    public static Node getNode(DatasetGraph ds, NodeId nodeId)
+    {
+        DatasetGraphTDB dsg = getDatasetGraphTDB(ds) ;
+        if ( dsg == null )
+            return null ;
+        NodeTable nodeTable = dsg.getQuadTable().getNodeTupleTable().getNodeTable() ;
+        Node node = nodeTable.getNodeForNodeId(nodeId) ;
+        return node ;
+    }
+
+    /**
+     * Return the DatasetGraphTDB for a Dataset, or null.
+     */
+    public static DatasetGraphTDB getDatasetGraphTDB(Dataset ds)
+    {
+        return getDatasetGraphTDB(ds.asDatasetGraph()) ;
+    }
+
+    
+    /**
+     * Return the DatasetGraphTDB for a DatasetGraph, or null.
+     */
+    public static DatasetGraphTDB getDatasetGraphTDB(DatasetGraph dsg)
+    {
+        if ( dsg instanceof DatasetGraphTransaction )
+            return ((DatasetGraphTransaction)dsg).getBaseDatasetGraph() ;
+        
+        if ( dsg instanceof DatasetGraphTDB )
+            return (DatasetGraphTDB)dsg ;
+        
+        return null ;
+    }
+    
+}
+
