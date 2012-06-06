485776c3a468eb9f91def4e914f1b054d4ab7e55
Andrew Franklin Seaborne <andy@apache.org>
2012-06-06 13:43:57 +0000

    JENA-247 (XSD datetiem conversion around the DST change time)
    git-svn-id: https://svn.apache.org/repos/asf/jena/trunk@1346890 13f79535-47bb-0310-9956-ffa450edef68

--- jena-core/src/main/java/com/hp/hpl/jena/datatypes/xsd/XSDDateTime.java
+++ jena-core/src/main/java/com/hp/hpl/jena/datatypes/xsd/XSDDateTime.java
@@ -130,13 +130,10 @@ public class XSDDateTime extends AbstractDateTime {
      */
     private static int[] convertCalendar(Calendar date) {
         int[] data = new int[TOTAL_SIZE];
-        int offset = date.get(Calendar.ZONE_OFFSET) + date.get(Calendar.DST_OFFSET);
-                                        //  Thanks to Greg Shueler for pointing out need for DST offset
-        Calendar cal = date;
-        if (offset != 0) {
-            cal = (Calendar)date.clone();
-            cal.add(Calendar.MILLISECOND, -offset);
-        }
+        
+        Calendar cal = (Calendar)date.clone();
+        cal.setTimeZone(TimeZone.getTimeZone("GMT"));   // Rebase to GMT.
+        
         data[AbstractDateTime.CY] = cal.get(Calendar.YEAR);
         data[AbstractDateTime.M] = cal.get(Calendar.MONTH) + 1;
         data[AbstractDateTime.D] = cal.get(Calendar.DAY_OF_MONTH);
