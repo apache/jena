048ab79426998942fb6d00df29758614f1aaaad1
Andy Seaborne <andy@apache.org>
2012-06-09 12:14:32 +0000

    Apply filter equality transformations to pre- and post- other optimization forms.
    git-svn-id: https://svn.apache.org/repos/asf/jena/trunk/jena-arq@1348369 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/com/hp/hpl/jena/sparql/algebra/optimize/TransformFilterPlacement.java
+++ src/main/java/com/hp/hpl/jena/sparql/algebra/optimize/TransformFilterPlacement.java
@@ -98,6 +98,7 @@ public class TransformFilterPlacement extends TransformCopy
     
     private static Op transform(ExprList exprs, Set<Var> varsScope, Op x)
     {
+        // OpAssign could be done if the assignment and exprs are independent.
         // TODO Dispatch by visitor
         if ( x instanceof OpBGP )
             return transformFilterBGP(exprs, varsScope, (OpBGP)x) ;
