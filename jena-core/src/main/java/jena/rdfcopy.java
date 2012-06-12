e9aeddf2e195c577d1f48a579cc50154adaf1b6a
Andrew Franklin Seaborne <andy@apache.org>
2012-06-12 17:57:55 +0000

    Use main(String...args) - nicer to call from java.
    git-svn-id: https://svn.apache.org/repos/asf/jena/trunk@1349458 13f79535-47bb-0310-9956-ffa450edef68

--- jena-core/src/main/java/jena/rdfcopy.java
+++ jena-core/src/main/java/jena/rdfcopy.java
@@ -60,7 +60,7 @@ public class rdfcopy extends java.lang.Object {
 	/**
 	* @param args the command line arguments
 	*/
-	public static void main(String args[]) {
+	public static void main(String ... args) {
 		if ( ( args.length < 1 ) || ( "-h".equals(args[0]) ) ) {
 			usage();
 			System.exit(-1);
