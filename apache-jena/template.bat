9c5629d912b94215b9e5d26a231a1d70d9862660
Andrew Franklin Seaborne <andy@apache.org>
2012-08-10 11:41:56 +0000

    Updates for tdbloader2
    git-svn-id: https://svn.apache.org/repos/asf/jena/trunk@1371666 13f79535-47bb-0310-9956-ffa450edef68

--- apache-jena/template.bat
+++ apache-jena/template.bat
@@ -1,15 +1,18 @@
 @echo off
 @rem Licensed under the terms of http://www.apache.org/licenses/LICENSE-2.0
 
-if NOT "%JENAROOT%" == "" goto :okRoot
-echo JENAROOT not set
+if "%JENAROOT%" == "" goto :rootNotSet
+set JENA_HOME=%JENAROOT%
+:rootNotSet
+
+if NOT "%JENA_HOME%" == "" goto :okHome
+echo JENA_HOME not set
 exit /B
 
-:okRoot
-
+:okHome
 set JVM_ARGS=-Xmx1024M
-set JENA_CP=%JENAROOT%\lib\*;
-set LOGGING=file:%JENAROOT%/jena-log4j.properties
+set JENA_CP=%JENA_HOME%\lib\*;
+set LOGGING=file:%JENA_HOME%/jena-log4j.properties
 
 java %JVM_ARGS% -Dlog4j.configuration="%LOGGING%" -cp "%JENA_CP%" JENA_CMD %*
 exit /B
