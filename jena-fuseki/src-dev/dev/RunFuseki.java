4424cc034386db02d6848c32995d4c67a0c59169
Andy Seaborne <andy@apache.org>
2012-09-14 13:50:23 +0000

    git-svn-id: https://svn.apache.org/repos/asf/jena/trunk@1384787 13f79535-47bb-0310-9956-ffa450edef68

--- jena-fuseki/src-dev/dev/RunFuseki.java
+++ jena-fuseki/src-dev/dev/RunFuseki.java
@@ -53,8 +53,8 @@ public class RunFuseki
     private static void main1()
     {
         FusekiCmd.main(
-                     //"--update", "--mem", "/ds"
-                     "--update", "--loc=DB", "/ds"
+                     "--update", "--mem", "/ds"
+                     //"--update", "--loc=DB", "/ds"
                        
                     //"-v", 
                     //"--debug",
