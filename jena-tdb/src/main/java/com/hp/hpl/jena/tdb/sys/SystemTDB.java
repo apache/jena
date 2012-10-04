7fef3fe6169dc57b64b92fdaff30656879973954
Andy Seaborne <andy@apache.org>
2012-10-04 22:33:26 +0000

    Remove wrong comented code.
    git-svn-id: https://svn.apache.org/repos/asf/jena/trunk@1394304 13f79535-47bb-0310-9956-ffa450edef68

--- jena-tdb/src/main/java/com/hp/hpl/jena/tdb/sys/SystemTDB.java
+++ jena-tdb/src/main/java/com/hp/hpl/jena/tdb/sys/SystemTDB.java
@@ -316,30 +316,6 @@ public class SystemTDB
         return b ;
     }
     
-
-//	// Not in use yet.
-//    private static void determineJVMSize()
-//    {
-//        Runtime runtime = Runtime.getRuntime() ;
-//        
-//        long totalMemory = runtime.totalMemory() ;
-//        long maxMemory = runtime.maxMemory() ;
-//        int cpus = runtime.availableProcessors() ;
-//        
-//        runtime.addShutdownHook(new Thread(new SyncCacheRunnable())) ;
-//    }
-//    
-//    static class SyncCacheRunnable implements Runnable
-//    {
-//        @Override
-//        public void run()
-//        {
-//            try {
-//                StoreConnection.killActiveTransactions() ;
-//            } catch (Exception ex) {} 
-//        }
-//    }
-    
     // ---- File mode
     
     private static FileMode fileMode = null ;
