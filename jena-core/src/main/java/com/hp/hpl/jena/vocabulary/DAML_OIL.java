ababa3c5a95046c69fb573b125b72294596d4bff
Ian Dickinson <ijd@apache.org>
2012-10-09 23:45:25 +0000

    Deprecated DAML vocabularies and supporting classes
    git-svn-id: https://svn.apache.org/repos/asf/jena/trunk@1396405 13f79535-47bb-0310-9956-ffa450edef68

--- jena-core/src/main/java/com/hp/hpl/jena/vocabulary/DAML_OIL.java
+++ jena-core/src/main/java/com/hp/hpl/jena/vocabulary/DAML_OIL.java
@@ -49,6 +49,7 @@ import com.hp.hpl.jena.rdf.model.*;
  *
  * @author Ian Dickinson, HP Labs (<a href="mailto:ian_dickinson@users.sourceforge.net">email</a>)
  * @version CVS info: $Id: DAML_OIL.java,v 1.2 2009-10-06 13:04:43 ian_dickinson Exp $
+ * @deprecated This DAML vocabulary will be removed from future versions of Jena because it is obsolete.
  */
 public class DAML_OIL
     implements DAMLVocabulary
@@ -61,7 +62,7 @@ public class DAML_OIL
 
     /** Model to hold the vocab resoures */
     private static Model s_model = ModelFactory.createDefaultModel();
-    
+
     /* Resources */
 
     /** DAML namespace resource for the current release */
