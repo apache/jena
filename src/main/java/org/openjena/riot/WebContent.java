967b0b100c3b484c4714bbe494ce7b35d13a1080
Andy Seaborne <andy@apache.org>
2012-10-16 14:44:49 +0000

    Names for multipart media types.
    git-svn-id: https://svn.apache.org/repos/asf/jena/trunk/jena-arq@1398824 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/openjena/riot/WebContent.java
+++ src/main/java/org/openjena/riot/WebContent.java
@@ -54,6 +54,9 @@ public class WebContent
 
     public static final String contentTypeTriX              = "application/trix+xml" ;
     public static final String contentTypeOctets            = "application/octet-stream" ;
+    public static final String contentTypeMultiMixed        = "multipart/mixed" ;
+    public static final String contentTypeMultiForm         = "multipart/form" ;
+    public static final String contentTypeMultiAlt          = "multipart/alternative" ;
 
     public static final String contentTypeRdfJson			= "application/rdf+json" ;
     
