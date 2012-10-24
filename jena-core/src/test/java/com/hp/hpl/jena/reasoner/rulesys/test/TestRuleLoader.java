c9c47b06a7f5cba2db4813369ea1abb0eb2e64f6
Rob Vesse <rvesse@apache.org>
2012-10-24 16:41:46 +0000

    Fix for JENA-338, Rule.rulesFromURL() now throws a RulesetNotFoundException only if FileManager.get().open(uri) returns null.  WrappedIOExceptions are not caught and rewrapped incorrectly as highlighted by this bug
    git-svn-id: https://svn.apache.org/repos/asf/jena/trunk@1401773 13f79535-47bb-0310-9956-ffa450edef68

---
+++ jena-core/src/test/java/com/hp/hpl/jena/reasoner/rulesys/test/TestRuleLoader.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2012 YarcData LLC All Rights Reserved.
+ */ 
+
+package com.hp.hpl.jena.reasoner.rulesys.test;
+
+import java.io.FileNotFoundException;
+
+import org.junit.Test;
+
+import com.hp.hpl.jena.reasoner.rulesys.Rule;
+import com.hp.hpl.jena.shared.RulesetNotFoundException;
+import com.hp.hpl.jena.shared.WrappedIOException;
+
+/**
+ * Tests for the rule loader
+ * @author rvesse
+ *
+ */
+public class TestRuleLoader  {
+    
+    @Test(expected=RulesetNotFoundException.class)
+    public void load_from_file_uri_non_existent() {
+        Rule.rulesFromURL("file:///no-such-file.txt");
+    }
+    
+    @Test(expected=WrappedIOException.class)
+    public void load_from_file_bad_encoding() {
+        Rule.rulesFromURL("testing/reasoners/bugs/bad-encoding.rules");
+    }
+}
