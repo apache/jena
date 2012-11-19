29d5618f6152c4b41e6b5e82f4cd14473d2f9bcd
Rob Vesse <rvesse@apache.org>
2012-11-19 21:18:13 +0000

    Fix copyright headers, start using ExprTransformExpand to expand user defined functions to avoid dependencies between user defined functions.  Removed @author tags
    git-svn-id: https://svn.apache.org/repos/asf/jena/trunk/jena-arq@1411421 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/com/hp/hpl/jena/sparql/function/user/ExprTransformExpand.java
+++ src/main/java/com/hp/hpl/jena/sparql/function/user/ExprTransformExpand.java
@@ -1,27 +1,40 @@
 /*
- * Copyright 2012 YarcData LLC All Rights Reserved.
- */ 
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 package com.hp.hpl.jena.sparql.function.user;
 
+import java.util.List;
 import java.util.Map;
 
-import com.hp.hpl.jena.sparql.algebra.Op;
+import com.hp.hpl.jena.sparql.core.Var;
 import com.hp.hpl.jena.sparql.expr.Expr;
 import com.hp.hpl.jena.sparql.expr.ExprFunction;
-import com.hp.hpl.jena.sparql.expr.ExprFunction0;
-import com.hp.hpl.jena.sparql.expr.ExprFunction1;
-import com.hp.hpl.jena.sparql.expr.ExprFunction2;
-import com.hp.hpl.jena.sparql.expr.ExprFunction3;
 import com.hp.hpl.jena.sparql.expr.ExprFunctionN;
-import com.hp.hpl.jena.sparql.expr.ExprFunctionOp;
 import com.hp.hpl.jena.sparql.expr.ExprList;
 import com.hp.hpl.jena.sparql.expr.ExprTransformCopy;
+import com.hp.hpl.jena.sparql.sse.builders.ExprBuildException;
 
 /**
- * An expression transformer that will expand user defined expressions so they do not explicitly rely on other user defined functions
- * @author rvesse
- *
+ * An expression transformer that will expand user defined function expressions
+ * so they do not explicitly rely on other user defined functions.
+ * <p>
+ * See {@link UserDefinedFunctionFactory#getAllowDependencies()} for discussion of what this means in practise
+ * </p>
  */
 public class ExprTransformExpand extends ExprTransformCopy {
     
@@ -37,44 +50,41 @@ public class ExprTransformExpand extends ExprTransformCopy {
     }
 
     @Override
-    public Expr transform(ExprFunction0 func) {
+    public Expr transform(ExprFunctionN func, ExprList args) {
         ExprFunction f = func.getFunction();
         if (this.shouldExpand(f)) {
-            //TODO Need to expand the function
-            return super.transform(func);
+            UserDefinedFunctionDefinition def = this.definitions.get(f.getFunction().getFunctionIRI());
+            UserDefinedFunction uFunc = (UserDefinedFunction) def.newFunctionInstance();
+            
+            //Need to watch out for the case where the arguments supplied to the invoked
+            //function are in a different order to the arguments supplied to the defined
+            //function
+            //Thus we will build the list of arguments used to expand the inner function
+            //manually
+            List<Var> defArgs = def.getArgList();
+            ExprList subArgs = new ExprList();
+            
+            for (int i = 0; i < args.size(); i++) {
+                Expr arg = args.get(i);
+                String var = arg.getVarName();
+                if (var == null) {
+                    //Non-variable args may be passed as-is
+                    subArgs.add(arg);
+                } else {
+                    //Variable args must be checked to ensure they are within the number of
+                    //arguments of the invoked function
+                    //We then use the arg as-is to substitute
+                    if (i > defArgs.size()) throw new ExprBuildException("Unable to expand function dependency, the function <" + def.getUri() + "> is called but uses an argument ?" + var + " which is not an argument to the outer function");
+                    subArgs.add(arg);
+                }
+            }
+            
+            //Expand the function
+            uFunc.build(def.getUri(), subArgs);
+            return uFunc.getActualExpr();
         } else {
-            return super.transform(func);
+            return super.transform(func, args);
         }
-    }
-
-    @Override
-    public Expr transform(ExprFunction1 func, Expr expr1) {
-        // TODO Auto-generated method stub
-        return super.transform(func, expr1);
-    }
-
-    @Override
-    public Expr transform(ExprFunction2 func, Expr expr1, Expr expr2) {
-        // TODO Auto-generated method stub
-        return super.transform(func, expr1, expr2);
-    }
-
-    @Override
-    public Expr transform(ExprFunction3 func, Expr expr1, Expr expr2, Expr expr3) {
-        // TODO Auto-generated method stub
-        return super.transform(func, expr1, expr2, expr3);
-    }
-
-    @Override
-    public Expr transform(ExprFunctionN func, ExprList args) {
-        // TODO Auto-generated method stub
-        return super.transform(func, args);
-    }
-
-    @Override
-    public Expr transform(ExprFunctionOp funcOp, ExprList args, Op opArg) {
-        // TODO Auto-generated method stub
-        return super.transform(funcOp, args, opArg);
     }
     
     private boolean shouldExpand(ExprFunction func) {
