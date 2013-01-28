5dabf61c0e45f68d4f6bb28007eeb3d6419aa810
Rob Vesse <rvesse@apache.org>
2013-01-28 16:00:13 +0000

    Clean up comments on LightweightPrefixMap
    git-svn-id: https://svn.apache.org/repos/asf/jena/trunk@1439461 13f79535-47bb-0310-9956-ffa450edef68

--- jena-arq/src/main/java/org/apache/jena/riot/system/LightweightPrefixMap.java
+++ jena-arq/src/main/java/org/apache/jena/riot/system/LightweightPrefixMap.java
@@ -26,39 +26,102 @@ import com.hp.hpl.jena.shared.PrefixMapping;
 
 /**
  * Interface for lightweight prefix maps, this is similar to
- * {@link PrefixMapping} but it omits any reverse lookup functionality. The
- * contract does not require an implementation to do any validation unlike a
- * {@link PrefixMapping} which require validation of prefixes
+ * {@link PrefixMapping} from Jena Core but it omits any reverse lookup
+ * functionality.
+ * <p>
+ * The contract also does not require an implementation to do any validation
+ * unlike the Jena Core {@link PrefixMapping} which require validation of
+ * prefixes.
+ * </p>
  * 
  */
 public interface LightweightPrefixMap {
 
-    /** return the underlying mapping - do not modify */
+    /**
+     * Return the underlying mapping, this is generally unsafe to modify and
+     * implementations may opt to return an unmodifiable view of the mapping if
+     * they wish
+     * 
+     * @return Underlying mapping
+     * */
     public abstract Map<String, IRI> getMapping();
 
-    /** return a copy of the underlying mapping */
+    /**
+     * Return a fresh copy of the underlying mapping, should be safe to modify
+     * unlike the mapping returned from {@link #getMapping()}
+     * 
+     * @return Copy of the mapping
+     */
     public abstract Map<String, IRI> getMappingCopy();
 
+    /**
+     * Gets a fresh copy of the mapping with the IRIs translated into their
+     * strings
+     * 
+     * @return Copy of the mapping
+     */
     public abstract Map<String, String> getMappingCopyStr();
 
-    /** Add a prefix, overwrites any existing association */
+    /**
+     * Add a prefix, overwrites any existing association
+     * 
+     * @param prefix
+     *            Prefix
+     * @param iriString
+     *            Namespace IRI
+     */
     public abstract void add(String prefix, String iriString);
 
-    /** Add a prefix, overwrites any existing association */
+    /**
+     * Add a prefix, overwrites any existing association
+     * 
+     * @param prefix
+     *            Prefix
+     * @param iri
+     *            Namespace IRI
+     */
     public abstract void add(String prefix, IRI iri);
 
-    /** Add a prefix, overwrites any existing association */
+    /**
+     * Add a prefix, overwrites any existing association
+     * 
+     * @param pmap
+     *            Prefix Map
+     */
     public abstract void putAll(LightweightPrefixMap pmap);
 
-    /** Add a prefix, overwrites any existing association */
+    /**
+     * Add a prefix, overwrites any existing association
+     * 
+     * @param pmap
+     *            Prefix Mapping
+     */
     public abstract void putAll(PrefixMapping pmap);
 
-    /** Delete a prefix */
+    /**
+     * Delete a prefix
+     * 
+     * @param prefix
+     *            Prefix to delete
+     */
     public abstract void delete(String prefix);
 
+    /**
+     * Gets whether the map contains a given prefix
+     * 
+     * @param prefix
+     *            Prefix
+     * @return True if the prefix is contained in the map, false otherwise
+     */
     public abstract boolean contains(String prefix);
 
-    /** Abbreviate an IRI or return null */
+    /**
+     * Abbreviate an IRI or return null
+     * 
+     * @param uriStr
+     *            URI to abbreviate
+     * @return URI in prefixed name form if possible, null otherwise
+     */
     public abstract String abbreviate(String uriStr);
 
     /**
@@ -66,14 +129,29 @@ public interface LightweightPrefixMap {
      * 
      * @param uriStr
      *            URI string to abbreviate
+     * @return Pair of prefix and local name
      * @see #abbreviate
      */
     public abstract Pair<String, String> abbrev(String uriStr);
 
-    /** Expand a prefix named, return null if it can't be expanded */
+    /**
+     * Expand a prefix named, return null if it can't be expanded
+     * 
+     * @param prefixedName
+     *            Prefixed Name
+     * @return Expanded URI if possible, null otherwise
+     */
     public abstract String expand(String prefixedName);
 
-    /** Expand a prefix, return null if it can't be expanded */
+    /**
+     * Expand a prefix, return null if it can't be expanded
+     * 
+     * @param prefix
+     *            Prefix
+     * @param localName
+     *            Local name
+     * @return Expanded URI if possible, null otherwise
+     */
     public abstract String expand(String prefix, String localName);
 
 }
