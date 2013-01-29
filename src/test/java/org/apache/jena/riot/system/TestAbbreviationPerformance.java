0c4709e9d7b5e5be9c8c75b89e78aa885269670c
Rob Vesse <rvesse@apache.org>
2013-01-29 17:46:56 +0000

    Add extend() method to PrefixMapFactory. Comment out a test which can pass/fail depending on machine conditions i.e. too indeterminate
    git-svn-id: https://svn.apache.org/repos/asf/jena/trunk/jena-arq@1440029 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/java/org/apache/jena/riot/system/TestAbbreviationPerformance.java
+++ src/test/java/org/apache/jena/riot/system/TestAbbreviationPerformance.java
@@ -85,19 +85,21 @@ public class TestAbbreviationPerformance {
         test_amalgamated_performance(pmap, fmap, 1, false);
     }
 
-    /**
-     * Expect {@link FastAbbreviatingPrefixMap} to outperform {@link PrefixMapStd} as soon as
-     * there are a few namespaces
-     */
-    @Test
-    public void prefixMap_abbrev_performance_02() {
-        PrefixMapStd pmap = new PrefixMapStd();
-        populate(pmap, 5);
-        FastAbbreviatingPrefixMap fmap = new FastAbbreviatingPrefixMap();
-        populate(fmap, 5);
-
-        test_amalgamated_performance(pmap, fmap, 5, true);
-    }
+    //This test can go either way depending on machine conditions
+    
+//    /**
+//     * Expect {@link FastAbbreviatingPrefixMap} to outperform {@link PrefixMapStd} as soon as
+//     * there are a few namespaces
+//     */
+//    @Test
+//    public void prefixMap_abbrev_performance_02() {
+//        PrefixMapStd pmap = new PrefixMapStd();
+//        populate(pmap, 5);
+//        FastAbbreviatingPrefixMap fmap = new FastAbbreviatingPrefixMap();
+//        populate(fmap, 5);
+//
+//        test_amalgamated_performance(pmap, fmap, 5, true);
+//    }
 
     /**
      * Expect {@link FastAbbreviatingPrefixMap} to significantly outperform
