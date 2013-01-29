2f186e62ccf05a08e8be2c722ef71a8457d3c4a5
Rob Vesse <rvesse@apache.org>
2013-01-29 17:01:26 +0000

    Add a factory for prefix maps, not yet used
    git-svn-id: https://svn.apache.org/repos/asf/jena/trunk@1440000 13f79535-47bb-0310-9956-ffa450edef68

---
+++ jena-arq/src/main/java/org/apache/jena/riot/system/PrefixMapFactory.java
@@ -0,0 +1,99 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.jena.riot.system;
+
+/**
+ * Factory which provides prefix maps
+ * 
+ */
+public class PrefixMapFactory {
+
+    /**
+     * Creates a new prefix map
+     * <p>
+     * Will use whatever the version of ARQ you are using considers the default
+     * implementation, this may change from release to release.
+     * </p>
+     * 
+     * @return Prefix map
+     */
+    public static PrefixMap create() {
+        return new PrefixMapStd();
+    }
+
+    /**
+     * Creates a new prefix map which is intended for use in input
+     * <p>
+     * Will use whatever the version of ARQ you are using considers the best
+     * implementation for input, this may change from release to release.
+     * </p>
+     * 
+     * @return Prefix map
+     */
+    public static PrefixMap createForInput() {
+        return new PrefixMapStd();
+    }
+
+    /**
+     * Creates a new prefix map which is intended for use in input which starts
+     * with a copy of an existing map
+     * <p>
+     * Will use whatever the version of ARQ you are using considers the best
+     * implementation for input, this may change from release to release.
+     * </p>
+     * 
+     * @param pmap
+     *            Prefix Map to copy
+     * @return Prefix Map
+     */
+    public static PrefixMap createForInput(PrefixMap pmap) {
+        return new PrefixMapStd(pmap);
+    }
+
+    /**
+     * Creates a new prefix map which is intended for use in output
+     * <p>
+     * Will use whatever the version of ARQ you are using considers the best
+     * implementation for output, this may change from release to release.
+     * </p>
+     * 
+     * @return Prefix Map
+     */
+    public static PrefixMap createForOutput() {
+        return new FastAbbreviatingPrefixMap();
+    }
+
+    /**
+     * Creates a new prefix map which is intended for use in output which starts
+     * with a copy of an existing map
+     * <p>
+     * Will use whatever the version of ARQ you are using considers the best
+     * implementation for output, this may change from release to release.
+     * </p>
+     * 
+     * @param pmap
+     *            Prefix Map to copy
+     * 
+     * @return Prefix Map
+     */
+    public static PrefixMap createForOutput(PrefixMap pmap) {
+        return new FastAbbreviatingPrefixMap(pmap);
+    }
+
+}
