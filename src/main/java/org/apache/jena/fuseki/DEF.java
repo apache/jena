6dd2ced67e9da313983f15bbefb0d47f585b2161
Andy Seaborne <andy@apache.org>
2013-04-07 19:12:43 +0000

    Tidy up.
    git-svn-id: https://svn.apache.org/repos/asf/jena/trunk/jena-fuseki@1465448 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/jena/fuseki/DEF.java
+++ src/main/java/org/apache/jena/fuseki/DEF.java
@@ -37,22 +37,20 @@ public class DEF
     public static final MediaType acceptNQuads        = MediaType.create(WebContent.contentTypeNQuads) ;
     public static final MediaType acceptNQuadsAlt1    = MediaType.create(WebContent.contentTypeNQuadsAlt1) ;
     public static final MediaType acceptNQuadsAlt2    = MediaType.create(WebContent.contentTypeNQuadsAlt2) ;
-    public static final MediaType charsetUTF8         = MediaType.create(WebContent.charsetUTF8) ;
-    
+   
     public static final MediaType acceptRSXML         = MediaType.create(WebContent.contentTypeResultsXML) ;
 
     public static final AcceptList rdfOffer           = AcceptList.create(acceptRDFXML, 
                                                                           acceptTurtle1, acceptTurtle2, acceptTurtle3, 
                                                                           acceptNTriples, acceptNTriplesAlt, 
                                                                           acceptRDFJSON) ;
+    
     public static final AcceptList quadsOffer         = AcceptList.create(acceptNQuads, acceptNQuadsAlt1, acceptNQuadsAlt2, 
                                                                           acceptTriG, acceptTriGAlt1, acceptTriGAlt2) ;
-    public static final AcceptList charsetOffer       = AcceptList.create(charsetUTF8) ;
     
     public static final AcceptList rsOffer            = AcceptList.create(WebContent.contentTypeResultsXML, 
                                                                           WebContent.contentTypeResultsJSON,
                                                                           WebContent.contentTypeTextCSV,
                                                                           WebContent.contentTypeTextTSV,
                                                                           WebContent.contentTypeTextPlain) ;
-    // New to WebContent - remove when update happens.
 }
