25aa741e45b0f51b8cae9f1e5b22ced0eef17cf1
Rob Vesse <rvesse@apache.org>
2013-04-19 21:01:08 +0000

    Expand DISTINCT to REDUCED optimization to cope with SELECT DISTINCT ?var { } ORDER BY ?var style queries (JENA-441)
    git-svn-id: https://svn.apache.org/repos/asf/jena/trunk@1470043 13f79535-47bb-0310-9956-ffa450edef68

--- jena-arq/src/main/java/com/hp/hpl/jena/sparql/algebra/optimize/TransformDistinctToReduced.java
+++ jena-arq/src/main/java/com/hp/hpl/jena/sparql/algebra/optimize/TransformDistinctToReduced.java
@@ -18,22 +18,49 @@
 
 package com.hp.hpl.jena.sparql.algebra.optimize;
 
-import com.hp.hpl.jena.sparql.algebra.Op ;
-import com.hp.hpl.jena.sparql.algebra.TransformCopy ;
-import com.hp.hpl.jena.sparql.algebra.op.OpDistinct ;
-import com.hp.hpl.jena.sparql.algebra.op.OpOrder ;
-import com.hp.hpl.jena.sparql.algebra.op.OpReduced ;
+import com.hp.hpl.jena.sparql.algebra.Op;
+import com.hp.hpl.jena.sparql.algebra.TransformCopy;
+import com.hp.hpl.jena.sparql.algebra.op.OpDistinct;
+import com.hp.hpl.jena.sparql.algebra.op.OpOrder;
+import com.hp.hpl.jena.sparql.algebra.op.OpProject;
+import com.hp.hpl.jena.sparql.algebra.op.OpReduced;
 
+/**
+ * <p>
+ * Transforms generic {@code DISTINCT} plus {@code ORDER BY} combinations to
+ * {@code REDUCED} plus {@code ORDER BY} which typically gives better
+ * performance and memory consumption because engines have to keep less data
+ * in-memory to evaluate it.
+ * </p>
+ * <p>
+ * See also {@link TransformOrderByDistinctAppplication} which is a better
+ * optimization for these kinds of queries but only applies to a limited
+ * range of queries. Where possible that optimization is applied in preference
+ * to this one.
+ * </p>
+ * <p>
+ * {@link TransformTopN} covers the case of {@code DISTINCT} plus
+ * {@code ORDER BY} where there is also a {@code LIMIT}. Where possible that
+ * optimization is applied in preference to either this or
+ * {@link TransformOrderByDistinctAppplication}.
+ * </p>
+ * 
+ */
 public class TransformDistinctToReduced extends TransformCopy {
 
     // Best is this is after TransformTopN but they are order independent
-    // TopN of "reduced or distinct of order" is handled. 
+    // TopN of "reduced or distinct of order" is handled.
     @Override
-    public Op transform(OpDistinct opDistinct, Op subOp) { 
-        if ( subOp instanceof OpOrder ) {
-            return OpReduced.create(subOp) ;
+    public Op transform(OpDistinct opDistinct, Op subOp) {
+        if (subOp instanceof OpOrder) {
+            return OpReduced.create(subOp);
+        } else if (subOp instanceof OpProject) {
+            OpProject project = (OpProject) subOp;
+            if (project.getSubOp() instanceof OpOrder) {
+                return OpReduced.create(subOp);
+            }
         }
-        return super.transform(opDistinct, subOp) ; 
+        return super.transform(opDistinct, subOp);
     }
 
 }
