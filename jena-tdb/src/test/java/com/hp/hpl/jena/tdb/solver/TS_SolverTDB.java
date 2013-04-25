e6a4a89016f45ccfa9970d7ce16c3df0ec2179a7
Andy Seaborne <andy@apache.org>
2013-04-25 17:28:11 +0000

    JENA-443 Some tests
    git-svn-id: https://svn.apache.org/repos/asf/jena/trunk@1475860 13f79535-47bb-0310-9956-ffa450edef68

--- jena-tdb/src/test/java/com/hp/hpl/jena/tdb/solver/TS_SolverTDB.java
+++ jena-tdb/src/test/java/com/hp/hpl/jena/tdb/solver/TS_SolverTDB.java
@@ -24,6 +24,7 @@ import org.junit.runners.Suite ;
 @RunWith(Suite.class)
 @Suite.SuiteClasses( {
     TestSolverTDB.class     // Tests the TDB connectivity
+    , TestStats.class
 })
 
 public class TS_SolverTDB
