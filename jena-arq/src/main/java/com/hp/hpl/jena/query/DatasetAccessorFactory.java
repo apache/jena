e759bda0d137a8bc87e6a015fc41e9bc69df3932
Rob Vesse <rvesse@apache.org>
2013-07-01 20:51:59 +0000

    Add authentication support to DatasetGraphAccessorHTTP, start adding some tests for this (JENA-480)
    git-svn-id: https://svn.apache.org/repos/asf/jena/trunk@1498680 13f79535-47bb-0310-9956-ffa450edef68

--- jena-arq/src/main/java/com/hp/hpl/jena/query/DatasetAccessorFactory.java
+++ jena-arq/src/main/java/com/hp/hpl/jena/query/DatasetAccessorFactory.java
@@ -18,6 +18,7 @@
 
 package com.hp.hpl.jena.query;
 
+import org.apache.jena.atlas.web.auth.HttpAuthenticator;
 import org.apache.jena.web.DatasetAdapter ;
 import org.apache.jena.web.DatasetGraphAccessor ;
 import org.apache.jena.web.DatasetGraphAccessorBasic ;
@@ -25,23 +26,58 @@ import org.apache.jena.web.DatasetGraphAccessorHTTP ;
 
 import com.hp.hpl.jena.sparql.core.DatasetGraph ;
 
+/**
+ * Factory which produces dataset accessors
+ *
+ */
 public class DatasetAccessorFactory
 {
+    /**
+     * Create an accessor for a remote HTTP service
+     * @param serviceURI Service URI
+     * @return Accessor
+     */
     public static DatasetAccessor createHTTP(String serviceURI)
     {
         return adapt(new DatasetGraphAccessorHTTP(serviceURI)) ;
     }
+    
+    /**
+     * Create an accessor for a remote HTTP service that requires authentication
+     * @param serviceURI Service URI
+     * @param authenticator HTTP authenticator
+     * @return Accessor
+     */
+    public static DatasetAccessor createHTTP(String serviceURI, HttpAuthenticator authenticator)
+    {
+        return adapt(new DatasetGraphAccessorHTTP(serviceURI, authenticator));
+    }
 
+    /**
+     * Create an accessor for a local dataset
+     * @param dataset Dataset
+     * @return Accessor
+     */
     public static DatasetAccessor create(DatasetGraph dataset)
     {
         return adapt(new DatasetGraphAccessorBasic(dataset)) ;
     }
     
+    /**
+     * Create an accessor for a local dataset
+     * @param dataset Dataset
+     * @return Accessor
+     */
     public static DatasetAccessor create(Dataset dataset)
     {
         return adapt(new DatasetGraphAccessorBasic(dataset.asDatasetGraph())) ;
     }
 
+    /**
+     * Makes an graph level accessor over a local dataset
+     * @param dataset Dataset
+     * @return Accessor
+     */
     public static DatasetGraphAccessor make(DatasetGraph dataset)
     {
         return new DatasetGraphAccessorBasic(dataset) ;
