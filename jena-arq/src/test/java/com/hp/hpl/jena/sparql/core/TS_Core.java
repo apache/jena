59bed20148ed621468dea3d1350d5651bb1d7e61
Rob Vesse <rvesse@apache.org>
2013-08-26 23:33:47 +0000

    Regression tests for JENA-522
    git-svn-id: https://svn.apache.org/repos/asf/jena/trunk@1517729 13f79535-47bb-0310-9956-ffa450edef68

--- jena-arq/src/test/java/com/hp/hpl/jena/sparql/core/TS_Core.java
+++ jena-arq/src/test/java/com/hp/hpl/jena/sparql/core/TS_Core.java
@@ -32,6 +32,7 @@ import org.junit.runners.Suite ;
     , TestGraphOverDatasetMem.class
     , TestDatasetGraphViewGraphs.class
     , TestDatasetMonitor.class
+    , TestDatasetGraphWithLock.class
 })
 
 public class TS_Core
