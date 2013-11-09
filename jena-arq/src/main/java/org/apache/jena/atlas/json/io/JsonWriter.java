657fbd05ea9f891fa3408d8378733e8bebc9f6c1
Andrew Franklin Seaborne <andy@apache.org>
2013-11-09 00:23:34 +0000

    Better JSON pretty printing
    git-svn-id: https://svn.apache.org/repos/asf/jena/trunk@1540229 13f79535-47bb-0310-9956-ffa450edef68

--- jena-arq/src/main/java/org/apache/jena/atlas/json/io/JsonWriter.java
+++ jena-arq/src/main/java/org/apache/jena/atlas/json/io/JsonWriter.java
@@ -28,9 +28,6 @@ import org.apache.jena.atlas.json.* ;
 
 public class JsonWriter implements JsonVisitor
 {
-    // Use JSWriter????!!!! Multiline control is tricky then.
-    // Or is it?  Because we know the length of things
-    
     IndentedWriter out ;
     
     public JsonWriter() { this(IndentedWriter.stdout) ; }
@@ -45,33 +42,79 @@ public class JsonWriter implements JsonVisitor
     private static String ArrayFinish       = "]" ;
     private static String ArraySep          = "," ; 
 
-    private static String ObjectStart       = "{ " ;
+    private static String ObjectStart       = "{" ;
     private static String ObjectFinish      = "}" ;
     private static String ObjectSep         = " ," ;
     private static String ObjectPairSep     = " : " ;
-    
-    // Make "unnecessary" space
     private static String SPC               = " " ;
+    
+    // Number of key/value pairs in an object before it is always written long form.
+    // It may still written long form if any of the values are consider complicated.   
+    private static int maxCompactObject = 1 ;
+    // Number of array elements before an array is always written long form.
+    // It may still written long form if any of the elements are consider complicated.   
+    private static int maxCompactArray = 1 ;
     
     @Override
     public void visit(JsonObject jsonObject)
     { 
-        out.print(ObjectStart) ;
-        out.incIndent() ;
         boolean first = true ; 
+        Set<String> x = jsonObject.keySet() ;
         
         // Sort keys.
-        Set<String> x = jsonObject.keySet() ;
-        SortedSet<String> y = new TreeSet<String>(x) ;
-        
-        boolean multiLine = x.size() > 1 ;
+        out.print(ObjectStart) ;
+        if ( x.size() == 0 ) {
+            out.print(SPC) ;
+            out.print(ObjectFinish) ;
+            return ;
+        }
 
+        out.incIndent() ;
+
+        if ( isJsonObjectCompact(jsonObject) )
+            writeObjectCompact(jsonObject, x) ;
+        else
+            writeObjectLong(jsonObject, x) ;
+        out.decIndent() ;
+        out.print(ObjectFinish) ;
+    }
+    
+    private static boolean isJsonObjectCompact(JsonObject jsonObject) {
+        Set<String> x = jsonObject.keySet() ;
+        if ( x.size() == 0 ) return true ;
+        if ( x.size() > maxCompactObject ) return false ;
         
-        for ( String k : y )
-        {
-            if ( ! first ) {
+        for ( String k : x ) {
+            if ( ! isCompactValue(jsonObject.get(k)) )
+                return false ;
+        }
+        return true ;
+    }
+    private void writeObjectCompact(JsonObject jsonObject, Set<String> x) {
+        SortedSet<String> y = new TreeSet<String>(x) ;
+        boolean first = true ;
+        for ( String k : y ) {
+            if ( ! first )
                 out.print(ObjectSep) ;
-                out.println() ;
+            first =  false ;
+            JSWriter.outputQuotedString(out, k) ;
+            out.print(ObjectPairSep) ;
+            out.incIndent() ;
+            jsonObject.get(k).visit(this) ;
+            out.decIndent() ;
+        }
+        out.print(SPC) ;
+
+    }
+    
+    private void writeObjectLong(JsonObject jsonObject, Set<String> x) {
+        SortedSet<String> y = new TreeSet<String>(x) ;
+        boolean first = true ;
+        //out.println();
+        for ( String k : y ) {
+            if ( ! first ) {    
+                out.print(ObjectSep) ;
+                out.println() ; 
             }
             first =  false ;
             JSWriter.outputQuotedString(out, k) ;
@@ -80,47 +123,100 @@ public class JsonWriter implements JsonVisitor
             jsonObject.get(k).visit(this) ;
             out.decIndent() ;
         }
-        out.decIndent() ;
-        if ( multiLine )
-            out.println() ;
-        else
-            out.print(SPC) ;
-        out.print(ObjectFinish) ;
-        out.print(SPC) ;
-        
+        out.println();
     }
-
+    
+    private static boolean isCompactValue(JsonValue v) {
+        if ( v.isPrimitive()) return true ;
+        if ( v.isArray() ) {
+            JsonArray a = v.getAsArray() ;
+            if ( a.size() == 0 ) return true ;
+            if ( a.size() > 1 ) return false ;
+            return a.get(0).isPrimitive() ;
+        }
+        if ( v.isObject() ) {
+            JsonObject obj = v.getAsObject() ;
+            Set<String> x = obj.keySet() ; 
+            if ( x.size() == 0 )
+                return true ;
+            if ( x.size() > 1  )
+                return false ;
+            String k = obj.keys().iterator().next();
+            return  obj.get(k).isPrimitive() ;
+        }
+        return false ;
+    }
+    
     @Override
     public void visit(JsonArray jsonArray)
     {
-        boolean multiLine = (jsonArray.size() > 1 ) ;
-        if ( multiLine )
-            out.ensureStartOfLine() ;
+        if ( jsonArray.size() == 0 ) {
+            out.print(ArrayStart) ;
+            out.incIndent() ;
+            out.print(ArrayFinish) ;
+            out.decIndent() ;
+            return ;
+        }
 
+        if ( isJsonArrayCompact(jsonArray) )
+            writeArrayCompact(jsonArray) ;
+        else
+            writeArrayLong(jsonArray) ;
+    }
+
+    private boolean isJsonArrayCompact(JsonArray jsonArray) {
+        if ( jsonArray.size() > maxCompactArray ) return false ;
+        for ( int i = 0 ; i < jsonArray.size() ; i++ ) {
+            if ( ! jsonArray.get(i).isPrimitive() ) 
+                return false ;  
+        }
+        return true ;
+    }
+    
+    private void writeArrayCompact(JsonArray jsonArray) {
         out.print(ArrayStart) ;
         out.incIndent() ;
         boolean first = true ; 
 
         for ( JsonValue elt : jsonArray )
         {
-            if ( ! first )
-            {
+            if ( ! first ) {
                 out.print(ArraySep) ;
-                out.println() ;
-                multiLine = true ; 
+                out.print(SPC) ;
             }
             first = false ;
             elt.visit(this) ;
-            out.print(SPC) ;
+            
         }
+        out.print(SPC) ;
         out.decIndent() ;
-        if ( multiLine )
-            out.ensureStartOfLine() ;
-//        else
-//            out.print(SPC) ;
         out.print(ArrayFinish) ;
     }
 
+    private void writeArrayLong(JsonArray jsonArray) {
+        //out.println() ;
+        out.print(ArrayStart) ;
+        
+        out.incIndent() ;
+        out.ensureStartOfLine() ;
+        boolean first = true ; 
+
+        for ( JsonValue elt : jsonArray )
+        {
+            if ( ! first )
+            {
+                out.print(SPC) ;
+                out.print(ArraySep) ;
+                out.println() ;
+            }
+            first = false ;
+            elt.visit(this) ;
+        }
+        out.decIndent() ;
+        out.ensureStartOfLine() ;
+        out.print(ArrayFinish) ;
+    }
+    
     @Override
     public void visit(JsonString jsonString)
     {
