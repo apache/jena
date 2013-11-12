8fc8015fa9bef2d9008a8237d7c8ec49f72456eb
Rob Vesse <rvesse@apache.org>
2013-11-12 16:55:16 +0000

    Re-enable TransformDistinctToReduced making it much stricter about the kinds of queries it will optimize. Expands the unit tests to cover various scenarios identified in the associated bug (JENA-587)
    git-svn-id: https://svn.apache.org/repos/asf/jena/trunk@1541139 13f79535-47bb-0310-9956-ffa450edef68

--- jena-arq/src/main/java/com/hp/hpl/jena/sparql/algebra/optimize/TransformDistinctToReduced.java
+++ jena-arq/src/main/java/com/hp/hpl/jena/sparql/algebra/optimize/TransformDistinctToReduced.java
@@ -18,12 +18,17 @@
 
 package com.hp.hpl.jena.sparql.algebra.optimize;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import com.hp.hpl.jena.query.SortCondition;
 import com.hp.hpl.jena.sparql.algebra.Op;
 import com.hp.hpl.jena.sparql.algebra.TransformCopy;
 import com.hp.hpl.jena.sparql.algebra.op.OpDistinct;
 import com.hp.hpl.jena.sparql.algebra.op.OpOrder;
 import com.hp.hpl.jena.sparql.algebra.op.OpProject;
 import com.hp.hpl.jena.sparql.algebra.op.OpReduced;
+import com.hp.hpl.jena.sparql.core.Var;
 
 /**
  * <p>
@@ -33,10 +38,22 @@ import com.hp.hpl.jena.sparql.algebra.op.OpReduced;
  * in-memory to evaluate it.
  * </p>
  * <p>
+ * As with most optimizations this is only applied when it is safe to do so. The
+ * criteria for being safe to do so are as follows:
+ * </p>
+ * <ul>
+ * <li>Uses both {@code ORDER BY} and {@code DISTINCT} on the same level of the
+ * query</li>
+ * <li>There is a fixed list of variables to project i.e. not {@code SELECT *}</li>
+ * <li>{@code ORDER BY} conditions cover all the projected variables prior to
+ * the use of any other variables</li>
+ * </ul>
+ * <h3>Related Optimizations</h3>
+ * <p>
  * See also {@link TransformOrderByDistinctAppplication} which is a better
- * optimization for these kinds of queries but only applies to a limited
- * range of queries. Where possible that optimization is applied in preference
- * to this one.
+ * optimization for these kinds of queries but only applies to a more limited range
+ * of queries. Where possible that optimization is applied in preference to this
+ * one.
  * </p>
  * <p>
  * {@link TransformTopN} covers the case of {@code DISTINCT} plus
@@ -52,15 +69,65 @@ public class TransformDistinctToReduced extends TransformCopy {
     // TopN of "reduced or distinct of order" is handled.
     @Override
     public Op transform(OpDistinct opDistinct, Op subOp) {
-        if (subOp instanceof OpOrder) {
-            return OpReduced.create(subOp);
-        } else if (subOp instanceof OpProject) {
-            OpProject project = (OpProject) subOp;
-            if (project.getSubOp() instanceof OpOrder) {
-                return OpReduced.create(subOp);
+        if (subOp instanceof OpProject) {
+            OpProject opProject = (OpProject) subOp;
+            if (opProject.getSubOp() instanceof OpOrder) {
+                OpOrder opOrder = (OpOrder) opProject.getSubOp();
+                if (isSafe(opProject, opOrder)) {
+                    return OpReduced.create(subOp);
+                }
             }
         }
         return super.transform(opDistinct, subOp);
     }
 
+    protected boolean isSafe(OpProject opProject, OpOrder opOrder) {
+        Set<Var> projectVars = new HashSet<Var>(opProject.getVars());
+        Set<Var> seenVars = new HashSet<Var>();
+
+        // For the optimization to be safe all project variables must appear in
+        // the ordering prior to any unprojected variables
+        // Ordering by expressions is fine provided they use only projected
+        // variables
+        boolean ok = true;
+        for (SortCondition cond : opOrder.getConditions()) {
+            if (!isValidSortCondition(cond, projectVars, seenVars)) {
+                ok = false;
+                break;
+            }
+
+            // As soon as we've seen all variables we know this is safe and any
+            // further sort conditions are irrelevant
+            if (seenVars.size() == projectVars.size())
+                break;
+        }
+
+        return ok;
+    }
+
+    /**
+     * Determines whether a sort condition is valid in terms of this optimizer
+     * 
+     * @param cond
+     *            Sort Condition
+     * @param projectVars
+     *            Project Variables
+     * @return True if valid, false otherwise
+     */
+    private boolean isValidSortCondition(SortCondition cond, Set<Var> projectVars, Set<Var> seenVars) {
+        if (cond.getExpression().isVariable()) {
+            if (projectVars.contains(cond.getExpression().asVar())) {
+                seenVars.add(cond.getExpression().asVar());
+                return true;
+            }
+            return false;
+        } else {
+            for (Var v : cond.getExpression().getVarsMentioned()) {
+                if (!projectVars.contains(v))
+                    return false;
+                seenVars.add(v);
+            }
+            return true;
+        }
+    }
 }
