d640f375bdc556cae308657e461b98ca53a1b42f
Andrew Franklin Seaborne <andy@apache.org>
2013-12-08 17:31:58 +0000

    Consoldiate on RDFLanguages for content-type to Lang
    git-svn-id: https://svn.apache.org/repos/asf/jena/trunk@1549071 13f79535-47bb-0310-9956-ffa450edef68

--- jena-arq/src/main/java/org/apache/jena/riot/WebContent.java
+++ jena-arq/src/main/java/org/apache/jena/riot/WebContent.java
@@ -18,19 +18,10 @@
 
 package org.apache.jena.riot;
 
-import java.util.HashMap ;
-import java.util.Map ;
-
-import org.apache.jena.atlas.web.MediaType ;
 
 public class WebContent
 {
     // Names for things.
-    
-    // contentType => ctStr
-    
-    // Rename as: 
-    // TEXT_TURTLE etc etc.
     
     public static final String contentTypeN3                = "text/rdf+n3" ;
     public static final String contentTypeN3Alt1            = "application/n3" ;
@@ -101,29 +92,6 @@ public class WebContent
     /** Java name for UTF-8 encoding */
     public static final String encodingUTF8         = "utf-8" ;
     
-    private static Map<String, Lang> mapContentTypeToLang = new HashMap<String, Lang>() ;
-    static {
-        // Or is code preferrable?
-        mapContentTypeToLang.put(contentTypeRDFXML,         RDFLanguages.RDFXML) ;
-        mapContentTypeToLang.put(contentTypeN3,             RDFLanguages.N3);
-        mapContentTypeToLang.put(contentTypeN3Alt1,         RDFLanguages.N3);
-        mapContentTypeToLang.put(contentTypeN3Alt2,         RDFLanguages.N3);
-        mapContentTypeToLang.put(contentTypeTurtle,         RDFLanguages.TURTLE) ;
-        mapContentTypeToLang.put(contentTypeTurtleAlt1,     RDFLanguages.TURTLE) ;
-        mapContentTypeToLang.put(contentTypeTurtleAlt2,     RDFLanguages.TURTLE) ;
-        mapContentTypeToLang.put(contentTypeNTriples,       RDFLanguages.NTRIPLES) ;
-        // Remove? Causes more trouble than it's worth.
-        mapContentTypeToLang.put(contentTypeNTriplesAlt,    RDFLanguages.NTRIPLES) ;    // text/plain
-        mapContentTypeToLang.put(contentTypeRdfJson,        RDFLanguages.RDFJSON) ;
-
-        mapContentTypeToLang.put(contentTypeNQuads,         RDFLanguages.NQUADS) ;
-        mapContentTypeToLang.put(contentTypeNQuadsAlt1,     RDFLanguages.NQUADS) ;
-        mapContentTypeToLang.put(contentTypeNQuadsAlt2,     RDFLanguages.NQUADS) ;
-        mapContentTypeToLang.put(contentTypeTriG,           RDFLanguages.TRIG) ;
-        mapContentTypeToLang.put(contentTypeTriGAlt1,       RDFLanguages.TRIG) ;
-        mapContentTypeToLang.put(contentTypeTriGAlt2,       RDFLanguages.TRIG) ;
-    }
-
     /** Accept header part when looking for a graph */
     private static final String defaultGraphAccept          =  "text/turtle,application/n-triples;q=0.9,application/rdf+xml;q=0.8,application/xml;q=0.7" ;
     /** Accept header when looking for a graph */
@@ -137,59 +105,33 @@ public class WebContent
     /** Accept header when looking for a graph or dataset */
     public static final String defaultRDFAcceptHeader       =  defaultGraphAccept+","+defaultDatasetAccept+",*/*;q=0.5" ;
     
-    
     /** Return our "canonical" name for a Content Type.
      * This should be the standard one, no X-*
      */
     public static String contentTypeCanonical(String contentType)
     { 
-        Lang lang = contentTypeToLang(contentType) ;
+        Lang lang = RDFLanguages.contentTypeToLang(contentType) ;
         if ( lang == null )
             return null ;
-        return mapLangToContentType.get(lang) ;
+        return lang.getHeaderString() ;
+        //return mapLangToContentType.get(lang) ;
     }
 
-    /** @deprecated Use RDFlanguages.contentTypeToLang(String)*/
+    /** @deprecated Use {@linkplain RDFLanguages#contentTypeToLang(String)}*/
     @Deprecated
     public static Lang contentTypeToLang(String contentType)
-    { 
-        if ( contentType == null )
-            return null ;
-        return mapContentTypeToLang.get(contentType) ;
+    {
+        return RDFLanguages.contentTypeToLang(contentType) ;
     }
 
-    /** Canonical names */
-    private static Map<Lang, String> mapLangToContentType =  new HashMap<Lang, String>() ;
-    static {
-        mapLangToContentType.put(RDFLanguages.N3,           contentTypeN3) ;
-        mapLangToContentType.put(RDFLanguages.TURTLE,       contentTypeTurtle) ;
-        mapLangToContentType.put(RDFLanguages.NTRIPLES,     contentTypeNTriples) ;
-        mapLangToContentType.put(RDFLanguages.RDFXML,       contentTypeRDFXML) ;
-        mapLangToContentType.put(RDFLanguages.RDFJSON,		contentTypeRdfJson) ;
-        
-        mapLangToContentType.put(RDFLanguages.NQUADS,       contentTypeNQuads) ;
-        mapLangToContentType.put(RDFLanguages.TRIG,         contentTypeTriG) ;
-    }
-    public static String mapLangToContentType(Lang lang) { return mapLangToContentType.get(lang) ; }
+    /** @deprecated Use {@linkplain Lang#getHeaderString()} */
+    @Deprecated
+    public static String mapLangToContentType(Lang lang) { return lang.getHeaderString() ; }
     
+    /** @deprecated Use {@linkplain RDFLanguages#getCharsetForContentType(String)} */
+    @Deprecated
     public static String getCharsetForContentType(String contentType)
     {
-        MediaType ct = MediaType.create(contentType) ;
-        if ( ct.getCharset() != null )
-            return ct.getCharset() ;
-        
-        String mt = ct.getContentType() ;
-        if ( contentTypeNTriples.equals(mt) )       return charsetUTF8 ;
-        if ( contentTypeNTriplesAlt.equals(mt) )    return charsetASCII ;
-        if ( contentTypeNQuads.equals(mt) )         return charsetUTF8 ;
-        if ( contentTypeNQuadsAlt1.equals(mt) )      return charsetASCII ;
-        if ( contentTypeNQuadsAlt2.equals(mt) )      return charsetASCII ;
-        return charsetUTF8 ;
+        return RDFLanguages.getCharsetForContentType(contentType) ;
     }
-
-//    public static ContentType contentTypeForFilename(String filename)
-//    {
-//    }
-    
-
 }
