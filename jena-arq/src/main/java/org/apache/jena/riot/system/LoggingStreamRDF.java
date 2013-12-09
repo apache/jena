0d35ad16f92d0a58a41288b5696432d6fc09156f
Andy Seaborne <andy@apache.org>
2013-12-09 16:17:43 +0000

    Utilities for debugging/output.
    git-svn-id: https://svn.apache.org/repos/asf/jena/trunk@1549601 13f79535-47bb-0310-9956-ffa450edef68

---
+++ jena-arq/src/main/java/org/apache/jena/riot/system/LoggingStreamRDF.java
@@ -0,0 +1,73 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.jena.riot.system;
+
+import org.apache.jena.atlas.lib.Tuple ;
+import org.apache.jena.riot.out.NodeFmtLib ;
+import org.slf4j.Logger ;
+
+import com.hp.hpl.jena.graph.Node ;
+import com.hp.hpl.jena.graph.Triple ;
+import com.hp.hpl.jena.sparql.core.Quad ;
+
+public class LoggingStreamRDF implements StreamRDF
+{
+    private Logger log ;
+
+    public LoggingStreamRDF(Logger log) { this.log = log ; }
+
+    @Override
+    public void start()
+    {}
+
+    @Override
+    public void triple(Triple triple)
+    {
+        String string = NodeFmtLib.str(triple) ;
+        log.info(string) ;
+    }
+
+    @Override
+    public void quad(Quad quad)
+    {
+        String string = NodeFmtLib.str(quad) ;
+        log.info(string) ;
+    }
+
+    @Override
+    public void tuple(Tuple<Node> tuple)
+    {}
+
+    @Override
+    public void base(String base)
+    {
+        log.info("BASE -- "+base) ;
+    }
+
+    @Override
+    public void prefix(String prefix, String iri)
+    {
+        log.info("Prefix ("+prefix+","+iri+")")  ;
+    }
+
+    @Override
+    public void finish()
+    {}
+
+}
---
+++ jena-arq/src/main/java/org/apache/jena/riot/system/PrintingStreamRDF.java
@@ -0,0 +1,56 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.jena.riot.system;
+
+import java.io.OutputStream ;
+
+import org.apache.jena.atlas.io.AWriter ;
+import org.apache.jena.atlas.io.IO ;
+import org.apache.jena.riot.writer.WriterStreamRDFTuples ;
+
+/** Primarily for debugging */
+public class PrintingStreamRDF extends WriterStreamRDFTuples //implements StreamRDF
+{
+    public PrintingStreamRDF(OutputStream out) { 
+        super(init(out)) ;
+    }
+
+    private static AWriter init(OutputStream out) {
+        return IO.wrapUTF8(out) ;
+    }
+
+    @Override
+    public void base(String base) {
+        out.print("BASE") ;
+        out.print("    ") ;
+        nodeFmt.formatURI(out, base); 
+        out.println();
+    }
+
+    @Override
+    public void prefix(String prefix, String iri) {
+        out.print("PREFIX") ;
+        out.print("  ") ;
+        out.print(prefix) ;
+        out.print(":  ") ;
+        nodeFmt.formatURI(out, iri);
+        out.println();
+    }
+
+}
