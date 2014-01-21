a6659dea3afd42a20f91abd2db4f8c75ceed1c19
Andy Seaborne <andy@apache.org>
2014-01-21 20:33:06 +0000

    Tidy up code and comments. Add tests for canonical form.
    git-svn-id: https://svn.apache.org/repos/asf/jena/trunk@1560173 13f79535-47bb-0310-9956-ffa450edef68

--- jena-arq/src/test/java/org/apache/jena/riot/web/TestLangTag.java
+++ jena-arq/src/test/java/org/apache/jena/riot/web/TestLangTag.java
@@ -62,16 +62,16 @@ public class TestLangTag extends BaseTest
     
     private static void parseGood(String input, String ex_output, String... ex_parts )
     {
-      String[] parts = LangTag.parse(input) ;
-      assertArrayEquals(ex_parts, parts) ;
-      
-      String output = LangTag.canonical(input) ;
-      assertEquals(ex_output, output) ;
-      
-      assertTrue(LangTag.check(input)) ;
+        String[] parts = LangTag.parse(input) ;
+        assertArrayEquals(ex_parts, parts) ;
+
+        String output = LangTag.canonical(input) ;
+        assertEquals(ex_output, output) ;
+
+        assertTrue(LangTag.check(input)) ;
     }
 
-    
+
     private static void parseBad(String input)
     {
         String[] parts = LangTag.parse(input) ;
@@ -80,5 +80,41 @@ public class TestLangTag extends BaseTest
         assertEquals(input, output) ;
         assertFalse(LangTag.check(input)) ;
     }
+    
+    private void testCanonical(String input, String ex_output) {
+        String output = LangTag.canonical(input) ;
+        assertEquals(ex_output, output) ;
+    }
 
+    // "x" extensions and irregular forms are left alone, including "sgn-be-fr" 
+
+    // Mentioned in BCP 47 tests
+//    @Test public void parseCanonical_01() { testCanonical("en-ca-x-ca","en-CA-x-ca"); }         // "x"
+//    @Test public void parseCanonical_02() { testCanonical("EN-ca-X-Ca","en-CA-x-ca"); }
+//    @Test public void parseCanonical_03() { testCanonical("En-Ca-X-Ca","en-CA-x-ca"); }
+//    @Test public void parseCanonical_04() { testCanonical("SGN-BE-FR","sgn-BE-FR"); }   // Irregular
+//    @Test public void parseCanonical_05() { testCanonical("sgn-be-fr","sgn-BE-FR"); }   // Irregular
+//    @Test public void parseCanonical_06() { testCanonical("AZ-latn-x-LATN","az-Latn-x-latn"); }
+//    @Test public void parseCanonical_07() { testCanonical("Az-latn-X-Latn","az-Latn-x-latn"); }
+    
+    @Test public void parseCanonical_10() { testCanonical("zh-hant",            "zh-Hant"); }
+    @Test public void parseCanonical_11() { testCanonical("zh-latn-wadegile",   "zh-Latn-wadegile"); }
+    @Test public void parseCanonical_12() { testCanonical("zh-latn-pinyin",     "zh-Latn-pinyin"); }
+    @Test public void parseCanonical_13() { testCanonical("en-us",              "en-US"); }
+    @Test public void parseCanonical_14() { testCanonical("EN-Gb",              "en-GB"); }
+    @Test public void parseCanonical_15() { testCanonical("qqq-002",            "qqq-002"); }
+    @Test public void parseCanonical_16() { testCanonical("ja-latn",            "ja-Latn"); }
+    @Test public void parseCanonical_17() { testCanonical("x-local",            "x-local"); }
+    @Test public void parseCanonical_18() { testCanonical("he-latn",            "he-Latn"); }
+    @Test public void parseCanonical_19() { testCanonical("und",                "und"); }
+    @Test public void parseCanonical_20() { testCanonical("nn",                 "nn"); }
+    @Test public void parseCanonical_21() { testCanonical("ko-latn",            "ko-Latn"); }
+    @Test public void parseCanonical_22() { testCanonical("ar-latn",            "ar-Latn"); }
+    @Test public void parseCanonical_23() { testCanonical("la-x-liturgic",      "la-x-liturgic"); }
+    @Test public void parseCanonical_24() { testCanonical("fa-x-middle",        "fa-x-middle"); }
+    @Test public void parseCanonical_25() { testCanonical("qqq-142",            "qqq-142"); }
+    @Test public void parseCanonical_26() { testCanonical("bnt",                "bnt"); }
+    @Test public void parseCanonical_27() { testCanonical("grc-x-liturgic",     "grc-x-liturgic"); }
+    @Test public void parseCanonical_28() { testCanonical("egy-Latn",           "egy-Latn"); }
+    @Test public void parseCanonical_29() { testCanonical("la-x-medieval",      "la-x-medieval"); }
 }
