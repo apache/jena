180ec2ff5750c43ef88b47440246598a5d6d366c
Andy Seaborne <andy@apache.org>
2014-01-29 17:58:52 +0000

    Add
    git-svn-id: https://svn.apache.org/repos/asf/jena/branches/jena-fuseki-new-ui@1562525 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/main/java/org/apache/jena/fuseki/server/DataService.java
@@ -0,0 +1,236 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.jena.fuseki.server;
+
+import static org.apache.jena.fuseki.server.DatasetStatus.CLOSING ;
+import static org.apache.jena.fuseki.server.DatasetStatus.UNINITIALIZED ;
+
+import java.util.Collection ;
+import java.util.HashMap ;
+import java.util.List ;
+import java.util.Map ;
+import java.util.concurrent.atomic.AtomicBoolean ;
+import java.util.concurrent.atomic.AtomicLong ;
+
+import org.apache.jena.atlas.lib.MultiMap ;
+import org.apache.jena.atlas.lib.MultiMapToList ;
+import org.apache.jena.fuseki.DEF ;
+import org.apache.jena.fuseki.Fuseki ;
+
+import com.hp.hpl.jena.query.ReadWrite ;
+import com.hp.hpl.jena.rdf.model.Resource ;
+import com.hp.hpl.jena.sparql.core.DatasetGraph ;
+import com.hp.hpl.jena.sparql.core.DatasetGraphFactory ;
+import com.hp.hpl.jena.sparql.core.DatasetGraphReadOnly ;
+import com.hp.hpl.jena.tdb.StoreConnection ;
+import com.hp.hpl.jena.tdb.transaction.DatasetGraphTransaction ;
+
+public class DataService { //implements DatasetMXBean {
+    // XXX Add a "null model assembler".
+    public static DataService dummy = new DataService(null, null) ;
+    static {
+        dummy.dataset = new DatasetGraphReadOnly(DatasetGraphFactory.createMemFixed()) ;
+        dummy.addEndpoint(OperationName.Query, DEF.ServiceQuery) ;
+        dummy.addEndpoint(OperationName.Query, DEF.ServiceQueryAlt) ;
+    }
+    
+    private final Resource assemblerItem ;
+    private DatasetGraph dataset = null ;              // Only valid if active.
+
+
+    private MultiMapToList<OperationName, Operation> operations     = MultiMap.createMapList() ;
+    private Map<String, Operation> endpoints                        = new HashMap<String, Operation>() ;
+    
+    private volatile DatasetStatus state = UNINITIALIZED ;
+
+    // DataService-level counters.
+    private final CounterSet counters                   = new CounterSet() ;
+    private final AtomicLong    requestCounter          = new AtomicLong(0) ;   
+    private final AtomicBoolean offlineInProgress       = new AtomicBoolean(false) ;
+    private final AtomicBoolean acceptingRequests       = new AtomicBoolean(true) ;
+
+    @Deprecated
+    // Change to use assemblers everywhere.
+    public static DataService create(DatasetGraph dataset) {
+        DataService dServ = new DataService(null, dataset) ; 
+        return dServ ;
+    }
+    
+    public DataService(Resource item, DatasetGraph dataset) { 
+        this.assemblerItem = item ;
+        this.dataset = dataset ;
+        counters.add(CounterName.Requests) ;
+        counters.add(CounterName.RequestsGood) ;
+        counters.add(CounterName.RequestsBad) ;
+    }
+    
+    public DatasetGraph getDataset() {
+        return dataset ; 
+    }
+    
+    public void addEndpoint(OperationName operationName, String endpointName) {
+        Operation oper = new Operation(operationName, endpointName) ;
+        endpoints.put(endpointName, oper) ;
+        operations.put(operationName, oper);
+    }
+    
+    public Operation getOperation(String endpointName) {
+        return endpoints.get(endpointName) ;
+    }
+
+    public List<Operation> getOperation(OperationName opName) {
+        return operations.get(opName) ;
+    }
+
+    public Collection<Operation> getOperations() {
+        return endpoints.values() ;
+    }
+
+    //@Override
+    public boolean allowUpdate()    { return true ; }
+
+    public void goOffline()         { 
+        offlineInProgress.set(true) ;
+        acceptingRequests.set(false) ;
+        state = DatasetStatus.OFFLINE ; 
+    }
+    
+    public void goActive()         { 
+        offlineInProgress.set(false) ;
+        acceptingRequests.set(true) ;
+        state = DatasetStatus.ACTIVE ; 
+    }
+
+    public boolean isAcceptingRequests() {
+        return acceptingRequests.get() ;
+    }
+    
+    //@Override
+    public  CounterSet getCounters() { return counters ; }
+    
+    //@Override 
+    public long getRequests() { 
+        return counters.value(CounterName.Requests) ;
+    }
+
+    //@Override
+    public long getRequestsGood() {
+        return counters.value(CounterName.RequestsGood) ;
+    }
+    //@Override
+    public long getRequestsBad() {
+        return counters.value(CounterName.RequestsBad) ;
+    }
+
+    
+    
+//    public static DataService nullRef() {
+//        return new DataService(null, null, null ) ; 
+//    }
+//
+//    public static DataService create(String name, DatasetGraph dataset) {
+//        return new DataService(name, dataset, null ) ; 
+//    }
+//
+//    public static DatasetRef createLink(String name, DatasetRef other) {
+//        return new DataService(name, null, other) ; 
+//    }
+
+//    public static DataService build(Model assembler) {
+//        List<Resource> services = FusekiConfig.getByType(FusekiVocab.fusekiService, assembler) ;
+//        if ( services.size() == 0 )
+//            throw new FusekiConfigException("No services found (no resource with type "
+//                                            + FusekiConfig.strForResource(FusekiVocab.fusekiService)) ;
+//        if ( services.size() > 1 )
+//            throw new FusekiConfigException("Found "+services.size()+
+//                                            " data services (must be exactly one in a configuration file)") ;
+//        
+//        Resource service = services.get(0) ;
+//        FusekiConfig.processService(service) ;
+//        
+//        return null ;
+//    }
+
+  /** Counter of active read transactions */
+  public AtomicLong   activeReadTxn           = new AtomicLong(0) ;
+  
+  /** Counter of active write transactions */
+  public AtomicLong   activeWriteTxn          = new AtomicLong(0) ;
+
+  /** Cumulative counter of read transactions */
+  public AtomicLong   totalReadTxn            = new AtomicLong(0) ;
+
+  /** Cumulative counter of writer transactions */
+  public AtomicLong   totalWriteTxn           = new AtomicLong(0) ;
+  
+  public void startTxn(ReadWrite mode)
+  {
+      switch(mode)
+      {
+          case READ:  
+              activeReadTxn.getAndIncrement() ;
+              totalReadTxn.getAndIncrement() ;
+              break ;
+          case WRITE:
+              activeWriteTxn.getAndIncrement() ;
+              totalWriteTxn.getAndIncrement() ;
+              break ;
+      }
+  }
+  
+  public void finishTxn(ReadWrite mode)
+  {
+      switch(mode)
+      {
+          case READ:  
+              activeReadTxn.decrementAndGet() ;
+              break ;
+          case WRITE:
+              activeWriteTxn.decrementAndGet() ;
+              break ;
+      }
+      checkShutdown() ;
+  }
+
+  private void checkShutdown() {
+      if ( state == CLOSING ) {
+          if ( activeReadTxn.get() == 0 && activeWriteTxn.get() == 0 )
+              shutdown() ;
+      }
+  }
+  
+  private void shutdown() {
+      Fuseki.serverLog.info("Shutting down dataset") ;
+      
+//      if ( dataset instanceof DatasetGraphTransaction )
+//          StoreConnection.release( ((DatasetGraphTransaction)dataset).getLocation() ) ;
+//      else 
+//          dataset.close() ;
+
+      dataset.close() ;
+      if ( dataset instanceof DatasetGraphTransaction ) {
+          DatasetGraphTransaction dsgtxn = (DatasetGraphTransaction)dataset ;
+          StoreConnection.release(dsgtxn.getLocation()) ;
+      }
+      dataset = null ; 
+  }
+
+    
+}
+
