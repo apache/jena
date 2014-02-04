7da83ada4381f3d5ec31ec8388e191783c8770e2
Andrew Franklin Seaborne <andy@apache.org>
2014-02-04 21:52:30 +0000

    Add JSON-LD to Accept strings.
    git-svn-id: https://svn.apache.org/repos/asf/jena/trunk@1564521 13f79535-47bb-0310-9956-ffa450edef68

--- jena-arq/src/main/java/org/apache/jena/riot/WebContent.java
+++ jena-arq/src/main/java/org/apache/jena/riot/WebContent.java
@@ -156,12 +156,16 @@ public class WebContent
     public static final String encodingUTF8         = "utf-8" ;
     
     /** Accept header part when looking for a graph */
-    private static final String defaultGraphAccept          =  "text/turtle,application/n-triples;q=0.9,application/rdf+xml;q=0.8,application/xml;q=0.7" ;
+    private static final String defaultGraphAccept          
+        =  "text/turtle,application/n-triples;q=0.9,application/rdf+xml;q=0.8,application/xml;q=0.7,application/ld+json;q=0.5" ;
+    
     /** Accept header when looking for a graph */
     public static final String defaultGraphAcceptHeader     =  defaultGraphAccept+",*/*;q=0.5" ; 
 
     /** Accept header part when looking for a dataset */
-    private static final String defaultDatasetAccept         =  "application/trig,application/n-quads;q=0.9,text/x-nquads;q=0.8,application/x-trig;q=0.7" ;
+    private static final String defaultDatasetAccept         
+        =  "application/trig,application/n-quads;q=0.9,text/x-nquads;q=0.8,application/x-trig;q=0.7,application/ld+json;q=0.5" ;
+    
     /** Accept header when looking for a dataset */
     public static final String defaultDatasetAcceptHeader   =  defaultDatasetAccept+",*/*;q=0.5" ;
     
