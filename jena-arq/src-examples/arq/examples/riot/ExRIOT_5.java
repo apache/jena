afed58878154af13ffb4ba24bdd672fc835780a3
Andrew Franklin Seaborne <andy@apache.org>
2014-02-06 16:44:57 +0000

    Tidy up.
    git-svn-id: https://svn.apache.org/repos/asf/jena/trunk@1565334 13f79535-47bb-0310-9956-ffa450edef68

--- jena-arq/src-examples/arq/examples/riot/ExRIOT_5.java
+++ jena-arq/src-examples/arq/examples/riot/ExRIOT_5.java
@@ -16,7 +16,7 @@
  * limitations under the License.
  */
 
-package arq.examples.riot;
+package arq.examples.riot ;
 
 import java.io.InputStream ;
 import java.io.Reader ;
@@ -36,77 +36,82 @@ import com.hp.hpl.jena.sparql.sse.SSE ;
 import com.hp.hpl.jena.sparql.sse.builders.BuilderGraph ;
 import com.hp.hpl.jena.sparql.util.Context ;
 
-/** Example of using RIOT : register a new language */ 
+/** Example of using RIOT : register a new language */
 public class ExRIOT_5
 {
-    public static void main(String...argv)
-    {
+    public static void main(String... argv) {
         Lang lang = LangBuilder.create("SSE", "text/x-sse").addFileExtensions("rsse").build() ;
         // This just registers the name, not the parser.
         RDFLanguages.register(lang) ;
-        
+
         // Register the parser factory.
         ReaderRIOTFactory factory = new SSEReaderFactory() ;
         RDFParserRegistry.registerLangTriples(lang, factory) ;
-        
+
         // use it ...
         String filename = "data.rsse" ;
-        // model.read(filename) 
+        // model.read(filename)
         Model model = RDFDataMgr.loadModel(filename) ;
-        
+
         // print results.
         model.write(System.out, "TTL") ;
-        
+
         // Optional extra:
-        // If needed to set or override the syntax, register the name explicitly ...
+        // If needed to set or override the syntax, register the name explicitly
+        // ...
         System.out.println("## --") ;
         IO_Jena.registerForModelRead("SSE", RDFReaderSSE.class) ;
         // and use read( , "SSE")
         Model model2 = ModelFactory.createDefaultModel().read(filename, "SSE") ;
         model2.write(System.out, "TTL") ;
     }
-    
+
     static class SSEReaderFactory implements ReaderRIOTFactory
     {
         @Override
-        public ReaderRIOT create(Lang language)
-        {
-            // This is just an example - it reads a graph in 
-            // http://jena.apache.org/documentation/notes/sse.html
-            // format.  It is not a streaming parser; it creates some triples,
-            // then send them to the output. This style might be useful for creating
-            // triples from a converter process or program.
-            
-            return new ReaderRIOT() {
-                @Override
-                public void read(InputStream in, String baseURI, ContentType ct, StreamRDF output, Context context)
-                {
-                    Item item = SSE.parse(in) ;
-                    read(item, baseURI, ct, output, context); 
-                    
-                }
-                @Override
-                public void read(Reader in, String baseURI, ContentType ct, StreamRDF output, Context context) {
-                    Item item = SSE.parse(in) ;
-                    read(item, baseURI, ct, output, context); 
-                }
-                private void read(Item item, String baseURI, ContentType ct, StreamRDF output, Context context) {
-                    Graph graph = BuilderGraph.buildGraph(item) ;
-                    Iterator<Triple> iter = graph.find(null, null, null) ;
-                    for ( ; iter.hasNext() ; )
-                        output.triple(iter.next()) ;
-                }
-            } ;
+        public ReaderRIOT create(Lang language) {
+            return new SSEReader() ;
         }
     }
-    
+
+    static class SSEReader implements ReaderRIOT
+    {
+        // This is just an example - it reads a graph in
+        // http://jena.apache.org/documentation/notes/sse.html
+        // format. It is not a streaming parser; it creates some triples,
+        // then send them to the output. This style might be useful for creating
+        // triples from a converter process or program.
+
+        @Override
+        public void read(InputStream in, String baseURI, ContentType ct, StreamRDF output, Context context) {
+            Item item = SSE.parse(in) ;
+            read(item, baseURI, ct, output, context) ;
+
+        }
+
+        @Override
+        public void read(Reader in, String baseURI, ContentType ct, StreamRDF output, Context context) {
+            Item item = SSE.parse(in) ;
+            read(item, baseURI, ct, output, context) ;
+        }
+
+        private void read(Item item, String baseURI, ContentType ct, StreamRDF output, Context context) {
+            Graph graph = BuilderGraph.buildGraph(item) ;
+            Iterator<Triple> iter = graph.find(null, null, null) ;
+            for ( ; iter.hasNext() ; )
+                output.triple(iter.next()) ;
+        }
+    }
+
     // Model.read adapter - must be public.
-    public static class RDFReaderSSE extends RDFReaderRIOT { public RDFReaderSSE() { super("SSE") ; } } 
-    
-    /* data.rsse :
-     * (graph
-     *   (<s> <p1> 123)
-     *   (<s> <p2> 456) )
+    public static class RDFReaderSSE extends RDFReaderRIOT
+    {
+        public RDFReaderSSE() {
+            super("SSE") ;
+        }
+    }
+
+    /*
+     * data.rsse : (graph (<s> <p1> 123) (<s> <p2> 456) )
      */
 }
- 
