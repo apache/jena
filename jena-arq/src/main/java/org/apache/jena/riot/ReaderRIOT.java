49b8c3c2dcbca40f1a6239f763789f3bbd9f5bed
Andy Seaborne <andy@apache.org>
2014-02-16 21:22:29 +0000

    Deprecation for future removal/refactoring of public RiotWriter operations (use RDFDataMgr alternatives).
    git-svn-id: https://svn.apache.org/repos/asf/jena/trunk@1568835 13f79535-47bb-0310-9956-ffa450edef68

--- jena-arq/src/main/java/org/apache/jena/riot/ReaderRIOT.java
+++ jena-arq/src/main/java/org/apache/jena/riot/ReaderRIOT.java
@@ -30,12 +30,33 @@ import com.hp.hpl.jena.sparql.util.Context ;
 /** Interface to parsing processes that takes an input stream and emit items.
  *  The "read" operation may be called repeatedly for a single ReaderRIOT, with different
  *  arguments.  The StreamRDF destination would have to cope with concurrent operation
- *  if these read operatuions overlap. 
+ *  if these read operations overlap. 
  */
 
 public interface ReaderRIOT
 {
+    /** Read from an InputStream and output RDF on the StreamRDF.
+     * @param in        InputStream
+     * @param baseURI   Base URI (or null)
+     * @param ct        Content-Type if available.  Routing to the right
+     *                   parser will have already been done so this only useful to get
+     *                   addition Content-Type information or if this ReaderRIOT can
+     *                   handle multiple media types.  
+     * @param output    Destintation for the parser output.
+     * @param context   Environment settings.
+     */
     public void read(InputStream in, String baseURI, ContentType ct, StreamRDF output, Context context) ;
+
+    /** Read from an InputStream and output RDF on the StreamRDF.
+     * @param reader    Reader. InputStreams are preferred because Reader do not allow RIOT to set the character set. 
+     * @param baseURI   Base URI (or null)
+     * @param ct        Content-Type if available.  Routing to the right
+     *                   parser will have already been done so this only useful to get
+     *                   addition Content-Type information or if this ReaderRIOT can
+     *                   handle multiple media types.  
+     * @param output    Destintation for the parser output.
+     * @param context   Environment settings.
+     */
     public void read(Reader reader, String baseURI, ContentType ct, StreamRDF output, Context context) ;
     
     public ErrorHandler getErrorHandler() ;
