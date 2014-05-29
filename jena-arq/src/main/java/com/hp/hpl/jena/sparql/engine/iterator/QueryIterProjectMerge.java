4e2eeea8038a5139971f957701b784fab4d064d6
Andy Seaborne <andy@apache.org>
2014-05-29 19:56:52 +0000

    JENA-705 Correct merging in of bindings in the presence of a projection.
    git-svn-id: https://svn.apache.org/repos/asf/jena/trunk@1598387 13f79535-47bb-0310-9956-ffa450edef68

--- jena-arq/src/main/java/com/hp/hpl/jena/sparql/engine/iterator/QueryIterProjectMerge.java
+++ jena-arq/src/main/java/com/hp/hpl/jena/sparql/engine/iterator/QueryIterProjectMerge.java
@@ -18,14 +18,17 @@
 
 package com.hp.hpl.jena.sparql.engine.iterator ;
 
+import java.util.Iterator ;
 import java.util.List ;
 
+import org.apache.jena.atlas.logging.Log ;
+
+import com.hp.hpl.jena.graph.Node ;
 import com.hp.hpl.jena.sparql.algebra.op.OpProject ;
 import com.hp.hpl.jena.sparql.core.Var ;
 import com.hp.hpl.jena.sparql.engine.ExecutionContext ;
 import com.hp.hpl.jena.sparql.engine.QueryIterator ;
-import com.hp.hpl.jena.sparql.engine.binding.Binding ;
-import com.hp.hpl.jena.sparql.engine.binding.BindingUtils ;
+import com.hp.hpl.jena.sparql.engine.binding.* ;
 import com.hp.hpl.jena.sparql.engine.main.OpExecutor ;
 
 /**
@@ -48,13 +51,13 @@ public class QueryIterProjectMerge extends QueryIterRepeatApply {
     protected QueryIterator nextStage(Binding binding) {
         QueryIterator qIter = engine.executeOp(opProject.getSubOp(),
                                                QueryIterSingleton.create(binding, getExecContext())) ;
-
         qIter = new QueryIterConvert(qIter,
                                      new ProjectEnsureBindingConverter(binding, opProject.getVars()),
                                      getExecContext()) ;
         return qIter ;
     }
 
+    /** Ensure binding from outer binding are present when projecting */  
     static class ProjectEnsureBindingConverter implements QueryIterConvert.Converter {
 
         private final Binding   outerBinding ;
@@ -67,7 +70,36 @@ public class QueryIterProjectMerge extends QueryIterRepeatApply {
 
         @Override
         public Binding convert(Binding bind) {
-            return BindingUtils.merge(outerBinding, bind) ;
+            return ensure(projectionVars, outerBinding, bind) ;
+            // Effectively, this is:
+            //bind = new BindingProject(projectionVars, bind) ;
+            // return BindingUtils.merge(outerBinding, bind) ;
+        }
+        
+        /** Merge two bindings, the outer and inner, projecting the inner with the give variables.
+         *  This is what happens in substitutIon execution, with a inner select-project.  
+         */
+        private static Binding ensure(List<Var> vars, Binding outer, Binding inner) {
+            // A specialised BindingUtils.merge that does project as well.
+            // Reduce small object churn.
+            BindingMap b2 = BindingFactory.create(outer) ;
+            Iterator<Var> vIter = (vars != null) ? vars.iterator() : inner.vars() ;
+            // Add any variables from the RHS
+            for ( ; vIter.hasNext() ; ) {
+                Var v = vIter.next() ;
+                Node n2 = inner.get(v) ;
+                if ( n2 == null )
+                    continue ;
+                if ( ! b2.contains(v) )
+                    b2.add(v, inner.get(v)) ;
+                else {
+                    // Checking!
+                    Node n1 = outer.get(v) ;
+                    if ( ! n1.equals(n2) )
+                        Log.warn(BindingUtils.class,  "merge: Mismatch : "+n1+" != "+n2);
+                }
+            }
+            return b2 ;
         }
     }
 }
