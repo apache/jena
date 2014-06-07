ee7dd49545ba167752b7e0623175191203d9136b
Ying Jiang <jpz6311whu@apache.org>
2014-06-07 13:03:06 +0000

    JENA-625: Design and implement PropertyTable, GraphPropertyTable and GraphCSV with an illustrative test demo original from the project proposal.
    git-svn-id: https://svn.apache.org/repos/asf/jena/Experimental/jena-csv@1601104 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/main/java/com/hp/hpl/jena/propertytable/impl/GraphPropertyTable.java
@@ -0,0 +1,117 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.hp.hpl.jena.propertytable.impl;
+
+import java.util.Locale;
+
+import com.hp.hpl.jena.graph.Node;
+import com.hp.hpl.jena.graph.NodeFactory;
+import com.hp.hpl.jena.graph.Triple;
+import com.hp.hpl.jena.graph.TripleMatch;
+import com.hp.hpl.jena.graph.impl.GraphBase;
+import com.hp.hpl.jena.propertytable.Column;
+import com.hp.hpl.jena.propertytable.PropertyTable;
+import com.hp.hpl.jena.util.iterator.ExtendedIterator;
+import com.hp.hpl.jena.util.iterator.Filter;
+import com.hp.hpl.jena.util.iterator.NullIterator;
+
+public class GraphPropertyTable extends GraphBase {
+
+	private PropertyTable pt;
+
+	public GraphPropertyTable(PropertyTable pt) {
+		this.pt = pt;
+	}
+
+	@Override
+	protected ExtendedIterator<Triple> graphBaseFind(TripleMatch m) {
+
+		if (this.pt == null) {
+			return NullIterator.instance();
+		}
+		
+		ExtendedIterator<Triple> iter= null;
+		Node p = m.getMatchPredicate();
+		
+		
+		if (p == null || p == Node.ANY) {
+			iter = pt.getTripleIterator();
+		} else {
+			Column column = this.pt.getColumn(p);
+			if (column !=null){
+				iter = pt.getTripleIterator();
+			}else {
+				return NullIterator.instance();
+			}
+		}
+		
+		return iter.filterKeep ( new TripleMatchFilterEquality( m.asTriple() ) );
+
+	}
+	
+	static class TripleMatchFilterEquality extends Filter<Triple>
+    {
+        final protected Triple tMatch;
+    
+        /** Creates new TripleMatchFilter */
+        public TripleMatchFilterEquality(Triple tMatch) 
+            { this.tMatch = tMatch; }
+        
+        @Override
+        public boolean accept(Triple t)
+        {
+            return tripleContained(tMatch, t) ;
+        }
+        
+    }
+	static boolean tripleContained(Triple patternTriple, Triple dataTriple)
+    {
+        return
+            equalNode(patternTriple.getSubject(),   dataTriple.getSubject()) &&
+            equalNode(patternTriple.getPredicate(), dataTriple.getPredicate()) &&
+            equalNode(patternTriple.getObject(),    dataTriple.getObject()) ;
+    }
+    
+    private static boolean equalNode(Node m, Node n)
+    {
+        // m should not be null unless .getMatchXXXX used to get the node.
+        // Language tag canonicalization
+        n = fixupNode(n) ;
+        m = fixupNode(m) ;
+        return (m==null) || (m == Node.ANY) || m.equals(n) ;
+    }
+    
+    private static Node fixupNode(Node node)
+    {
+        if ( node == null || node == Node.ANY )
+            return node ;
+
+        // RDF says ... language tags should be canonicalized to lower case.
+        if ( node.isLiteral() )
+        {
+            String lang = node.getLiteralLanguage() ;
+            if ( lang != null && ! lang.equals("") )
+                node = NodeFactory.createLiteral(node.getLiteralLexicalForm(),
+                                          lang.toLowerCase(Locale.ROOT),
+                                          node.getLiteralDatatype()) ;
+        }
+        return node ; 
+    }
+
+}
