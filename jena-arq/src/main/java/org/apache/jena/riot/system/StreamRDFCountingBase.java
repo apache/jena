b88eea750a7afe546d2d598583123a5c2c8b7b55
Andy Seaborne <andy@apache.org>
2014-06-12 13:16:20 +0000

    Make public a counting StreamRDF implementation.
    git-svn-id: https://svn.apache.org/repos/asf/jena/trunk@1602155 13f79535-47bb-0310-9956-ffa450edef68

---
+++ jena-arq/src/main/java/org/apache/jena/riot/system/StreamRDFCountingBase.java
@@ -0,0 +1,77 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.jena.riot.system ;
+
+import org.apache.jena.atlas.lib.Tuple ;
+import org.apache.jena.riot.lang.StreamRDFCounting ;
+
+import com.hp.hpl.jena.graph.Node ;
+import com.hp.hpl.jena.graph.Triple ;
+import com.hp.hpl.jena.sparql.core.Quad ;
+
+/** Wrap another StreamRDF and provide counts of items */
+public class StreamRDFCountingBase extends StreamRDFWrapper implements StreamRDF, StreamRDFCounting {
+    private long countTriples  = 0 ;
+    private long countQuads    = 0 ;
+    private long countTuples   = 0 ;
+    private long countBase     = 0 ;
+    private long countPrefixes = 0 ;
+
+    public StreamRDFCountingBase(StreamRDF other) {
+        super(other) ;
+    }
+
+    @Override
+    public void triple(Triple triple) {
+        countTriples++ ;
+        super.triple(triple) ;
+    }
+
+    @Override
+    public void quad(Quad quad) {
+        countQuads++ ;
+        super.quad(quad) ;
+    }
+
+    @Override
+    public void tuple(Tuple<Node> tuple) {
+        countTuples++ ;
+        super.tuple(tuple) ;
+    }
+
+    @Override
+    public long count() {
+        return countTriples + countQuads + countTuples ;
+    }
+
+    @Override
+    public long countTriples() {
+        return countTriples ;
+    }
+
+    @Override
+    public long countQuads() {
+        return countQuads ;
+    }
+
+    @Override
+    public long countTuples() {
+        return countTuples ;
+    }
+}
