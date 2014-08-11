96716fe37c1c4350a780c4f7c88a8d0cbd81b7ac
Andy Seaborne <andy@apache.org>
2014-08-11 10:44:17 +0000

    Remove unused import
    git-svn-id: https://svn.apache.org/repos/asf/jena/trunk@1617245 13f79535-47bb-0310-9956-ffa450edef68

--- jena-arq/src/test/java/org/apache/jena/riot/lang/TestLangCSV.java
+++ jena-arq/src/test/java/org/apache/jena/riot/lang/TestLangCSV.java
@@ -30,7 +30,6 @@ import org.junit.Test;
 
 import com.hp.hpl.jena.rdf.model.Model;
 import com.hp.hpl.jena.rdf.model.ModelFactory;
-import com.hp.hpl.jena.util.PrintUtil;
 
 public class TestLangCSV extends BaseTest {
 	private static final String FILE_NAME = "src/test/resources/test.csv";
