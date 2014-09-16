d05ebadf5ed9839762790b932a081c3c70eb6d71
Rob Vesse <rvesse@apache.org>
2014-09-16 12:48:34 +0000

    Make UpdateWriter API extensible
    git-svn-id: https://svn.apache.org/repos/asf/jena/trunk@1625265 13f79535-47bb-0310-9956-ffa450edef68

--- jena-arq/src/main/java/com/hp/hpl/jena/sparql/modify/request/UpdateWriter.java
+++ jena-arq/src/main/java/com/hp/hpl/jena/sparql/modify/request/UpdateWriter.java
@@ -19,26 +19,18 @@
 package com.hp.hpl.jena.sparql.modify.request;
 
 import java.util.Iterator ;
-import java.util.List ;
 
 import org.apache.jena.atlas.io.IndentedWriter ;
-import org.apache.jena.atlas.iterator.Iter ;
 import org.apache.jena.atlas.lib.Closeable ;
-import org.apache.jena.atlas.lib.Sink ;
-import org.apache.jena.riot.out.SinkQuadBracedOutput ;
 import org.apache.jena.riot.system.IRIResolver ;
 
 import com.hp.hpl.jena.graph.Node ;
 import com.hp.hpl.jena.graph.Triple ;
-import com.hp.hpl.jena.sparql.ARQException ;
 import com.hp.hpl.jena.sparql.core.Prologue ;
 import com.hp.hpl.jena.sparql.core.Quad ;
 import com.hp.hpl.jena.sparql.modify.request.UpdateDataWriter.UpdateMode ;
-import com.hp.hpl.jena.sparql.serializer.FormatterElement ;
 import com.hp.hpl.jena.sparql.serializer.PrologueSerializer ;
 import com.hp.hpl.jena.sparql.serializer.SerializationContext ;
-import com.hp.hpl.jena.sparql.syntax.Element ;
-import com.hp.hpl.jena.sparql.util.FmtUtils ;
 import com.hp.hpl.jena.sparql.util.NodeToLabelMapBNode ;
 import com.hp.hpl.jena.update.Update ;
 import com.hp.hpl.jena.update.UpdateRequest ;
@@ -77,13 +69,13 @@ public class UpdateWriter implements Closeable
         opened = true;
     }
     
-    private void checkOpen()
+    protected void checkOpen()
     {
         if (!opened)
             throw new IllegalStateException("UpdateStreamWriter is not opened.  Call open() first.");
     }
     
-    private void prologue()
+    protected void prologue()
     {
         int row1 = out.getRow() ;
         PrologueSerializer.output(out, sCxt.getPrologue()) ;
@@ -92,7 +84,7 @@ public class UpdateWriter implements Closeable
             out.newline() ;
     }
     
-    private void prepareForDataUpdate(UpdateMode mode)
+    protected void prepareForDataUpdate(UpdateMode mode)
     {
         if ((null != udw) && !udw.getMode().equals(mode))
         {
@@ -190,10 +182,18 @@ public class UpdateWriter implements Closeable
         {
             out.println(" ;");
         }
-        Writer writer = new Writer(out, sCxt) ;
+        UpdateVisitor writer = prepareWriterVisitor() ;
         update.visit(writer) ; 
         
         firstOp = false;
+    }
+
+    /**
+     * Prepares a visitor which is used to visit the actual updates that make up the update request and write them out
+     * @return Update visitor
+     */
+    protected UpdateVisitor prepareWriterVisitor() {
+        return new UpdateWriterVisitor(out, sCxt);
     }
     
     public void update(Iterable<? extends Update> updates)
@@ -258,260 +258,5 @@ public class UpdateWriter implements Closeable
         uw.open();
         uw.update(update);
         uw.close();
-    }
-
-    // newline policy - don't add until needed.
-    private static class Writer implements UpdateVisitor
-    {
-        private static final int BLOCK_INDENT = 2 ;
-        private final IndentedWriter out ;
-        private final SerializationContext sCxt ;
-
-        public Writer(IndentedWriter out, SerializationContext sCxt)
-        {
-            this.out = out ;
-            this.sCxt = sCxt ;
-        }
-
-        private void visitDropClear(String name, UpdateDropClear update)
-        {
-            out.ensureStartOfLine() ;
-            out.print(name) ;
-            out.print(" ") ;
-            if ( update.isSilent() )
-                out.print("SILENT ") ;
-            
-            printTarget(update.getTarget()) ;
-            
-        }
-    
-        private void printTarget(Target target)
-        {
-            if ( target.isAll() )               { out.print("ALL") ; }
-            else if ( target.isAllNamed() )     { out.print("NAMED") ; }
-            else if ( target.isDefault() )      { out.print("DEFAULT") ; }
-            else if ( target.isOneNamedGraph() )
-            { 
-                out.print("GRAPH ") ;
-                String s = FmtUtils.stringForNode(target.getGraph(), sCxt) ;
-                out.print(s) ;
-            }
-            else
-            {
-                out.print("Target BROKEN") ;
-                throw new ARQException("Malformed Target") ;
-            }
-        }
-
-        @Override
-        public void visit(UpdateDrop update)
-        { visitDropClear("DROP", update) ; }
-
-        @Override
-        public void visit(UpdateClear update)
-        { visitDropClear("CLEAR", update) ; }
-
-        @Override
-        public void visit(UpdateCreate update)
-        {
-            out.ensureStartOfLine() ;
-            out.print("CREATE") ;
-            out.print(" ") ;
-            if ( update.isSilent() )
-                out.print("SILENT ") ;
-            out.print("GRAPH") ;
-            out.print(" ") ;
-            String s = FmtUtils.stringForNode(update.getGraph(), sCxt) ;
-            out.print(s) ;
-        }
-
-        @Override
-        public void visit(UpdateLoad update)
-        {
-            out.ensureStartOfLine() ;
-            out.print("LOAD") ;
-            out.print(" ") ;
-            if ( update.getSilent() )
-                out.print("SILENT ") ;
-            
-            outputStringAsURI(update.getSource()) ;
-            
-            if ( update.getDest() != null )
-            {
-                out.print(" INTO GRAPH ") ;
-                output(update.getDest()) ;
-            }
-        }
-
-        private void outputStringAsURI(String uriStr)
-        {
-            String x = FmtUtils.stringForURI(uriStr, sCxt) ;
-            out.print(x) ;
-        }
-        
-        private void printTargetUpdate2(Target target)
-        {
-            if ( target.isDefault() )      { out.print("DEFAULT") ; }
-            else if ( target.isOneNamedGraph() )
-            { 
-                //out.print("GRAPH ") ;
-                String s = FmtUtils.stringForNode(target.getGraph(), sCxt) ;
-                out.print(s) ;
-            }
-            else
-            {
-                out.print("Target BROKEN / Update2") ;
-                throw new ARQException("Malformed Target / Update2") ;
-            }
-        }
-        
-        private void printUpdate2(UpdateBinaryOp update, String name)
-        {
-            out.print(name) ;
-            if ( update.getSilent() )
-                out.print(" SILENT") ;
-            out.print(" ") ;
-            printTargetUpdate2(update.getSrc()) ;
-            out.print(" TO ") ;
-            printTargetUpdate2(update.getDest()) ;
-        }
-        
-        
-        @Override
-        public void visit(UpdateAdd update)
-        { printUpdate2(update, "ADD") ; }
-
-        @Override
-        public void visit(UpdateCopy update)
-        { printUpdate2(update, "COPY") ; }
-
-        @Override
-        public void visit(UpdateMove update)
-        { printUpdate2(update, "MOVE") ; }
-
-
-        @Override
-        public Sink<Quad> createInsertDataSink()
-        {
-            UpdateDataWriter udw = new UpdateDataWriter(UpdateMode.INSERT, out, sCxt);
-            udw.open();
-            return udw;
-        }
-
-        @Override
-        public void visit(UpdateDataInsert update)
-        {
-            Iter.sendToSink(update.getQuads(), createInsertDataSink());  // Iter.sendToSink() will call close() on the sink
-        }
-        
-        @Override
-        public Sink<Quad> createDeleteDataSink()
-        {
-            UpdateDataWriter udw = new UpdateDataWriter(UpdateMode.DELETE, out, sCxt);
-            udw.open();
-            return udw;
-        }
-
-        @Override
-        public void visit(UpdateDataDelete update)
-        {
-            Iter.sendToSink(update.getQuads(), createDeleteDataSink()); // Iter.sendToSink() will call close() on the sink
-        }
-
-        // Prettier later.
-        
-        private void outputQuadsBraced(List<Quad> quads)
-        {
-            if ( quads.size() == 0 )
-            {
-                out.print("{ }") ;
-                return ;
-            }
-            
-            SinkQuadBracedOutput sink = new SinkQuadBracedOutput(out, sCxt);
-            sink.open();
-            Iter.sendToSink(quads, sink);
-        }
-        
-        private void output(Node node)
-        { 
-            String $ = FmtUtils.stringForNode(node, sCxt) ;
-            out.print($) ;
-        }
-
-        
-        @Override
-        public void visit(UpdateDeleteWhere update)
-        {
-            out.ensureStartOfLine() ;
-            out.println("DELETE WHERE ") ;
-            outputQuadsBraced(update.getQuads()) ;
-        }
-
-        @Override
-        public void visit(UpdateModify update)
-        {
-            out.ensureStartOfLine() ;
-            if ( update.getWithIRI() != null )
-            {
-                //out.ensureStartOfLine() ;
-                out.print("WITH ") ;
-                output(update.getWithIRI()) ;
-            }
-            
-            
-            if ( update.hasDeleteClause() )
-            {
-                List<Quad> deleteQuads = update.getDeleteQuads() ;
-                out.ensureStartOfLine() ;
-                out.print("DELETE ") ;
-                outputQuadsBraced(deleteQuads) ;
-            }
-            
-            
-            if ( update.hasInsertClause() )
-            {
-                List<Quad> insertQuads = update.getInsertQuads() ;
-                out.ensureStartOfLine() ;
-                out.print("INSERT ") ;
-                outputQuadsBraced(insertQuads) ;
-            }
-            
-            if ( ! update.hasInsertClause() && ! update.hasDeleteClause() )
-            {
-                // Fake a clause to make it legal syntax.
-                out.ensureStartOfLine() ;
-                out.println("INSERT { }") ;
-            }
-            
-            for ( Node x : update.getUsing() )
-            {
-                out.ensureStartOfLine() ;
-                out.print("USING ") ;
-                output(x) ;
-            }
-            
-            for ( Node x : update.getUsingNamed() )
-            {
-                out.ensureStartOfLine() ;
-                out.print("USING NAMED ") ;
-                output(x) ;
-            }
-             
-            Element el = update.getWherePattern() ;
-            out.ensureStartOfLine() ;
-            out.print("WHERE") ;
-            out.incIndent(BLOCK_INDENT) ;
-            out.newline() ;
-
-            if ( el != null )
-            {
-                FormatterElement fmtElement = new FormatterElement(out, sCxt) ;
-                fmtElement.visitAsGroup(el) ;
-            }
-            else
-                out.print("{}") ;
-            out.decIndent(BLOCK_INDENT) ;
-        }
     }
 }
