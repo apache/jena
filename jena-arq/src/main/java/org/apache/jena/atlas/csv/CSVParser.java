f01b69e0cec45bae8fd51c448fb629c835812404
Andy Seaborne <andy@apache.org>
2014-09-17 14:20:41 +0000

    JENA-699 : Use Apache Commons CSV parser.
    git-svn-id: https://svn.apache.org/repos/asf/jena/trunk@1625607 13f79535-47bb-0310-9956-ffa450edef68

--- jena-arq/src/main/java/org/apache/jena/atlas/csv/CSVParser.java
+++ jena-arq/src/main/java/org/apache/jena/atlas/csv/CSVParser.java
@@ -18,109 +18,70 @@
 
 package org.apache.jena.atlas.csv ;
 
-import java.io.InputStream ;
-import java.io.Reader ;
-import java.util.ArrayList ;
-import java.util.Iterator ;
-import java.util.List ;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
-import org.apache.jena.atlas.io.IO ;
-import org.apache.jena.atlas.iterator.IteratorSlotted ;
-import org.apache.jena.atlas.iterator.PeekIterator ;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
+import org.apache.jena.atlas.io.IO;
 
-/** Written specifically to handle SPARQL results CSv files.
- *  Replace with a real parser (e.g. Apache Commons CSV when released)
+/** Written specifically to handle SPARQL results CSV files.
+ *  Acts as a wrapper for Commons CSV parser.
  */
 public class CSVParser implements Iterable<List<String>>
 {
+    
+    private final org.apache.commons.csv.CSVParser parser;
+    
     public static CSVParser create(String filename) {
         InputStream input = IO.openFile(filename) ;
         return create(input) ;
     }
 
     public static CSVParser create(InputStream input) {
-        CSVTokenIterator iter = new CSVTokenIterator(input) ;
-        CSVParser parser = new CSVParser(iter) ;
+        CSVParser parser = new CSVParser(new InputStreamReader(input)) ;
         return parser ; 
     }
     
     public static CSVParser create(Reader input) {
-        CSVTokenIterator iter = new CSVTokenIterator(input) ;
-        CSVParser parser = new CSVParser(iter) ;
+        CSVParser parser = new CSVParser(input) ;
         return parser ; 
     }
 
-    private final CSVTokenIterator iter ;
-    private final PeekIterator<CSVToken> pIter ;
-
-    public CSVParser(CSVTokenIterator iter) {
-        this.iter = iter ;
-        this.pIter = new PeekIterator<>(iter) ;
+    public CSVParser(Reader input) {
+        try {
+            this.parser = CSVFormat.EXCEL.withQuote('\'').parse(input);
+        } catch (IOException e) {
+            throw new CSVParseException("Failed to create the CSV parser: " + e.getMessage(), e);
+        }
     }
     
     @Override
     public Iterator<List<String>> iterator() {
-        return new IteratorSlotted<List<String>>() {
-            @Override
-            protected List<String> moveToNext() {
-                return CSVParser.this.parse1() ;
+        List<List<String>> list = new ArrayList<>();
+        for (CSVRecord record : parser) {
+            List<String> row = new ArrayList<>();
+            for (String columnValue : record) {
+                row.add(columnValue);
             }
-
-            @Override
-            protected boolean hasMore() {
-                return true ;
-            }};
+            list.add(row);
+        }
+        return list.iterator();
     }
 
     public List<String> parse1() {
-        // Get rid of switches. break problems.
-        List<String> line = new ArrayList<>(100) ;
-        
-        loop: while (pIter.hasNext()) {
-            CSVToken t = pIter.next() ;
-            switch (t.type) {
-                case EOF :
-                    return null ;
-                case NL :
-                    // Blank line = one or none?
-                    line.add("") ;
-                    return line ;
-                case STRING :
-                case QSTRING :
-                    line.add(t.image) ;
-                    break ;
-                case COMMA :
-                    // Immediate COMMA is an empty term.
-                    line.add("") ;
-                    continue loop ;
-                default :
-                    exception("Syntax error: expected a string or comma.", t) ;
-            }
-            // Expect COMMA or NL
-            if ( !pIter.hasNext() ) {
-                // File ends, no NL.
-                return line ;
-            }
-            // Look at separateor or end
-            CSVToken t2 = pIter.peek() ;
-            switch (t2.type) {
-                case COMMA :
-                    pIter.next() ;
-                    continue loop ;
-                case NL :
-                case EOF :
-                    pIter.next() ;
-                    return line ;
-                default :
-                    exception("Syntax error: expect comma or end of line.", t) ;
-            }
+        Iterator<List<String>> iterator = iterator();
+        if (iterator.hasNext()) 
+        {
+            final List<String> firstRow = iterator.next();
+            return firstRow;
         }
-        return null ;
-    }
-    static void exception(String msg, CSVToken t) {
-        if ( t != null && t.line >= 0 && t.col > 0 )
-            msg = String.format("[%s, %s] %s", t.line, t.col, msg) ;
-        throw new CSVParseException(msg) ;
+        return null;
     }
 
     static void exception(String msg, long line, long col) {
