2b75617a95b96d9eecbfdb869abf86a4a6c1c429
Rob Vesse <rvesse@apache.org>
2014-09-18 09:30:35 +0000

    Add missing license headers and improve javadoc on new SerializerRegistry classes
    git-svn-id: https://svn.apache.org/repos/asf/jena/trunk@1625925 13f79535-47bb-0310-9956-ffa450edef68

--- jena-arq/src/main/java/com/hp/hpl/jena/sparql/serializer/SerializerRegistry.java
+++ jena-arq/src/main/java/com/hp/hpl/jena/sparql/serializer/SerializerRegistry.java
@@ -1,3 +1,21 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.hp.hpl.jena.sparql.serializer;
 
 import java.util.HashMap;
@@ -15,6 +33,19 @@ import com.hp.hpl.jena.sparql.util.NodeToLabelMapBNode;
 
 /**
  * Provides a registry of serializers for queries and updates
+ * <p>
+ * By registering custom {@link QuerySerializerFactory} or
+ * {@link UpdateSerializerFactory} instances the mapping of a syntax to a
+ * serialization can be customised and this allows the serialization of queries
+ * and updates to be customised if desired.
+ * </p>
+ * <p>
+ * This feature is primarily intended for system programmers as changing how
+ * queries and updates are serialized could have knock on effects particularly
+ * if you use ARQ to interact with remote systems. The default registered
+ * serializers produce standards compliant SPARQL syntax and should be more than
+ * sufficient in most cases
+ * </p>
  * 
  */
 public class SerializerRegistry {
@@ -60,9 +91,9 @@ public class SerializerRegistry {
 
             @Override
             public UpdateSerializer create(Syntax syntax, Prologue prologue, IndentedWriter writer) {
-                if ( ! prologue.explicitlySetBaseURI() )
-                    prologue = new Prologue(prologue.getPrefixMapping(), (IRIResolver)null) ;
-                
+                if (!prologue.explicitlySetBaseURI())
+                    prologue = new Prologue(prologue.getPrefixMapping(), (IRIResolver) null);
+
                 SerializationContext context = new SerializationContext(prologue);
                 return new UpdateWriter(writer, context);
             }
@@ -82,6 +113,12 @@ public class SerializerRegistry {
         registry = reg;
     }
 
+    /**
+     * Gets the serializer registry which is a singleton lazily instantiating it
+     * if this is the first time this method has been called
+     * 
+     * @return Registry
+     */
     public static SerializerRegistry get() {
         if (registry == null)
             init();
@@ -89,38 +126,104 @@ public class SerializerRegistry {
         return registry;
     }
 
+    /**
+     * Adds a query serializer factory for the given syntax
+     * 
+     * @param syntax
+     *            Syntax
+     * @param factory
+     *            Serializer factory
+     * @throws IllegalArgumentException
+     *             Thrown if the given factory does not accept the given syntax
+     */
     public void addQuerySerializer(Syntax syntax, QuerySerializerFactory factory) {
         if (!factory.accept(syntax))
             throw new IllegalArgumentException("Factory does not accept the specified syntax");
         querySerializers.put(syntax, factory);
     }
 
+    /**
+     * Adds an update serializer factory for the given syntax
+     * 
+     * @param syntax
+     *            Syntax
+     * @param factory
+     *            Serializer factory
+     * @throws IllegalArgumentException
+     *             Thrown if the given factory does not accept the given syntax
+     */
     public void addUpdateSerializer(Syntax syntax, UpdateSerializerFactory factory) {
         if (!factory.accept(syntax))
             throw new IllegalArgumentException("Factory does not accept the specified syntax");
         updateSerializers.put(syntax, factory);
     }
 
+    /**
+     * Gets whether a query serializer factory is registered for the given
+     * syntax
+     * 
+     * @param syntax
+     *            Syntax
+     * @return True if registered, false otherwise
+     */
     public boolean containsQuerySerializer(Syntax syntax) {
-        return querySerializers.containsKey(syntax);
+        return querySerializers.containsKey(syntax) && querySerializers.get(syntax) != null;
     }
 
+    /**
+     * Gets whether an update serializer factory is registered for the given
+     * syntax
+     * 
+     * @param syntax
+     *            Syntax
+     * @return True if registered, false otherwise
+     */
     public boolean containsUpdateSerializer(Syntax syntax) {
-        return updateSerializers.containsKey(syntax);
+        return updateSerializers.containsKey(syntax) && updateSerializers.get(syntax) != null;
     }
 
+    /**
+     * Gets the query serializer factory for the given syntax which may be null
+     * if there is none registered
+     * 
+     * @param syntax
+     *            Syntax
+     * @return Query Serializer Factory or null if none registered for the given
+     *         syntax
+     */
     public QuerySerializerFactory getQuerySerializerFactory(Syntax syntax) {
         return querySerializers.get(syntax);
     }
 
+    /**
+     * Gets the update serializer factory for the given syntax which may be null
+     * if there is none registered
+     * 
+     * @param syntax
+     *            Syntax
+     * @return Update Serializer Factory or null if none registered for the
+     *         given syntax
+     */
     public UpdateSerializerFactory getUpdateSerializerFactory(Syntax syntax) {
         return updateSerializers.get(syntax);
     }
 
+    /**
+     * Removes the query serializer factory for the given syntax
+     * 
+     * @param syntax
+     *            Syntax
+     */
     public void removeQuerySerializer(Syntax syntax) {
         querySerializers.remove(syntax);
     }
 
+    /**
+     * Removes the update serializer factory for the given syntax
+     * 
+     * @param syntax
+     *            Syntax
+     */
     public void removeUpdateSerializer(Syntax syntax) {
         updateSerializers.remove(syntax);
     }
