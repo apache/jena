e87a8a37c52f9ff97afe031613d7de2add4b9e31
Andy Seaborne <andy@apache.org>
2014-09-23 14:46:03 +0100

    Deprecate

--- jena-arq/src/main/java/com/hp/hpl/jena/sparql/lib/DatasetLib.java
+++ jena-arq/src/main/java/com/hp/hpl/jena/sparql/lib/DatasetLib.java
@@ -18,21 +18,19 @@
 
 package com.hp.hpl.jena.sparql.lib;
 
-import java.util.Iterator ;
-
 import org.apache.jena.atlas.io.IndentedWriter ;
 
-import com.hp.hpl.jena.graph.Graph ;
-import com.hp.hpl.jena.graph.Node ;
 import com.hp.hpl.jena.query.Dataset ;
 import com.hp.hpl.jena.sparql.core.DatasetGraph ;
 import com.hp.hpl.jena.sparql.core.DatasetGraphFactory ;
 import com.hp.hpl.jena.sparql.sse.writers.WriterGraph ;
+import com.hp.hpl.jena.sparql.util.IsoMatcher ;
 
 public class DatasetLib
 {
     /** Write, in SSE format (a debugging format).
      */
+    @Deprecated
     public static void dump(DatasetGraph dataset)
     {
         WriterGraph.output(IndentedWriter.stdout, dataset, null) ;
@@ -40,8 +38,10 @@ public class DatasetLib
     }
     
     /**
-     * Return true if the datasets are isomorphic - same names for graphs, graphs isomorphic. 
+     * Return true if the datasets are isomorphic - same names for graphs, graphs isomorphic.
+     * @deprecated Use {@linkplain IsoMatcher#isomorphic(DatasetGraph, DatasetGraph)}
      */
+    @Deprecated
     public static boolean isomorphic(Dataset dataset1, Dataset dataset2)
     {
         return isomorphic(dataset1.asDatasetGraph(), dataset2.asDatasetGraph()) ;
@@ -49,33 +49,19 @@ public class DatasetLib
     
     /**
      * Return true if the datasets are isomorphic - same names for graphs, graphs isomorphic. 
+     * @deprecated Use {@linkplain IsoMatcher#isomorphic(DatasetGraph, DatasetGraph)}
      */
+    @Deprecated
     public static boolean isomorphic(DatasetGraph dataset1, DatasetGraph dataset2)
     {
-        long x1 = dataset1.size() ;
-        long x2 = dataset2.size() ;
-        if ( x1 >=0 && x1 != x2 )
-            return false ;
-        
-        boolean b = dataset1.getDefaultGraph().isIsomorphicWith(dataset2.getDefaultGraph()) ;
-        if ( ! b )
-            return b ;
-        
-        for ( Iterator<Node> iter1 = dataset1.listGraphNodes() ; iter1.hasNext() ; )
-        {
-            Node gn = iter1.next() ;
-            Graph g1 = dataset1.getGraph(gn) ;
-            Graph g2 = dataset2.getGraph(gn) ;
-            if ( g2 == null )
-                return false ;
-            if ( ! g1.isIsomorphicWith(g2) )
-                return false ;
-        }
-        
-        return true ;
+        return IsoMatcher.isomorphic(dataset1, dataset2) ;
     }
     
-    // A DatasetGraph that creates memory graphs on mention */
+    // A DatasetGraph that creates memory graphs on mention 
+    /** 
+     * @deprecated Use {@linkplain DatasetGraphFactory#createMem()}
+     */
+    @Deprecated
     public static DatasetGraph createDatasetGraphMem()
     {
         return DatasetGraphFactory.createMem() ;
