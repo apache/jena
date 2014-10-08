0ac95f4fcbbcea2a9fdbca58642a000016d8db5a
Andy Seaborne <andy@apache.org>
2014-10-08 17:57:30 +0100

    Fix some javadoc

--- jena-arq/src/main/java/org/apache/jena/riot/system/StreamRDFLib.java
+++ jena-arq/src/main/java/org/apache/jena/riot/system/StreamRDFLib.java
@@ -73,15 +73,13 @@ public class StreamRDFLib
     public static StreamRDF dataset(DatasetGraph dataset)    { return new ParserOutputDataset(dataset) ; }
     
     /** Set triples to a StreamRDF - does not call .start/.finish 
-     * @deprecated Use {@link StreamOps#triplesToStream} instead*/ 
-    @SuppressWarnings("javadoc")
+     * @deprecated Use {@link StreamOps#sendTriplesToStream} instead*/ 
     @Deprecated
     public static void triplesToStream(StreamRDF dest, Iterator<Triple> iter)
     { StreamOps.sendTriplesToStream(iter, dest) ; }
 
     /** Set quads to a StreamRDF - does not call .start/.finish 
-     * @deprecated Use {@link StreamOps#quadsToStream} instead*/
-    @SuppressWarnings("javadoc")
+     * @deprecated Use {@link StreamOps#sendQuadsToStream} instead*/
     @Deprecated
     public static void quadsToStream(StreamRDF dest, Iterator<Quad> iter)
     { StreamOps.sendQuadsToStream(iter, dest) ; }
