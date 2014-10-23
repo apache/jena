29b6a32cd8acdc56706f00bb028ff296c4eef1cc
Rob Vesse <rvesse@apache.org>
2014-10-23 14:11:24 +0100

    Get jena-querybuilder to pass RAT checks
    Adds missing license headers and adds another Javadoc related exclusion
    for package-info.java files

--- jena-extras/jena-querybuilder/src/main/java/org/apache/jena/arq/querybuilder/rewriters/package-info.java
+++ jena-extras/jena-querybuilder/src/main/java/org/apache/jena/arq/querybuilder/rewriters/package-info.java
@@ -2,6 +2,6 @@
  * Classes to rewrite Query algebra, expressions and other objects to handle variable replacement for 
  * the prepared statement functionality.
  * 
- * In most cases developers will not need to access the rewriteres directly.
+ * In most cases developers will not need to access the rewriters directly.
  */
 package org.apache.jena.arq.querybuilder.rewriters;
