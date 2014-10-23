535a8250fdb49a2207a9ff88d10f12dd3bef9f6b
Rob Vesse <rvesse@apache.org>
2014-10-23 10:34:52 +0100

    Add missing license headers
    Adds the missing license headers to ensure that Jena Hadoop RDF Tools
    passes the RAT checks

--- jena-hadoop-rdf/hadoop-rdf-mapreduce/src/test/java/org/apache/jena/hadoop/rdf/mapreduce/TestDistinctTriples.java
+++ jena-hadoop-rdf/hadoop-rdf-mapreduce/src/test/java/org/apache/jena/hadoop/rdf/mapreduce/TestDistinctTriples.java
@@ -1,3 +1,21 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *     
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.jena.hadoop.rdf.mapreduce;
 
 import java.io.IOException;
@@ -8,9 +26,6 @@ import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mrunit.mapreduce.MapReduceDriver;
 import org.apache.jena.hadoop.rdf.types.TripleWritable;
-import org.apache.log4j.BasicConfigurator;
-import org.apache.log4j.Level;
-import org.apache.log4j.Logger;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -61,7 +76,7 @@ public class TestDistinctTriples
 
         driver.runTest();
     }
-    
+
     @Test
     public void distinct_triples_03() throws IOException {
         MapReduceDriver<LongWritable, TripleWritable, TripleWritable, NullWritable, NullWritable, TripleWritable> driver = this
@@ -71,44 +86,44 @@ public class TestDistinctTriples
                 NodeFactory.createLiteral("1"));
         Triple t2 = new Triple(t.getSubject(), t.getPredicate(), NodeFactory.createLiteral("2"));
         Assert.assertNotEquals(t, t2);
-        
+
         TripleWritable tw = new TripleWritable(t);
         TripleWritable tw2 = new TripleWritable(t2);
         Assert.assertNotEquals(tw, tw2);
-        
+
         driver.addInput(new LongWritable(1), tw);
         driver.addInput(new LongWritable(2), tw2);
         driver.addOutput(NullWritable.get(), tw);
         driver.addOutput(NullWritable.get(), tw2);
-        
+
         driver.runTest(false);
     }
-    
+
     @Test
     public void distinct_triples_04() throws IOException {
         MapReduceDriver<LongWritable, TripleWritable, TripleWritable, NullWritable, NullWritable, TripleWritable> driver = this
                 .getMapReduceDriver();
-        
+
         Node s1 = NodeFactory.createURI("urn:nf#cbf2b2c7-109e-4097-bbea-f67f272c7fcc");
         Node s2 = NodeFactory.createURI("urn:nf#bb08b75c-1ad2-47ef-acd2-eb2d92b94b89");
         Node p = NodeFactory.createURI("urn:p");
         Node o = NodeFactory.createURI("urn:66.230.159.118");
         Assert.assertNotEquals(s1, s2);
-        
+
         Triple t1 = new Triple(s1, p, o);
         Triple t2 = new Triple(s2, p, o);
         Assert.assertNotEquals(t1, t2);
-        
+
         TripleWritable tw1 = new TripleWritable(t1);
         TripleWritable tw2 = new TripleWritable(t2);
         Assert.assertNotEquals(tw1, tw2);
         Assert.assertNotEquals(0, tw1.compareTo(tw2));
-        
+
         driver.addInput(new LongWritable(1), tw1);
         driver.addInput(new LongWritable(2), tw2);
         driver.addOutput(NullWritable.get(), tw1);
         driver.addOutput(NullWritable.get(), tw2);
-        
+
         driver.runTest(false);
     }
 }
