5d08dc3aa0955ce28189b9ffa5596177f3cd6070
Andy Seaborne <andy@seaborne.org>
2015-01-29 18:43:53 +0000

    dev

--- cohort-dev/src/main/java/dev/MainIndex.java
+++ cohort-dev/src/main/java/dev/MainIndex.java
@@ -17,23 +17,18 @@
 
 package dev;
 
-import java.io.File ;
 import java.io.PrintStream ;
-import java.util.ArrayList ;
 import java.util.Arrays ;
-import java.util.Iterator ;
 import java.util.List ;
 import java.util.stream.Collectors ;
 
-import org.apache.jena.atlas.iterator.Iter ;
-import org.apache.jena.atlas.iterator.Transform ;
-import org.apache.jena.atlas.lib.Bytes ;
+import org.apache.jena.atlas.logging.LogCtl ;
 import org.seaborne.dboe.base.block.BlockMgrFactory ;
 import org.seaborne.dboe.base.file.Location ;
 import org.seaborne.dboe.base.record.Record ;
+import static dev.RecordLib.* ;
 import org.seaborne.dboe.base.record.RecordFactory ;
 import org.seaborne.dboe.index.RangeIndex ;
-import org.seaborne.dboe.index.bplustree.BPTreeNode ;
 import org.seaborne.dboe.index.bplustree.BPlusTree ;
 import org.seaborne.dboe.index.bplustree.BPlusTreeParams ;
 import org.seaborne.dboe.sys.SystemIndex ;
@@ -41,7 +36,7 @@ import org.seaborne.dboe.transaction.txn.TransactionCoordinator ;
 import org.seaborne.dboe.transaction.txn.journal.Journal ;
 
 public class MainIndex {
-    static { setLog4j() ; }
+    static { LogCtl.setLog4j() ; }
     
     static RecordFactory recordFactory = new RecordFactory(4, 0) ;
     
@@ -58,7 +53,7 @@ public class MainIndex {
         
         //List<Integer> data1 = Arrays.asList( 1 , 3 , 5 , 7 , 9 , 8 , 6 , 4 , 2) ;
         
-        List<Integer> data1 = Arrays.asList( 2 ) ; // , 7 , 8 , 9 ) ;
+        List<Integer> data1 = Arrays.asList( 2 , 3, 4 ) ; // , 7 , 8 , 9 ) ;
         
         List<Integer> data2a = Arrays.asList( 2 , 4, 3) ; // , 7 , 8 , 9 ) ;
         List<Integer> data2b= Arrays.asList( 1 ) ; // , 7 , 8 , 9 ) ;
@@ -69,12 +64,12 @@ public class MainIndex {
         
 //        Runnable r = () -> data2.forEach((x) -> idx.add(r(x)) ) ;
         
-        
         bpt.startBatch();
         
         BPlusTreeParams.Logging = false ;
         bpt.dump();
-        dataRecords1.forEach(bpt::add) ;
+        
+        verbose(true, ()->dataRecords1.forEach(bpt::add)) ;
         
         if ( false ) {
             // Two part.
@@ -117,6 +112,18 @@ public class MainIndex {
 //        bpt.complete() ;
     }
     
+    static void verbose(boolean yesOrNo, Runnable r) {
+        boolean b = BPlusTreeParams.Logging ;
+        try {
+            BPlusTreeParams.Logging = yesOrNo ;
+            r.run(); 
+        } finally { 
+            BPlusTreeParams.Logging = b ;
+        }
+        
+            
+    }
+    
     static void printTxnCoordState(TransactionCoordinator txnCoord) {
         printTxnCoordState(System.out, txnCoord) ;
     }
@@ -128,96 +135,96 @@ public class MainIndex {
         ps.printf("  Finished: %4d\n", txnCoord.countFinished()) ;
     }
     
-    static Record record(int key) {
-        return intToRecord(key) ;
-    }
-
-    // Size of a record when testing (one integer)
-    public final static int TestRecordLength = 4 ;
-    
-    public static Record intToRecord(int v) { return intToRecord(v, recordFactory) ; }
-    public static Record intToRecord(int v, int recLen) { return intToRecord(v, new RecordFactory(recLen, 0)) ; }
-    
-    public static Record intToRecord(int v, RecordFactory factory)
-    {
-        byte[] vb = Bytes.packInt(v) ;
-
-        int recLen = factory.recordLength() ;
-        byte[] bb = new byte[recLen] ;
-        int x = 0 ; // Start point in bb.
-        if ( recLen > 4 )
-            x = recLen-4 ;
-        
-        int len = Math.min(4, recLen) ;
-        int z = 4-len ; // Start point in vb
-    
-        // Furthest right bytes.
-        for ( int i = len-1 ; i >= 0 ; i-- ) 
-           bb[x+i] = vb[z+i] ; 
-        
-        return factory.create(bb) ;
-    }
-
-    public static List<Record> intToRecord(int[] v) { return intToRecord(v, recordFactory) ; }
-
-    public static List<Record> intToRecord(int[] v, int recLen)
-    { return intToRecord(v, new RecordFactory(recLen, 0)) ; }
-    
-    static List<Record> intToRecord(int[] v, RecordFactory factory)
-    {
-        List<Record> x = new ArrayList<>() ;
-        for ( int i : v )
-            x.add(intToRecord(i, factory)) ;
-        return x ;
-    }
-
-    public static int recordToInt(Record key)
-    {
-        return Bytes.getInt(key.getKey()) ;
-    }
-
-    public static List<Integer> toIntList(Iterator<Record> iter)
-    {
-        return Iter.toList(Iter.map(iter, new Transform<Record, Integer>(){
-            @Override
-            public Integer convert(Record item)
-            {
-                return recordToInt(item) ;
-            }}
-        )) ;
-    }
-    
-    public static Record r(int v)
-    {
-        return intToRecord(v, recordFactory) ; 
-    }
-
-    public static int r(Record rec)
-    {
-        return recordToInt(rec) ; 
-    }
-
-    public static List<Integer> toIntList(int... vals)
-    {
-        List<Integer> x = new ArrayList<>() ;
-        for ( int i : vals )
-            x.add(i) ;
-        return x ;
-    }
-
-    public static List<Integer> r(Iterator<Record> iter)
-    {
-        return toIntList(iter) ;
-    }
-
-    public static void setLog4j() {
-        if ( System.getProperty("log4j.configuration") == null ) {
-            String fn = "log4j.properties" ;
-            File f = new File(fn) ;
-            if ( f.exists() )
-                System.setProperty("log4j.configuration", "file:" + fn) ;
-        }
-    }
+//    static Record record(int key) {
+//        return intToRecord(key) ;
+//    }
+//
+//    // Size of a record when testing (one integer)
+//    public final static int TestRecordLength = 4 ;
+//    
+//    public static Record intToRecord(int v) { return intToRecord(v, recordFactory) ; }
+//    public static Record intToRecord(int v, int recLen) { return intToRecord(v, new RecordFactory(recLen, 0)) ; }
+//    
+//    public static Record intToRecord(int v, RecordFactory factory)
+//    {
+//        byte[] vb = Bytes.packInt(v) ;
+//
+//        int recLen = factory.recordLength() ;
+//        byte[] bb = new byte[recLen] ;
+//        int x = 0 ; // Start point in bb.
+//        if ( recLen > 4 )
+//            x = recLen-4 ;
+//        
+//        int len = Math.min(4, recLen) ;
+//        int z = 4-len ; // Start point in vb
+//    
+//        // Furthest right bytes.
+//        for ( int i = len-1 ; i >= 0 ; i-- ) 
+//           bb[x+i] = vb[z+i] ; 
+//        
+//        return factory.create(bb) ;
+//    }
+//
+//    public static List<Record> intToRecord(int[] v) { return intToRecord(v, recordFactory) ; }
+//
+//    public static List<Record> intToRecord(int[] v, int recLen)
+//    { return intToRecord(v, new RecordFactory(recLen, 0)) ; }
+//    
+//    static List<Record> intToRecord(int[] v, RecordFactory factory)
+//    {
+//        List<Record> x = new ArrayList<>() ;
+//        for ( int i : v )
+//            x.add(intToRecord(i, factory)) ;
+//        return x ;
+//    }
+//
+//    public static int recordToInt(Record key)
+//    {
+//        return Bytes.getInt(key.getKey()) ;
+//    }
+//
+//    public static List<Integer> toIntList(Iterator<Record> iter)
+//    {
+//        return Iter.toList(Iter.map(iter, new Transform<Record, Integer>(){
+//            @Override
+//            public Integer convert(Record item)
+//            {
+//                return recordToInt(item) ;
+//            }}
+//        )) ;
+//    }
+//    
+//    public static Record r(int v)
+//    {
+//        return intToRecord(v, recordFactory) ; 
+//    }
+//
+//    public static int r(Record rec)
+//    {
+//        return recordToInt(rec) ; 
+//    }
+//
+//    public static List<Integer> toIntList(int... vals)
+//    {
+//        List<Integer> x = new ArrayList<>() ;
+//        for ( int i : vals )
+//            x.add(i) ;
+//        return x ;
+//    }
+//
+//    public static List<Integer> r(Iterator<Record> iter)
+//    {
+//        return toIntList(iter) ;
+//    }
+//
+//    public static void setLog4j() {
+//        if ( System.getProperty("log4j.configuration") == null ) {
+//            String fn = "log4j.properties" ;
+//            File f = new File(fn) ;
+//            if ( f.exists() )
+//                System.setProperty("log4j.configuration", "file:" + fn) ;
+//        }
+//    }
 }
 
 
