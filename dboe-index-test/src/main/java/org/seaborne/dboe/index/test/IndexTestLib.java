129c2dc08f118ba70b1d82d072c3ab98b5485712
Andy Seaborne <andy@seaborne.org>
2015-02-14 19:19:16 +0000

    Format

--- dboe-index-test/src/main/java/org/seaborne/dboe/index/test/IndexTestLib.java
+++ dboe-index-test/src/main/java/org/seaborne/dboe/index/test/IndexTestLib.java
@@ -15,7 +15,7 @@
  *  information regarding copyright ownership.
  */
 
-package org.seaborne.dboe.index.test;
+package org.seaborne.dboe.index.test ;
 
 import static java.lang.String.format ;
 import static org.apache.jena.atlas.lib.ListUtils.asList ;
@@ -42,81 +42,75 @@ import org.seaborne.dboe.index.Index ;
 import org.seaborne.dboe.index.RangeIndex ;
 import org.seaborne.dboe.test.RecordLib ;
 
-public class IndexTestLib
-{
+public class IndexTestLib {
 
     // ---------- Utilities
-    
-//    public static RangeIndex buildRangeIndex(RangeIndexMaker maker, int[] keys)
-//    {
-//        RangeIndex index = maker.make() ;
-//        IndexTestLib.add(index, keys) ;
-//        return index ;
-//    }
 
-    public static Index buildIndex(IndexMaker maker, int[] keys)
-    {
+    // public static RangeIndex buildRangeIndex(RangeIndexMaker maker, int[]
+    // keys)
+    // {
+    // RangeIndex index = maker.make() ;
+    // IndexTestLib.add(index, keys) ;
+    // return index ;
+    // }
+
+    public static Index buildIndex(IndexMaker maker, int[] keys) {
         Index index = maker.makeIndex() ;
         IndexTestLib.add(index, keys) ;
         return index ;
     }
 
-    
-    public static void testIteration(RangeIndex index, int[] keys, int numIterations)
-    {
+    public static void testIteration(RangeIndex index, int[] keys, int numIterations) {
         // Shared across test-lets
         SortedSet<Integer> x = new TreeSet<>() ;
         for ( int v : keys )
             x.add(v) ;
-        
-        for ( int i = 0 ; i < numIterations ; i++ )
-        {
+
+        for ( int i = 0 ; i < numIterations ; i++ ) {
             int lo = random.nextInt(keys.length) ;
             int hi = random.nextInt(keys.length) ;
-            if ( lo > hi )
-            {
+            if ( lo > hi ) {
                 int t = lo ;
                 lo = hi ;
                 hi = t ;
             }
-            // Does not consider nulls - assumed to be part of functional testing.
+            // Does not consider nulls - assumed to be part of functional
+            // testing.
             // Tweak lo and hi
             if ( lo != 0 && random.nextFloat() < 0.5 )
-                lo-- ;  // Negatives confuse the int/record code.
+                lo-- ; // Negatives confuse the int/record code.
             if ( random.nextFloat() < 0.5 )
                 hi++ ;
-            
+
             List<Integer> slice = r(index.iterator(r(lo), r(hi))) ;
             List<Integer> expected = new ArrayList<>(keys.length) ;
             for ( Integer ii : x.subSet(lo, hi) )
                 expected.add(ii) ;
-            assertEquals(format("(%d,%d)",lo, hi), expected, slice) ;
+            assertEquals(format("(%d,%d)", lo, hi), expected, slice) ;
         }
     }
 
-    /* One random test : print the keys if there was a problem */ 
-    
-    public static void randTest(Index index, int maxValue, int numKeys)
-    {
+    /* One random test : print the keys if there was a problem */
+
+    public static void randTest(Index index, int maxValue, int numKeys) {
         if ( numKeys >= 5000 )
             System.err.printf("Warning: too many keys\n") ;
-            
+
         int[] keys1 = rand(numKeys, 0, maxValue) ;
-        int[] keys2 = permute(keys1, 4*numKeys) ;
+        int[] keys2 = permute(keys1, 4 * numKeys) ;
         try {
-            testInsert(index, keys1);
-            if ( true )
-            {
+            testInsert(index, keys1) ;
+            if ( true ) {
                 // Checking tests.
-                testIndexContents(index, keys2);
+                testIndexContents(index, keys2) ;
                 // Test iteration - quite expensive.
                 if ( index instanceof RangeIndex )
                     testIteration((RangeIndex)index, keys1, 10) ;
             }
             testDelete(index, keys2) ;
             index.close() ;
-        } catch (RuntimeException ex)
-        {
+        }
+        catch (RuntimeException ex) {
             System.err.printf("Index : %s\n", index.getClass().getName()) ;
             System.err.printf("int[] keys1 = {%s} ;\n", strings(keys1)) ;
             System.err.printf("int[] keys2 = {%s}; \n", strings(keys2)) ;
@@ -125,100 +119,86 @@ public class IndexTestLib
     }
 
     // ---- Test utils
-    
-    public static void testInsert(Index index, int[] keys)
-    {
+
+    public static void testInsert(Index index, int[] keys) {
         IndexTestLib.add(index, keys) ;
-        testIndexContents(index, keys);
+        testIndexContents(index, keys) ;
     }
 
-    public static Index testInsert(IndexMaker maker, int[] keys)
-    {
+    public static Index testInsert(IndexMaker maker, int[] keys) {
         Index index = maker.makeIndex() ;
-        testInsert(index, keys);
-        return index ; 
+        testInsert(index, keys) ;
+        return index ;
     }
 
-    public static void testInsertDelete(Index index, int[] buildKeys, int[] deleteKeys)
-    {
+    public static void testInsertDelete(Index index, int[] buildKeys, int[] deleteKeys) {
         testInsert(index, buildKeys) ;
         testDelete(index, deleteKeys) ;
     }
 
-    public static void testDelete(Index index, int[] vals)
-    {
+    public static void testDelete(Index index, int[] vals) {
         long size1 = index.size() ;
-        
+
         int count = 0 ;
         count = delete(index, vals) ;
-    
-        List<Record> x =  intToRecord(vals, RecordLib.TestRecordLength) ;
-        for ( Record r : x )
-        {
+
+        List<Record> x = intToRecord(vals, RecordLib.TestRecordLength) ;
+        for ( Record r : x ) {
             boolean b = index.delete(r) ;
             if ( b )
-                count ++ ;
+                count++ ;
         }
-        
+
         for ( Record r : x )
             Assert.assertFalse(index.contains(r)) ;
         long size2 = index.size() ;
 
-        assertEquals(size1-count, size2) ;
+        assertEquals(size1 - count, size2) ;
     }
 
-    public static int delete(Index index, int[] vals)
-    {
+    public static int delete(Index index, int[] vals) {
         int count = 0 ;
-        for ( int v : vals )
-        {
+        for ( int v : vals ) {
             boolean b = index.delete(r(v)) ;
             if ( b )
-                count ++ ;
+                count++ ;
         }
         return count ;
     }
 
-    public static void add(Index index, int[] vals)
-    {
-        //System.out.println("Add: "+Arrays.toString(vals)) ;
+    public static void add(Index index, int[] vals) {
+        // System.out.println("Add: "+Arrays.toString(vals)) ;
         List<Record> x = intToRecord(vals, RecordLib.TestRecordLength) ;
-        for ( Record r : x )
-        {
-            //System.out.println("  Add: "+r) ;
+        for ( Record r : x ) {
+            // System.out.println("  Add: "+r) ;
             index.add(r) ;
         }
     }
 
-    public static void testIndexContents(Index index, int[] records)
-    {
-        List<Integer> x = toIntList(index.iterator());
-        
-        // Make a unique list of expected records.  Remove duplicates
+    public static void testIndexContents(Index index, int[] records) {
+        List<Integer> x = toIntList(index.iterator()) ;
+
+        // Make a unique list of expected records. Remove duplicates
         List<Integer> y = unique(asList(records)) ;
-        
+
         assertEquals("Expected records size and tree size different", y.size(), index.size()) ;
         assertEquals("Expected records size and iteration over all keys are of different sizes", y.size(), x.size()) ;
-        
-        if ( index instanceof RangeIndex )
-        {
+
+        if ( index instanceof RangeIndex ) {
             // Check sorted order
-            for ( int i = 0 ; i < x.size()-2 ; i++ )
-            {
-                if ( x.get(i) > x.get(i+1) )
-                {
-                    fail("check failed: "+strings(records)) ;
+            for ( int i = 0 ; i < x.size() - 2 ; i++ ) {
+                if ( x.get(i) > x.get(i + 1) ) {
+                    fail("check failed: " + strings(records)) ;
                     return ;
                 }
             }
         }
-        
+
         // Check each expected record is in the tree
-        for ( int k : y)
-        {
+        for ( int k : y ) {
             Record rec = intToRecord(k) ;
             Record r2 = index.find(rec) ;
-            assertNotNull("Finding "+rec, r2) ;
+            assertNotNull("Finding " + rec, r2) ;
         }
     }
 }
