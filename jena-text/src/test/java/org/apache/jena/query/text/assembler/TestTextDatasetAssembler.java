a8002e8ce3452aad7c51f20600c9389ef71a6dd5
Chris Dollin <ehog.hedge@googlemail.com>
2015-03-12 15:25:27 +0000

    Added test to check that the assembler gives access to the two-argument constructor of a docProducer.

--- jena-text/src/test/java/org/apache/jena/query/text/assembler/TestTextDatasetAssembler.java
+++ jena-text/src/test/java/org/apache/jena/query/text/assembler/TestTextDatasetAssembler.java
@@ -23,6 +23,7 @@ import org.apache.jena.query.text.TextDocProducer ;
 import org.apache.jena.query.text.TextIndex ;
 import org.apache.jena.query.text.TextIndexLucene ;
 import org.apache.jena.query.text.TextQuery ;
+import org.junit.Ignore;
 import org.junit.Test ;
 
 import com.hp.hpl.jena.assembler.Assembler ;
@@ -30,6 +31,7 @@ import com.hp.hpl.jena.assembler.exceptions.AssemblerException ;
 import com.hp.hpl.jena.graph.Node ;
 import com.hp.hpl.jena.query.Dataset ;
 import com.hp.hpl.jena.rdf.model.Resource ;
+import com.hp.hpl.jena.sparql.core.DatasetGraph;
 import com.hp.hpl.jena.sparql.core.QuadAction ;
 import com.hp.hpl.jena.tdb.assembler.AssemblerTDB ;
 import com.hp.hpl.jena.vocabulary.RDF ;
@@ -47,6 +49,7 @@ public class TestTextDatasetAssembler extends AbstractTestTextAssembler {
 	private static final Resource noDatasetPropertySpec;
 	private static final Resource noIndexPropertySpec;
 	private static final Resource customTextDocProducerSpec;
+	private static final Resource customDyadicTextDocProducerSpec;
 	
 	@Test
 	public void testSimpleDatasetAssembler() {
@@ -69,6 +72,15 @@ public class TestTextDatasetAssembler extends AbstractTestTextAssembler {
 	    Dataset dataset = (Dataset)Assembler.general.open(customTextDocProducerSpec) ;
 	    DatasetGraphText dsgText = (DatasetGraphText)dataset.asDatasetGraph() ;
         assertTrue(dsgText.getMonitor() instanceof CustomTextDocProducer) ;
+        dataset.close();
+	}
+	
+	@Test
+	public void testCustomTextDocProducerDyadicConstructor() {
+	    Dataset dataset = (Dataset)Assembler.general.open(customDyadicTextDocProducerSpec) ;
+	    DatasetGraphText dsgText = (DatasetGraphText)dataset.asDatasetGraph() ;
+        assertTrue(dsgText.getMonitor() instanceof CustomDyadicTextDocProducer) ;
+        dataset.close();
 	}
 	
 	static {
@@ -94,11 +106,33 @@ public class TestTextDatasetAssembler extends AbstractTestTextAssembler {
                      .addProperty(TextVocab.pIndex, SIMPLE_INDEX_SPEC5)
                      .addProperty(TextVocab.pTextDocProducer, model.createResource("java:org.apache.jena.query.text.assembler.TestTextDatasetAssembler$CustomTextDocProducer"));
 		
+		customDyadicTextDocProducerSpec =
+                model.createResource(TESTBASE + "customDyadicTextDocProducerSpec")
+                     .addProperty(RDF.type, TextVocab.textDataset)
+                     .addProperty(TextVocab.pDataset, SIMPLE_DATASET_SPEC)
+                     .addProperty(TextVocab.pIndex, SIMPLE_INDEX_SPEC5)
+                     .addProperty(TextVocab.pTextDocProducer, model.createResource("java:org.apache.jena.query.text.assembler.TestTextDatasetAssembler$CustomDyadicTextDocProducer"));
+		
 	}
 	
 	private static class CustomTextDocProducer implements TextDocProducer {
 	    
 	    public CustomTextDocProducer(TextIndex textIndex) { }
+
+        @Override
+        public void start() { }
+
+        @Override
+        public void finish() { }
+
+        @Override
+        public void change(QuadAction qaction, Node g, Node s, Node p, Node o) { }
+	}
+	
+	
+	private static class CustomDyadicTextDocProducer implements TextDocProducer {
+	    
+	    public CustomDyadicTextDocProducer(DatasetGraph dg, TextIndex textIndex) { }
 
         @Override
         public void start() { }
