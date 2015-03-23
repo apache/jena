a9b8dc1c55e5f350cf17494455a159f32128b3b4
Stian Soiland-Reyes <stain@apache.org>
2015-03-23 23:05:08 +0000

    JENA-901 LPBRuleEngine.tabledGoals now a Guava Cache
    with a boundary of 512k entries (override with system
    property jena.rulesys.lp.max_cached_tabled_goals)
    and weak references, so it should not run out of memory.

--- jena-core/src/main/java/org/apache/jena/reasoner/rulesys/impl/LPBRuleEngine.java
+++ jena-core/src/main/java/org/apache/jena/reasoner/rulesys/impl/LPBRuleEngine.java
@@ -18,14 +18,28 @@
 
 package org.apache.jena.reasoner.rulesys.impl;
 
-import org.apache.jena.graph.* ;
-import org.apache.jena.reasoner.* ;
-import org.apache.jena.reasoner.rulesys.* ;
-import org.apache.jena.util.iterator.* ;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+
+import org.apache.jena.ext.com.google.common.cache.Cache;
+import org.apache.jena.ext.com.google.common.cache.CacheBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.*;
+import org.apache.jena.graph.Node;
+import org.apache.jena.graph.Triple;
+import org.apache.jena.reasoner.ReasonerException;
+import org.apache.jena.reasoner.TriplePattern;
+import org.apache.jena.reasoner.rulesys.BackwardRuleInfGraphI;
+import org.apache.jena.reasoner.rulesys.Rule;
+import org.apache.jena.util.iterator.ExtendedIterator;
+import org.apache.jena.util.iterator.WrappedIterator;
 
 /**
  * LP version of the core backward chaining engine. For each parent inference
@@ -35,46 +49,49 @@ import java.util.*;
  * of the LPInterpreter - one per query.
  */
 public class LPBRuleEngine {
-    
+
 //  =======================================================================
 //   variables
 
     /** Store which holds the raw and compiled rules */
     protected LPRuleStore ruleStore;
-    
+
     /** The parent inference graph to which this engine is attached */
     protected BackwardRuleInfGraphI infGraph;
-    
+
     /** True if debug information should be written out */
     protected boolean traceOn = false;
-    
+
     /** Set to true to flag that derivations should be logged */
     protected boolean recordDerivations;
-        
+
     /** List of engine instances which are still processing queries */
-    protected List<LPInterpreter> activeInterpreters = new ArrayList<>();
-    
+    protected List<LPInterpreter> activeInterpreters = new LinkedList<>();
+
+    protected final int MAX_CACHED_TABLED_GOALS =
+			Integer.getInteger("jena.rulesys.lp.max_cached_tabled_goals", 512*1024);
+
     /** Table mapping tabled goals to generators for those goals.
-     *  This is here so that partial goal state can be shared across multiple queries. */
-    protected HashMap<TriplePattern, Generator> tabledGoals = new HashMap<>();
-    
+     *  This is here so that partial goal state can be shared across multiple queries.
+     */
+    protected Cache<TriplePattern, Generator> tabledGoals = CacheBuilder.newBuilder()
+    	       .maximumSize(MAX_CACHED_TABLED_GOALS).weakValues().build();
+
     /** Set of generators waiting to be run */
     protected LinkedList<LPAgendaEntry> agenda = new LinkedList<>();
-//    protected List agenda = new ArrayList();
-//    protected Collection agenda = new HashSet();
-    
+
     /** Optional profile of number of time each rule is entered, set to non-null to profile */
     protected HashMap<String, Count> profile;
-    
+
     /** The number of generator cycles to wait before running a completion check.
      *  If set to 0 then checks will be done in the generator each time. */
     public static final int CYCLES_BETWEEN_COMPLETION_CHECK = 3;
-    
+
     static Logger logger = LoggerFactory.getLogger(LPBRuleEngine.class);
-    
+
 //  =======================================================================
 //  Constructors
-    
+
     /**
      * Constructor.
      * @param infGraph the parent inference graph which is using this engine
@@ -84,7 +101,7 @@ public class LPBRuleEngine {
         this.infGraph = infGraph;
         ruleStore = rules;
     }
-    
+
     /**
      * Constructor. Creates an empty engine to which rules must be added.
      * @param infGraph the parent inference graph which is using this engine
@@ -93,10 +110,10 @@ public class LPBRuleEngine {
         this.infGraph = infGraph;
         ruleStore = new LPRuleStore();
     }
-    
+
 //  =======================================================================
 //  Control methods
-    
+
     /**
      * Start a new interpreter running to answer a query.
      * @param goal the query to be processed
@@ -107,20 +124,20 @@ public class LPBRuleEngine {
         activeInterpreters.add(interpreter);
         return WrappedIterator.create( new LPTopGoalIterator(interpreter));
     }
-    
+
     /**
      * Clear all tabled results.
      */
     public synchronized void reset() {
         checkSafeToUpdate();
-        tabledGoals = new HashMap<>();
+        tabledGoals.invalidateAll();
         agenda.clear();
     }
-    
+
     /**
      * Add a single rule to the store.
      * N.B. This will invalidate current partial results and the engine
-     * should be reset() before future queries. 
+     * should be reset() before future queries.
      */
     public synchronized void addRule(Rule rule) {
         checkSafeToUpdate();
@@ -129,17 +146,17 @@ public class LPBRuleEngine {
         }
         ruleStore.addRule(rule);
     }
-    
+
     /**
      * Remove a single rule from the store.
      * N.B. This will invalidate current partial results and the engine
-     * should be reset() before future queries. 
+     * should be reset() before future queries.
      */
     public synchronized void deleteRule(Rule rule) {
         checkSafeToUpdate();
         ruleStore.deleteRule(rule);
     }
-    
+
     /**
      * Return an ordered list of all registered rules.
      */
@@ -147,15 +164,15 @@ public class LPBRuleEngine {
         checkSafeToUpdate();
         return ruleStore.getAllRules();
     }
-    
+
     /**
      * Delete all the rules.
      */
     public synchronized void deleteAllRules() {
         checkSafeToUpdate();
-        ruleStore.deleteAllRules();     
+        ruleStore.deleteAllRules();
     }
-    
+
     /**
      * Stop the current work. Forcibly stop all current query instances over this engine.
      */
@@ -167,7 +184,7 @@ public class LPBRuleEngine {
             aCopy.close();
         }
     }
-       
+
     /**
      * Set the state of the trace flag. If set to true then rule firings
      * are logged out to the Log at "INFO" level.
@@ -175,21 +192,21 @@ public class LPBRuleEngine {
     public void setTraceOn(boolean state) {
         traceOn = state;
     }
-    
+
     /**
      * Return true if traces of rule firings should be logged.
      */
     public boolean isTraceOn() {
         return traceOn;
     }
-       
+
     /**
      * Set to true to enable derivation caching
      */
     public void setDerivationLogging(boolean recordDerivations) {
         this.recordDerivations = recordDerivations;
     }
-    
+
     /**
      * Return true in derivations should be logged.
      */
@@ -201,23 +218,23 @@ public class LPBRuleEngine {
     public LPRuleStore getRuleStore() {
         return ruleStore;
     }
-    
+
     /** Return the parent infernce graph associated with this engine */
     public BackwardRuleInfGraphI getInfGraph() {
         return infGraph;
     }
-    
+
     /** Detatch the given engine from the list of active engines for this inf graph */
     public synchronized void detach(LPInterpreter engine) {
         activeInterpreters.remove(engine);
     }
-    
+
     /**
      * Check that there are no currently processing queries.
      * Could throw an exception here but often this can be caused by simply leaving
      * an unclosed iterator. So instead we try to close the iterators and assume the
      * rest of the context will be reset by the add call.
-     * 
+     *
      * <p>Should be called from within a synchronized block.
      */
     public void checkSafeToUpdate() {
@@ -236,8 +253,8 @@ public class LPBRuleEngine {
             }
         }
     }
-    
-    
+
+
 //  =======================================================================
 //  Interface for tabled operations
 
@@ -248,42 +265,73 @@ public class LPBRuleEngine {
     public synchronized void tablePredicate(Node predicate) {
         ruleStore.tablePredicate(predicate);
     }
-    
+
     /**
      * Return a generator for the given goal (assumes that the caller knows that
      * the goal should be tabled).
+     *
+     * Note: If an earlier Generator for the same <code>goal</code> exists in the
+     * cache, it will be returned without considering the provided <code>clauses</code>.
+     *
      * @param goal the goal whose results are to be generated
      * @param clauses the precomputed set of code blocks used to implement the goal
      */
-    public synchronized Generator generatorFor(TriplePattern goal, List<RuleClauseCode> clauses) {
-        Generator generator = tabledGoals.get(goal);
-        if (generator == null) {
-            LPInterpreter interpreter = new LPInterpreter(this, goal, clauses, false);
-            activeInterpreters.add(interpreter);
-            generator = new Generator(interpreter, goal);
-            schedule(generator);
-            tabledGoals.put(goal, generator);
-        }
-        return generator;
+    public synchronized Generator generatorFor(final TriplePattern goal, final List<RuleClauseCode> clauses) {
+        try {
+			return tabledGoals.get(goal, new Callable<Generator>() {
+			 	@Override
+			    public Generator call() {
+			 		/** FIXME: Unify with #generatorFor(TriplePattern) - but investigate what about
+			 		 * the edge case that this method might have been called with the of goal == null
+			 		 * or goal.size()==0 -- which gives different behaviour in
+			 		 * LPInterpreter constructor than through the route of
+			 		 * generatorFor(TriplePattern) which calls a different LPInterpreter constructor
+			 		 * which would fill in from RuleStore.
+			 		 */
+			        LPInterpreter interpreter = new LPInterpreter(LPBRuleEngine.this, goal, clauses, false);
+			        activeInterpreters.add(interpreter);
+			        Generator generator = new Generator(interpreter, goal);
+			        schedule(generator);
+			        return generator;
+			 	}
+			});
+		} catch (ExecutionException e) {
+			if (e.getCause() instanceof RuntimeException) {
+				throw (RuntimeException)e.getCause();
+			}
+			throw new RuntimeException(e);
+		}
     }
-        
+
     /**
      * Return a generator for the given goal (assumes that the caller knows that
      * the goal should be tabled).
      * @param goal the goal whose results are to be generated
      */
-    public synchronized Generator generatorFor(TriplePattern goal) {
-        Generator generator = tabledGoals.get(goal);
-        if (generator == null) {
-            LPInterpreter interpreter = new LPInterpreter(this, goal, false);
-            activeInterpreters.add(interpreter);
-            generator = new Generator(interpreter, goal);
-            schedule(generator);
-            tabledGoals.put(goal, generator);
-        }
-        return generator;
+    public synchronized Generator generatorFor(final TriplePattern goal) {
+        try {
+			return tabledGoals.get(goal, new Callable<Generator>() {
+			 	@Override
+			    public Generator call() {
+		            LPInterpreter interpreter = new LPInterpreter(LPBRuleEngine.this, goal, false);
+		            activeInterpreters.add(interpreter);
+		            Generator generator = new Generator(interpreter, goal);
+		            schedule(generator);
+		            return generator;
+			 	}
+			});
+		} catch (ExecutionException e) {
+			if (e.getCause() instanceof RuntimeException) {
+				throw (RuntimeException)e.getCause();
+			}
+			throw new RuntimeException(e);
+		}
     }
-    
+
+    long cachedTabledGoals() {
+    	return tabledGoals.size();
+    }
+
     /**
      * Register that a generator or specific generator state (Consumer choice point)
      * is now ready to run.
@@ -291,7 +339,7 @@ public class LPBRuleEngine {
     public void schedule(LPAgendaEntry state) {
         agenda.add(state);
     }
-    
+
     /**
      * Run the scheduled generators until the given generator is ready to run.
      */
@@ -300,14 +348,14 @@ public class LPBRuleEngine {
         if (CYCLES_BETWEEN_COMPLETION_CHECK > 0) {
             batch = new ArrayList<>(CYCLES_BETWEEN_COMPLETION_CHECK);
         }
-        int count = 0; 
+        int count = 0;
         while(!gen.isReady()) {
             if (agenda.isEmpty()) {
 //                System.out.println("Cycled " + this + ", " + count);
                 return;
             }
-            
-            LPAgendaEntry next = getNextAgendaEntry(); 
+
+            LPAgendaEntry next = getNextAgendaEntry();
             next.pump();
             count ++;
             if (CYCLES_BETWEEN_COMPLETION_CHECK > 0) {
@@ -321,10 +369,10 @@ public class LPBRuleEngine {
         if (CYCLES_BETWEEN_COMPLETION_CHECK > 0 && !batch.isEmpty()) {
             Generator.checkForCompletions(batch);
         }
-        
+
 //        System.out.println("Cycled " + this + ", " + count);
     }
-    
+
     /**
      * Pick and agenda entry to progress and remove it from the queue
      */
@@ -336,7 +384,7 @@ public class LPBRuleEngine {
             return next;
         }
     }
-    
+
     /**
      * Check all known interpeter contexts to see if any are complete.
      */
@@ -356,10 +404,10 @@ public class LPBRuleEngine {
         }
         Generator.checkForCompletions( contexts );
     }
-    
+
 //  =======================================================================
 //  Profiling support
-   
+
     /**
      * Record a rule invocation in the profile count.
      */
@@ -374,7 +422,7 @@ public class LPBRuleEngine {
             }
         }
     }
-    
+
     /**
      * Reset the profile.
      * @param enable it true then profiling will continue with a new empty profile table,
@@ -383,7 +431,7 @@ public class LPBRuleEngine {
     public void resetProfile(boolean enable) {
         profile = enable ? new HashMap<String, Count>() : null;
     }
-    
+
     /**
      * Print a profile of rules used since the last reset.
      */
@@ -401,7 +449,7 @@ public class LPBRuleEngine {
             }
         }
     }
-    
+
     /**
      * Record count of number of rule invocations, used in profile structure only.
      */
@@ -413,29 +461,29 @@ public class LPBRuleEngine {
         public Count(RuleClauseCode clause) {
             this.clause = clause;
         }
-        
+
         /** return the count value */
         public int getCount() {
-            return count;        
+            return count;
         }
-        
+
         /** increment the count value, return the count object */
         public Count inc() {
             count++;
             return this;
         }
-        
+
         /** Ordering */
         @Override
         public int compareTo(Count other) {
             return (count < other.count) ? -1 : ( (count == other.count) ? 0 : +1);
         }
-        
+
         /** Printable form */
         @Override
         public String toString() {
             return " " + count + "\t - " + clause;
         }
-        
+
     }
 }
