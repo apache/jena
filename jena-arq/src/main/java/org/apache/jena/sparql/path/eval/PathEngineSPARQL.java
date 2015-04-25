aa770525ebd0ca1db4adf49e178edad356987b7d
Andy Seaborne <andy@apache.org>
2015-04-26 00:13:22 +0100

    Some java8 streams usage.

--- jena-arq/src/main/java/org/apache/jena/sparql/path/eval/PathEngineSPARQL.java
+++ jena-arq/src/main/java/org/apache/jena/sparql/path/eval/PathEngineSPARQL.java
@@ -50,8 +50,7 @@ public class PathEngineSPARQL extends PathEngine
 
         // Feed one side into the other
         Iter<Node> iter = eval(part1, node) ;
-        for (Node n : iter)
-            eval(part2, n, output) ;
+        iter.forEachRemaining((n)->eval(part2, n, output)) ;
     }
 
     @Override
