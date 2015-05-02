461cf36f6cdba77ca6b35f874995019453a9fbe8
Andy Seaborne <andy@apache.org>
2015-05-02 18:22:29 +0100

    Surppress hiding warnings

--- jena-core/src/main/java/org/apache/jena/ontology/impl/OntPropertyImpl.java
+++ jena-core/src/main/java/org/apache/jena/ontology/impl/OntPropertyImpl.java
@@ -54,6 +54,7 @@ public class OntPropertyImpl
      * Note: should not be invoked directly by user code: use
      * {@link org.apache.jena.rdf.model.RDFNode#as as()} instead.
      */
+    @SuppressWarnings("hiding")
     public static Implementation factory = new Implementation() {
         @Override
         public EnhNode wrap( Node n, EnhGraph eg ) {
