3f163bff6b1d4020c0f2a77321bbb47baf743ebb
confidencesun <confidence.sun@gmail.com>
2015-05-27 07:38:58 +0800

    delete JENA-491 example code

--- jena-arq/src/main/java/com/hp/hpl/jena/sparql/lang/sparql_11_plus/QueryExecutionPlus.java
+++
@@ -1,8 +0,0 @@
-package com.hp.hpl.jena.sparql.lang.sparql_11_plus;
-
-import com.hp.hpl.jena.query.Dataset;
-import com.hp.hpl.jena.query.QueryExecution;
-
-public interface QueryExecutionPlus extends QueryExecution {
-	public Dataset execConstructDataset();
-}
