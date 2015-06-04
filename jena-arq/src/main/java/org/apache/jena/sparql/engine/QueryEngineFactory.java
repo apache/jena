60bca65d5288ce59a697f1a737e35e43bffd1d63
ajs6f <ajs6f@virginia.edu>
2015-06-04 09:26:17 -0400

    Enhance Javadocs for QueryEngineFactory

--- jena-arq/src/main/java/org/apache/jena/sparql/engine/QueryEngineFactory.java
+++ jena-arq/src/main/java/org/apache/jena/sparql/engine/QueryEngineFactory.java
@@ -18,19 +18,48 @@
 
 package org.apache.jena.sparql.engine;
 
-import org.apache.jena.query.Query ;
-import org.apache.jena.sparql.algebra.Op ;
-import org.apache.jena.sparql.core.DatasetGraph ;
-import org.apache.jena.sparql.engine.binding.Binding ;
-import org.apache.jena.sparql.util.Context ;
+import org.apache.jena.query.Query;
+import org.apache.jena.sparql.algebra.Op;
+import org.apache.jena.sparql.core.DatasetGraph;
+import org.apache.jena.sparql.engine.binding.Binding;
+import org.apache.jena.sparql.util.Context;
 
-/** Additional QueryEngineFactory's 
- * can be dynamically added to the registry. */ 
-public interface QueryEngineFactory
-{
-    public boolean accept(Query query, DatasetGraph dataset, Context context) ;
-    public Plan create(Query query, DatasetGraph dataset, Binding inputBinding, Context context) ;
-    
-    public boolean accept(Op op, DatasetGraph dataset, Context context) ;
-    public Plan create(Op op, DatasetGraph dataset, Binding inputBinding, Context context) ;
+/**
+ * A QueryEngineFactory builds query {@link Plan}s via {@link #create(Query, DatasetGraph, Binding, Context)} or
+ * {@link #create(Op, DatasetGraph, Binding, Context)}. Before calling either of those methods, a client should call
+ * {@link #accept(Query, DatasetGraph, Context)} or {@link #accept(Op, DatasetGraph, Context)} to determine whether the
+ * particular type of query engine produced by this factory can handle the particular task in hand. A QueryEngineFactory
+ * can be registered for use via {@link QueryEngineRegistry#addFactory(QueryEngineFactory)}. It can also be unregistered
+ * via {@link QueryEngineRegistry#removeFactory(QueryEngineFactory)}.
+ */
+public interface QueryEngineFactory {
+	/**
+	 * Detect appropriate requests for a particular query engine for a particular graph type.
+	 * 
+	 * @param query
+	 *            a {@link Query} to be executed
+	 * @param dataset
+	 *            the {@link DatasetGraph} over which the query is to be executed
+	 * @param context
+	 *            the {@link Context} in which the query is to be executed
+	 * @return whether the kind of query engine produced by this factory can handle this task
+	 */
+	public boolean accept(Query query, DatasetGraph dataset, Context context);
+
+	public Plan create(Query query, DatasetGraph dataset, Binding inputBinding, Context context);
+
+	/**
+	 * Detect appropriate requests for a particular query engine for a particular graph type.
+	 * 
+	 * @param op
+	 *            an {@link Op} to be executed
+	 * @param dataset
+	 *            the {@link DatasetGraph} over which the operation is to be executed
+	 * @param context
+	 *            the {@link Context} in which the operation is to be executed
+	 * @return whether the kind of query engine produced by this factory can handle this task
+	 */
+	public boolean accept(Op op, DatasetGraph dataset, Context context);
+
+	public Plan create(Op op, DatasetGraph dataset, Binding inputBinding, Context context);
 }
