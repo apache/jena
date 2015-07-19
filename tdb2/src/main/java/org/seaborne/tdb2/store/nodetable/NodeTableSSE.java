246ba2e42426d638b21257999ed810e55bd938e3
Andy Seaborne <andy@seaborne.org>
2015-07-19 21:32:32 +0100

    Adjust to jena changes.

--- tdb2/src/main/java/org/seaborne/tdb2/store/nodetable/NodeTableSSE.java
+++ tdb2/src/main/java/org/seaborne/tdb2/store/nodetable/NodeTableSSE.java
@@ -23,7 +23,6 @@ import org.apache.jena.atlas.io.BlockUTF8 ;
 import org.apache.jena.atlas.lib.StrUtils ;
 import org.apache.jena.graph.Node ;
 import org.apache.jena.graph.NodeFactory ;
-import org.apache.jena.rdf.model.AnonId ;
 import org.apache.jena.riot.RiotException ;
 import org.apache.jena.riot.out.NodeFmtLib ;
 import org.apache.jena.riot.system.PrefixMap ;
@@ -236,7 +235,7 @@ public class NodeTableSSE extends NodeTableNative {
                 // In particular, bnode labels can contain ":" from Jena
                 // TokenizerText does not recognize these.
                 str = str.substring(2) ;
-                return NodeFactory.createAnon(new AnonId(str)) ;
+                return NodeFactory.createBlankNode(str) ;
             }
 
             if ( str.startsWith("<") )
