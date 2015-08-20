b54af5d99ad5ae7c92b0c6f49bdd35d31aa9b78f
Andy Seaborne <andy@apache.org>
2015-08-20 13:03:42 +0100

    Don't call out.finish in either path.

--- jena-arq/src/main/java/org/apache/jena/riot/RiotReader.java
+++ jena-arq/src/main/java/org/apache/jena/riot/RiotReader.java
@@ -285,7 +285,7 @@ public class RiotReader
                 {
                     out.start();
                     parse(input, lang, baseIRI, out);
-                    out.finish() ;
+                    // Unsafe: out.finish() ;
                 }
             });
             t.start();
@@ -321,7 +321,7 @@ public class RiotReader
                 {
                     out.start();
                     parse(input, lang, baseIRI, out);
-                    //out.finish() ;
+                    // Unsafe: out.finish() ;
                 }
             });
             t.start();
