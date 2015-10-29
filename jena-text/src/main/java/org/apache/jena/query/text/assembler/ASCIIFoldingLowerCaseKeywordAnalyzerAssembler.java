b0af9b111fc6fcb7d922a668dc1ed7209bac5bc5
Osma Suominen <osma.suominen@aalto.fi>
2015-10-29 17:08:54 +0200

    first implementation of ASCIIFoldingLowerCaseKeywordAnalyzer

---
+++ jena-text/src/main/java/org/apache/jena/query/text/assembler/ASCIIFoldingLowerCaseKeywordAnalyzerAssembler.java
@@ -0,0 +1,47 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.jena.query.text.assembler ;
+
+import org.apache.jena.assembler.Assembler ;
+import org.apache.jena.assembler.Mode ;
+import org.apache.jena.assembler.assemblers.AssemblerBase ;
+import org.apache.jena.query.text.TextIndexLucene;
+import org.apache.jena.query.text.analyzer.ASCIIFoldingLowerCaseKeywordAnalyzer;
+import org.apache.jena.rdf.model.Resource ;
+import org.apache.lucene.analysis.Analyzer;
+
+/**
+ * Assembler to create lowercase keyword analyzers.
+ */
+public class ASCIIFoldingLowerCaseKeywordAnalyzerAssembler extends AssemblerBase {
+    /*
+    text:map (
+         [ text:field "text" ; 
+           text:predicate rdfs:label;
+           text:analyzer [
+               a  lucene:LowerCaseKeywordAnalyzer ;           ]
+         ]
+        .
+     */
+
+    @Override
+    public Analyzer open(Assembler a, Resource root, Mode mode) {
+        return new ASCIIFoldingLowerCaseKeywordAnalyzer(TextIndexLucene.VER);
+    }
+}
---
+++ jena-text/src/test/java/org/apache/jena/query/text/TestDatasetWithASCIIFoldingLowerCaseKeywordAnalyzer.java
@@ -0,0 +1,59 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.jena.query.text;
+
+import java.util.Arrays ;
+import java.util.HashSet ;
+import java.util.Set ;
+
+import org.apache.jena.atlas.lib.StrUtils ;
+import org.junit.Before ;
+import org.junit.Test ;
+
+/**
+ * This class defines a setup configuration for a dataset that uses an ASCII folding lowercase keyword analyzer with a Lucene index.
+ */
+public class TestDatasetWithASCIIFoldingLowerCaseKeywordAnalyzer extends TestDatasetWithLowerCaseKeywordAnalyzer {
+    @Override
+    @Before
+    public void before() {
+        init("text:ASCIIFoldingLowerCaseKeywordAnalyzer");
+    }    
+    
+    @Test
+    public void testASCIIFoldingLowerCaseKeywordAnalyzerIsCaseAndAccentInsensitive() {
+        final String testName = "testASCIIFoldingLowerCaseKeywordAnalyzerIsCaseAndAccentInsensitive";
+        final String turtle = StrUtils.strjoinNL(
+                TURTLE_PROLOG,
+                "<" + RESOURCE_BASE + testName + ">",
+                "  rdfs:label 'Feeling a déjà vu'",
+                "."
+                );
+        String queryString = StrUtils.strjoinNL(
+                QUERY_PROLOG,
+                "SELECT ?s",
+                "WHERE {",
+                "    ?s text:query ( rdfs:label '\"feeling ä déja\"*' 10 ) .",
+                "}"
+                );
+        Set<String> expectedURIs = new HashSet<>() ;
+        expectedURIs.addAll( Arrays.asList(RESOURCE_BASE + testName)) ;
+        doTestSearch(turtle, queryString, expectedURIs);
+    }
+}
