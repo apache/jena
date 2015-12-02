aecb5f1be44311e868f0978ca435ff8479980d8d
Andy Seaborne <andy@apache.org>
2015-12-02 16:02:15 +0000

    Reformat

--- jena-arq/src/main/java/org/apache/jena/sparql/resultset/XMLOutputResultSet.java
+++ jena-arq/src/main/java/org/apache/jena/sparql/resultset/XMLOutputResultSet.java
@@ -35,8 +35,7 @@ import org.apache.jena.rdf.model.impl.Util ;
 /** XML Output (ResultSet format) */
 
 
-public class XMLOutputResultSet
-    implements ResultSetProcessor, XMLResults
+public class XMLOutputResultSet implements ResultSetProcessor, XMLResults
 {
     static boolean outputExplicitUnbound = false ;
     
@@ -50,222 +49,201 @@ public class XMLOutputResultSet
     int bNodeCounter = 0 ;
     Map<Resource, String> bNodeMap = new HashMap<>() ;
     
-    XMLOutputResultSet(OutputStream outStream)
-    {
-        this(new IndentedWriter(outStream)) ;
+    XMLOutputResultSet(OutputStream outStream) {
+        this(new IndentedWriter(outStream));
     }
-    
-    XMLOutputResultSet(IndentedWriter indentedOut)
-    {
-        out = indentedOut ;
+
+    XMLOutputResultSet(IndentedWriter indentedOut) {
+        out = indentedOut;
     }
-    
+
     @Override
-    public void start(ResultSet rs)
-    {
+    public void start(ResultSet rs) {
         if ( xmlInst )
-            out.println("<?xml version=\"1.0\"?>") ;
-        
-        if ( stylesheetURL != null )
-        {
-            out.print("<?xml-stylesheet type=\"text/xsl\" href=\"") ;
-            out.print(stylesheetURL) ;
-            out.println("\"?>") ;
+            out.println("<?xml version=\"1.0\"?>");
+
+        if ( stylesheetURL != null ) {
+            out.print("<?xml-stylesheet type=\"text/xsl\" href=\"");
+            out.print(stylesheetURL);
+            out.println("\"?>");
         }
-        
+
         // ---- Root
-        out.print("<") ;
-        out.print(dfRootTag) ;
-        out.print(" xmlns=\"") ;
-        out.print(dfNamespace) ;
-        out.println("\">") ;
+        out.print("<");
+        out.print(dfRootTag);
+        out.print(" xmlns=\"");
+        out.print(dfNamespace);
+        out.println("\">");
 
         // ---- Header
 
-        out.incIndent(INDENT) ;
-        out.print("<") ;
-        out.print(dfHead) ;
-        out.println(">") ;
-        
-        if ( false )
-        {
-            String link = "UNSET" ;
-            out.print("<link href=\"") ;
-            out.print(link) ;
-            out.println("\"/>") ;
+        out.incIndent(INDENT);
+        out.print("<");
+        out.print(dfHead);
+        out.println(">");
+
+        if ( false ) {
+            String link = "UNSET";
+            out.print("<link href=\"");
+            out.print(link);
+            out.println("\"/>");
         }
-        
-        for (String n : rs.getResultVars())
-        {
-            out.incIndent(INDENT) ;
-            out.print("<") ;
-            out.print(dfVariable) ;
-            out.print(" ") ;
-            out.print(dfAttrVarName) ;
-            out.print("=\"") ;
-            out.print(n) ;
-            out.print("\"") ;
-            out.println("/>") ;
-            out.decIndent(INDENT) ;
+
+        for ( String n : rs.getResultVars() ) {
+            out.incIndent(INDENT);
+            out.print("<");
+            out.print(dfVariable);
+            out.print(" ");
+            out.print(dfAttrVarName);
+            out.print("=\"");
+            out.print(n);
+            out.print("\"");
+            out.println("/>");
+            out.decIndent(INDENT);
         }
-        out.print("</") ;
-        out.print(dfHead) ;
-        out.println(">") ;
-        out.decIndent(INDENT) ;
-        
+        out.print("</");
+        out.print(dfHead);
+        out.println(">");
+        out.decIndent(INDENT);
+
         // Start results proper
-        out.incIndent(INDENT) ;
-        out.print("<") ;
-        out.print(dfResults) ;
-        out.println(">") ;
-        out.incIndent(INDENT) ;
+        out.incIndent(INDENT);
+        out.print("<");
+        out.print(dfResults);
+        out.println(">");
+        out.incIndent(INDENT);
     }
 
     @Override
-    public void finish(ResultSet rs)
-    {
-        out.decIndent(INDENT) ;
-        out.print("</") ;
-        out.print(dfResults) ;
-        out.println(">") ;
-        out.decIndent(INDENT) ;
-        out.print("</") ;
-        out.print(dfRootTag) ;
-        out.println(">") ;
-        out.flush() ;
+    public void finish(ResultSet rs) {
+        out.decIndent(INDENT);
+        out.print("</");
+        out.print(dfResults);
+        out.println(">");
+        out.decIndent(INDENT);
+        out.print("</");
+        out.print(dfRootTag);
+        out.println(">");
+        out.flush();
     }
 
     @Override
-    public void start(QuerySolution qs)
-    {
-        out.print("<") ;
-        out.print(dfSolution) ;
-        out.println(">") ;
-        index ++ ;
-        out.incIndent(INDENT) ;
+    public void start(QuerySolution qs) {
+        out.print("<");
+        out.print(dfSolution);
+        out.println(">");
+        index++;
+        out.incIndent(INDENT);
     }
 
     @Override
-    public void finish(QuerySolution qs)
-    {
-        out.decIndent(INDENT) ;
-        out.print("</") ;
-        out.print(dfSolution) ;
-        out.println(">") ;
+    public void finish(QuerySolution qs) {
+        out.decIndent(INDENT);
+        out.print("</");
+        out.print(dfSolution);
+        out.println(">");
     }
 
     @Override
-    public void binding(String varName, RDFNode node)
-    {
-        if ( node == null && ! outputExplicitUnbound )
-            return ;
-        
-        out.print("<") ; 
-        out.print(dfBinding) ;
-        out.print(" name=\"") ;
-        out.print(varName) ;
-        out.println("\">") ;
-        out.incIndent(INDENT) ;
-        printBindingValue(node) ;
-        out.decIndent(INDENT) ;
-        out.print("</") ;
-        out.print(dfBinding) ;
-        out.println(">") ;
+    public void binding(String varName, RDFNode node) {
+        if ( node == null && !outputExplicitUnbound )
+            return;
+
+        out.print("<");
+        out.print(dfBinding);
+        out.print(" name=\"");
+        out.print(varName);
+        out.println("\">");
+        out.incIndent(INDENT);
+        printBindingValue(node);
+        out.decIndent(INDENT);
+        out.print("</");
+        out.print(dfBinding);
+        out.println(">");
     }
-        
-    void printBindingValue(RDFNode node)
-    {
-        if ( node == null )
-        {
+
+    void printBindingValue(RDFNode node) {
+        if ( node == null ) {
             // Unbound
-            out.print("<") ;
-            out.print(dfUnbound) ;
-            out.println("/>") ;
-            return ;
+            out.print("<");
+            out.print(dfUnbound);
+            out.println("/>");
+            return;
         }
-        
-        if ( node instanceof Literal )
-        {
-            printLiteral((Literal)node) ;
-            return ;
+
+        if ( node instanceof Literal ) {
+            printLiteral((Literal)node);
+            return;
         }
-        
-        if ( node instanceof Resource )
-        {
-            printResource((Resource)node) ;
-            return ;
+
+        if ( node instanceof Resource ) {
+            printResource((Resource)node);
+            return;
         }
-        
-        Log.warn(this,"Unknown RDFNode type in result set: "+node.getClass()) ;
+
+        Log.warn(this, "Unknown RDFNode type in result set: " + node.getClass());
     }
-    
-    void printLiteral(Literal literal)
-    {
-        out.print("<") ;
-        out.print(dfLiteral) ;
-        
-        if ( Util.isLangString(literal)) {
-            String lang = literal.getLanguage() ;
-            out.print(" xml:lang=\"") ;
-            out.print(literal.getLanguage()) ;
-            out.print("\"") ;
-        } else if ( ! Util.isSimpleString(literal) ) {
+
+    void printLiteral(Literal literal) {
+        out.print("<");
+        out.print(dfLiteral);
+
+        if ( Util.isLangString(literal) ) {
+            String lang = literal.getLanguage();
+            out.print(" xml:lang=\"");
+            out.print(literal.getLanguage());
+            out.print("\"");
+        } else if ( !Util.isSimpleString(literal) ) {
             // Datatype
             // (RDF 1.1) not xsd:string nor rdf:langString.
             // (RDF 1.0) any datatype.
-            String datatype = literal.getDatatypeURI() ;
-            out.print(" ") ;
-            out.print(dfAttrDatatype) ;
-            out.print("=\"") ;
-            out.print(datatype) ;
-            out.print("\"") ;
+            String datatype = literal.getDatatypeURI();
+            out.print(" ");
+            out.print(dfAttrDatatype);
+            out.print("=\"");
+            out.print(datatype);
+            out.print("\"");
         }
-        
-        out.print(">") ;
-        out.print(xml_escape(literal.getLexicalForm())) ;
-        out.print("</") ;
-        out.print(dfLiteral) ;
-        out.println(">") ;
+
+        out.print(">");
+        out.print(xml_escape(literal.getLexicalForm()));
+        out.print("</");
+        out.print(dfLiteral);
+        out.println(">");
     }
-    
-    void printResource(Resource r)
-    {
-        if ( r.isAnon() ) 
-        {
-            String label ;
-            
+
+    void printResource(Resource r) {
+        if ( r.isAnon() ) {
+            String label;
+
             if ( outputGraphBNodeLabels )
-                label = r.asNode().getBlankNodeId().getLabelString() ;
-            else
-            {
-                if ( ! bNodeMap.containsKey(r))
-                    bNodeMap.put(r, "b"+(bNodeCounter++)) ;
-                label = bNodeMap.get(r) ;
+                label = r.asNode().getBlankNodeId().getLabelString();
+            else {
+                if ( !bNodeMap.containsKey(r) )
+                    bNodeMap.put(r, "b" + (bNodeCounter++));
+                label = bNodeMap.get(r);
             }
-            out.print("<") ;
-            out.print(dfBNode) ;
-            out.print(">") ;
-            out.print(xml_escape(label)) ;
-            out.print("</") ;
-            out.print(dfBNode) ;
-            out.println(">") ;
-        }
-        else
-        {
-            out.print("<") ;
-            out.print(dfURI) ;
-            out.print(">") ;
-            out.print(xml_escape(r.getURI())) ;
-            out.print("</") ;
-            out.print(dfURI) ;
-            out.println(">") ;
+            out.print("<");
+            out.print(dfBNode);
+            out.print(">");
+            out.print(xml_escape(label));
+            out.print("</");
+            out.print(dfBNode);
+            out.println(">");
+        } else {
+            out.print("<");
+            out.print(dfURI);
+            out.print(">");
+            out.print(xml_escape(r.getURI()));
+            out.print("</");
+            out.print(dfURI);
+            out.println(">");
         }
     }
-    
-    private static String xml_escape(String string)
-    {
+
+    private static String xml_escape(String string) {
         final StringBuilder sb = new StringBuilder(string);
-        
+
         int offset = 0;
         String replacement;
         char found;
