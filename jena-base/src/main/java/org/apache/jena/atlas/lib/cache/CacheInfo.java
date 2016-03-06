8192fbb476d7aa31faad9abe77900e3690b1a1a0
Andy Seaborne <andy@apache.org>
2016-03-06 10:24:16 +0000

    Remove commented code.  Correct indent.

--- jena-base/src/main/java/org/apache/jena/atlas/lib/cache/CacheInfo.java
+++ jena-base/src/main/java/org/apache/jena/atlas/lib/cache/CacheInfo.java
@@ -21,42 +21,28 @@ package org.apache.jena.atlas.lib.cache;
 import org.apache.jena.ext.com.google.common.cache.CacheStats ;
 
 /** Simplified version of Guava's CacheStats (and abstractign away from Guava cache implementation) */
-    public class CacheInfo {
-        public final long requests;
-        public final long hits;
-        public final long misses;
-        public final double hitRate;
-        public final int cacheSize;
+public class CacheInfo {
+    public final long requests;
+    public final long hits;
+    public final long misses;
+    public final double hitRate;
+    public final int cacheSize;
 
-        public CacheInfo(int cacheSize, CacheStats stats) {
-            this(cacheSize, stats.requestCount(), stats.hitCount(), stats.missCount(), stats.hitRate() ) ;
-        }
-        
-        public CacheInfo(int cacheSize, long requests, long hits, long misses, double hitRate) {
-            this.cacheSize = cacheSize ;
-            this.requests = requests ;
-            this.hits = hits ;
-            this.misses = misses ;
-            this.hitRate = hitRate ;
-        }
-
-        @Override
-        public String toString() {
-            return String.format("size=%,d  count=%,d  hits=%,d  misses=%,d  rate=%.1f",
-                                 cacheSize, requests, hits, misses, hitRate) ;
-        }
-        
-//      private void details(String label, CacheGuava<?, ?> cache, int cacheSize) {
-//      System.out.printf("%s [%,d]\n", label, cacheSize) ;
-//      CacheStats stats = ((CacheGuava<?, ?>)cache).stats() ;
-////      System.out.printf("  Cache usage:      %,d\n", cache.size()) ;
-//      System.out.printf("  Requests:         %,d\n", stats.requestCount()) ;
-//      System.out.printf("  Hit rate:         %.1f%%\n", 100*stats.hitRate()) ; 
-////      System.out.printf("  Hits:             %,d\n", stats.hitCount()) ;
-////      System.out.printf("  Misses:           %,d\n", stats.missCount()) ;
-////      if ( stats.loadSuccessCount() != stats.missCount() ) {
-////          System.out.printf("  Load success:     %,d\n", stats.loadSuccessCount()) ;
-////          System.out.printf("  Load ex:          %,d\n", stats.loadExceptionCount()) ;
-////      }
-
+    public CacheInfo(int cacheSize, CacheStats stats) {
+        this(cacheSize, stats.requestCount(), stats.hitCount(), stats.missCount(), stats.hitRate() ) ;
     }
+
+    public CacheInfo(int cacheSize, long requests, long hits, long misses, double hitRate) {
+        this.cacheSize = cacheSize ;
+        this.requests = requests ;
+        this.hits = hits ;
+        this.misses = misses ;
+        this.hitRate = hitRate ;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("size=%,d  count=%,d  hits=%,d  misses=%,d  rate=%.1f",
+                             cacheSize, requests, hits, misses, hitRate) ;
+    }
+}
