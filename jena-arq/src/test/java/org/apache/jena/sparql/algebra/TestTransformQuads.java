bcc4aa92b1e5390d8ccebe2406e1031aeb93ed73
Andy Seaborne <andy@apache.org>
2016-04-25 11:55:52 +0100

    Conversion to quads does not alter (service)

--- jena-arq/src/test/java/org/apache/jena/sparql/algebra/TestTransformQuads.java
+++ jena-arq/src/test/java/org/apache/jena/sparql/algebra/TestTransformQuads.java
@@ -151,7 +151,38 @@ public class TestTransformQuads extends BaseTest
                                          "                         (quadpattern (quad ?g ?x ?y ?z))))))",
                                          "    (table unit))") ; }
 
-        
+    // Don't touch SERVICE 
+    @Test public void quads38() { test ("{ {?s ?p ?o } UNION { SERVICE <http://host/endpoint> { GRAPH ?gr { ?sr ?pr ?or }}} }",
+                                        "(union",
+                                        "  (quadpattern (quad <urn:x-arq:DefaultGraphNode> ?s ?p ?o))",
+                                        "  (service <http://host/endpoint>",
+                                        "    (graph ?gr",
+                                        "      (bgp (triple ?sr ?pr ?or))))",
+                                        ")") ; }
+    
+    // Don't touch SERVICE 
+    @Test public void quads39() { test ("{ { GRAPH ?g { ?s ?p ?o } } UNION { SERVICE <http://host/endpoint> { GRAPH ?gr { ?sr ?pr ?or }}} }",
+                                        "(union",
+                                        "  (quadpattern (?g ?s ?p ?o))",
+                                        "  (service <http://host/endpoint>",
+                                        "    (graph ?gr",
+                                        "      (bgp (triple ?sr ?pr ?or))))",
+                                        ")") ; }
+    
+    // Don't touch SERVICE 
+    @Test public void quads40() { test ("{ GRAPH ?g { SERVICE <http://host/endpoint> { ?s ?p ?o }}}",
+                                        "(service <http://host/endpoint> (bgp (triple ?s ?p ?o)))") ;
+                                }
+
+    // Don't touch SERVICE 
+    @Test public void quads41() { test ("{ GRAPH ?g1 { SERVICE <http://host/endpoint> { ?s ?p ?o } ?s1 ?p1 ?o1 } }",
+                                        "(sequence",
+                                        "   (service <http://host/endpoint> (bgp (triple ?s ?p ?o)))",
+                                        "   (quadpattern (?g1 ?s1 ?p1 ?o1))",
+                                        ")") ;
+                                }
+
+
 
     private static void test(String patternString, String... strExpected) {
         test(patternString, true, strExpected) ;
