ffbd310dfd23859380e56cdca325214ce1e4863c
Andrew Dolby <andrewdolby@gmail.com>
2016-05-16 21:32:09 -0400

    Inherit from AbstractTestDatasetWithAnalyzer to reduce code repetition

--- jena-text/src/test/java/org/apache/jena/query/text/TestDatasetWithComplexPhraseQueryParser.java
+++ jena-text/src/test/java/org/apache/jena/query/text/TestDatasetWithComplexPhraseQueryParser.java
@@ -36,68 +36,7 @@ import org.junit.Test ;
 /**
  * This class defines a setup configuration for a dataset that uses a standard analyzer with a Lucene index.
  */
-public class TestDatasetWithComplexPhraseQueryParser extends AbstractTestDatasetWithTextIndexBase {
-
-    private static final String SPEC_BASE = "http://example.org/spec#";
-    private static final String SPEC_ROOT_LOCAL = "lucene_text_dataset";
-    private static final String SPEC_ROOT_URI = SPEC_BASE + SPEC_ROOT_LOCAL;
-
-    private static String makeSpec(String analyzer, String parser) {
-        return StrUtils.strjoinNL(
-                    "prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> ",
-                    "prefix ja:   <http://jena.hpl.hp.com/2005/11/Assembler#> ",
-                    "prefix tdb:  <http://jena.hpl.hp.com/2008/tdb#>",
-                    "prefix text: <http://jena.apache.org/text#>",
-                    "prefix :     <" + SPEC_BASE + ">",
-                    "",
-                    "[] ja:loadClass    \"org.apache.jena.query.text.TextQuery\" .",
-                    "text:TextDataset      rdfs:subClassOf   ja:RDFDataset .",
-                    "text:TextIndexLucene  rdfs:subClassOf   text:TextIndex .",
-
-                    ":" + SPEC_ROOT_LOCAL,
-                    "    a              text:TextDataset ;",
-                    "    text:dataset   :dataset ;",
-                    "    text:index     :indexLucene ;",
-                    "    .",
-                    "",
-                    ":dataset",
-                    "    a               ja:RDFDataset ;",
-                    "    ja:defaultGraph :graph ;",
-                    ".",
-                    ":graph",
-                    "    a               ja:MemoryModel ;",
-                    ".",
-                    "",
-                    ":indexLucene",
-                    "    a text:TextIndexLucene ;",
-                    "    text:directory \"mem\" ;",
-                    "    text:queryParser " + parser + ";",
-                    "    text:entityMap :entMap ;",
-                    "    .",
-                    "",
-                    ":entMap",
-                    "    a text:EntityMap ;",
-                    "    text:entityField      \"uri\" ;",
-                    "    text:defaultField     \"label\" ;",
-                    "    text:map (",
-                    "         [ text:field \"label\" ; ",
-                    "           text:predicate rdfs:label ;",
-                    "           text:analyzer [ a " + analyzer + " ]",
-                    "         ]",
-                    "         [ text:field \"comment\" ; text:predicate rdfs:comment ]",
-                    "         ) ."
-                    );
-    }
-
-    public void init(String analyzer, String parser) {
-        Reader reader = new StringReader(makeSpec(analyzer, parser));
-        Model specModel = ModelFactory.createDefaultModel();
-        specModel.read(reader, "", "TURTLE");
-        TextAssembler.init();
-        Resource root = specModel.getResource(SPEC_ROOT_URI);
-        dataset = (Dataset) Assembler.general.open(root);
-    }
-
+public class TestDatasetWithComplexPhraseQueryParser extends AbstractTestDatasetWithAnalyzer {
     @Before
     public void before() {
         init("text:StandardAnalyzer", "text:ComplexPhraseQueryParser");
