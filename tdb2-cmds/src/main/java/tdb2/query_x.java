f9935bb5ba262c5bb96f8c8427b8ef3b94f3040b
Andy Seaborne <andy@seaborne.org>
2016-12-31 18:16:08 +0000

    Cmds for TDB2

---
+++ tdb2-cmds/src/main/java/tdb2/query_x.java
@@ -0,0 +1,253 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package tdb2;
+
+import arq.cmdline.* ;
+import jena.cmd.ArgDecl;
+import jena.cmd.CmdException;
+import jena.cmd.TerminationException;
+import org.apache.jena.atlas.io.IndentedWriter ;
+import org.apache.jena.atlas.lib.Lib ;
+import org.apache.jena.atlas.logging.LogCtl ;
+import org.apache.jena.query.* ;
+import org.apache.jena.riot.RiotException ;
+import org.apache.jena.riot.RiotNotFoundException ;
+import org.apache.jena.riot.SysRIOT ;
+import org.apache.jena.shared.JenaException ;
+import org.apache.jena.sparql.ARQInternalErrorException ;
+import org.apache.jena.sparql.core.Transactional ;
+import org.apache.jena.sparql.core.TransactionalNull;
+import org.apache.jena.sparql.mgt.Explain ;
+import org.apache.jena.sparql.resultset.ResultSetException ;
+import org.apache.jena.sparql.resultset.ResultsFormat ;
+import org.apache.jena.sparql.util.QueryExecUtils ;
+import org.apache.jena.system.Txn ;
+
+// Copy of arq.query from Jena 3.2.0 development.
+public class query_x extends CmdARQ
+{
+    private ArgDecl argRepeat   = new ArgDecl(ArgDecl.HasValue, "repeat") ;
+    private ArgDecl argExplain  = new ArgDecl(ArgDecl.NoValue, "explain") ;
+    private ArgDecl argOptimize = new ArgDecl(ArgDecl.HasValue, "opt", "optimize") ;
+
+    protected int repeatCount = 1 ; 
+    protected int warmupCount = 0 ;
+    protected boolean queryOptimization = true ;
+    
+    protected ModTime       modTime =     new ModTime() ;
+    protected ModQueryIn    modQuery =    null;
+    protected ModDataset    modDataset =  null ;
+    protected ModResultsOut modResults =  new ModResultsOut() ;
+    protected ModEngine     modEngine =   new ModEngine() ;
+
+    public static void main (String... argv)
+    {
+        new query_x(argv).mainRun() ;
+    }
+
+    public query_x(String[] argv)
+    {
+        super(argv) ;
+        modQuery = new ModQueryIn(getDefaultSyntax()) ; 
+        modDataset = setModDataset() ;
+
+        super.addModule(modQuery) ;
+        super.addModule(modResults) ;
+        super.addModule(modDataset) ;
+        super.addModule(modEngine) ;
+        super.addModule(modTime) ;
+
+        super.getUsage().startCategory("Control") ;
+        super.add(argExplain,  "--explain", "Explain and log query execution") ;
+        super.add(argRepeat,   "--repeat=N or N,M", "Do N times or N warmup and then M times (use for timing to overcome start up costs of Java)");
+        super.add(argOptimize, "--optimize=", "Turn the query optimizer on or off (default: on)") ;
+    }
+
+    /** Default syntax used when the syntax can not be determined from the command name or file extension
+     *  The order of determination is:
+     *  <ul>
+     *  <li>Explicitly given --syntax</li>
+     *  <li>File extension</li>
+     *  <li>Command default</li>
+     *  <li>System default</li>
+     *  </ul>
+     *  
+     */
+    protected Syntax getDefaultSyntax()     { return Syntax.defaultQuerySyntax ; }
+
+    @Override
+    protected void processModulesAndArgs()
+    {
+        super.processModulesAndArgs() ;
+        if ( contains(argRepeat) )
+        {
+            String[] x = getValue(argRepeat).split(",") ;
+            if ( x.length == 1 ) 
+            {
+                try { repeatCount = Integer.parseInt(x[0]) ; }
+                catch (NumberFormatException ex)
+                { throw new CmdException("Can't parse "+x[0]+" in arg "+getValue(argRepeat)+" as an integer") ; }
+            }
+            else if ( x.length == 2 ) {
+                try { warmupCount = Integer.parseInt(x[0]) ; }
+                catch (NumberFormatException ex)
+                { throw new CmdException("Can't parse "+x[0]+" in arg "+getValue(argRepeat)+" as an integer") ; }
+                try { repeatCount = Integer.parseInt(x[1]) ; }
+                catch (NumberFormatException ex)
+                { throw new CmdException("Can't parse "+x[1]+" in arg "+getValue(argRepeat)+" as an integer") ; }
+            } else
+                throw new CmdException("Wrong format for repeat count: "+getValue(argRepeat)) ;
+        }
+        if ( isVerbose() )
+            ARQ.getContext().setTrue(ARQ.symLogExec) ;
+        
+        if ( hasArg(argExplain) )
+            ARQ.setExecutionLogging(Explain.InfoLevel.ALL) ;
+        
+        if ( hasArg(argOptimize) )
+        {
+            String x1 = getValue(argOptimize) ;
+            if ( hasValueOfTrue(argOptimize) || x1.equalsIgnoreCase("on") || x1.equalsIgnoreCase("yes") ) 
+                queryOptimization = true ;
+            else if ( hasValueOfFalse(argOptimize) || x1.equalsIgnoreCase("off") || x1.equalsIgnoreCase("no") )
+                queryOptimization = false ;
+            else throw new CmdException("Optimization flag must be true/false/on/off/yes/no. Found: "+getValue(argOptimize)) ;
+        }
+    }
+    
+    protected ModDataset setModDataset()
+    {
+        return new ModDatasetGeneralAssembler() ;
+    }
+    
+    @Override
+    protected void exec()
+    {
+        if ( ! queryOptimization )
+            ARQ.getContext().setFalse(ARQ.optimization) ;
+        if ( cmdStrictMode )
+            ARQ.getContext().setFalse(ARQ.optimization) ;
+        
+        // Warm up.
+        for ( int i = 0 ; i < warmupCount ; i++ )
+        {
+            queryExec(false, ResultsFormat.FMT_NONE) ;
+        }
+        
+        for ( int i = 0 ; i < repeatCount ; i++ )
+            queryExec(modTime.timingEnabled(),  modResults.getResultsFormat()) ;
+        
+        if ( modTime.timingEnabled() && repeatCount > 1 )
+        {
+            long avg = totalTime/repeatCount ;
+            String avgStr = modTime.timeStr(avg) ;
+            System.err.println("Total time: "+modTime.timeStr(totalTime)+" sec for repeat count of "+repeatCount+ " : average: "+avgStr) ;
+        }
+    }
+
+    @Override
+    protected String getCommandName() { return Lib.className(this) ; }
+    
+    @Override
+    protected String getSummary() { return getCommandName()+" --data=<file> --query=<query>" ; }
+    
+    protected Dataset getDataset()  { 
+        try {
+            Dataset ds = modDataset.getDataset();
+            if ( ds == null )
+                ds = dealWithNoDataset();
+            return ds;
+        }
+        catch (RiotNotFoundException ex) {
+            System.err.println("Failed to load data: " + ex.getMessage());
+            throw new TerminationException(1);
+        }
+        catch (RiotException ex) {
+            System.err.println("Failed to load data");
+            throw new TerminationException(1);
+        }
+    }
+    
+    protected Dataset dealWithNoDataset()  {
+        return DatasetFactory.create() ;
+        //throw new CmdException("No dataset provided") ; 
+    }
+    
+    protected long totalTime = 0 ;
+    protected void queryExec(boolean timed, ResultsFormat fmt)
+    {
+        try {
+            Query query = modQuery.getQuery() ;
+            if ( isVerbose() ) {
+                IndentedWriter out = new IndentedWriter(System.out, true);
+                query.serialize(out);
+                out.flush();
+                System.out.println();
+            }
+            
+            if ( isQuiet() )
+                LogCtl.setError(SysRIOT.riotLoggerName) ;
+            Dataset dataset = getDataset() ;
+            // Check there is a dataset
+            if ( dataset == null && !query.hasDatasetDescription() ) {
+                System.err.println("Dataset not specified in query nor provided on command line.");
+                throw new TerminationException(1);
+            }
+            Transactional transactional = (dataset.supportsTransactionAbort()) ? dataset : new TransactionalNull() ;
+            Txn.executeRead(transactional, ()->{
+                modTime.startTimer() ;
+                try ( QueryExecution qe = QueryExecutionFactory.create(query, dataset) ) {
+                    try { QueryExecUtils.executeQuery(query, qe, fmt); }
+                    catch (QueryCancelledException ex) {
+                        System.out.flush();
+                        System.err.println("Query timed out");
+                    }
+
+                    long time = modTime.endTimer();
+                    if ( timed ) {
+                        totalTime += time;
+                        System.err.println("Time: " + modTime.timeStr(time) + " sec");
+                    }
+                }
+                catch (ResultSetException ex) {
+                    System.err.println(ex.getMessage());
+                    ex.printStackTrace(System.err);
+                }
+                catch (QueryException qEx) {
+                    // System.err.println(qEx.getMessage()) ;
+                    throw new CmdException("Query Exeception", qEx);
+                }
+            });
+        }
+        catch (ARQInternalErrorException intEx) {
+            System.err.println(intEx.getMessage()) ;
+            if ( intEx.getCause() != null )
+            {
+                System.err.println("Cause:") ;
+                intEx.getCause().printStackTrace(System.err) ;
+                System.err.println() ;
+            }
+            intEx.printStackTrace(System.err) ;
+        }
+        catch (JenaException | CmdException ex) { throw ex ; }
+        catch (Exception ex) {
+            throw new CmdException("Exception", ex) ;
+        }
+    }
+}
--- tdb2/src/main/java/org/seaborne/tdb2/sys/TDBInternal.java
+++ tdb2/src/main/java/org/seaborne/tdb2/sys/TDBInternal.java
@@ -17,117 +17,88 @@
 
 package org.seaborne.tdb2.sys;
 
-import org.apache.jena.atlas.lib.NotImplemented ;
-import org.apache.jena.graph.Node ;
-import org.apache.jena.query.Dataset ;
-import org.apache.jena.sparql.core.DatasetGraph ;
-import org.seaborne.tdb2.store.DatasetGraphTDB ;
-import org.seaborne.tdb2.store.NodeId ;
-import org.seaborne.tdb2.store.nodetable.NodeTable ;
+import org.apache.jena.graph.Node;
+import org.apache.jena.query.Dataset;
+import org.apache.jena.sparql.core.DatasetGraph;
+import org.seaborne.tdb2.store.DatasetGraphTDB;
+import org.seaborne.tdb2.store.NodeId;
+import org.seaborne.tdb2.store.nodetable.NodeTable;
 
-/** A collection of helpers to abstract away from internal details of TDB. 
- * Use with care.
+/**
+ * A collection of helpers to abstract away from internal details of TDB. Use with care.
  */
-public class TDBInternal
-{
-    /** Return the NodeId for a node.
-     * Returns NodeId.NodeDoesNotExist when the node is not found. 
-     * Returns null when not a TDB-backed dataset.
+public class TDBInternal {
+    /**
+     * Return the NodeId for a node. Returns NodeId.NodeDoesNotExist when the node is not
+     * found. Returns null when not a TDB-backed dataset.
      */
-    public static NodeId getNodeId(Dataset ds, Node node)
-    {
-        return  getNodeId(ds.asDatasetGraph(), node) ;
-    }
-    
-    /** Return the NodeId for a node.
-     * Returns NodeId.NodeDoesNotExist when the node is not found. 
-     * Returns null when not a TDB-backed dataset.
-     */
-    public static NodeId getNodeId(DatasetGraph ds, Node node)
-    {
-        DatasetGraphTDB dsg = getDatasetGraphTDB(ds) ;
-        return getNodeId(dsg, node) ;
-    }
-    
-    /** Return the NodeId for a node.
-     * Returns NodeId.NodeDoesNotExist when the node is not found. 
-     * Returns null when not a TDB-backed dataset.
-     */
-    public static NodeId getNodeId(DatasetGraphTDB dsg, Node node)
-    {
-        if ( dsg == null )
-            return null ;
-        NodeTable nodeTable = dsg.getQuadTable().getNodeTupleTable().getNodeTable() ;
-        NodeId nodeId = nodeTable.getNodeIdForNode(node) ;
-        return nodeId ;
+    public static NodeId getNodeId(Dataset ds, Node node) {
+        return getNodeId(ds.asDatasetGraph(), node);
     }
 
-    /** Return the node for a NodeId (if any).
-     *  Returns null if the NodeId does not exist in the dataset. 
+    /**
+     * Return the NodeId for a node. Returns NodeId.NodeDoesNotExist when the node is not
+     * found. Returns null when not a TDB-backed dataset.
      */
-    public static Node getNode(Dataset ds, NodeId nodeId)
-    {
-        return getNode(ds.asDatasetGraph(), nodeId) ;
-    }
-    
-    /** Return the node for a NodeId (if any).
-     *  Returns null if the NodeId does not exist in the dataset. 
-     */
-    public static Node getNode(DatasetGraph ds, NodeId nodeId)
-    {
-        DatasetGraphTDB dsg = getDatasetGraphTDB(ds) ;
-        return getNode(dsg, nodeId) ;
+    public static NodeId getNodeId(DatasetGraph ds, Node node) {
+        DatasetGraphTDB dsg = getDatasetGraphTDB(ds);
+        return getNodeId(dsg, node);
     }
 
-    /** Return the node for a NodeId (if any).
-     *  Returns null if the NodeId does not exist in the dataset. 
+    /**
+     * Return the NodeId for a node. Returns NodeId.NodeDoesNotExist when the node is not
+     * found. Returns null when not a TDB-backed dataset.
      */
-    public static Node getNode(DatasetGraphTDB dsg, NodeId nodeId)
-    {
+    public static NodeId getNodeId(DatasetGraphTDB dsg, Node node) {
         if ( dsg == null )
-            return null ;
-        NodeTable nodeTable = dsg.getQuadTable().getNodeTupleTable().getNodeTable() ;
-        Node node = nodeTable.getNodeForNodeId(nodeId) ;
-        return node ;
+            return null;
+        NodeTable nodeTable = dsg.getQuadTable().getNodeTupleTable().getNodeTable();
+        NodeId nodeId = nodeTable.getNodeIdForNode(node);
+        return nodeId;
+    }
+
+    /**
+     * Return the node for a NodeId (if any). Returns null if the NodeId does not exist in
+     * the dataset.
+     */
+    public static Node getNode(Dataset ds, NodeId nodeId) {
+        return getNode(ds.asDatasetGraph(), nodeId);
+    }
+
+    /**
+     * Return the node for a NodeId (if any). Returns null if the NodeId does not exist in
+     * the dataset.
+     */
+    public static Node getNode(DatasetGraph ds, NodeId nodeId) {
+        DatasetGraphTDB dsg = getDatasetGraphTDB(ds);
+        return getNode(dsg, nodeId);
+    }
+
+    /**
+     * Return the node for a NodeId (if any). Returns null if the NodeId does not exist in
+     * the dataset.
+     */
+    public static Node getNode(DatasetGraphTDB dsg, NodeId nodeId) {
+        if ( dsg == null )
+            return null;
+        NodeTable nodeTable = dsg.getQuadTable().getNodeTupleTable().getNodeTable();
+        Node node = nodeTable.getNodeForNodeId(nodeId);
+        return node;
     }
 
     /**
      * Return the DatasetGraphTDB for a Dataset, or null.
      */
-    public static DatasetGraphTDB getDatasetGraphTDB(Dataset ds)
-    {
-        return getDatasetGraphTDB(ds.asDatasetGraph()) ;
+    public static DatasetGraphTDB getDatasetGraphTDB(Dataset ds) {
+        return getDatasetGraphTDB(ds.asDatasetGraph());
     }
 
-    
     /**
-     * Return the DatasetGraphTDB for a DatasetGraph, or null.
-     * May not be up-to-date.
+     * Return the DatasetGraphTDB for a DatasetGraph, or null. May not be up-to-date.
      */
-    public static DatasetGraphTDB getDatasetGraphTDB(DatasetGraph dsg)
-    {
-        throw new NotImplemented() ;
-//        if ( dsg instanceof DatasetGraphTransaction )
-//            // Latest.
-//            return ((DatasetGraphTransaction)dsg).getDatasetGraphToQuery() ;
-//            // Core.
-//            //.getBaseDatasetGraph() ;
-//        
-//        if ( dsg instanceof DatasetGraphTDB )
-//            return (DatasetGraphTDB)dsg ;
-//        
-//        return null ;
-    }
-    
-    /** Return a DatasetGraphTDB that uses the raw storage for tables 
-     * Use with care.
-     */ 
-    public static DatasetGraphTDB getBaseDatasetGraphTDB(DatasetGraph datasetGraph)
-    {
-        throw new NotImplemented() ;
-//        if ( datasetGraph instanceof DatasetGraphTransaction )
-//            return ((DatasetGraphTransaction)datasetGraph).getBaseDatasetGraph() ;
-//        throw new TDBException("Not a suitable DatasetGraph to get it's base storage: "+Lib.classShortName(datasetGraph.getClass())) ; 
+    public static DatasetGraphTDB getDatasetGraphTDB(DatasetGraph dsg) {
+        if ( dsg instanceof DatasetGraphTDB )
+            return (DatasetGraphTDB)dsg;
+        return null;
     }
 }
-
