5ace28e360d4649f21aee3f6fdbbb77d5d61a151
Andy Seaborne <andy@seaborne.org>
2017-01-05 16:39:31 +0000

    Tidy up code

--- dboe-base/src/main/java/org/seaborne/dboe/base/record/RecordFactory.java
+++ dboe-base/src/main/java/org/seaborne/dboe/base/record/RecordFactory.java
@@ -17,7 +17,7 @@
 
 package org.seaborne.dboe.base.record;
 
-import static java.lang.String.format ;
+import static java.lang.String.format;
 
 import java.nio.ByteBuffer;
 
@@ -25,174 +25,159 @@ import java.nio.ByteBuffer;
 final
 public class RecordFactory
 {
-    private final int keyLength ;
-    private final int valueLength ;
-    private final int slotLen ;
-    private final boolean checking = false ;
+    private final int keyLength;
+    private final int valueLength;
+    private final int slotLen;
+    private final boolean checking = false;
 
-    public RecordFactory(int keyLength, int valueLength)
-    {
+    public RecordFactory(int keyLength, int valueLength) {
         if ( keyLength <= 0 )
-            throw new IllegalArgumentException("Bad key length: "+keyLength) ;
+            throw new IllegalArgumentException("Bad key length: " + keyLength);
         if ( valueLength < 0 )
-            throw new IllegalArgumentException("Bad value length: "+valueLength) ;
+            throw new IllegalArgumentException("Bad value length: " + valueLength);
 
-        this.keyLength = keyLength ;
-        this.valueLength = valueLength ;
-        // by here valueLength >= 0 
-        this.slotLen = keyLength + valueLength ;
-    }
-    
-    /** Return a RecordFactory that makes key-only records of the same key size */ 
-    public RecordFactory keyFactory()
-    {
-        return new RecordFactory(keyLength, 0) ;
+        this.keyLength = keyLength;
+        this.valueLength = valueLength;
+        this.slotLen = keyLength + valueLength;
     }
 
-    /** Create a key-only record, allocating blank space for the key  */
-    public Record createKeyOnly()
-    {
-        return create(new byte[keyLength], null) ;
+    /** Return a RecordFactory that makes key-only records of the same key size */
+    public RecordFactory keyFactory() {
+        return new RecordFactory(keyLength, 0);
     }
-    
+
+    /** Create a key-only record, allocating blank space for the key */
+    public Record createKeyOnly() {
+        return create(new byte[keyLength], null);
+    }
+
     /** Create a key-only record */
-    public Record createKeyOnly(Record record)
-    {
-        checkKey(record.getKey()) ;
+    public Record createKeyOnly(Record record) {
+        checkKey(record.getKey());
         if ( record.getValue() == null )
-            return record ;
-        
-        return create(record.getKey(), null) ;
+            return record;
+
+        return create(record.getKey(), null);
     }
-    
+
     /** Create a key and value record (value uninitialized) */
-    public Record create(byte[] key)
-    { 
-        checkKey(key) ;
-        byte[] v = null ;
+    public Record create(byte[] key) {
+        checkKey(key);
+        byte[] v = null;
         if ( valueLength > 0 )
-            v = new byte[valueLength] ;
-        return create(key, v) ;
+            v = new byte[valueLength];
+        return create(key, v);
     }
-    
+
     /** Create a record, allocating space for the key and value (if any) */
-    public Record create()
-    { return create(new byte[keyLength], 
-                    (valueLength > 0) ? new byte[valueLength] : null) ;
+    public Record create() {
+        return create(new byte[keyLength], (valueLength > 0) ? new byte[valueLength] : null);
     }
-    
+
     /** Create a key and value record */
-    public Record create(byte[] key, byte[] value)
-    {
-        check(key, value) ;
-        return new Record(key, value) ;
+    public Record create(byte[] key, byte[] value) {
+        check(key, value);
+        return new Record(key, value);
     }
-    
-    public void insertInto(Record record, ByteBuffer bb, int idx)
-    {
-        check(record) ;
-        bb.position(idx*slotLen) ;
-        bb.put(record.getKey(), 0, keyLength) ;
+
+    public void insertInto(Record record, ByteBuffer bb, int idx) {
+        check(record);
+        bb.position(idx * slotLen);
+        bb.put(record.getKey(), 0, keyLength);
         if ( hasValue() && record.getValue() != null )
-            bb.put(record.getValue(), 0, valueLength) ;
+            bb.put(record.getValue(), 0, valueLength);
     }
-    
+
     public static final RecordMapper<Record> mapperRecord = new RecordMapper<Record>() {
         /*
-         * RecordRangeIterator calls getRecordBuffer().get(slotidx) ;
+         * RecordRangeIterator calls getRecordBuffer().get(slotidx);
          */
         @Override
         public Record map(ByteBuffer bb, int idx, byte[] keyBytes, RecordFactory factory) {
-            byte[] key = new byte[factory.keyLength] ;
-            byte[] value = (factory.hasValue() ? new byte[factory.valueLength] :null ) ;
+            byte[] key = new byte[factory.keyLength];
+            byte[] value = (factory.hasValue() ? new byte[factory.valueLength] :null );
             // Is it better to avoid the synchronize needed for bb.position./bb.get
-            // but not use a (relative) bulk bb.get(byte[],,) which may be anative operation.
+            // but not use a (relative) bulk bb.get(byte[],,) which may be a native operation?
             
-//            int posnKey = idx*slotLen ;
+//            int posnKey = idx*slotLen;
 //            // Avoid using position() so we can avoid needing synchronized.
-//            copyInto(key, bb, posnKey, keyLength) ;
+//            copyInto(key, bb, posnKey, keyLength);
 //            if ( value != null )
 //            {
-//                int posnValue = idx*slotLen+keyLength ;
-//                copyInto(value, bb, posnValue, valueLength) ;
+//                int posnValue = idx*slotLen+keyLength;
+//                copyInto(value, bb, posnValue, valueLength);
 //            }
             
             // Using bb.get(byte[],,) may be potentially faster but requires the synchronized
             // There's no absolute version.
             synchronized(bb)
             {
-                bb.position(idx*factory.slotLen) ;
-                bb.get(key, 0, factory.keyLength) ;
+                bb.position(idx*factory.slotLen);
+                bb.get(key, 0, factory.keyLength);
                 if ( value != null )
-                    bb.get(value, 0, factory.valueLength) ;
+                    bb.get(value, 0, factory.valueLength);
             }
             if ( keyBytes != null )
                 System.arraycopy(key, 0, keyBytes, 0, factory.keyLength);
-            return factory.create(key, value) ;
+            return factory.create(key, value);
         }
-    } ;
+    };
     
     public <X> X access(ByteBuffer bb, int idx, byte[] keyBytes, RecordMapper<X> mapper) {
-        return mapper.map(bb, idx, keyBytes, this) ; 
+        return mapper.map(bb, idx, keyBytes, this); 
     }
     
     public Record buildFrom(ByteBuffer bb, int idx) {
-        return access(bb, idx, null, mapperRecord) ; 
+        return access(bb, idx, null, mapperRecord); 
     }
     
-    private final void copyInto(byte[] dst, ByteBuffer src, int start, int length)
-    {
+    private final void copyInto(byte[] dst, ByteBuffer src, int start, int length) {
         // Thread safe.
-        for ( int i = 0 ; i < length ; i++ )
-            dst[i] = src.get(start+i) ;
+        for ( int i = 0; i < length; i++ )
+            dst[i] = src.get(start+i);
         // Would otherwise be ...
-//        src.position(start) ;
-//        src.get(dst, 0, length) ;
+//        src.position(start);
+//        src.get(dst, 0, length);
     }
     
-    public boolean hasValue()   { return valueLength > 0 ; }
+    public boolean hasValue()   { return valueLength > 0; }
 
-    public int recordLength()   { return keyLength + valueLength ; }
+    public int recordLength()   { return keyLength + valueLength; }
     
-    public int keyLength()      { return keyLength ; }
+    public int keyLength()      { return keyLength; }
 
-    public int valueLength()    { return valueLength ; }
+    public int valueLength()    { return valueLength; }
     
     @Override
-    public String toString()
-    {
-        return format("<RecordFactory k=%d v=%d>", keyLength, valueLength) ; 
+    public String toString() {
+        return format("<RecordFactory k=%d v=%d>", keyLength, valueLength);
     }
-    
-    private final void check(Record record)
-    {
-        if ( ! checking ) return ;
-        check(record.getKey(), record.getValue()) ;
+
+    private final void check(Record record) {
+        if ( ! checking ) return;
+        check(record.getKey(), record.getValue());
     }
-    
-    private final void checkKey(byte[] k)
-    {
-        if ( ! checking ) return ;
+
+    private final void checkKey(byte[] k) {
+        if ( ! checking ) return;
         if ( k == null )
-            throw new RecordException("Null key byte[]") ;
-        if ( keyLength != k.length ) 
-            throw new RecordException(format("Key length error: This RecordFactory manages records of key length %d, not %d", keyLength, k.length)) ;
+            throw new RecordException("Null key byte[]");
+        if ( keyLength != k.length )
+            throw new RecordException(format("Key length error: This RecordFactory manages records of key length %d, not %d", keyLength,
+                                             k.length));
     }
-    
-    private final void check(byte[] k, byte[] v)
-    {
-        if ( ! checking ) return ;
-        checkKey(k) ;
-        if ( valueLength <= 0 )
-        {
-            if ( v != null ) 
-                throw new RecordException("Value array error: This RecordFactory manages records that are all key") ;
-        }
-        else
-        {
+
+    private final void check(byte[] k, byte[] v) {
+        if ( ! checking ) return;
+        checkKey(k);
+        if ( valueLength <= 0 ) {
+            if ( v != null )
+                throw new RecordException("Value array error: This RecordFactory manages records that are all key");
+        } else {
             // v == null for a key-only record from this factory.
             if ( v != null && v.length != valueLength )
-                throw new RecordException(format("This RecordFactory manages record of value length %d, not (%d,-)", valueLength, v.length)) ;
+                throw new RecordException(format("This RecordFactory manages record of value length %d, not (%d,-)", valueLength,
+                                                 v.length));
         }
     }
 }
