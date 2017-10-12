9e3d39bd88f697e0dc66239c8f72840ccdee9171
Andy Seaborne <andy@apache.org>
2017-10-12 12:28:03 +0100

    JENA-1400: Proper name management

---
+++ jena-fuseki2/jena-fuseki-core/src/main/java/org/apache/jena/fuseki/server/NameMgr.java
@@ -0,0 +1,42 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.jena.fuseki.server;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.Function;
+
+/**
+ * Management of intern'ed names {@code T} that can be used as keys. {@link #register}
+ * creates an intern'ed {@code T}; if the object with the same name has already been
+ * created, return the original. There is only ever one object for a given name.
+ * <p>
+ * {@code T.==} can be used to test of name match, though providing
+ * {@code .hashCode} and {@code .equals} is preferred.
+ */
+public class NameMgr<T> {
+    private final Map<String, T> registered = new ConcurrentHashMap<>();
+    
+    /** register, creating an object is necessary */
+    public T register(String name, Function<String, T> maker) {
+        return registered.computeIfAbsent(name, maker);
+    }
+    
+    public NameMgr() { }
+}
