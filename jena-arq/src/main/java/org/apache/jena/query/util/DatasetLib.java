d967c1efca1a2d1a5854804b17699fe3d5108c4c
ajs6f <ajs6f@apache.org>
2017-11-03 15:25:04 -0400

    Adding method impls to DatasetLib

--- jena-arq/src/main/java/org/apache/jena/query/util/DatasetLib.java
+++ jena-arq/src/main/java/org/apache/jena/query/util/DatasetLib.java
@@ -1,21 +1,31 @@
 package org.apache.jena.query.util;
 
 import org.apache.jena.query.Dataset;
+import org.apache.jena.query.DatasetFactory;
+import org.apache.jena.sparql.util.Context;
+import org.apache.jena.sparql.util.DifferenceDatasetGraph;
+import org.apache.jena.sparql.util.UnionDatasetGraph;
 
 public class DatasetLib {
 
-    public static Dataset union(final Dataset d1, final Dataset d2) {
-        // TODO
-        throw new UnsupportedOperationException();
-    }
+	public static Dataset union(final Dataset d1, final Dataset d2) {
+		return DatasetFactory.wrap(new UnionDatasetGraph(d1.asDatasetGraph(), d2.asDatasetGraph()));
+	}
 
-    public static Dataset intersection(final Dataset d1, final Dataset d2) {
-        // TODO
-        throw new UnsupportedOperationException();
-    }
+	public static Dataset union(final Dataset d1, final Dataset d2, Context c) {
+		return DatasetFactory.wrap(new UnionDatasetGraph(d1.asDatasetGraph(), d2.asDatasetGraph(), c));
+	}
 
-    public static Dataset difference(final Dataset d1, final Dataset d2) {
-        // TODO
-        throw new UnsupportedOperationException();
-    }
+	public static Dataset intersection(final Dataset d1, final Dataset d2) {
+		// TODO
+		throw new UnsupportedOperationException();
+	}
+
+	public static Dataset difference(final Dataset d1, final Dataset d2) {
+		return DatasetFactory.wrap(new DifferenceDatasetGraph(d1.asDatasetGraph(), d2.asDatasetGraph()));
+	}
+
+	public static Dataset difference(final Dataset d1, final Dataset d2, Context c) {
+		return DatasetFactory.wrap(new DifferenceDatasetGraph(d1.asDatasetGraph(), d2.asDatasetGraph(), c));
+	}
 }
