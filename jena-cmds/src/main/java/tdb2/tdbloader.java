3cad038dfda3a919bcfbf7c174622cf55a870931
Andy Seaborne <andy@apache.org>
2017-11-12 12:25:59 +0000

    Increase the tick points.

--- jena-cmds/src/main/java/tdb2/tdbloader.java
+++ jena-cmds/src/main/java/tdb2/tdbloader.java
@@ -16,9 +16,9 @@
  * limitations under the License.
  */
 
-package tdb2 ;
+package tdb2;
 
-import java.util.List ;
+import java.util.List;
 
 import jena.cmd.ArgDecl;
 import jena.cmd.CmdException;
@@ -26,10 +26,10 @@ import org.apache.jena.atlas.lib.FileOps;
 import org.apache.jena.atlas.lib.NotImplemented;
 import org.apache.jena.atlas.lib.ProgressMonitor;
 import org.apache.jena.graph.Graph;
-import org.apache.jena.query.ARQ ;
-import org.apache.jena.riot.Lang ;
+import org.apache.jena.query.ARQ;
+import org.apache.jena.riot.Lang;
 import org.apache.jena.riot.RDFDataMgr;
-import org.apache.jena.riot.RDFLanguages ;
+import org.apache.jena.riot.RDFLanguages;
 import org.apache.jena.riot.system.ProgressStreamRDF;
 import org.apache.jena.riot.system.StreamRDF;
 import org.apache.jena.riot.system.StreamRDFLib;
@@ -41,61 +41,61 @@ import tdb2.cmdline.CmdTDB;
 import tdb2.cmdline.CmdTDBGraph;
 
 public class tdbloader extends CmdTDBGraph {
-    // private static final ArgDecl argParallel = new ArgDecl(ArgDecl.NoValue, "parallel") ;
-    // private static final ArgDecl argIncremental = new ArgDecl(ArgDecl.NoValue, "incr", "incremental") ;
-    private static final ArgDecl argNoStats = new ArgDecl(ArgDecl.NoValue, "nostats") ;
-    private static final ArgDecl argStats = new ArgDecl(ArgDecl.HasValue,  "stats") ;
+    // private static final ArgDecl argParallel = new ArgDecl(ArgDecl.NoValue, "parallel");
+    // private static final ArgDecl argIncremental = new ArgDecl(ArgDecl.NoValue, "incr", "incremental");
+    private static final ArgDecl argNoStats = new ArgDecl(ArgDecl.NoValue, "nostats");
+    private static final ArgDecl argStats = new ArgDecl(ArgDecl.HasValue,  "stats");
 
-    private boolean showProgress  = true ;
-    private boolean generateStats  = true ;
+    private boolean showProgress  = true;
+    private boolean generateStats  = true;
 
     static public void main(String... argv) {
-        CmdTDB.init() ;
-        new tdbloader(argv).mainRun() ;
+        CmdTDB.init();
+        new tdbloader(argv).mainRun();
     }
 
     protected tdbloader(String[] argv) {
-        super(argv) ;
-        super.add(argNoStats, "--nostats", "Switch off statistics gathering") ;
-        super.add(argStats) ;   // Hidden argument
+        super(argv);
+        super.add(argNoStats, "--nostats", "Switch off statistics gathering");
+        super.add(argStats);   // Hidden argument
     }
 
     @Override
     protected void processModulesAndArgs() {
-        super.processModulesAndArgs() ;
+        super.processModulesAndArgs();
     }
 
     @Override
     protected String getSummary() {
-        return getCommandName() + " [--desc DATASET | -loc DIR] FILE ..." ;
+        return getCommandName() + " [--desc DATASET | -loc DIR] FILE ...";
     }
 
     @Override
     protected void exec() {
         if ( isVerbose() ) {
-            System.out.println("Java maximum memory: " + Runtime.getRuntime().maxMemory()) ;
-            System.out.println(ARQ.getContext()) ;
+            System.out.println("Java maximum memory: " + Runtime.getRuntime().maxMemory());
+            System.out.println(ARQ.getContext());
         }
         if ( isVerbose() )
-            showProgress = true ;
+            showProgress = true;
         if ( isQuiet() )
-            showProgress = false ;
+            showProgress = false;
         if ( super.contains(argStats) ) {
             if ( ! hasValueOfTrue(argStats) && ! hasValueOfFalse(argStats) )
-                throw new CmdException("Not a boolean value: "+getValue(argStats)) ;
-            generateStats = super.hasValueOfTrue(argStats) ;
+                throw new CmdException("Not a boolean value: "+getValue(argStats));
+            generateStats = super.hasValueOfTrue(argStats);
         }
 
         if ( super.contains(argNoStats))
-            generateStats = false ;
+            generateStats = false;
         
-        List<String> urls = getPositional() ;
+        List<String> urls = getPositional();
         if ( urls.size() == 0 )
-            urls.add("-") ;
+            urls.add("-");
 
         if ( graphName == null ) {
-            loadQuads(urls) ;
-            return ;
+            loadQuads(urls);
+            return;
         }
         
         // There's a --graph.
@@ -103,42 +103,41 @@ public class tdbloader extends CmdTDBGraph {
         // (RIOT will take the default graph from quads).  
         
         for ( String url : urls ) {
-            Lang lang = RDFLanguages.filenameToLang(url) ;
+            Lang lang = RDFLanguages.filenameToLang(url);
             if ( lang != null && RDFLanguages.isQuads(lang) ) {
-                System.err.println("Warning: Quads format given - only the default graph is loaded into the graph for --graph") ;
-                break ;
+                System.err.println("Warning: Quads format given - only the default graph is loaded into the graph for --graph");
+                break;
             }
         }
         
-        loadNamedGraph(urls) ;
+        loadOneGraph(urls);
     }
 
-//    void loadDefaultGraph(List<String> urls) {
-//        GraphTDB graph = getGraph() ;
-//        TDBLoader.load(graph, urls, showProgress) ;
-//        return ;
-//    }
-
-    void loadNamedGraph(List<String> urls) {
-        Graph graph = getGraph() ;
-        TDBLoader.load(graph, urls, showProgress) ;
-        return ;
+    private void loadOneGraph(List<String> urls) {
+        Graph graph = getGraph();
+        TDBLoader.load(graph, urls, showProgress);
+        return;
     }
 
-    void loadQuads(List<String> urls) {
-        TDBLoader.load(getDatasetGraph(), urls, showProgress, generateStats) ;
-        return ;
+    private void loadQuads(List<String> urls) {
+        TDBLoader.load(getDatasetGraph(), urls, showProgress, generateStats);
+        return;
     }
     
     /** Tick point for messages during loading of data */
-    public static int       DataTickPoint         = 50 * 1000 ;
+    public static int       DataTickPoint         = 100 * 1000;
     /** Tick point for messages during secondary index creation */
-    public static long      IndexTickPoint        = 100 * 1000 ;
+    public static long      IndexTickPoint        = 500 * 1000;
 
     /** Number of ticks per super tick */
-    public static int       superTick             = 10 ;
+    public static int       superTick             = 10;
     
     private static Logger LOG = LoggerFactory.getLogger("TDB2");
+    
+    /**
+     *  For now, this is a simple loader that parses the input and adds triples/quads via {@code .add}.
+     *  @see org.apache.jena.tdb.TDBLoader TDB1 Loader.
+     */
     
     static class TDBLoader {
 
