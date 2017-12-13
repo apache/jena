94921dc0da8650b1cf7472d17ac0b737fcee2d44
Andy Seaborne <andy@apache.org>
2017-12-13 16:34:24 +0000

    Fix test suite naming.

--- jena-arq/src/test/java/org/apache/jena/sparql/expr/TestFunctionDynamic.java
+++ jena-arq/src/test/java/org/apache/jena/sparql/expr/TestFunctionDynamic.java
@@ -20,7 +20,7 @@ package org.apache.jena.sparql.expr;
 import static org.junit.Assert.*;
 import org.junit.Test;
 
-public class TestE_FunctionDynamic {
+public class TestFunctionDynamic {
 	
 	@Test
 	public void testConstructor_ExprList() {
