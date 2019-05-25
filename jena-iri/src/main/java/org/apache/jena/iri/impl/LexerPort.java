1749bd740133559618993bf913a5120f60c43d4a
Andy Seaborne <andy@apache.org>
2019-05-25 18:31:01 +0100

    Remove suppression of warning because it causes (info) warnings.

--- jena-iri/src/main/java/org/apache/jena/iri/impl/LexerPort.java
+++ jena-iri/src/main/java/org/apache/jena/iri/impl/LexerPort.java
@@ -475,8 +475,7 @@ class LexerPort extends AbsLexer implements org.apache.jena.iri.ViolationCodes, 
    * @return      the next token
    * @exception   java.io.IOException  if any I/O-Error occurs
    */
-  @SuppressWarnings("fallthrough")
-@Override
+  @Override
 public int yylex() throws java.io.IOException {
     int zzInput;
     int zzAction;
