90767a9d096b376a2b1c118594c38212a2939909
Andy Seaborne <andy@apache.org>
2019-06-21 11:12:40 +0100

    JENA-1724: Print query to stderr when versbose

--- jena-cmds/src/main/java/arq/query.java
+++ jena-cmds/src/main/java/arq/query.java
@@ -202,10 +202,11 @@ public class query extends CmdARQ
         try {
             Query query = modQuery.getQuery() ;
             if ( isVerbose() ) {
-                IndentedWriter out = new IndentedWriter(System.out, true);
+                IndentedWriter out = new IndentedWriter(System.err, true);
                 query.serialize(out);
+                out.setLineNumbers(false);
+                out.println();
                 out.flush();
-                System.out.println();
             }
             
             if ( isQuiet() )
