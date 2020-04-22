b0b2c8cce41e7879c41d09f9dc2562e0164c0581
Andy Seaborne <andy@apache.org>
2020-04-22 22:22:12 +0100

    JENA-1861: Safe cache of digest result

--- jena-arq/src/main/java/org/apache/jena/sparql/expr/ExprDigest.java
+++ jena-arq/src/main/java/org/apache/jena/sparql/expr/ExprDigest.java
@@ -18,111 +18,83 @@
 
 package org.apache.jena.sparql.expr;
 
-import java.nio.charset.StandardCharsets ;
-import java.security.MessageDigest ;
-import java.security.NoSuchAlgorithmException ;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
-import org.apache.jena.atlas.lib.Bytes ;
-import org.apache.jena.datatypes.xsd.XSDDatatype ;
-import org.apache.jena.graph.Node ;
-import org.apache.jena.sparql.ARQInternalErrorException ;
+import org.apache.jena.atlas.lib.Bytes;
+import org.apache.jena.atlas.lib.Cache;
+import org.apache.jena.atlas.lib.CacheFactory;
+import org.apache.jena.datatypes.xsd.XSDDatatype;
+import org.apache.jena.graph.Node;
+import org.apache.jena.sparql.ARQInternalErrorException;
 
 public abstract class ExprDigest extends ExprFunction1
 {
-    private final String digestName ;
-    private MessageDigest digestCache ;
+    private final String digestName;
+    private MessageDigest digestCache;
 
     public ExprDigest(Expr expr, String symbol, String digestName)
     {
-        super(expr, symbol) ;
-        this.digestName = digestName ; 
-        try
-        {
-            digestCache = MessageDigest.getInstance(digestName) ;
-        } catch (NoSuchAlgorithmException e)
-        {
-            throw new ARQInternalErrorException("Digest not provided in this Java system: "+digestName) ;
+        super(expr, symbol);
+        this.digestName = digestName;
+        try {
+            digestCache = MessageDigest.getInstance(digestName);
+        } catch (NoSuchAlgorithmException e) {
+            throw new ARQInternalErrorException("Digest not provided in this Java system: " + digestName);
         }
     }
 
-    // Each digest expr function is unique 
-    // Pool needs to be static and per-digest-type
-
-//    private Pool<MessageDigest> pool = PoolSync.create(new PoolBase<MessageDigest>()) ;
-//    private MessageDigest getDigest()
-//    {
-//        MessageDigest md = pool.get() ;
-//        if ( md == null )
-//        {
-//            synchronized (pool)
-//            {
-//                md = pool.get() ;
-//                if ( md == null )
-//                {
-//                    md = createDigest() ;
-//                    pool.put(md) ;
-//                }
-//            }
-//        }
-//        return md ;
-//    }
-    
     private MessageDigest getDigest()
     {
         if ( digestCache != null )
         {
-            MessageDigest digest2 = null ;
+            MessageDigest digest2 = null;
             try {
-                digest2 = (MessageDigest)digestCache.clone() ;
-                return digest2 ;
-            } catch (CloneNotSupportedException ex)
-            {
+                digest2 = (MessageDigest)digestCache.clone();
+                return digest2;
+            } catch (CloneNotSupportedException ex) {
                 // Can't clone - remove cache copy.
-                digestCache = null ;
+                digestCache = null;
             }
         }
-        return createDigest() ;
+        return createDigest();
     }
-    
+
     private MessageDigest createDigest()
     {
-        try { return MessageDigest.getInstance(digestName) ; }
-        catch (Exception ex2) { throw new ARQInternalErrorException(ex2) ; } 
+        try { return MessageDigest.getInstance(digestName); }
+        catch (Exception ex2) { throw new ARQInternalErrorException(ex2); }
     }
-    
-    NodeValue lastSeen = null ;
-    NodeValue lastCalc = null ;
-    
+
+    private final Cache<NodeValue, NodeValue> cache = CacheFactory.createOneSlotCache();
+
     @Override
-    public NodeValue eval(NodeValue v)
-    {
-        if ( lastSeen != null && lastSeen.equals(v) )
-            return lastCalc ;
-        
-        Node n = v.asNode() ;
-        if ( ! n.isLiteral() )
-            throw new ExprEvalException("Not a literal: "+v) ;
-        if ( n.getLiteralLanguage() != null && ! n.getLiteralLanguage().equals("") )
-            throw new ExprEvalException("Can't make a digest of an RDF term with a language tag") ; 
+    public NodeValue eval(NodeValue v) {
+        return cache.getOrFill(v, () -> calculate(v));
+    }
+
+    private NodeValue calculate(NodeValue v) {
+        Node n = v.asNode();
+        if ( !n.isLiteral() )
+            throw new ExprEvalException("Not a literal: " + v);
+        if ( n.getLiteralLanguage() != null && !n.getLiteralLanguage().equals("") )
+            throw new ExprEvalException("Can't make a digest of an RDF term with a language tag");
         // Literal, no language tag.
-        if ( n.getLiteralDatatype() != null && ! XSDDatatype.XSDstring.equals(n.getLiteralDatatype()) )
-            throw new ExprEvalException("Not a simple literal nor an XSD string") ;
-        
-        try { 
-            MessageDigest digest = getDigest() ;
-            String x = n.getLiteralLexicalForm() ;
-            byte b[] = x.getBytes(StandardCharsets.UTF_8) ;
-            byte d[] = digest.digest(b) ;
-            String y = Bytes.asHexLC(d) ;
-            NodeValue result = NodeValue.makeString(y) ;
-            
-            // Cache
-            lastSeen = v ;
-            lastCalc = result ;
-            
-            return result ;
-            
-        } catch (Exception ex2) { throw new ARQInternalErrorException(ex2) ; } 
-        
+        if ( n.getLiteralDatatype() != null && !XSDDatatype.XSDstring.equals(n.getLiteralDatatype()) )
+            throw new ExprEvalException("Not a simple literal nor an XSD string");
+
+        try {
+            MessageDigest digest = getDigest();
+            String x = n.getLiteralLexicalForm();
+            byte b[] = x.getBytes(StandardCharsets.UTF_8);
+            byte d[] = digest.digest(b);
+            String y = Bytes.asHexLC(d);
+            NodeValue result = NodeValue.makeString(y);
+            return result;
+
+        } catch (Exception ex2) {
+            throw new ARQInternalErrorException(ex2);
+        }
     }
 }
