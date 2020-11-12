f10a26b336d204cb9ffada6ebc07d4cf90712b97
Andy Seaborne <andy@apache.org>
2020-11-12 16:23:00 +0000

    Fix Fuseki logging for test

--- jena-fuseki2/jena-fuseki-main/src/test/java/org/apache/jena/fuseki/main/TestFusekiMainCmd.java
+++ jena-fuseki2/jena-fuseki-main/src/test/java/org/apache/jena/fuseki/main/TestFusekiMainCmd.java
@@ -27,6 +27,7 @@ import java.util.stream.Stream;
 import org.apache.jena.atlas.json.JSON;
 import org.apache.jena.atlas.web.WebLib;
 import org.apache.jena.fuseki.main.cmds.FusekiMain;
+import org.apache.jena.fuseki.system.FusekiLogging;
 import org.apache.jena.query.ResultSetFormatter;
 import org.apache.jena.rdfconnection.RDFConnection;
 import org.apache.jena.rdfconnection.RDFConnectionFactory;
@@ -40,6 +41,8 @@ public class TestFusekiMainCmd {
     // Fuseki Main server
     private FusekiServer server = null;
     private String serverURL = null;
+    
+    static { FusekiLogging.setLogging(); }
 
     private void server(String... cmdline) {
         int port = WebLib.choosePort();
