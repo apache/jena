adf4149e1856c639429f5a76753523d56d1dfd12
Florian Kleedorfer <florian.kleedorfer@austria.fm>
2020-12-07 15:37:01 +0100

    Add ConstraintVisitorBase
    Also remove empty default implementations in ConstraintVisitor

--- jena-shacl/src/main/java/org/apache/jena/shacl/parser/ConstraintVisitor.java
+++ jena-shacl/src/main/java/org/apache/jena/shacl/parser/ConstraintVisitor.java
@@ -21,36 +21,36 @@ package org.apache.jena.shacl.parser;
 import org.apache.jena.shacl.engine.constraint.*;
 
 public interface ConstraintVisitor {
-    default void visit(ReportConstraint constraint){}
-    default void visit(SparqlConstraint constraint){}
-    default void visit(ConstraintComponentSPARQL constraint){}
-    default void visit(UniqueLangConstraint constraint){}
-    default void visit(HasValueConstraint constraint){}
-    default void visit(MinCount constraint){}
-    default void visit(MaxCount constraint){}
-    default void visit(ShXone constraint){}
-    default void visit(ShAnd constraint){}
-    default void visit(ShOr constraint){}
-    default void visit(ShNot constraint){}
-    default void visit(ShNode constraint){}
-    default void visit(QualifiedValueShape constraint){}
-    default void visit(LessThanOrEqualsConstraint constraint){}
-    default void visit(DisjointConstraint constraint){}
-    default void visit(EqualsConstraint constraint){}
-    default void visit(LessThanConstraint constraint){}
-    default void visit(ClosedConstraint constraint){}
-    default void visit(ClassConstraint constraint){}
-    default void visit(StrMaxLengthConstraint constraint){}
-    default void visit(StrLanguageIn constraint){}
-    default void visit(StrMinLengthConstraint constraint){}
-    default void visit(JViolationConstraint constraint){}
-    default void visit(DatatypeConstraint constraint){}
-    default void visit(JLogConstraint constraint){}
-    default void visit(PatternConstraint constraint){}
-    default void visit(ValueMinExclusiveConstraint constraint){}
-    default void visit(ValueMinInclusiveConstraint constraint){}
-    default void visit(ValueMaxInclusiveConstraint constraint){}
-    default void visit(ValueMaxExclusiveConstraint constraint){}
-    default void visit(InConstraint constraint){}
-    default void visit(NodeKindConstraint constraint){}
+    void visit(ReportConstraint constraint);
+    void visit(SparqlConstraint constraint);
+    void visit(ConstraintComponentSPARQL constraint);
+    void visit(UniqueLangConstraint constraint);
+    void visit(HasValueConstraint constraint);
+    void visit(MinCount constraint);
+    void visit(MaxCount constraint);
+    void visit(ShXone constraint);
+    void visit(ShAnd constraint);
+    void visit(ShOr constraint);
+    void visit(ShNot constraint);
+    void visit(ShNode constraint);
+    void visit(QualifiedValueShape constraint);
+    void visit(LessThanOrEqualsConstraint constraint);
+    void visit(DisjointConstraint constraint);
+    void visit(EqualsConstraint constraint);
+    void visit(LessThanConstraint constraint);
+    void visit(ClosedConstraint constraint);
+    void visit(ClassConstraint constraint);
+    void visit(StrMaxLengthConstraint constraint);
+    void visit(StrLanguageIn constraint);
+    void visit(StrMinLengthConstraint constraint);
+    void visit(JViolationConstraint constraint);
+    void visit(DatatypeConstraint constraint);
+    void visit(JLogConstraint constraint);
+    void visit(PatternConstraint constraint);
+    void visit(ValueMinExclusiveConstraint constraint);
+    void visit(ValueMinInclusiveConstraint constraint);
+    void visit(ValueMaxInclusiveConstraint constraint);
+    void visit(ValueMaxExclusiveConstraint constraint);
+    void visit(InConstraint constraint);
+    void visit(NodeKindConstraint constraint);
 }
---
+++ jena-shacl/src/main/java/org/apache/jena/shacl/parser/ConstraintVisitorBase.java
@@ -0,0 +1,120 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.jena.shacl.parser;
+
+import org.apache.jena.shacl.engine.constraint.*;
+
+public abstract class ConstraintVisitorBase implements ConstraintVisitor {
+
+    @Override
+    public void visit(ReportConstraint constraint) {}
+
+    @Override
+    public void visit(SparqlConstraint constraint) {}
+
+    @Override
+    public void visit(ConstraintComponentSPARQL constraint) {}
+
+    @Override
+    public void visit(UniqueLangConstraint constraint) {}
+
+    @Override
+    public void visit(HasValueConstraint constraint) {}
+
+    @Override
+    public void visit(MinCount constraint) {}
+
+    @Override
+    public void visit(MaxCount constraint) {}
+
+    @Override
+    public void visit(ShXone constraint) {}
+
+    @Override
+    public void visit(ShAnd constraint) {}
+
+    @Override
+    public void visit(ShOr constraint) {}
+
+    @Override
+    public void visit(ShNot constraint) {}
+
+    @Override
+    public void visit(ShNode constraint) {}
+
+    @Override
+    public void visit(QualifiedValueShape constraint) {}
+
+    @Override
+    public void visit(LessThanOrEqualsConstraint constraint) {}
+
+    @Override
+    public void visit(DisjointConstraint constraint) {}
+
+    @Override
+    public void visit(EqualsConstraint constraint) {}
+
+    @Override
+    public void visit(LessThanConstraint constraint) {}
+
+    @Override
+    public void visit(ClosedConstraint constraint) {}
+
+    @Override
+    public void visit(ClassConstraint constraint) {}
+
+    @Override
+    public void visit(StrMaxLengthConstraint constraint) {}
+
+    @Override
+    public void visit(StrLanguageIn constraint) {}
+
+    @Override
+    public void visit(StrMinLengthConstraint constraint) {}
+
+    @Override
+    public void visit(JViolationConstraint constraint) {}
+
+    @Override
+    public void visit(DatatypeConstraint constraint) {}
+
+    @Override
+    public void visit(JLogConstraint constraint) {}
+
+    @Override
+    public void visit(PatternConstraint constraint) {}
+
+    @Override
+    public void visit(ValueMinExclusiveConstraint constraint) {}
+
+    @Override
+    public void visit(ValueMinInclusiveConstraint constraint) {}
+
+    @Override
+    public void visit(ValueMaxInclusiveConstraint constraint) {}
+
+    @Override
+    public void visit(ValueMaxExclusiveConstraint constraint) {}
+
+    @Override
+    public void visit(InConstraint constraint) {}
+
+    @Override
+    public void visit(NodeKindConstraint constraint) {}
+}
