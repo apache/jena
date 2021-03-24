e66c00adad144a774f520728dc6156b977284ad8
Andy Seaborne <andy@apache.org>
2021-03-24 20:39:14 +0000

    JENA-2077: Tidy files for RAT processing (jena-querybuilder)

--- jena-extras/jena-querybuilder/src/main/java/org/apache/jena/arq/querybuilder/handlers/HandlerBlock.java
+++ jena-extras/jena-querybuilder/src/main/java/org/apache/jena/arq/querybuilder/handlers/HandlerBlock.java
@@ -1,5 +1,3 @@
-package org.apache.jena.arq.querybuilder.handlers;
-
 /*
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements. See the NOTICE file
@@ -17,6 +15,8 @@ package org.apache.jena.arq.querybuilder.handlers;
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package org.apache.jena.arq.querybuilder.handlers;
+
 import java.util.Map;
 import org.apache.jena.graph.Node;
 import org.apache.jena.query.Query;
