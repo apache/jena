2865ad2d75e86501f4f287ca7c3dd74768a924a6
Andy Seaborne <andy@apache.org>
2021-04-07 22:05:16 +0100

    Comment for LocalhostFilter

--- jena-fuseki2/jena-fuseki-webapp/src/main/java/org/apache/jena/fuseki/authz/LocalhostFilter.java
+++ jena-fuseki2/jena-fuseki-webapp/src/main/java/org/apache/jena/fuseki/authz/LocalhostFilter.java
@@ -53,8 +53,12 @@ public class LocalhostFilter extends AuthorizationFilter403 {
 
     private static String LOCALHOST_IpV6_a = "[0:0:0:0:0:0:0:1]";
     private static String LOCALHOST_IpV6_b = "0:0:0:0:0:0:0:1";
+    // This is what appears in the Chrome developer tools client-side.
+    // "[0:0:0:0:0:0:0:1]" by the time it arrives here, It is not clear which
+    // software component is responsible for that.     
+    // To be safe we add "[::1]". 
     private static String LOCALHOST_IpV6_c = "[::1]";
-    private static String LOCALHOST_IpV4 =  "127.0.0.1";   // Strictly, 127.*.*.*
+    private static String LOCALHOST_IpV4   = "127.0.0.1";   // Strictly, 127.*.*.*
 
     private static final Collection<String> localhosts = new HashSet<>(
         Arrays.asList(LOCALHOST_IpV4, LOCALHOST_IpV6_a, LOCALHOST_IpV6_b, LOCALHOST_IpV6_c));
