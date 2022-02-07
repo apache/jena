c369f5c837b4c0b0c1e57c9a2619aea405e44c55
Andy Seaborne <andy@apache.org>
2022-02-07 12:24:39 +0000

    Add GraphUtils.getAsRDFNode

--- jena-arq/src/main/java/org/apache/jena/sparql/util/graph/GraphUtils.java
+++ jena-arq/src/main/java/org/apache/jena/sparql/util/graph/GraphUtils.java
@@ -142,24 +142,33 @@ public class GraphUtils {
         return s.getBoolean();
     }
 
+    /** Get a string literal. */
     public static String getStringValue(Resource r, Property p) {
-        if ( !atmostOneProperty(r, p) )
-            throw new NotUniqueException(r, p) ;
-        Statement s = r.getProperty(p) ;
-        if ( s == null )
-            return null ;
-        return s.getString() ;
+        RDFNode obj = getAsRDFNode(r, p);
+        if ( obj == null )
+            return null;
+        return obj.asLiteral().getString();
     }
 
+    /** Get a string literal or a URI as a string. */
     public static String getAsStringValue(Resource r, Property p) {
+        RDFNode obj = getAsRDFNode(r, p);
+        if ( obj == null )
+            return null;
+        if ( obj.isResource() )
+            return obj.asResource().getURI() ;
+        if ( obj.isLiteral() )
+            return obj.asLiteral().getString();
+        throw new UnsupportedOperationException("Not a URI or a string");
+    }
+
+    public static RDFNode getAsRDFNode(Resource r, Property p) {
         if ( !atmostOneProperty(r, p) )
             throw new NotUniqueException(r, p) ;
         Statement s = r.getProperty(p) ;
         if ( s == null )
             return null ;
-        if ( s.getObject().isResource() )
-            return s.getResource().getURI() ;
-        return s.getString() ;
+        return s.getObject();
     }
 
     public static Resource getResourceValue(Resource r, Property p) {
