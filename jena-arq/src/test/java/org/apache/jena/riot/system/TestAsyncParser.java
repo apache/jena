96e379e2c40a8b43fffa611f072d72bb3a59bd57
Claus Stadler <RavenArkadon@googlemail.com>
2022-08-12 17:37:22 +0200

    GH-1477: AsyncParserBuilder and improved resource management

--- jena-arq/src/test/java/org/apache/jena/riot/system/TestAsyncParser.java
+++ jena-arq/src/test/java/org/apache/jena/riot/system/TestAsyncParser.java
@@ -22,14 +22,40 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.lang.management.ManagementFactory;
+import java.nio.ByteBuffer;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
+import java.nio.charset.StandardCharsets;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Objects;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
+import org.apache.jena.atlas.iterator.IteratorCloseable;
+import org.apache.jena.ext.com.google.common.base.Preconditions;
+import org.apache.jena.ext.com.google.common.base.Stopwatch;
 import org.apache.jena.graph.Graph;
 import org.apache.jena.graph.Triple;
-import org.apache.jena.riot.*;
+import org.apache.jena.riot.Lang;
+import org.apache.jena.riot.RDFFormat;
+import org.apache.jena.riot.RDFParser;
+import org.apache.jena.riot.RDFParserBuilder;
+import org.apache.jena.riot.RiotException;
+import org.apache.jena.riot.RiotNotFoundException;
 import org.apache.jena.sparql.graph.GraphFactory;
 import org.apache.jena.sparql.util.IsoMatcher;
+import org.junit.Assert;
 import org.junit.Test;
 
 public class TestAsyncParser {
@@ -91,4 +117,216 @@ public class TestAsyncParser {
         assertTrue( IsoMatcher.isomorphic(graph1, graph2));
     }
 
+    /** Tests the parser on an infinite amount of data with a failing client.
+     * This should stop the parser. */
+    @Test
+    public void failingSink() {
+        int beforeThreadCount = ManagementFactory.getThreadMXBean().getThreadCount();
+        for (int i = 0; i < 100; ++i) {
+            try (InputStream in = openInfiniteNtStream();
+                    OutputStream out = new FailingOutputStream()) {
+                StreamRDF sink = StreamRDFWriter.getWriterStream(out, RDFFormat.NT);
+                sink.start();
+                // Use small read ahead amount due to the infinite input and always failing sink
+                AsyncParser.of(in, Lang.NT, null).setChunkSize(10).setQueueSize(3).asyncParseSources(sink);
+                sink.finish();
+            } catch (Exception e) {
+                // Expected to fail
+                continue;
+            }
+            throw new RuntimeException("Parsing unexpectedly succeeded");
+        }
+        int afterThreadCount = ManagementFactory.getThreadMXBean().getThreadCount();
+        int threadCountDifference = Math.abs(afterThreadCount - beforeThreadCount);
+        int maxAllowedThreadCountDifference = 5;
+
+        Assert.assertTrue("Cancelling RDF parsing resulted in too many dangling threads ("
+                + threadCountDifference + ")",
+                threadCountDifference <= maxAllowedThreadCountDifference);
+    }
+
+    /** Tests to ensure threads are not piling up when repeatedly canceling parsers */
+    @Test
+    public void repeatedParsingCancellation_1() throws Exception {
+        int beforeThreadCount = ManagementFactory.getThreadMXBean().getThreadCount();
+        for (int i = 0; i < 1000; ++i) {
+            IteratorCloseable<Triple> iter = null;
+            try {
+                iter = AsyncParser.of(DIR+"data.ttl").setDaemonMode(false).asyncParseTriples();
+
+                // Ensure parsing is triggered
+                iter.hasNext();
+            } finally {
+                if (iter != null) {
+                    iter.close();
+                }
+            }
+        }
+        int afterThreadCount = ManagementFactory.getThreadMXBean().getThreadCount();
+        int threadCountDifference = Math.abs(afterThreadCount - beforeThreadCount);
+
+        // Give some room in how much the number of threads may differ
+        // The main point is that the acceptable margin is significantly lower than
+        // the number of started parser threads
+        int maxAllowedThreadCountDifference = 5;
+
+        Assert.assertTrue("Cancelling RDF parsing resulted in too many dangling threads ("
+                + threadCountDifference + ")",
+                threadCountDifference <= maxAllowedThreadCountDifference);
+    }
+
+    /** A test that checks that only a limited number of bytes is read when using a small chunk size*/
+    @Test
+    public void lowLatencyParse() {
+        long expectedLimit = 10;
+        RepeatingReadableByteChannel channel = openInfiniteNtChannel();
+        try (Stream<Triple> s = AsyncParser.of(Channels.newInputStream(channel), Lang.TURTLE, null)
+                .setChunkSize(100).streamTriples().limit(expectedLimit)) {
+            // s.forEach(t -> System.out.println("Triple: " + t));
+            long actualLimit = s.count();
+            Assert.assertEquals(expectedLimit, actualLimit);
+        }
+
+        long pos = channel.position();
+        // The exact number of bytes consumed from the channel when writing the test was 10050 bytes
+        // However to give room for implementation changes the value tested against here is about twice as large
+        Assert.assertTrue("Too many bytes consumed from input stream (" + pos + ")", pos < 20000);
+    }
+
+    /** This test first creates some 'good' data for reference and then appends some 'bad' data in order
+     * to provoke a parse error.
+     * Parsing the bad data should then yield all the good data followed by an error event.
+     * Conversely, errors must not cause loss of any events. */
+    @Test
+    public void testNoEventsAreLost() {
+        // Generate some "good" sample data - good means: no parse errors
+        StringBuilder sb = new StringBuilder();
+        sb.append("PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>");
+        for (int i = 0; i < 4; ++i) {
+            sb.append("<urn:example:s> rdfs:label \"item # " + i + "\" .\n");
+        }
+        String goodInputStr = sb.toString();
+
+        List<EltStreamRDF> expected;
+        try (Stream<EltStreamRDF> s = AsyncParser.of(new ByteArrayInputStream(goodInputStr.getBytes()), Lang.TURTLE, null).streamElements()) {
+            expected = s.collect(Collectors.toList());
+        }
+
+        // Validate parser on the good data
+        int expectedGoodEventCount = 5;
+        Assert.assertEquals(expected.size(), expectedGoodEventCount);
+
+        // Append some bad data
+        String badInputStr = sb.append("parse error").toString();
+
+        // Now test that we get back all the good data plus the error event
+        for (int chunkSize = 1; chunkSize < 10; ++chunkSize) {
+            try (Stream<EltStreamRDF> s = AsyncParser.of(new ByteArrayInputStream(badInputStr.getBytes()), Lang.TURTLE, null)
+                    .setChunkSize(chunkSize)
+                    .streamElements()) {
+                List<EltStreamRDF> actual = s.collect(Collectors.toList());
+                Assert.assertEquals(expectedGoodEventCount + 1, actual.size());
+                Assert.assertEquals(expected, actual.subList(0, expectedGoodEventCount));
+                Assert.assertTrue(actual.get(actual.size() - 1).isException());
+            }
+        }
+    }
+
+    /** Test an AsyncParser setup with a custom dispatch (flush) policy */
+    @Test
+    public void testPrematureDispatch() {
+        final int expectedSize = 10;
+
+        AtomicInteger counter = new AtomicInteger(1);
+        String expectedErrorMsg = "Forced abort";
+        List<EltStreamRDF> actual;
+        try (Stream<EltStreamRDF> stream = AsyncParser.of(openInfiniteNtStream(), Lang.TURTLE, null)
+            .setPrematureDispatch(elt -> {
+                if (counter.getAndIncrement() < expectedSize) {
+                    return true;
+                }
+                throw new RuntimeException(expectedErrorMsg);
+            })
+            .streamElements()
+            .limit(1000)) { // The limit is just a safety net to prevent infinite parsing in case of malfunction
+            actual = stream.collect(Collectors.toList());
+        }
+
+        // Check that only the expected number of elements were dispatched
+        Assert.assertEquals(expectedSize, actual.size());
+
+        // The payload of the last element must be an exception, all others must be triples
+        for (int i = 0; i < expectedSize; ++i) {
+            boolean isLastItem = i + 1 == expectedSize;
+            EltStreamRDF elt = actual.get(i);
+            if (isLastItem) {
+                Assert.assertTrue("Last element expected to be an exception", elt.isException());
+                Assert.assertEquals(expectedErrorMsg, elt.exception().getMessage());
+            } else {
+                Assert.assertTrue("Non-last element expected to be a triple", elt.isTriple());
+            }
+        }
+
+    }
+
+    /** An infinite stream of ntriple data */
+    private static InputStream openInfiniteNtStream() {
+        return Channels.newInputStream(openInfiniteNtChannel());
+    }
+
+    private static RepeatingReadableByteChannel openInfiniteNtChannel() {
+        return new RepeatingReadableByteChannel(
+                "<urn:example:s> <urn:example:p> <urn:example:o> .\n".getBytes(StandardCharsets.UTF_8));
+    }
+
+    /** An always failing output stream */
+    private static class FailingOutputStream
+        extends OutputStream
+    {
+        @Override
+        public void write(int b) throws IOException {
+            throw new IOException("Mocked IO Error");
+        }
+    }
+
+    /** A byte channel that infinitely repeats a given chunk of data */
+    private static class RepeatingReadableByteChannel
+        implements ReadableByteChannel
+    {
+        protected byte[] data;
+        protected boolean isOpen;
+        protected long pos;
+
+        public RepeatingReadableByteChannel(byte[] data) {
+            this(data, 0l);
+        }
+
+        public RepeatingReadableByteChannel(byte[] data, long pos) {
+            super();
+            Objects.requireNonNull(data);
+            Preconditions.checkArgument(data.length > 0, "Provided data array must have at least 1 item");
+            this.data = data;
+            this.pos = pos;
+            this.isOpen = true;
+        }
+        @Override public boolean isOpen() { return isOpen; }
+        @Override public void close() throws IOException { isOpen = false; }
+        @Override
+        public int read(ByteBuffer dst) throws IOException {
+            int l = data.length;
+            int arrOffset = (int)(pos % l);
+            int n = Math.min(dst.remaining(), l - arrOffset);
+            dst.put(data, arrOffset, n);
+            pos += n;
+            return n;
+        }
+
+        public long position() {
+            return pos;
+        }
+
+        public void position(long pos) {
+            this.pos = pos;
+        }
+    }
 }
