f86ff413428c55b00c33778dd229f393994e3a09
Andy Seaborne <andy@apache.org>
2022-10-06 10:28:11 +0100

    Tidy up code

--- jena-arq/src/main/java/org/apache/jena/sparql/core/DatasetGraphQuads.java
+++ jena-arq/src/main/java/org/apache/jena/sparql/core/DatasetGraphQuads.java
@@ -20,35 +20,52 @@ package org.apache.jena.sparql.core;
 
 import java.util.Iterator ;
 import org.apache.jena.atlas.iterator.Iter ;
+import org.apache.jena.graph.Graph;
 import org.apache.jena.graph.Node ;
 
 
-/** A DatasetGraph base class for pure quad-centric storage.     
+/** A DatasetGraph base class for pure quad-centric storage.
  */
 public abstract class DatasetGraphQuads extends DatasetGraphBase
-{   
+{
     @Override
-    public Iterator<Node> listGraphNodes()
-    {
-        Iter<Quad> iter = Iter.iter(find(Node.ANY, Node.ANY, Node.ANY, Node.ANY)) ;
-        return iter.map(Quad::getGraph).distinct() ;
+    public Iterator<Node> listGraphNodes() {
+        Iter<Quad> iter = Iter.iter(find(Node.ANY, Node.ANY, Node.ANY, Node.ANY));
+        return iter.map(Quad::getGraph).distinct();
     }
 
     @Override
-    public void removeGraph(Node graphName)
-    { 
-        deleteAny(graphName, Node.ANY, Node.ANY, Node.ANY) ;
+    public void removeGraph(Node graphName) {
+        deleteAny(graphName, Node.ANY, Node.ANY, Node.ANY);
     }
-    
-    @Override
-    public abstract Iterator<Quad> find(Node g, Node s, Node p, Node o) ;
+
+    // See DatasetGraphViewGraphs
+    // If that is merged to DatasetGraphBase, these are unnecessary.
 
     @Override
-    public abstract Iterator<Quad> findNG(Node g, Node s, Node p, Node o) ;
+    public void addGraph(Node graphName, Graph graph) {
+        graph.find().forEachRemaining(t -> add(Quad.create(graphName, t)));
+    }
+
+//    @Override
+//    public Graph getDefaultGraph() {
+//        return GraphView.createDefaultGraph(this);
+//    }
+//
+//    @Override
+//    public Graph getGraph(Node graphNode) {
+//        return GraphView.createNamedGraph(this, graphNode);
+//    }
 
     @Override
-    public abstract void add(Quad quad) ;
+    public abstract Iterator<Quad> find(Node g, Node s, Node p, Node o);
 
     @Override
-    public abstract void delete(Quad quad) ;
+    public abstract Iterator<Quad> findNG(Node g, Node s, Node p, Node o);
+
+    @Override
+    public abstract void add(Quad quad);
+
+    @Override
+    public abstract void delete(Quad quad);
 }
