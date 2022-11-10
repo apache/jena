4b887b9529fe19edf156889e42541b3b60d6e108
Olaf Hartig <ohartig@amazon.com>
2022-11-10 13:09:21 +0100

    adds the constructor function cdt:List

--- jena-arq/src/main/java/org/apache/jena/sparql/function/library/collection/CollectionLiteralFunctions.java
+++ jena-arq/src/main/java/org/apache/jena/sparql/function/library/collection/CollectionLiteralFunctions.java
@@ -5,6 +5,7 @@ import org.apache.jena.sparql.function.FunctionRegistry;
 
 public class CollectionLiteralFunctions {
 	public static void register( final FunctionRegistry functionRegistry ) {
+		functionRegistry.put( ARQConstants.CDTFunctionLibraryURI + "List",    ListFct.class );
 		functionRegistry.put( ARQConstants.CDTFunctionLibraryURI + "size",    SizeFct.class );
 		functionRegistry.put( ARQConstants.CDTFunctionLibraryURI + "concat",  ConcatFct.class );
 	}
