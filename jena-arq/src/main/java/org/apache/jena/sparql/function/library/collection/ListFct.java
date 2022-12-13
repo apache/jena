53043337c57754fe39b6e2e0784fca252f2d748a
Olaf Hartig <ohartig@amazon.com>
2022-12-13 15:42:49 +0100

    changes the implementation of the cdt:List constructor to be a functional form (rather than a function) which can be used to have null values in the constructed lists

--- jena-arq/src/main/java/org/apache/jena/sparql/function/library/collection/ListFct.java
+++ jena-arq/src/main/java/org/apache/jena/sparql/function/library/collection/ListFct.java
@@ -9,9 +9,13 @@ import org.apache.jena.cdt.LiteralLabelForList;
 import org.apache.jena.graph.Node;
 import org.apache.jena.graph.NodeFactory;
 import org.apache.jena.graph.impl.LiteralLabel;
+import org.apache.jena.sparql.engine.binding.Binding;
+import org.apache.jena.sparql.expr.Expr;
+import org.apache.jena.sparql.expr.ExprException;
 import org.apache.jena.sparql.expr.ExprList;
 import org.apache.jena.sparql.expr.NodeValue;
 import org.apache.jena.sparql.function.FunctionBase;
+import org.apache.jena.sparql.function.FunctionEnv;
 
 public class ListFct extends FunctionBase
 {
@@ -21,17 +25,29 @@ public class ListFct extends FunctionBase
 	}
 
 	@Override
-	public NodeValue exec( final List<NodeValue> args ) {
+	public NodeValue exec(Binding binding, ExprList args, String uri, FunctionEnv env) {
 		final List<CDTValue> list = new ArrayList<>( args.size() );
 
-		for ( final NodeValue nv : args ) {
-			final CDTValue v = (nv == null) ? CDTFactory.getNullValue() : CDTFactory.createValue( nv.asNode() );
+		for ( final Expr e : args ) {
+			CDTValue v;
+			try {
+				final NodeValue nv = e.eval(binding, env);
+				v = CDTFactory.createValue( nv.asNode() );
+			} catch ( final ExprException ex ) {
+				v = CDTFactory.getNullValue();
+			}
+
 			list.add(v);
 		}
 
 		final LiteralLabel lit = new LiteralLabelForList(list);
 		final Node n = NodeFactory.createLiteral(lit);
 		return NodeValue.makeNode(n);
+	}
+
+	@Override
+	public NodeValue exec( final List<NodeValue> args ) {
+		throw new IllegalStateException("should never end up here");
 	}
 
 }
