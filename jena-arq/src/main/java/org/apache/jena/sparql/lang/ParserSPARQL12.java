6f629c814a893c6f1c007927d7bfb2dc91027a4b
Andy Seaborne <andy@apache.org>
2023-04-08 18:49:09 +0100

    GH-1835: Setup for SPARQL 1.2 grammar

--- jena-arq/src/main/java/org/apache/jena/sparql/lang/ParserSPARQL12.java
+++ jena-arq/src/main/java/org/apache/jena/sparql/lang/ParserSPARQL12.java
@@ -27,21 +27,21 @@ import org.apache.jena.query.QueryException ;
 import org.apache.jena.query.QueryParseException ;
 import org.apache.jena.query.Syntax ;
 import org.apache.jena.shared.JenaException ;
-import org.apache.jena.sparql.lang.sparql_10.SPARQLParser10 ;
+import org.apache.jena.sparql.lang.sparql_12.SPARQLParser12 ;
 import org.apache.jena.sparql.syntax.Element ;
 import org.apache.jena.sparql.syntax.Template ;
 
-public class ParserSPARQL10 extends SPARQLParser
+public class ParserSPARQL12 extends SPARQLParser
 {
-    private interface Action { void exec(SPARQLParser10 parser) throws Exception ; }
+    private interface Action { void exec(SPARQLParser12 parser) throws Exception ; }
 
     @Override
     protected Query parse$(final Query query, String queryString) {
-        query.setSyntax(Syntax.syntaxSPARQL_10);
+        query.setSyntax(Syntax.syntaxSPARQL_12);
 
         Action action = new Action() {
             @Override
-            public void exec(SPARQLParser10 parser) throws Exception {
+            public void exec(SPARQLParser12 parser) throws Exception {
                 parser.QueryUnit();
             }
         };
@@ -54,7 +54,7 @@ public class ParserSPARQL10 extends SPARQLParser
         final Query query = new Query();
         Action action = new Action() {
             @Override
-            public void exec(SPARQLParser10 parser) throws Exception {
+            public void exec(SPARQLParser12 parser) throws Exception {
                 Element el = parser.GroupGraphPattern();
                 query.setQueryPattern(el);
             }
@@ -67,7 +67,7 @@ public class ParserSPARQL10 extends SPARQLParser
         final Query query = new Query();
         Action action = new Action() {
             @Override
-            public void exec(SPARQLParser10 parser) throws Exception {
+            public void exec(SPARQLParser12 parser) throws Exception {
                 Template t = parser.ConstructTemplate();
                 query.setConstructTemplate(t);
             }
@@ -78,23 +78,22 @@ public class ParserSPARQL10 extends SPARQLParser
 
     // All throwable handling.
     private static void perform(Query query, String string, Action action) {
-        Reader in = new StringReader(string) ;
-        SPARQLParser10 parser = new SPARQLParser10(in) ;
+        Reader in = new StringReader(string);
+        SPARQLParser12 parser = new SPARQLParser12(in);
+
         try {
             query.setStrict(true);
             parser.setQuery(query);
             action.exec(parser);
-        } catch (org.apache.jena.sparql.lang.sparql_10.ParseException ex) {
+        } catch (org.apache.jena.sparql.lang.sparql_12.ParseException ex) {
             throw new QueryParseException(ex.getMessage(), ex.currentToken.beginLine, ex.currentToken.beginColumn);
-        } catch (org.apache.jena.sparql.lang.sparql_10.TokenMgrError tErr) {
+        } catch (org.apache.jena.sparql.lang.sparql_12.TokenMgrError tErr) {
             // Last valid token : not the same as token error message - but this
             // should not happen
             int col = parser.token.endColumn;
             int line = parser.token.endLine;
             throw new QueryParseException(tErr.getMessage(), line, col);
-        }
-
-        catch (QueryException ex) {
+        } catch (QueryException ex) {
             throw ex;
         } catch (JenaException ex) {
             throw new QueryException(ex.getMessage(), ex);
@@ -102,8 +101,8 @@ public class ParserSPARQL10 extends SPARQLParser
             // The token stream can throw errors.
             throw new QueryParseException(err.getMessage(), err, -1, -1);
         } catch (Throwable th) {
-            Log.warn(ParserSPARQL10.class, "Unexpected throwable: ", th);
-            throw new QueryException(th.getMessage(), th) ;
+            Log.warn(ParserSPARQL12.class, "Unexpected throwable: ", th);
+            throw new QueryException(th.getMessage(), th);
         }
     }
 }
