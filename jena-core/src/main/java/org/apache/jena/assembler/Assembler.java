7b1734d6791df1e5b3bfa771dc8520a14c665a50
Andy Seaborne <andy@apache.org>
2023-07-09 21:56:45 +0100

    GH-2027: Remove unused assemblers.
    ModelSourceAssembler
    NamedModelAssembler
    FileManagerAssembler
    LocationMapperAssemble

--- jena-core/src/main/java/org/apache/jena/assembler/Assembler.java
+++ jena-core/src/main/java/org/apache/jena/assembler/Assembler.java
@@ -25,15 +25,15 @@ import org.apache.jena.rdf.model.* ;
     An Assembler creates objects from their RDF descriptions. The root motivation
     is to create Models, but other objects are required as sub-components of
     those Models, so a general mechanism is available.
-    
+
     @apiNote Methods accepting {@link Model} as a parameter treat appropriate values of {@code Model} as passing down
     to sub-assemblers a requirement to share for certain setups that work as tightly coordinated groups. It's a hint,
     not an instruction.
-   
+
 */
 public interface Assembler
     {
-    
+
     /**
         The core operation: answer a new object constructed according to the
         object description hanging from <code>root</code>, using the assembler
@@ -42,12 +42,12 @@ public interface Assembler
         to all sub-object construction.
     */
     public Object open( Assembler a, Resource root, Mode mode );
-    
+
     /**
         Answer <code>open( a, root, Mode.DEFAULT )</code>.
     */
     public Object open( Assembler a, Resource root );
-    
+
     /**
         Answer <code>open( this, root, Mode.DEFAULT )</code>.
     */
@@ -64,16 +64,16 @@ public interface Assembler
         the result cannot be or is not a Model, in which case throw an exception.
     */
     public Model openModel( Resource root, Mode mode );
-    
+
     // This slightly bizarre way to initial "constants" is because Assembler is
     // an interface. Interfaces inside class intialization processes do not have
     // their computer static fields initialized yet. To keep that, for
     // compatibility, initialization code must use the ConstAssembler functions
     // and not use constants here.  Assembler.general is the key field used
     // in ARQ.init (AssemblerUtils) and TDB.init (VocabTDB).
-    
+
     public static final Assembler defaultModel = ConstAssembler.defaultModel() ;
-    
+
     public static final Assembler memoryModel = ConstAssembler.memoryModel() ;
 
     public static final Assembler infModel = ConstAssembler.infModel();
@@ -83,22 +83,16 @@ public interface Assembler
     public static final Assembler reasonerFactory = ConstAssembler.reasonerFactory();
 
     public static final Assembler content = ConstAssembler.content();
-    
+
     public static final Assembler prefixMapping = ConstAssembler.prefixMapping();
 
     public static final Assembler unionModel = ConstAssembler.unionModel();
 
     public static final Assembler ontModelSpec = ConstAssembler.ontModelSpec();
-    
-    public static final Assembler ruleSet = ConstAssembler.ruleSet();
-    
-    public static final Assembler modelSource = ConstAssembler.modelSource();
-    
-    public static final Assembler locationMapper = ConstAssembler.locationMapper();
 
-    public static final Assembler fileManager = ConstAssembler.fileManager();
+    public static final Assembler ruleSet = ConstAssembler.ruleSet();
 
     public static final Assembler documentManager = ConstAssembler.documentManager();
-    
+
     public static final AssemblerGroup general = ConstAssembler.general() ;
     }
