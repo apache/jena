b6009855c89222ea3a487476e9a743457fc56790
Olaf Hartig <ohartig@amazon.com>
2023-08-10 16:49:32 +0200

    adds the map version of cdt:get, in this context also changes 'equals' of CDTKey (it must be true only for same terms not for same values)

--- jena-arq/src/main/java/org/apache/jena/sparql/function/library/collection/GetFct.java
+++ jena-arq/src/main/java/org/apache/jena/sparql/function/library/collection/GetFct.java
@@ -1,10 +1,15 @@
 package org.apache.jena.sparql.function.library.collection;
 
 import java.util.List;
+import java.util.Map;
 
+import org.apache.jena.cdt.CDTFactory;
+import org.apache.jena.cdt.CDTKey;
 import org.apache.jena.cdt.CDTValue;
 import org.apache.jena.cdt.CompositeDatatypeList;
+import org.apache.jena.cdt.CompositeDatatypeMap;
 import org.apache.jena.graph.Node;
+import org.apache.jena.graph.impl.LiteralLabel;
 import org.apache.jena.sparql.expr.ExprEvalException;
 import org.apache.jena.sparql.expr.NodeValue;
 import org.apache.jena.sparql.function.FunctionBase2;
@@ -15,9 +20,16 @@ public class GetFct extends FunctionBase2
 	public NodeValue exec( final NodeValue nv1, final NodeValue nv2 ) {
 		final Node n1 = nv1.asNode();
 
-		if ( ! CompositeDatatypeList.isListLiteral(n1) )
-			throw new ExprEvalException("Not a list literal: " + nv1);
+		if ( CompositeDatatypeList.isListLiteral(n1) )
+			return getFromList( n1.getLiteral(), nv2 );
 
+		if ( CompositeDatatypeMap.isMapLiteral(n1) )
+			return getFromMap( n1.getLiteral(), nv2 );
+
+		throw new ExprEvalException("Neither a list nor a map literal: " + nv1);
+	}
+
+	protected NodeValue getFromList( final LiteralLabel n1, final NodeValue nv2 ) {
 		if ( ! nv2.isInteger() )
 			throw new ExprEvalException("Not an integer literal: " + nv2);
 
@@ -26,7 +38,7 @@ public class GetFct extends FunctionBase2
 		if ( index < 1 )
 			throw new ExprEvalException("Out of bounds index value: " + nv2);
 
-		final List<CDTValue> list = CompositeDatatypeList.getValue( n1.getLiteral() );
+		final List<CDTValue> list = CompositeDatatypeList.getValue(n1);
 
 		if ( index > list.size() )
 			throw new ExprEvalException("Out of bounds index value: " + nv2);
@@ -34,6 +46,33 @@ public class GetFct extends FunctionBase2
 		final CDTValue value = list.get( index-1 );
 		if ( value.isNull() ) {
 			throw new ExprEvalException("accessing null value from list" );
+		}
+		else if ( value.isNode() ) {
+			return NodeValue.makeNode( value.asNode() );
+		}
+		else {
+			throw new ExprEvalException("Unexpected type of CDTValue: " + value.getClass().getName() );
+		}
+	}
+
+	protected NodeValue getFromMap( final LiteralLabel n1, final NodeValue nv2 ) {
+		final Node n2 = nv2.asNode();
+		if ( ! n2.isURI() && ! n2.isLiteral() )
+			throw new ExprEvalException("Not a valid map key: " + nv2);
+
+		final Map<CDTKey,CDTValue> map = CompositeDatatypeMap.getValue(n1);
+
+		if ( map.isEmpty() )
+			throw new ExprEvalException("empty map");
+
+		final CDTKey key = CDTFactory.createKey(n2);
+		final CDTValue value = map.get(key);
+
+		if ( value == null ) {
+			throw new ExprEvalException("key is not in the map");
+		}
+		else if ( value.isNull() ) {
+			throw new ExprEvalException("value for key is in null");
 		}
 		else if ( value.isNode() ) {
 			return NodeValue.makeNode( value.asNode() );
