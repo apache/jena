4ea569f240793f5a5cbc2444ade834cb7974942c
Olaf Hartig <ohartig@amazon.com>
2023-08-11 16:31:56 +0200

    adds CDTLiteralFunctionUtils

--- jena-arq/src/main/java/org/apache/jena/sparql/function/library/collection/KeysFct.java
+++ jena-arq/src/main/java/org/apache/jena/sparql/function/library/collection/KeysFct.java
@@ -7,12 +7,6 @@ import java.util.Map;
 import org.apache.jena.cdt.CDTFactory;
 import org.apache.jena.cdt.CDTKey;
 import org.apache.jena.cdt.CDTValue;
-import org.apache.jena.cdt.CompositeDatatypeMap;
-import org.apache.jena.cdt.LiteralLabelForList;
-import org.apache.jena.graph.Node;
-import org.apache.jena.graph.NodeFactory;
-import org.apache.jena.graph.impl.LiteralLabel;
-import org.apache.jena.sparql.expr.ExprEvalException;
 import org.apache.jena.sparql.expr.NodeValue;
 import org.apache.jena.sparql.function.FunctionBase1;
 
@@ -20,12 +14,7 @@ public class KeysFct extends FunctionBase1
 {
 	@Override
 	public NodeValue exec( final NodeValue nv ) {
-		final Node n = nv.asNode();
-
-		if ( ! CompositeDatatypeMap.isMapLiteral(n) )
-			throw new ExprEvalException("Not a map literal: " + nv);
-
-		final Map<CDTKey,CDTValue> map = CompositeDatatypeMap.getValue( n.getLiteral() );
+		final Map<CDTKey,CDTValue> map = CDTLiteralFunctionUtils.checkAndGetMap(nv);
 
 		final List<CDTValue> list = new ArrayList<>( map.size() );
 		for ( final CDTKey key : map.keySet() ) {
@@ -33,8 +22,7 @@ public class KeysFct extends FunctionBase1
 			list.add(value);
 		}
 
-		final LiteralLabel lit = new LiteralLabelForList(list);
-		return NodeValue.makeNode( NodeFactory.createLiteral(lit) );
+		return CDTLiteralFunctionUtils.createNodeValue(list);
 	}
 
 }
