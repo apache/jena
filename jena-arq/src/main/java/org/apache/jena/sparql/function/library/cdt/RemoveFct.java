39e1cde97c3d86cded06d8126a20865b488ef301
Olaf Hartig <ohartig@amazon.com>
2023-08-11 16:36:46 +0200

    renames 'org.apache.jena.sparql.function.library.collections' package to 'org.apache.jena.sparql.function.library.cdt'

--- jena-arq/src/main/java/org/apache/jena/sparql/function/library/cdt/RemoveFct.java
+++ jena-arq/src/main/java/org/apache/jena/sparql/function/library/cdt/RemoveFct.java
@@ -1,5 +1,6 @@
-package org.apache.jena.sparql.function.library.collection;
+package org.apache.jena.sparql.function.library.cdt;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import org.apache.jena.cdt.CDTFactory;
@@ -9,27 +10,31 @@ import org.apache.jena.graph.Node;
 import org.apache.jena.sparql.expr.NodeValue;
 import org.apache.jena.sparql.function.FunctionBase2;
 
-public class ContainsKeyFct extends FunctionBase2
+public class RemoveFct extends FunctionBase2
 {
 	@Override
 	public NodeValue exec( final NodeValue nv1, final NodeValue nv2 ) {
 		final Map<CDTKey,CDTValue> map = CDTLiteralFunctionUtils.checkAndGetMap(nv1);
 
+		if ( map.isEmpty() )
+			return nv1;
+
 		final Node n2 = nv2.asNode();
 
-		// If the second argument is not an IRI or a literal, then it cannot be
-		// a map key in the given map and, by definition of cdt:containsKey, we
-		// have to return false.
+		// If the second term is not a map key (and the first term is a well-
+		// formed cdt:Map literal), the first term needs to be returned as is.
 		if ( ! n2.isURI() && ! n2.isLiteral() )
-			return NodeValue.booleanReturn(false);
-
-		if ( map.isEmpty() )
-			return NodeValue.booleanReturn(false);
+			return nv1;
 
 		final CDTKey key = CDTFactory.createKey(n2);
-		final CDTValue value = map.get(key);
 
-		return NodeValue.booleanReturn( value != null );
+		if ( ! map.containsKey(key) )
+			return nv1;
+
+		final Map<CDTKey,CDTValue> newMap = new HashMap<>(map);
+		newMap.remove(key);
+
+		return CDTLiteralFunctionUtils.createNodeValue(newMap);
 	}
 
 }
