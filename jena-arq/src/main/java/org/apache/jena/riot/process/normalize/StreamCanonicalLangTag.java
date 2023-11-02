dd5d50f77a80c72fce8e1391704eb5ae6474d975
Andy Seaborne <andy@apache.org>
2023-11-02 18:14:05 +0000

    Fix white space

--- jena-arq/src/main/java/org/apache/jena/riot/process/normalize/StreamCanonicalLangTag.java
+++ jena-arq/src/main/java/org/apache/jena/riot/process/normalize/StreamCanonicalLangTag.java
@@ -30,26 +30,24 @@ import org.apache.jena.sparql.util.NodeUtils;
 
 /** {@link StreamRDF} that converts language tags to lower case o rto canokcal form (RFC 4646). */
 public class StreamCanonicalLangTag extends StreamRDFApplyObject {
-    /** Return a {@link StreamRDF} that converts language tags to lower case */ 
+    /** Return a {@link StreamRDF} that converts language tags to lower case */
     public static StreamRDF toLC(StreamRDF other) {
         Locale.Builder locBuild = new Locale.Builder();
         return new StreamCanonicalLangTag(other, locBuild,
                                           (b,n) -> canonical(locBuild, n, StreamCanonicalLangTag::langTagLC));
     }
-    
-    /** Return a {@link StreamRDF} that converts language tags to canonical form (RFC 4646, 5646). */ 
+
+    /** Return a {@link StreamRDF} that converts language tags to canonical form (RFC 4646, 5646). */
     public static StreamRDF toCanonical(StreamRDF other) {
         Locale.Builder locBuild = new Locale.Builder();
-        return new StreamCanonicalLangTag(other, locBuild, 
+        return new StreamCanonicalLangTag(other, locBuild,
                                           (b,n) -> canonical(locBuild, n, StreamCanonicalLangTag::langTagCanonical));
     }
 
-    
-    
     private StreamCanonicalLangTag(StreamRDF other, Locale.Builder locBuild, BiFunction<Locale.Builder, Node, Node> stringMapper) {
         super(other, (n)->stringMapper.apply(locBuild, n));
     }
-    
+
     private static Node canonical(Locale.Builder locBuild, Node n, BiFunction<Locale.Builder, String, String> tagMapper ) {
         // If no change, return the original object; this prevents object churn.
         if ( ! NodeUtils.hasLang(n) )
@@ -63,7 +61,7 @@ public class StreamCanonicalLangTag extends StreamRDFApplyObject {
         Node obj2 = NodeFactory.createLiteral(n.getLiteralLexicalForm(), langTag2);
         return obj2;
     }
-    
+
     // From taken from "xsd4ld" (which has an Apache License).
     public static String langTagCanonical(Locale.Builder locBuild, String str) {
         try {
