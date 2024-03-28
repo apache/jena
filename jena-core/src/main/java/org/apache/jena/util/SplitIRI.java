b1670d24177d209cde596d2355af75ca623db12e
Andy Seaborne <andy@apache.org>
2024-03-28 16:15:45 +0000

    Clean white space

--- jena-core/src/main/java/org/apache/jena/util/SplitIRI.java
+++ jena-core/src/main/java/org/apache/jena/util/SplitIRI.java
@@ -18,9 +18,9 @@
 
 package org.apache.jena.util;
 
-import org.apache.jena.graph.Node ;
-import org.apache.jena.rdf.model.impl.Util ;
-//import org.apache.jena.riot.system.RiotChars ;
+import org.apache.jena.graph.Node;
+import org.apache.jena.rdf.model.impl.Util;
+//import org.apache.jena.riot.system.RiotChars;
 
 /**
  * Code to split an URI or IRI into prefix and local part.
@@ -28,16 +28,15 @@ import org.apache.jena.rdf.model.impl.Util ;
  * reflecting RDF/XML history.
  * <p>
  * For display, use {@link #localname} and {@link #namespace}.
- * This follows Turtle, adds some pragmatic rulesm but does not escape
+ * This follows Turtle, adds some pragmatic rules but does not escape
  * any characters. A URI is split never split before the last {@code /}
  * or last {@code #}, if present.
  * See {@link #splitpoint} for more details.
  * <p>
- * This code form the machinery behind {@link Node#getLocalName}
- * {@link Node#getNameSpace} for URI Nodes.
+ * This code forms the machinery behind {@link Node#getLocalName}
+ * {@link Node#getNameSpace} for URI Nodes following the XML rules.
  * <p>
- * {@link #localnameTTL} is strict Turtle; it is the same local name as
- * before, but escaped if necessary.
+ * {@link #namespaceTTL} and {@link #localnameTTL} is strict Turtle; the local name is escaped if necessary.
  * <p>
  * The functions {@link #namespaceXML} and {@link #localnameXML}
  * apply the rules for XML qnames.
@@ -45,42 +44,46 @@ import org.apache.jena.rdf.model.impl.Util ;
 public class SplitIRI
 {
     /** Return the 'namespace' (prefix) for a URI string.
-     * Use with {@link #localname}
+     * Use with {@link #localname}.
+     * Return the input string if there is no splitpoint.
      */
     public static String namespace(String string) {
-        int i = splitpoint(string) ;
+        int i = splitpoint(string);
         if ( i < 0 )
-            return string ;
-        return string.substring(0, i) ;
+            return string;
+        return string.substring(0, i);
     }
 
     /** Calculate a localname - do not escape PN_LOCAL_ESC.
      * This is not guaranteed to be legal Turtle.
      * Use with {@link #namespace}
+     * Return an empty string if there is no splitpoint.
      */
     public static String localname(String string) {
-        int i = splitpoint(string) ;
+        int i = splitpoint(string);
         if ( i < 0 )
-            return "" ;
-        return string.substring(i) ;
+            return "";
+        return string.substring(i);
     }
 
-    /** Return the 'namespace' (prefix) for a URI string,
+    /**
+     * Return the 'namespace' (prefix) for a URI string,
      * legal for Turtle and goes with {@link #localnameTTL}
      */
     public static String namespaceTTL(String string) {
-        return namespace(string) ;
+        return namespace(string);
     }
 
-    /** Calculate a localname - enforce legal Turtle
+    /**
+     * Calculate a localname - enforce legal Turtle
      * escape PN_LOCAL_ESC, check for final '.'
      * Use with {@link #namespaceTTL}
      */
     public static String localnameTTL(String string) {
-        String x = localname(string) ;
+        String x = localname(string);
         if ( x.isEmpty())
-            return x ;
-        return escape_PN_LOCAL_ESC(x) ;
+            return x;
+        return escape_PN_LOCAL_ESC(x);
     }
 
     private static String escape_PN_LOCAL_ESC(String x) {
@@ -88,32 +91,32 @@ public class SplitIRI
         // is work to do then scan again doing the work.
         //'\' ('_' | '~' | '.' | '-' | '!' | '$' | '&' | "'" | '(' | ')' | '*' | '+' | ',' | ';' | '=' | '/' | '?' | '#' | '@' | '%')
 
-        int N = x.length() ;
-        boolean escchar = false ;
-        for ( int i = 0 ; i < N ; i++ ) {
-            char ch = x.charAt(i) ;
+        int N = x.length();
+        boolean escchar = false;
+        for ( int i = 0; i < N; i++ ) {
+            char ch = x.charAt(i);
             if ( needsEscape(ch, (i==N-1)) ) {
-                escchar = true ;
-                break ;
+                escchar = true;
+                break;
             }
         }
         if ( ! escchar )
-            return x ;
-        StringBuilder sb = new StringBuilder(N+10) ;
-        for ( int i = 0 ; i < N ; i++ ) {
-            char ch = x.charAt(i) ;
+            return x;
+        StringBuilder sb = new StringBuilder(N+10);
+        for ( int i = 0; i < N; i++ ) {
+            char ch = x.charAt(i);
             // DOT only needs escaping at the end
             if ( needsEscape(ch, (i==N-1) )  )
-                sb.append('\\') ;
-            sb.append(ch) ;
+                sb.append('\\');
+            sb.append(ch);
         }
-        return sb.toString() ;
+        return sb.toString();
     }
 
     private static boolean needsEscape(char ch, boolean finalChar) {
         if ( ch == '.' )
-            return finalChar ;
-        return isPN_LOCAL_ESC(ch) ;
+            return finalChar;
+        return isPN_LOCAL_ESC(ch);
     }
 
     // @formatter:off
@@ -148,14 +151,14 @@ public class SplitIRI
      * @return The split point, or -1 for "not found".
      */
     public static int splitpoint(String uri) {
-        boolean isURN = uri.startsWith("urn:") ;
+        boolean isURN = uri.startsWith("urn:");
         // Fast track.  Still need to check validity of the prefix part.
-        int idx1 = uri.lastIndexOf('#') ;
+        int idx1 = uri.lastIndexOf('#');
         // Not so simple - \/ in local names
-        int idx2 = isURN ? uri.lastIndexOf(':') : uri.lastIndexOf('/') ;
+        int idx2 = isURN ? uri.lastIndexOf(':') : uri.lastIndexOf('/');
 
         // If absolute.
-        int idx3 = uri.indexOf(':') ;
+        int idx3 = uri.indexOf(':');
 
         // Note: local names can't end in "." in Turtle.
         // This is handled by escape_PN_LOCAL_ESC which will escape it as "\."
@@ -177,52 +180,52 @@ public class SplitIRI
         } else if ( idx1 >= 0 && idx2 >= 0 ) {
             // Fragment and path. Use fragment.
             // If "/" is in the fragment, it is not the split point.
-            limit = idx1 ;
+            limit = idx1;
         } else {
             limit = -1;
         }
 
         // At least idx3, the case of no "/" and no "#" in an absolute IRI
         if ( idx3 >= 0 )
-            limit = Math.max(limit, idx3) ;
+            limit = Math.max(limit, idx3);
 
         // Limit is our guess.
         // Now search end of URI to this guess checking the characters found.
 
-        int splitPoint = -1 ;
+        int splitPoint = -1;
         // Work backwards, checking for
         // ((PN_CHARS | '.' | ':' | PLX)*
-        for ( int i = uri.length()-1 ; i > limit ; i-- ) {
-            char ch = uri.charAt(i) ;
+        for ( int i = uri.length()-1; i > limit; i-- ) {
+            char ch = uri.charAt(i);
             if ( /*RiotChars.*/isPNChars_U_N(ch) || /*RiotChars.*/isPN_LOCAL_ESC(ch) || ch == ':' || ch == '-' || ch == '.' )
-                continue ;
-            splitPoint = i+1 ;
-            break ;
+                continue;
+            splitPoint = i+1;
+            break;
         }
         // limit was at the end.  No split point (we could escape the limit point)
         if ( splitPoint == -1 )
-            splitPoint = limit+1 ;
+            splitPoint = limit+1;
         // No split point.
         if ( splitPoint >= uri.length() )
-            return -1 ;
+            return -1;
 
         // Check the first character of the local name.
         // All characters are legal localname name characters but may not satisfy the additional
         // first character rule.  Move forward to first legal first character.
-        int ch = uri.charAt(splitPoint) ;
+        int ch = uri.charAt(splitPoint);
         while ( ch == '.' || ch == '-' ) {
-            splitPoint++ ;
+            splitPoint++;
             if ( splitPoint >= uri.length() )
-                return -1 ;
-            ch = uri.charAt(splitPoint) ;
+                return -1;
+            ch = uri.charAt(splitPoint);
         }
 
         // Checking the final '.' is done when checking for escapes.
-        return splitPoint ;
+        return splitPoint;
     }
 
     private static boolean checkhex(String uri, int i) {
-        return /*RiotChars.*/isHexChar(uri.charAt(i)) ;
+        return /*RiotChars.*/isHexChar(uri.charAt(i));
     }
 
     // Assuming legal URIs, there is no work to be done
@@ -233,10 +236,10 @@ public class SplitIRI
         if ( ch == '%' ) {
             if ( i+2 >= uri.length() ) {
                 // Too short
-                return -1 ;
+                return -1;
             }
             if ( ! checkhex(uri, i+1) || ! checkhex(uri, i+2) )
-                return -1 ;
+                return -1;
         }
 
      */
@@ -244,20 +247,20 @@ public class SplitIRI
      * This is the longest NCName at the end of the uri.
      * See {@link Util#splitNamespaceXML}.
      */
-    public static int splitXML(String string) { return Util.splitNamespaceXML(string) ; }
+    public static int splitXML(String string) { return Util.splitNamespaceXML(string); }
 
     /** Namespace, according to XML qname rules.
      * Use with {@link #localnameXML}.
      */
     public static String namespaceXML(String string) {
-        int i = splitXML(string) ;
-        return string.substring(0, i) ;
+        int i = splitXML(string);
+        return string.substring(0, i);
     }
 
     /** Localname, according to XML qname rules. */
     public static String localnameXML(String string) {
-        int i = splitXML(string) ;
-        return string.substring(i) ;
+        int i = splitXML(string);
+        return string.substring(i);
     }
 
     // Extracted from RiotChars
@@ -268,15 +271,15 @@ public class SplitIRI
             case '\\': case '_':  case '~': case '.': case '-': case '!': case '$':
             case '&':  case '\'': case '(': case ')': case '*': case '+': case ',':
             case ';':  case '=':  case '/': case '?': case '#': case '@': case '%':
-                return true ;
+                return true;
             default:
-                return false ;
+                return false;
         }
     }
 
     /** ASCII 0-9 */
     private static boolean isDigit(int ch) {
-        return range(ch, '0', '9') ;
+        return range(ch, '0', '9');
     }
 
     private static boolean isPNCharsBase(int ch) {
@@ -291,43 +294,43 @@ public class SplitIRI
             // Surrogate pairs
             r(ch, 0xD800, 0xDFFF) ||
             r(ch, 0xF900, 0xFDCF) || r(ch, 0xFDF0, 0xFFFD) ||
-            r(ch, 0x10000, 0xEFFFF) ; // Outside the basic plane.
+            r(ch, 0x10000, 0xEFFFF); // Outside the basic plane.
     }
 
     private static boolean isPNChars_U(int ch) {
         //PN_CHARS_BASE | '_'
-        return isPNCharsBase(ch) || ( ch == '_' ) ;
+        return isPNCharsBase(ch) || ( ch == '_' );
     }
 
     private static boolean isPNChars_U_N(int ch) {
         // PN_CHARS_U | [0-9]
-        return isPNCharsBase(ch) || ( ch == '_' ) || isDigit(ch) ;
+        return isPNCharsBase(ch) || ( ch == '_' ) || isDigit(ch);
     }
 
     private static boolean isPNChars(int ch) {
         // PN_CHARS ::=  PN_CHARS_U | '-' | [0-9] | #x00B7 | [#x0300-#x036F] | [#x203F-#x2040]
-        return isPNChars_U(ch) || isDigit(ch) || ( ch == '-' ) || ch == 0x00B7 || r(ch, 0x300, 0x036F) || r(ch, 0x203F, 0x2040) ;
+        return isPNChars_U(ch) || isDigit(ch) || ( ch == '-' ) || ch == 0x00B7 || r(ch, 0x300, 0x036F) || r(ch, 0x203F, 0x2040);
     }
 
     /** Hexadecimal character */
     private static boolean isHexChar(int ch) {
-        return range(ch, '0', '9') || range(ch, 'a', 'f') || range(ch, 'A', 'F') ;
+        return range(ch, '0', '9') || range(ch, 'a', 'f') || range(ch, 'A', 'F');
     }
 
     private static int valHexChar(int ch) {
         if ( range(ch, '0', '9') )
-            return ch - '0' ;
+            return ch - '0';
         if ( range(ch, 'a', 'f') )
-            return ch - 'a' + 10 ;
+            return ch - 'a' + 10;
         if ( range(ch, 'A', 'F') )
-            return ch - 'A' + 10 ;
-        return -1 ;
+            return ch - 'A' + 10;
+        return -1;
     }
 
-    private static boolean r(int ch, int a, int b) { return ( ch >= a && ch <= b ) ; }
+    private static boolean r(int ch, int a, int b) { return ( ch >= a && ch <= b ); }
 
     private static boolean range(int ch, char a, char b) {
-        return (ch >= a && ch <= b) ;
+        return (ch >= a && ch <= b);
     }
 }
 
