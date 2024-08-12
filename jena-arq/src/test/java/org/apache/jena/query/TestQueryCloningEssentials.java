59ee2fe26fdd4b9d8b4657863bde2acc5c190321
Andy Seaborne <andy@apache.org>
2024-08-12 13:24:49 +0100

    GH-2638: Silence warnings (works with maven surefire)

--- jena-arq/src/test/java/org/apache/jena/query/TestQueryCloningEssentials.java
+++ jena-arq/src/test/java/org/apache/jena/query/TestQueryCloningEssentials.java
@@ -21,14 +21,12 @@ import java.io.IOException;
 import java.nio.file.*;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.apache.jena.sparql.expr.E_Function;
 import org.apache.jena.sparql.expr.NodeValue;
-import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -56,10 +54,10 @@ public class TestQueryCloningEssentials {
         Assert.assertEquals(query, actual);
         Assert.assertEquals(expected, actual);
 
-        // Check that the cloned query is OK. 
+        // Check that the cloned query is OK.
         Query again = slowClone(actual);
         Assert.assertEquals(query, again);
-        
+
         return actual;
     }
 
@@ -71,33 +69,28 @@ public class TestQueryCloningEssentials {
         this.query = query;
     }
 
-    private static boolean bVerboseWarnings;
-    private static boolean bWarnOnUnknownFunction;
-
-    //@BeforeClass -- call earlier
-    public static void beforeClass() {
-        bVerboseWarnings = NodeValue.VerboseWarnings;
-        bWarnOnUnknownFunction = E_Function.WarnOnUnknownFunction;
+    private static void silenceWarnings(Runnable action) {
+        boolean bVerboseWarnings = NodeValue.VerboseWarnings;
+        boolean bWarnOnUnknownFunction = E_Function.WarnOnUnknownFunction;
         NodeValue.VerboseWarnings = false;
         E_Function.WarnOnUnknownFunction = false;
+        try {
+            action.run();
+        } finally {
+            NodeValue.VerboseWarnings = bVerboseWarnings;
+            E_Function.WarnOnUnknownFunction = bWarnOnUnknownFunction;
+        }
     }
 
-    @AfterClass
-    public static void afterClass() {
-        NodeValue.VerboseWarnings = bVerboseWarnings;
-        E_Function.WarnOnUnknownFunction = bWarnOnUnknownFunction;
-    }
-    
     @Test
     public void runTest() {
-        checkedClone(query);
+        silenceWarnings(()->checkedClone(query));
     }
 
     @Parameters(name = "Query.clone {0}")
     public static Collection<Object[]> generateTestParams() throws Exception
     {
-        beforeClass();
-        List<String> exclusions = Arrays.asList(/* no exclusions as all test cases work */);
+        List<String> exclusions = List.of(/* no exclusions as all test cases work */);
 
         Path startPath = Path.of("./testing/ARQ").toAbsolutePath().normalize();
         PathMatcher pathMatcher = startPath.getFileSystem().getPathMatcher("glob:**/*.rq");
@@ -113,9 +106,10 @@ public class TestQueryCloningEssentials {
                     if(!isExcluded) {
                         String queryStr = Files.lines(file).collect(Collectors.joining("\n"));
                         try {
-                            Query query = QueryFactory.create(queryStr);
-
-                            testParams.add(new Object[] {file, query});
+                            silenceWarnings(()->{
+                                Query query = QueryFactory.create(queryStr);
+                                testParams.add(new Object[] {file, query});
+                            });
                         } catch(Exception e) {
                             // Silently ignore queries that fail to parse
                         }
@@ -127,6 +121,4 @@ public class TestQueryCloningEssentials {
 
         return testParams;
     }
-
-
 }
