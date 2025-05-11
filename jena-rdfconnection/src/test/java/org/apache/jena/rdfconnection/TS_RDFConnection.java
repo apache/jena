294cbedceaeb5c4ea7d51954e461419cbb6ce8c6
Claus Stadler <RavenArkadon@googlemail.com>
2025-05-11 13:04:33 +0200

    GH2701: Fuseki Mod to list and abort running executions.

--- jena-rdfconnection/src/test/java/org/apache/jena/rdfconnection/TS_RDFConnection.java
+++ jena-rdfconnection/src/test/java/org/apache/jena/rdfconnection/TS_RDFConnection.java
@@ -28,6 +28,7 @@ import org.junit.runners.Suite;
     TestRDFConnectionLocalTxnMem.class
     , TestRDFConnectionLocalMRSW.class
     , TestLibRDFConn.class
+    , TestRDFConnectionToDatasetGraphOverRDFLink.class
     , TestRDFConnectionRewrapping.class
 })
 
