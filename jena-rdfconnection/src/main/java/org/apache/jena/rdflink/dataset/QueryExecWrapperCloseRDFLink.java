a522b978444bc5444a71b574c356ef44a4d7c88b
Claus Stadler <RavenArkadon@googlemail.com>
2025-05-11 13:04:33 +0200

    GH2701: Fuseki Mod to list and abort running executions.

---
+++ jena-rdfconnection/src/main/java/org/apache/jena/rdflink/dataset/QueryExecWrapperCloseRDFLink.java
@@ -0,0 +1,43 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.jena.rdflink.dataset;
+
+import org.apache.jena.rdflink.RDFLink;
+import org.apache.jena.sparql.exec.QueryExec;
+import org.apache.jena.sparql.exec.QueryExecWrapper;
+
+public class QueryExecWrapperCloseRDFLink
+    extends QueryExecWrapper
+{
+    protected RDFLink link;
+
+    public QueryExecWrapperCloseRDFLink(QueryExec delegate, RDFLink link) {
+        super(delegate);
+        this.link = link;
+    }
+
+    @Override
+    public void close() {
+        try {
+            super.close();
+        } finally {
+            link.close();
+        }
+    }
+}
