f88a0fc5f139adb27f6ed4e10e6f64c5bb6c87df
Andy Seaborne <andy@apache.org>
2025-09-04 09:45:46 +0100

    Reformat E_Conditional

--- jena-arq/src/main/java/org/apache/jena/sparql/expr/E_Conditional.java
+++ jena-arq/src/main/java/org/apache/jena/sparql/expr/E_Conditional.java
@@ -18,51 +18,45 @@
 
 package org.apache.jena.sparql.expr;
 
-import org.apache.jena.sparql.ARQInternalErrorException ;
-import org.apache.jena.sparql.engine.binding.Binding ;
-import org.apache.jena.sparql.function.FunctionEnv ;
-import org.apache.jena.sparql.sse.Tags ;
+import org.apache.jena.sparql.ARQInternalErrorException;
+import org.apache.jena.sparql.engine.binding.Binding;
+import org.apache.jena.sparql.function.FunctionEnv;
+import org.apache.jena.sparql.sse.Tags;
 
-/** IF(expr, expr, expr) */ 
+/** IF(expr, expr, expr) */
 
-public class E_Conditional extends ExprFunction3
-{
-    private static final String functionName = Tags.tagIf ;
-    
-    private final Expr condition ;
-    private final Expr thenExpr ;
-    private final Expr elseExpr ;
-    
-    public E_Conditional(Expr condition, Expr thenExpr, Expr elseExpr)
-    {
-        // Don't let the parent eval the theEpxr or ifExpr.
-        super(condition, thenExpr, elseExpr, functionName) ;
-        // Better names,
-        this.condition = condition ;
-        this.thenExpr = thenExpr ;
-        this.elseExpr = elseExpr ;
+public class E_Conditional extends ExprFunction3 {
+    private static final String functionName = Tags.tagIf;
+
+    private final Expr condition;
+    private final Expr thenExpr;
+    private final Expr elseExpr;
+
+    public E_Conditional(Expr condition, Expr thenExpr, Expr elseExpr) {
+        super(condition, thenExpr, elseExpr, functionName);
+        // Better names that arg1, arg2, arg3.
+        this.condition = condition;
+        this.thenExpr = thenExpr;
+        this.elseExpr = elseExpr;
     }
 
     @Override
-    public Expr copy(Expr arg1, Expr arg2, Expr arg3)
-    {
-        return new E_Conditional(arg1, arg2, arg3) ;
+    public Expr copy(Expr arg1, Expr arg2, Expr arg3) {
+        return new E_Conditional(arg1, arg2, arg3);
     }
 
-    /** Special form evaluation (example, don't eval the arguments first) */
+    /** Functional form evaluation */
     @Override
-    protected NodeValue evalSpecial(Binding binding, FunctionEnv env)
-    {
-        NodeValue nv = condition.eval(binding, env) ;
+    protected NodeValue evalSpecial(Binding binding, FunctionEnv env) {
+        NodeValue nv = condition.eval(binding, env);
         if ( condition.isSatisfied(binding, env) )
-            return thenExpr.eval(binding, env) ;
+            return thenExpr.eval(binding, env);
         else
-            return elseExpr.eval(binding, env) ;
+            return elseExpr.eval(binding, env);
     }
-    
+
     @Override
-    public NodeValue eval(NodeValue x, NodeValue y, NodeValue z)
-    {
-        throw new ARQInternalErrorException() ;
+    public NodeValue eval(NodeValue x, NodeValue y, NodeValue z) {
+        throw new ARQInternalErrorException();
     }
 }
