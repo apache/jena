b6872860cbfaf3387224434355e2ede02f5b08d3
Andy Seaborne <andy@apache.org>
2025-11-05 16:04:12 +0000

    Move CDT test scripts to their own test suite

---
+++ jena-arq/src/test/java/org/apache/jena/sparql/Scripts_CDTs.java
@@ -0,0 +1,43 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.jena.sparql;
+
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.DynamicNode;
+import org.junit.jupiter.api.TestFactory;
+
+import org.apache.jena.arq.junit.Scripts;
+import org.apache.jena.arq.junit.sparql.SparqlTests;
+
+public class Scripts_CDTs {
+    @TestFactory
+    @DisplayName("SPARQL CDTs")
+    public Stream<DynamicNode> testFactorySPARQL_CDTs_bnodes() {
+        return all("testing/SPARQL-CDTs/bnodes/manifest.ttl");
+    }
+
+    private static Stream<DynamicNode> all(String... manifests) {
+        if ( manifests == null || manifests.length == 0 )
+            throw new ARQException("No manifest files");
+        return Scripts.all(SparqlTests::makeSPARQLTest, manifests);
+    }
+
+}
