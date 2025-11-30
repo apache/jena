6964b4dd9fddf8ca3c835d142d4f5ad7e07d03a7
Claus Stadler <RavenArkadon@googlemail.com>
2025-11-30 17:43:56 +0100

    GH-3510: Sparql Adapter System

---
+++ jena-arq/src/main/java/org/apache/jena/sparql/adapter/QueryExecBuilderProvider.java
@@ -0,0 +1,42 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *
+ *   SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.apache.jena.sparql.adapter;
+
+import org.apache.jena.sparql.core.DatasetGraph;
+import org.apache.jena.sparql.exec.QueryExecBuilder;
+import org.apache.jena.sparql.util.Context;
+
+public interface QueryExecBuilderProvider {
+    /**
+     * Tests whether {@link #create(DatasetGraph)} will return an
+     * appropriate query execution builder for the given dataset graph.
+     */
+    boolean accept(DatasetGraph dsg, Context context);
+
+    /**
+     * Create a new query execution builder for the given dataset graph.
+     * <p>
+     * This method must only be called with arguments for which
+     * {@link #accept(DatasetGraph)} returns true.
+     */
+    QueryExecBuilder create(DatasetGraph dsg, Context context);
+}
---
+++ jena-rdfconnection/src/main/java/org/apache/jena/rdflink/dataset/QueryExecWrapperCloseRDFLink.java
@@ -0,0 +1,55 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *
+ *   SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.apache.jena.rdflink.dataset;
+
+import java.util.Objects;
+
+import org.apache.jena.rdflink.RDFLink;
+import org.apache.jena.sparql.exec.QueryExec;
+import org.apache.jena.sparql.exec.QueryExecWrapper;
+
+/**
+ * Query exec wrapper whose close action first closes the delegate and then the given RDF link.
+ */
+public class QueryExecWrapperCloseRDFLink
+    extends QueryExecWrapper<QueryExec>
+{
+    protected RDFLink link;
+
+    public QueryExecWrapperCloseRDFLink(QueryExec delegate, RDFLink link) {
+        super(delegate);
+        this.link = Objects.requireNonNull(link);
+    }
+
+    public RDFLink getLink() {
+        return link;
+    }
+
+    @Override
+    public void close() {
+        try {
+            super.close();
+        } finally {
+            link.close();
+        }
+    }
+}
