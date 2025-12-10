775204d811b7c51dc847f4f8b4003c4ff39a90f3
Andy Seaborne <andy@apache.org>
2025-12-10 17:20:32 +0000

    GH-3634: Partial separation of TurtleJCC from RIOT

--- jena-arq/src/main/java/org/apache/jena/riot/lang/turtlejcc/TurtleJavaccReaderRIOT.java
+++ jena-arq/src/main/java/org/apache/jena/riot/lang/turtlejcc/TurtleJavaccReaderRIOT.java
@@ -23,6 +23,7 @@ import java.io.Reader;
 
 import org.apache.jena.atlas.io.IO;
 import org.apache.jena.atlas.web.ContentType;
+import org.apache.jena.graph.Triple;
 import org.apache.jena.riot.ReaderRIOT;
 import org.apache.jena.riot.RiotParseException;
 import org.apache.jena.riot.lang.LangTurtle;
@@ -31,6 +32,7 @@ import org.apache.jena.riot.lang.turtlejcc.javacc.TokenMgrError;
 import org.apache.jena.riot.lang.turtlejcc.javacc.TurtleJavacc;
 import org.apache.jena.riot.system.ParserProfile;
 import org.apache.jena.riot.system.StreamRDF;
+import org.apache.jena.sparql.core.Quad;
 import org.apache.jena.sparql.util.Context;
 
 /**
@@ -48,11 +50,11 @@ public class TurtleJavaccReaderRIOT implements ReaderRIOT {
     public TurtleJavaccReaderRIOT(ParserProfile profile) { this.profile = profile; }
 
     @Override
-    public void read(InputStream in, String baseURI, ContentType ct, StreamRDF output, Context context) {
+    public void read(InputStream in, String baseURI, ContentType ct, StreamRDF streamDest, Context context) {
         // Do bytes -> chars in big units.
         Reader r = IO.asBufferedUTF8(in);
         TurtleJavacc parser = new TurtleJavacc(r);
-        read(parser, baseURI, ct, output, context);
+        read(parser, baseURI, ct, streamDest, context);
     }
 
     @Override
@@ -63,7 +65,8 @@ public class TurtleJavaccReaderRIOT implements ReaderRIOT {
         read(parser, baseURI, ct, output, context);
     }
 
-    private void read(TurtleJavacc parser, String baseURI, ContentType ct, StreamRDF output, Context context) {
+    private void read(TurtleJavacc parser, String baseURI, ContentType ct, StreamRDF streamDest, Context context) {
+        OutputRDF output = new StreamRDFJCC(streamDest);
         parser.setDest(output);
         parser.setProfile(profile);
         // profile should be setup correctly for the base
@@ -79,6 +82,51 @@ public class TurtleJavaccReaderRIOT implements ReaderRIOT {
         catch (TokenMgrError ex) {
             profile.getErrorHandler().error(ex.getMessage(), -1, -1);
             throw new RiotParseException(ex.getMessage(), -1 , -1);
+        }
+    }
+
+    // Could add OutputRDF to StreamRDF.
+    // This parser is not used for speed so keep OutputRDF local.
+    // OutputRDF is part of isolating the parser engine from RIOT.
+    static class StreamRDFJCC implements OutputRDF {
+        private final StreamRDF dest;
+        StreamRDFJCC(StreamRDF dest) {
+            this.dest = dest;
+        }
+
+        @Override
+        public void start() {
+            dest.start();
+        }
+
+        @Override
+        public void triple(Triple triple) {
+            dest.triple(triple);
+        }
+
+        @Override
+        public void quad(Quad quad) {
+            dest.quad(quad);
+        }
+
+        @Override
+        public void base(String base) {
+            dest.base(base);
+        }
+
+        @Override
+        public void prefix(String prefix, String iri) {
+            dest.prefix(prefix, iri);
+        }
+
+        @Override
+        public void version(String version) {
+            dest.version(version);
+        }
+
+        @Override
+        public void finish() {
+            dest.finish();
         }
     }
 }
