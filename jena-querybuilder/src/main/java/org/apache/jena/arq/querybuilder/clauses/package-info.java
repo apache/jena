bcaefc40b9fd973445fda18990fa1bc6bdf2aafb
Andy Seaborne <andy@apache.org>
2026-01-06 19:14:12 +0000

    GH-3589: Regularize the format of Apache License headers

--- jena-querybuilder/src/main/java/org/apache/jena/arq/querybuilder/clauses/package-info.java
+++ jena-querybuilder/src/main/java/org/apache/jena/arq/querybuilder/clauses/package-info.java
@@ -1,13 +1,13 @@
 /*
  * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements. See the NOTICE file
+ * or more contributor license agreements.  See the NOTICE file
  * distributed with this work for additional information
- * regarding copyright ownership. The ASF licenses this file
+ * regarding copyright ownership.  The ASF licenses this file
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
- * with the License. You may obtain a copy of the License at
+ * with the License.  You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -16,11 +16,4 @@
  * limitations under the License.
  */
 
-/**
- * Interfaces that describe various SPARQL clauses as per
- * http://www.w3.org/TR/2013/REC-sparql11-query-20130321.
- * 
- * These classes are used to compose consistent interfaces for specific builder
- * implementations.
- */
 package org.apache.jena.arq.querybuilder.clauses;
--- jena-querybuilder/src/main/java/org/apache/jena/arq/querybuilder/handlers/package-info.java
+++ jena-querybuilder/src/main/java/org/apache/jena/arq/querybuilder/handlers/package-info.java
@@ -1,13 +1,13 @@
 /*
  * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements. See the NOTICE file
+ * or more contributor license agreements.  See the NOTICE file
  * distributed with this work for additional information
- * regarding copyright ownership. The ASF licenses this file
+ * regarding copyright ownership.  The ASF licenses this file
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
- * with the License. You may obtain a copy of the License at
+ * with the License.  You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -16,14 +16,4 @@
  * limitations under the License.
  */
 
-/**
- * Classes that implement SPARQL clauses but do not return instances of the
- * builder.
- * 
- * These classes are used by the various Builder implementations to consistently
- * implement the various clauses.
- * 
- * In most cases users of the Query Builder module will not need to manipulate
- * these directly.
- */
 package org.apache.jena.arq.querybuilder.handlers;
