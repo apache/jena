b2b238d67b2da48c65182d96ecfd446d9f7d88ab
Andy Seaborne <andy@apache.org>
2026-04-05 20:10:14 +0100

    Reformat code

--- jena-base/src/main/java/org/apache/jena/atlas/io/OutputUtils.java
+++ jena-base/src/main/java/org/apache/jena/atlas/io/OutputUtils.java
@@ -30,51 +30,47 @@ import org.apache.jena.atlas.lib.Chars ;
 
 public class OutputUtils
 {
-    /** Print the number x in width hex chars.  x must fit */
-    public static void printHex(StringBuilder out, int x, int width)
-    {
-        for ( int i = width-1 ; i >= 0 ; i-- )
-            x = oneHex(out, x, i) ;
+    /** Print the number x in width hex chars. x must fit */
+    public static void printHex(StringBuilder out, int x, int width) {
+        for ( int i = width - 1 ; i >= 0 ; i-- )
+            x = oneHex(out, x, i);
     }
 
     /** Print one hex digit of the number */
-    public static int oneHex(StringBuilder out, int x, int i)
-    {
-        int y = BitsInt.unpack(x, 4*i, 4*i+4) ;
-        char charHex = Chars.hexDigitsUC[y] ;
-        out.append(charHex) ; 
-        return BitsInt.clear(x, 4*i, 4*i+4) ;
+    public static int oneHex(StringBuilder out, int x, int i) {
+        int y = BitsInt.unpack(x, 4 * i, 4 * i + 4);
+        char charHex = Chars.hexDigitsUC[y];
+        out.append(charHex);
+        return BitsInt.clear(x, 4 * i, 4 * i + 4);
     }
-    
-    /** Print the number x in width hex chars.  x must fit */
-    public static void printHex(Writer out, int x, int width)
-    {
-        for ( int i = width-1 ; i >= 0 ; i-- )
-            x = oneHex(out, x, i) ;
+
+    /** Print the number x in width hex chars. x must fit */
+    public static void printHex(Writer out, int x, int width) {
+        for ( int i = width - 1 ; i >= 0 ; i-- )
+            x = oneHex(out, x, i);
     }
 
     /** Print one hex digit of the number */
-    public static int oneHex(Writer out, int x, int i)
-    {
-        int y = BitsInt.unpack(x, 4*i, 4*i+4) ;
-        char charHex = Chars.hexDigitsUC[y] ;
-        try { out.write(charHex) ; } catch (IOException ex) {} 
-        return BitsInt.clear(x, 4*i, 4*i+4) ;
+    public static int oneHex(Writer out, int x, int i) {
+        int y = BitsInt.unpack(x, 4 * i, 4 * i + 4);
+        char charHex = Chars.hexDigitsUC[y];
+        try {
+            out.write(charHex);
+        } catch (IOException ex) {}
+        return BitsInt.clear(x, 4 * i, 4 * i + 4);
     }
 
-    /** Print the number x in width hex chars.  x must fit */
-    public static void printHex(AWriter out, int x, int width)
-    {
-        for ( int i = width-1 ; i >= 0 ; i-- )
-            x = oneHex(out, x, i) ;
+    /** Print the number x in width hex chars. x must fit */
+    public static void printHex(AWriter out, int x, int width) {
+        for ( int i = width - 1 ; i >= 0 ; i-- )
+            x = oneHex(out, x, i);
     }
 
     /** Print one hex digit of the number */
-    public static int oneHex(AWriter out, int x, int i)
-    {
-        int y = BitsInt.unpack(x, 4*i, 4*i+4) ;
-        char charHex = Chars.hexDigitsUC[y] ;
-        out.print(charHex) ; 
-        return BitsInt.clear(x, 4*i, 4*i+4) ;
+    public static int oneHex(AWriter out, int x, int i) {
+        int y = BitsInt.unpack(x, 4 * i, 4 * i + 4);
+        char charHex = Chars.hexDigitsUC[y];
+        out.print(charHex);
+        return BitsInt.clear(x, 4 * i, 4 * i + 4);
     }
 }
